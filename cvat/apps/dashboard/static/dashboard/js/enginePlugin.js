f15621b567cc64486cf56296ccc63d46305ed694
Boris Sekachev <boris.sekachev@intel.com>
2018-10-25 12:13:53 +0300

    Simplified code

--- cvat/apps/dashboard/static/dashboard/js/enginePlugin.js
+++ cvat/apps/dashboard/static/dashboard/js/enginePlugin.js
@@ -7,12 +7,9 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
-    let button = $(`<button id="dashboardOpenTaskButton" class="menuButton semiBold h2"> Open Task </button>`);
-    $('#engineMenuButtons').prepend(button);
-
-    button.on('click', () => {
+    $(`<button class="menuButton semiBold h2"> Open Task </button>`).on('click', () => {
         let win = window.open(`${window.location.origin }/dashboard/?jid=${window.cvat.job.id}`, '_blank');
         win.focus();
-    });
+    }).prependTo('#engineMenuButtons');
 });
 
