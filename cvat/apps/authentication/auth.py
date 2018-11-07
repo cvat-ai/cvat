fdb693fd8917d5a3cc6744713858d815b78116e2
Nikita Manovich <nikita.manovich@intel.com>
2018-11-07 13:41:42 +0300

    Fix task/job access and change permissions.
    Now only task's owner and assignee can change a task.
    If a person has task access it has also job access.

--- cvat/apps/authentication/auth.py
+++ cvat/apps/authentication/auth.py
@@ -37,47 +37,44 @@ has_observer_role = rules.is_group_member(str(AUTH_ROLE.OBSERVER))
 @rules.predicate
 def is_task_owner(db_user, db_task):
     # If owner is None (null) the task can be accessed/changed/deleted
-    # by any user (publically available tasks)
-    return db_task.owner == db_user or db_task.owner is None
+    # only by admin. At the moment each task has an owner.
+    return db_task.owner == db_user
 
 @rules.predicate
 def is_task_assignee(db_user, db_task):
-    # If assignee is None (null) the task can be accessed/changeed by any user.
-    return db_task.assignee == db_user or db_task.assignee is None
-
-@rules.predicate
-def is_job_owner(db_user, db_job):
-    return is_task_owner(db_user, db_job.segment.task)
-
-@rules.predicate
-def is_job_assignee(db_user, db_job):
-    # If assignee is None (null) the job can be accessed/changed by any user.
-    return db_user == db_job.assignee or db_job.assignee is None
+    return db_task.assignee == db_user
 
 @rules.predicate
 def is_task_annotator(db_user, db_task):
     from functools import reduce
 
     db_segments = list(db_task.segment_set.prefetch_related('job_set__assignee').all())
-    # If job.assignee is None it doesn't mean that everybody can access the task.
-    return is_task_assignee(db_user, db_task) or \
-        reduce((lambda x, y: x | y), [db_user == db_job.assignee
-            for db_segment in db_segments for db_job in db_segment.job_set.all()])
+    return any([is_job_annotator(db_user, db_job)
+        for db_segment in db_segments for db_job in db_segment.job_set.all()])
+
+@rules.predicate
+def is_job_owner(db_user, db_job):
+    return is_task_owner(db_user, db_job.segment.task)
 
 @rules.predicate
 def is_job_annotator(db_user, db_job):
     db_task = db_job.segment.task
-    return is_job_assignee(db_user, db_job) or is_task_assignee(db_user, db_task)
+    # A job can be annotated by any user if the task's assignee is None.
+    has_rights = db_task.assignee is None or is_task_assignee(db_user, db_task)
+    if db_job.assignee is not None:
+        has_rights |= (db_user == db_job.assignee)
+
+    return has_rights
 
 # AUTH PERMISSIONS RULES
 rules.add_perm('engine.task.create', has_admin_role | has_user_role)
-rules.add_perm('engine.task.access', has_admin_role | is_task_owner |
-    has_observer_role | is_task_annotator)
+rules.add_perm('engine.task.access', has_admin_role | has_observer_role |
+    is_task_owner | is_task_annotator)
 rules.add_perm('engine.task.change', has_admin_role | is_task_owner |
     is_task_assignee)
 rules.add_perm('engine.task.delete', has_admin_role | is_task_owner)
 
-rules.add_perm('engine.job.access', has_admin_role | is_job_owner |
-    has_observer_role | is_job_annotator)
+rules.add_perm('engine.job.access', has_admin_role | has_observer_role |
+    is_job_owner | is_job_annotator)
 rules.add_perm('engine.job.change', has_admin_role | is_job_owner |
     is_job_annotator)
