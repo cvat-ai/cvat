666c4f172bec75e6ef3e1dc89294d8e540df9c97
bsekachev <boris.sekachev@yandex.ru>
2018-11-28 13:26:37 +0300

    SSH auto generating

--- components/ssh/install.sh
+++ components/ssh/install.sh
@@ -12,3 +12,10 @@ apt-get update && apt-get install -y netcat-openbsd && rm -rf /var/lib/apt/lists
 rm ${HOME}/.ssh -fr
 mkdir ${HOME}/.ssh -p
 mv /tmp/components/ssh/keys/* ${HOME}/.ssh/
+
+cd ${HOME}/.ssh/
+if test `ssh-add -l | grep "DSA\|RSA\|ECDSA\|ED25519\|RSA1" | wc -l` -eq 0
+then
+    ssh-keygen -b 4096 -t rsa -f `pwd`/id_rsa -q -N ""
+fi
+
