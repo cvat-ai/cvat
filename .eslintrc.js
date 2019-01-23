df1d1eddb01adbb366992bc49b6ed45cd5d055d4
Andrey Zhavoronkov <andrey.zhavoronkov@intel.com>
2019-01-23 18:06:05 +0300

    json -> js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
-{
+/*
+ * Copyright (C) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+ module.exports = {
     "env": {
         "browser": true,
         "es6": true,
@@ -21,7 +27,7 @@
         "no-new": 2,
         "no-extend-native": 2,
         "guard-for-in": 2,
-        "eqeqeq": 2,
+        "eqeqeq": 1,
         "quotes": ["warn", "double"],
         "camelcase": 1,
         "object-shorthand": 1,
@@ -31,18 +37,23 @@
         "semi": 1,
         "no-undefined": 1,
         "no-unsafe-innerhtml/no-unsafe-innerhtml": 1,
+        // This rule actual for user input data on the node.js environment mainly.
         "security/detect-object-injection": 0
     },
     "globals": {
+        // from attributeAnnotationMode.js
         "AAMController": true,
         "AAMModel": true,
         "AAMView": true,
         "AAMUndefinedKeyword": true,
+        // from annotationParser.js
         "AnnotationParser": true,
+        // from annotationUI.js
         "callAnnotationUI": true,
         "blurAllElements": true,
         "drawBoxSize": true,
         "copyToClipboard": true,
+        // from base.js
         "showMessage": true,
         "showOverlay": true,
         "confirm": true,
@@ -50,23 +61,32 @@
         "createExportContainer": true,
         "ExportType": true,
         "getExportTargetContainer": true,
+        // from idGenerator.js
         "IncrementIdGenerator": true,
         "ConstIdGenerator": true,
+        // from shapeCollection.js
         "ShapeCollectionModel": true,
         "ShapeCollectionController": true,
         "ShapeCollectionView": true,
+        // from shapeCreator.js
         "ShapeCreatorModel": true,
         "ShapeCreatorController": true,
         "ShapeCreatorView": true,
+        // from shapeGrouper.js
         "ShapeGrouperModel": true,
         "ShapeGrouperController": true,
         "ShapeGrouperView": true,
+        // from labelsInfo.js
         "LabelsInfo": true,
+        // from listener.js
         "Listener": true,
+        // from logger.js
         "Logger": true,
+        // from shapeMerger.js
         "ShapeMergerModel": true,
         "ShapeMergerController": true,
         "ShapeMergerView": true,
+        // from shapes.js
         "PolyShapeModel": true,
         "PolyShapeView": true,
         "buildShapeModel": true,
@@ -76,31 +96,45 @@
         "POINT_RADIUS": true,
         "AREA_TRESHOLD": true,
         "SELECT_POINT_STROKE_WIDTH": true,
+        // from mousetrap.js
         "Mousetrap": true,
+        // from platform.js
         "platform": true,
+        // from player.js
         "PlayerController": true,
         "PlayerModel": true,
         "PlayerView": true,
+        // from server.js
         "serverRequest": true,
         "saveJobRequest": true,
+        // from shapeBuffer.js
         "ShapeBufferController": true,
         "ShapeBufferModel": true,
         "ShapeBufferView": true,
+        // from trackFilter.js
         "FilterModel": true,
         "FilterController": true,
         "FilterView": true,
+        // from shapeSplitter.js
         "ShapeSplitter": true,
+        // from userConfig.js
         "Config": true,
+        // from cookies.js
         "Cookies": true,
+        // from dashboard django template
         "maxUploadCount": true,
         "maxUploadSize": true,
+        // from SVG.js
         "SVG": true,
+        // from history.js
         "HistoryModel": true,
         "HistoryController": true,
         "HistoryView": true,
+        // from polyshapeEditor.js
         "PolyshapeEditorModel": true,
         "PolyshapeEditorController": true,
         "PolyshapeEditorView": true,
-        "CoordinateTranslator": true
-    }
-}
+        // from coordinateTranslator
+        "CoordinateTranslator": true,
+    },
+};
