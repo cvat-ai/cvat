f2a80637e788888193ba7d57b11f4a1edfadf268
Happyzippy <emtyge@gmail.com>
2019-03-11 09:34:44 +0100

    fix: TypeError: dtype('float64') to dtype('int32')
    Traceback (most recent call last):
      File "/usr/local/lib/python3.5/dist-packages/rq/worker.py", line 799, in perform_job
        rv = job.perform()
      File "/usr/local/lib/python3.5/dist-packages/rq/job.py", line 600, in perform
        self._result = self._execute()
      File "/usr/local/lib/python3.5/dist-packages/rq/job.py", line 606, in _execute
        return self.func(*self.args, **self.kwargs)
      File "/home/django/cvat/apps/engine/plugins.py", line 56, in function_wrapper
        result = function_to_decorate(*args, **kwargs)
      File "/usr/lib/python3.5/contextlib.py", line 30, in inner
        return func(*args, **kwds)
      File "/home/django/cvat/apps/engine/task.py", line 710, in _create_thread
        task_params['compress'], task_params['flip'], job)
      File "/home/django/cvat/apps/engine/task.py", line 587, in _find_and_compress_images
        im_data *=  2**8 / im_data.max()
    TypeError: Cannot cast ufunc multiply output from dtype('float64') to dtype('int32') with casting rule 'same_kind'
    
    Add more commits by pushing to the develop branch on Happyzippy/cvat.
    Review required
    At least 1 approving review is required by reviewers with write access.
    All checks have passed
    3 successful checks
    Codacy/PR Quality Review — Up to standards. A positive pull request.
    Details
    @wip
    WIP Successful in 1s — Ready for review
    Details
    continuous-integration/travis-ci/pr — The Travis CI build passed
    Details
    Merging is blocked
    Merging can be performed automatically with 1 approving review.
    @Happyzippy
    
    Attach files by dragging & dropping, selecting them, or pasting from the clipboard.
    Styling with Markdown is supported
    ProTip! Add .patch or .diff to the end of URLs for Git’s plaintext views.

--- cvat/apps/engine/task.py
+++ cvat/apps/engine/task.py
@@ -584,7 +584,7 @@ def _find_and_compress_images(upload_dir, output_dir, db_task, compress_quality,
                 # Image mode is 32bit integer pixels.
                 # Autoscale pixels by factor 2**8 / im_data.max() to fit into 8bit
                 im_data = np.array(image)
-                im_data *=  2**8 / im_data.max()
+                im_data = im_data * (2**8 / im_data.max())
                 image = Image.fromarray(im_data.astype(np.int32))
             image = image.convert('RGB')
             if flip_flag:
