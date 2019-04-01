0e8213810135c62cd1a884a104eb9a2cd315912f
Boris Sekachev <40690378+bsekachev@users.noreply.github.com>
2019-04-01 18:27:05 +0300

    Fixed git permissions (#371)

--- cvat/apps/git/views.py
+++ cvat/apps/git/views.py
@@ -34,7 +34,7 @@ def check_process(request, rq_id):
 
 
 @login_required
-@permission_required(perm=['engine.task.delete'],
+@permission_required(perm=['engine.task.access'],
     fn=objectgetter(models.Task, 'tid'), raise_exception=True)
 def push_repository(request, tid):
     try:
