76511a002f7d94915844cd32f334a673d00be152
Boris Sekachev <boris.sekachev@yandex.ru>
2019-04-05 12:53:00 +0300

    Fixed task overlap

--- cvat/apps/engine/task.py
+++ cvat/apps/engine/task.py
@@ -239,18 +239,23 @@ def _save_task_to_db(db_task):
     job.meta['status'] = 'Task is being saved in database'
     job.save_meta()
 
+    segment_size = db_task.segment_size
+    segment_step = segment_size
+    if segment_size == 0:
+        segment_size = db_task.size
+        segment_step = db_task.size * 2
+
     default_overlap = 5 if db_task.mode == 'interpolation' else 0
     if db_task.overlap is None:
         db_task.overlap = default_overlap
-    db_task.overlap = min(db_task.overlap, db_task.size - 1)
+    db_task.overlap = min(db_task.overlap, segment_size  // 2)
 
-    segment_size = db_task.segment_size
-    if segment_size == 0:
-        segment_size = db_task.size
+    segment_step -= db_task.overlap
 
-    for x in range(0, db_task.size, segment_size - db_task.overlap):
+    for x in range(0, db_task.size, segment_step):
         start_frame = x
         stop_frame = min(x + segment_size - 1, db_task.size - 1)
+
         slogger.glob.info("New segment for task #{}: start_frame = {}, \
             stop_frame = {}".format(db_task.id, start_frame, stop_frame))
 
