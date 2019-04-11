61fda1916f8ab7fdabc17ee091e5016c8a7f922d
Nikita Manovich <nikita.manovich@intel.com>
2019-04-11 19:03:39 +0300

    Add TagManager, ShapeManager, TrackManager

--- cvat/apps/engine/annotation_v2.py
+++ cvat/apps/engine/annotation_v2.py
@@ -702,49 +702,213 @@ class XmlAnnotationWriter(AnnotationWriter):
         self.xmlgen.endElement("annotations")
         self.xmlgen.endDocument()
 
-def _calc_shape_area(shape):
-    return shape.area
+class DataManager:
+    def __init__(self, data):
+        self.data = data
 
-def _calc_overlap_shape_area(shape0, shape1):
-    shape = shape0.intersection(shape1)
-    return shape.area
+    @staticmethod
+    def _get_objects_by_frame(objects, start_frame):
+        objects_by_frame = {}
+        for obj in objects:
+            if obj["frame"] >= start_frame:
+                if obj["frame"] in objects_by_frame:
+                    objects_by_frame[obj["frame"]].append(obj)
+                else:
+                    objects_by_frame[obj["frame"]] = [obj]
 
-def _calc_similarity(p0, p1):
-    overlap_area = _calc_overlap_shape_area(p0, p1)
-    shape0_area = _calc_shape_area(p0)
-    shape1_area = _calc_shape_area(p1)
+        return objects_by_frame
 
-    return overlap_area / (shape0_area + shape1_area - overlap_area)
+    @staticmethod
+    def _get_cost_threshold():
+        raise NotImplementedError()
 
-def _pairwise(iterable):
-    a = iter(iterable)
-    return zip(a, a)
+    @staticmethod
+    def _calc_objects_similarity(obj0, obj1, start_frame, overlap):
+        raise NotImplementedError()
 
-def _calc_shapes_similarity(shape0, shape1):
-    if shape0["type"] == shape1["type"]:
-        if shape0["type"] == models.ShapeType.RECTANGLE:
-            p0 = geometry.box(*shape0["points"])
-            p1 = geometry.box(*shape1["points"])
+    @staticmethod
+    def _unite_objects(obj0, obj1):
+        raise NotImplementedError()
 
-            return _calc_similarity(p0, p1)
-        elif shape0["type"] == models.ShapeType.POLYGON:
-            p0 = geometry.Polygon(_pairwise(shape0["points"]))
-            p1 = geometry.Polygon(_pairwise(shape0["points"]))
+    def merge(self, objects, start_frame, overlap):
+        # 1. Split objects on two parts: new and which can be intersected
+        # with existing objects.
+        new_objects = [obj for obj in objects
+            if obj["frame"] >= start_frame + overlap]
+        int_objects = [obj for obj in objects
+            if obj["frame"] < start_frame + overlap]
+        assert len(new_objects) + len(int_objects) == len(objects)
 
-            return _calc_similarity(p0, p1)
+        # 2. Convert to more convenient data structure (objects by frame)
+        int_objects_by_frame = self._get_objects_by_frame(int_objects, start_frame)
+        old_objects_by_frame = self._get_objects_by_frame(self.data, start_frame)
+
+        # 3. Add new objects as is. It should be done only after old_objects_by_frame
+        # variable is initialized.
+        self.data.extend(new_objects)
+
+        # Nothing to merge here. Just add all int_objects if any.
+        if not old_objects_by_frame or not int_objects_by_frame:
+            self.data.extend(int_objects)
+            return
+
+        # 4. Build cost matrix for each frame and find correspondence using
+        # Hungarian algorithm. In this case min_cost_thresh is stronger
+        # because we compare only on one frame.
+        min_cost_thresh = self._get_cost_threshold()
+        for frame in int_objects_by_frame:
+            if frame in old_objects_by_frame:
+                int_objects = int_objects_by_frame[frame]
+                old_objects = old_objects_by_frame[frame]
+                cost_matrix = np.empty(obj=(len(int_objects), len(old_objects)),
+                    dtype=float)
+                # 5.1 Construct cost matrix for the frame.
+                for i, int_obj in enumerate(int_objects):
+                    for j, old_obj in enumerate(old_objects):
+                        cost_matrix[i][j] = 1 - self._calc_objects_similarity(
+                            int_obj, old_obj, start_frame, overlap)
+
+                # 6. Find optimal solution using Hungarian algorithm.
+                row_ind, col_ind = linear_sum_assignment(cost_matrix)
+                int_objects_indexes = list(range(0, len(int_objects)))
+                for i, j in zip(row_ind, col_ind):
+                    # Reject the solution if the cost is too high. Remember
+                    # inside int_objects_indexes objects which were handled.
+                    if cost_matrix[i][j] <= min_cost_thresh:
+                        old_objects[j] = self._unite_objects(int_objects[i], old_objects[j])
+                        int_objects_indexes[i] = -1
+
+                # 7. Add all objects which were not processed.
+                for i in int_objects_indexes:
+                    if i != -1:
+                        self.data.append(int_objects[i])
+            else:
+                # We don't have old objects on the frame. Let's add all new ones.
+                self.data.extend(int_objects_by_frame[frame])
+
+class TagManager(DataManager):
+    @staticmethod
+    def _get_cost_threshold():
+        raise 0.25
+
+    @staticmethod
+    def _calc_objects_similarity(obj0, obj1, start_frame, overlap):
+        # TODO: improve the trivial implementation, compare attributes
+        return 1 if obj0["label_id"] == obj1["label_id"] else 0
+
+    @staticmethod
+    def _unite_objects(obj0, obj1):
+        # TODO: improve the trivial implementation
+        return obj0 if obj0["frame"] < obj1["frame"] else obj1
+
+class ShapeManager(DataManager):
+    @staticmethod
+    def _get_cost_threshold():
+        raise 0.25
+
+    @staticmethod
+    def _calc_objects_similarity(obj0, obj1, start_frame, overlap):
+        def pairwise(iterable):
+            a = iter(iterable)
+            return zip(a, a)
+
+        def _calc_polygons_similarity(p0, p1):
+            overlap_area = p0.intersection(p1).area
+            return overlap_area / (p0.area + p1.area - overlap_area)
+
+        has_same_type  = obj0["type"] == obj1["type"]
+        has_same_label = obj0["label_id"] == obj1["label_id"]
+        if has_same_type and has_same_label:
+            if obj0["type"] == models.ShapeType.RECTANGLE:
+                p0 = geometry.box(*obj0["points"])
+                p1 = geometry.box(*obj1["points"])
+
+                return self._calc_polygons_similarity(p0, p1)
+            elif obj0["type"] == models.ShapeType.POLYGON:
+                p0 = geometry.Polygon(pairwise(obj0["points"]))
+                p1 = geometry.Polygon(pairwise(obj0["points"]))
+
+                return self._calc_polygons_similarity(p0, p1)
+            else:
+                return 0 # FIXME: need some similarity for points and polylines
+        return 0
+
+    @staticmethod
+    def _unite_objects(obj0, obj1):
+        # TODO: improve the trivial implementation
+        return obj0 if obj0["frame"] < obj1["frame"] else obj1
+
+class TrackManager(DataManager):
+    @staticmethod
+    def _get_objects_by_frame(objects, start_frame):
+        # Just for unification. All tracks are assigned on the same frame
+        objects_by_frame = {0: []}
+        for obj in objects:
+            shape = obj["shapes"][-1] # optimization for old tracks
+            if shape["frame"] >= start_frame or shape["outside"]:
+                objects_by_frame[0].append(obj)
+
+        if not objects_by_frame[0]:
+            objects_by_frame = {}
+
+        return objects_by_frame
+
+    @staticmethod
+    def _get_cost_threshold():
+        raise 0.5
+
+    @staticmethod
+    def _calc_objects_similarity(obj0, obj1, start_frame, overlap):
+        if obj0["label_id"] == obj1["label_id"]:
+            # Here start_frame is the start frame of next segment
+            # and stop_frame is the stop frame of current segment
+            stop_frame = start_frame + overlap - 1
+            obj0_shapes = self._get_interpolated_shapes(obj0, start_frame, stop_frame)
+            obj1_shapes = self._get_interpolated_shapes(obj1, start_frame, stop_frame)
+            obj0_shapes_by_frame = {shape["frame"]:shape for shape in obj0_shapes}
+            obj1_shapes_by_frame = {shape["frame"]:shape for shape in obj1_shapes}
+            assert obj0_shapes_by_frame and obj1_shapes_by_frame
+
+            count, error = 0, 0
+            for frame in range(start_frame, stop_frame + 1):
+                shape0 = obj0_shapes_by_frame.get(frame)
+                shape1 = obj1_shapes_by_frame.get(frame)
+                if shape0 and shape1:
+                    if shape0["outside"] != shape1["outside"]:
+                        error += 1
+                    else:
+                        error += 1 - ShapeManager._calc_objects_similarity(shape0, shape1)
+                    count += 1
+                elif shape0 or shape1:
+                    error += 1
+                    count += 1
+
+            return 1 - error / count
         else:
-            return 0 # FIXME: need some similarity for points and polylines
-    return 0
+            return 0
 
-def _calc_avg_shape(shape0, shape1):
-    # FIXME: need to calculate an average shape here
-    if shape0["type"] == shape1["type"]:
-        if shape0["type"] == models.ShapeType.RECTANGLE:
-            return shape0
-        elif shape0["type"] == models.ShapeType.POLYGON:
-            return shape0
-        else:
-            return shape0
+    @staticmethod
+    def _get_interpolated_shapes(obj0, start_frame, stop_frame):
+        pass
+
+    @staticmethod
+    def _unite_objects(obj0, obj1):
+        track = obj0 if obj0["frame"] < obj1["frame"] else obj1
+        assert obj0["label_id"] == obj1["label_id"]
+        shapes = {shape["frame"]:shape for shape in obj0["shapes"]}
+        for shape in obj1["shapes"]:
+            frame = shape["frame"]
+            if frame in shapes:
+                shapes[frame] = ShapeManager._unite_objects(shapes[frame], shape)
+            else:
+                shapes[frame] = shape
+
+        track["frame"] = min(obj0["frame"], obj1["frame"])
+        track["stop_frame"] = max(obj0["stop_frame"], obj1["stop_frame"])
+        track["shapes"] = list(sorted(shapes.values(), key=lambda shape: shape["frame"]))
+        track["interpoated_shapes"] = {}
+
+        return track
 
 class TaskAnnotation:
     def __init__(self, pk):
@@ -803,97 +967,19 @@ class TaskAnnotation:
 
     def init_from_db(self):
         self.reset()
+        tags   = TagManager(self.data["tags"])
+        shapes = ShapeManager(self.data["shapes"])
+        tracks = TrackManager(self.data["tracks"])
 
         for db_job in self.db_jobs:
             annotation = JobAnnotation(db_job.id)
             annotation.init_from_db()
             db_segment = db_job.segment
-            self._merge_data(annotation.data, db_segment.start_frame,
-                self.db_task.overlap)
-
-    def _merge_data(self, data, start_frame, overlap):
-        self._merge_tags(data["tags"], start_frame, overlap)
-        self._merge_shapes(data["shapes"], start_frame, overlap)
-        self._merge_tracks(data["tracks"], start_frame, overlap)
-
-
-    def _merge_tags(self, tags, start_frame, overlap):
-        # FIXME: implement merge algorithm here
-        self.data["tags"].extend(tags)
-
-    def _merge_shapes(self, shapes, start_frame, overlap):
-        # 1. Split shapes on two parts: new and which can be intersected
-        # with existing boxes.
-        new_shapes = [shape for shape in shapes
-            if shape["frame"] >= start_frame + overlap]
-        int_shapes = [shape for shape in shapes
-            if shape["frame"] < start_frame + overlap]
-        assert len(new_shapes) + len(int_shapes) == len(shapes)
-
-        # 2. Convert to more convenient data structure (shapes by frame)
-        int_shapes_by_frame = {}
-        for shape in int_shapes:
-            if shape["frame"] in int_shapes_by_frame:
-                int_shapes_by_frame[shape["frame"]].append(shape)
-            else:
-                int_shapes_by_frame[shape["frame"]] = [shape]
-
-        old_shapes_by_frame = {}
-        for shape in self.data["shapes"]:
-            if shape["frame"] >= start_frame:
-                if shape["frame"] in old_shapes_by_frame:
-                    old_shapes_by_frame[shape["frame"]].append(shape)
-                else:
-                    old_shapes_by_frame[shape["frame"]] = [shape]
-
-        # 3. Add new shapes as is. It should be done only after old_shapes_by_frame
-        # variable is initialized.
-        self.data["shapes"].extend(new_shapes)
-
-        # Nothing to merge here. Just add all int_shapes if any.
-        if not old_shapes_by_frame or not int_shapes_by_frame:
-            self.data["shapes"].extend(int_shapes)
-            return
-
-        # 4. Build cost matrix for each frame and find correspondence using
-        # Hungarian algorithm. In this case min_cost_thresh is stronger
-        # because we compare only on one frame.
-        min_cost_thresh = 0.25
-        for frame in int_shapes_by_frame:
-            if frame in old_shapes_by_frame:
-                int_shapes = int_shapes_by_frame[frame]
-                old_shapes = old_shapes_by_frame[frame]
-                cost_matrix = np.empty(shape=(len(int_shapes), len(old_shapes)),
-                    dtype=float)
-                # 5.1 Construct cost matrix for the frame.
-                for i, shape0 in enumerate(int_shapes):
-                    for j, shape1 in enumerate(old_shapes):
-                        if shape0["label_id"] == shape1["label_id"]:
-                            cost_matrix[i][j] = 1 - _calc_shapes_similarity(shape0, shape1)
-                        else:
-                            cost_matrix[i][j] = 1
-
-                # 6. Find optimal solution using Hungarian algorithm.
-                row_ind, col_ind = linear_sum_assignment(cost_matrix)
-                int_shapes_indexes = list(range(0, len(int_shapes)))
-                for i, j in zip(row_ind, col_ind):
-                    # Reject the solution if the cost is too high. Remember
-                    # inside int_boxes_indexes boxes which were handled.
-                    if cost_matrix[i][j] <= min_cost_thresh:
-                        old_shapes[j] = _calc_avg_shape(old_shapes[j], int_shapes[i])
-                        int_shapes_indexes[i] = -1
-
-                # 7. Add all boxes which were not processed.
-                for i in int_shapes_indexes:
-                    if i != -1:
-                        self.data["shapes"].append(int_shapes[i])
-            else:
-                # We don't have old boxes on the frame. Let's add all new ones.
-                self.data["shapes"].extend(int_shapes_by_frame[frame])
-
-    def _merge_tracks(self, tracks, start_frame, overlap):
-        # FIXME: implement merge algorithm here
-        self.data["tracks"].extend(tracks)
+            start_frame = db_segment.start_frame
+            overlap = self.db_task.overlap
+            tags.merge(annotation.data["tags"], start_frame, overlap)
+            shapes.merge(annotation.data["shapes"], start_frame, overlap)
+            tracks.merge(annotation.data["tracks"], start_frame, overlap)
 
     def dump(self, file_path, scheme, host, query_params):
         def _flip_box(box, im_w, im_h):
