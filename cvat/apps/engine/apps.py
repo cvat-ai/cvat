644ae27e305f4f625b51ab59a0a3d6f600b8bc17
Boris Sekachev <boris.sekachev@yandex.ru>
2019-04-19 11:30:56 +0300

    Fixed post_save signal

--- cvat/apps/engine/apps.py
+++ cvat/apps/engine/apps.py
@@ -4,14 +4,14 @@
 # SPDX-License-Identifier: MIT
 
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 
 class EngineConfig(AppConfig):
     name = 'cvat.apps.engine'
 
     def ready(self):
+        from django.db.models.signals import post_save
         from .signals import update_task_status
 
         # FIXME: the signal isn't called (why?)
-        post_save.connect(update_task_status, sender='cvat.apps.engine.Job',
+        post_save.connect(update_task_status, sender='engine.Job',
             dispatch_uid="update_task_status")
