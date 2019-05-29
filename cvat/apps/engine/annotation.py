697eb2ab3602e1d48a9d9ffec91146c3b6eab8b1
Edu <motyaedu@gmail.com>
2019-05-29 22:42:15 +0200

    Add video source in dumped file for interpolation mode.

--- cvat/apps/engine/annotation.py
+++ cvat/apps/engine/annotation.py
@@ -1293,7 +1293,8 @@ class TaskAnnotation:
                 ("width", str(im_meta_data[0]["width"])),
                 ("height", str(im_meta_data[0]["height"]))
             ])
-
+            meta["source"] = str(db_task.video.path)
+            
         with open(file_path, "w") as dump_file:
             dumper = XmlAnnotationWriter(dump_file)
             dumper.open_root()
