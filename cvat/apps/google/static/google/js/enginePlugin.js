bb0383e83ffb8f4d854d8690af5869dbdcfea2ff
Bill Heffelfinger <35704164+HeffRaleigh@users.noreply.github.com>
2019-06-03 16:33:00 -0400

    Update enginePlugin.js

--- cvat/apps/google/static/google/js/enginePlugin.js
+++ cvat/apps/google/static/google/js/enginePlugin.js
@@ -1,11 +1,11 @@
 $(`
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=<Your_GA_Property_ID_Here>"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=<GA_Tracking_ID_Here>"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', '<Your_GA_Property_ID_Here>');
+  gtag('config', '<GA_Tracking_ID_Here>');
 </script>
 `).appendTo('head');
