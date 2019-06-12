f24cbd50e5a3a7cef644b17fdf749cbb4f29466f
Artyom Zankevich <zankevich.artyom@gmail.com>
2019-06-12 17:43:16 +0300

    CVAT new UI (#464)
    * Add initial project structure

---
+++ cvat-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
