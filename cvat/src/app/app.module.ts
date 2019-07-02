f88506b1021e5bd464b62cddc9bd15146c2ca79b
Shannon Jin <shannon.jin@axleinfo.com>
2019-07-02 15:55:59 -0400

    Created a dashboard component

--- cvat/src/app/app.module.ts
+++ cvat/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
