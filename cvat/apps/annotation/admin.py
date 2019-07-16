5eee4a6060fe686e22a13bcf8c91ddce6c5d8471
Andrey Zhavoronkov <andrey.zhavoronkov@intel.com>
2019-07-16 11:43:09 +0300

    created new django app

---
+++ cvat/apps/annotation/admin.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from django.contrib import admin
+
+# Register your models here.
---
+++ cvat/apps/annotation/apps.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from django.apps import AppConfig
+
+
+class AnnotationConfig(AppConfig):
+    name = 'annotation'
