42e4be838de40a7c0b014a57ae00a2373b528e64
Boris Sekachev <boris.sekachev@yandex.ru>
2019-07-26 14:24:39 +0300

    Fixed clientID

--- cvat-core/src/config.js
+++ cvat-core/src/config.js
@@ -3,15 +3,10 @@
 * SPDX-License-Identifier: MIT
 */
 
-
-const clientID = +Date.now().toString().substr(-6);
-
 module.exports = {
     backendAPI: 'http://localhost:7000/api/v1',
     proxy: false,
     taskID: undefined,
     jobID: undefined,
-    clientID: {
-        get: () => clientID,
-    },
+    clientID: +Date.now().toString().substr(-6),
 };
