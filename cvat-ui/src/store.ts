7595219b5c85de0e07f67c787f4e3a6ad1f11b85
Artyom Zankevich <zankevich.artyom@gmail.com>
2019-07-30 22:45:23 +0300

    Add `redux-logger`

--- cvat-ui/src/store.ts
+++ cvat-ui/src/store.ts
@@ -1,14 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger'
 
 import rootReducer from './reducers/root.reducer';
 
 export default function configureStore(initialState = {}) {
+  const logger = createLogger({
+    collapsed: true,
+  });
+
+  const middlewares = [];
+
+  if (process.env.NODE_ENV === `development`) {
+    middlewares.push(logger);
+  }
+
+  middlewares.push(thunk);
+
   return createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk),
+      applyMiddleware(...middlewares),
       (window as any).__REDUX_DEVTOOLS_EXTENSION__
         ?
       (window as any).__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
