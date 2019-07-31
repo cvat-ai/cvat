86e9da96de9b82dc11b3a36fd6e27564722825d5
HagegeR <Hagege.ruben@gmail.com>
2019-08-01 02:14:02 +0300

    Update install.sh
    updated to ubuntu 18.04, cuda 10.1 and all other libraries... 
    I don't know how to update NVIDIA_GPGKEY_SUM and NVIDIA_GPGKEY_FPR yet

--- components/cuda/install.sh
+++ components/cuda/install.sh
@@ -8,16 +8,16 @@ set -e
 
 NVIDIA_GPGKEY_SUM=d1be581509378368edeec8c1eb2958702feedf3bc3d17011adbf24efacce4ab5 && \
 NVIDIA_GPGKEY_FPR=ae09fe4bbd223a84b2ccfce3f60f4b3d7fa2af80 && \
-apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub && \
+apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && \
 apt-key adv --export --no-emit-version -a $NVIDIA_GPGKEY_FPR | tail -n +5 > cudasign.pub && \
 echo "$NVIDIA_GPGKEY_SUM  cudasign.pub" | sha256sum -c --strict - && rm cudasign.pub && \
-echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list && \
-echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list && \
+echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/nvidia-ml.list
 
-CUDA_VERSION=9.0.176
-NCCL_VERSION=2.1.15
-CUDNN_VERSION=7.0.5.15
-CUDA_PKG_VERSION="9-0=${CUDA_VERSION}-1"
+CUDA_VERSION=10.1.168
+NCCL_VERSION=2.4.7
+CUDNN_VERSION=7.6.2.24
+CUDA_PKG_VERSION="10-1=${CUDA_VERSION}-1"
 echo 'export PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}' >> ${HOME}/.bashrc
 echo 'export LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:${LD_LIBRARY_PATH}' >> ${HOME}/.bashrc
 
@@ -27,8 +27,8 @@ apt-get update && apt-get install -y --no-install-recommends --allow-unauthentic
     protobuf-compiler \
     cuda-cudart-$CUDA_PKG_VERSION \
     cuda-libraries-$CUDA_PKG_VERSION \
-    libnccl2=$NCCL_VERSION-1+cuda9.0 \
-    libcudnn7=$CUDNN_VERSION-1+cuda9.0 && \
-    ln -s cuda-9.0 /usr/local/cuda && \
+    libnccl2=$NCCL_VERSION-1+cuda10.1 \
+    libcudnn7=$CUDNN_VERSION-1+cuda10.1 && \
+    ln -s cuda-10.1 /usr/local/cuda && \
 rm -rf /var/lib/apt/lists/* \
     /etc/apt/sources.list.d/nvidia-ml.list /etc/apt/sources.list.d/cuda.list
