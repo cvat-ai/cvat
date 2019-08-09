739bf7ebf343c1bd4c5b7fe6b6d3e190b924004b
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2019-08-09 15:54:16 +0200

    Add Firefox to supported platforms

--- cvat/apps/engine/static/engine/js/checkPlatform.js
+++ cvat/apps/engine/static/engine/js/checkPlatform.js
@@ -10,11 +10,11 @@
 
 // legacy syntax for IE support
 
-var supportedPlatforms = ['Chrome'];
+var supportedPlatforms = ['Chrome', 'Firefox'];
 if (supportedPlatforms.indexOf(platform.name) === -1) {
     try {
         document.documentElement.innerHTML = "<center><h1> Your browser is detected as " + platform.name +
-        ". This tool does not support it. Please use the latest version of Google Chrome.</h1></center>";
+        ". This tool does not support it. Please use the latest version of Google Chrome, Chromium or Firefox.</h1></center>";
         window.stop();
     } catch (err) {
         document.execCommand('Stop');
