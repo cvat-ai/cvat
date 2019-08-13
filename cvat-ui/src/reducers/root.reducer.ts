a83ae9b918c33fc15229663ea778b95ff6f01a08
Artyom Zankevich <zankevich.artyom@gmail.com>
2019-08-13 15:17:00 +0300

    Get annotation formats from server

--- cvat-ui/src/reducers/root.reducer.ts
+++ cvat-ui/src/reducers/root.reducer.ts
@@ -3,11 +3,11 @@ import { combineReducers } from 'redux';
 import authContext from './auth.reducer';
 import tasks from './tasks.reducer';
 import tasksFilter from './tasks-filter.reducer';
-import shareFiles from './server.reducer';
+import server from './server.reducer';
 
 export default combineReducers({
   authContext,
   tasks,
   tasksFilter,
-  shareFiles,
+  server,
 });
--- cvat-ui/src/reducers/server.reducer.ts
+++ cvat-ui/src/reducers/server.reducer.ts
@@ -1,12 +1,28 @@
 export default (
   state = {
+    info: null,
     files: [],
+    annotationFormats: [],
     isFetching: false,
     error: null,
   },
   action: any,
 ) => {
   switch (action.type) {
+    case 'GET_SERVER_INFO':
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+    case 'GET_SERVER_INFO_SUCCESS':
+      return Object.assign({}, state, {
+        isFetching: false,
+        info: action.payload,
+      });
+    case 'GET_SERVER_INFO_ERROR':
+      return Object.assign({}, state, {
+        isFetching: false,
+        error: action.payload,
+      });
     case 'GET_SHARE_FILES':
       return Object.assign({}, state, {
         isFetching: true,
@@ -17,6 +33,20 @@ export default (
         files: action.payload,
       });
     case 'GET_SHARE_FILES_ERROR':
+      return Object.assign({}, state, {
+        isFetching: false,
+        error: action.payload,
+      });
+    case 'GET_ANNOTATION_FORMATS':
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+    case 'GET_ANNOTATION_FORMATS_SUCCESS':
+      return Object.assign({}, state, {
+        isFetching: false,
+        annotationFormats: action.payload,
+      });
+    case 'GET_ANNOTATION_FORMATS_ERROR':
       return Object.assign({}, state, {
         isFetching: false,
         error: action.payload,
