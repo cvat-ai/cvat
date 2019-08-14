3bbb9eea6b1f6eb147c2c32a350f034d36078191
Artyom Zankevich <zankevich.artyom@gmail.com>
2019-08-14 20:34:22 +0300

    Add annotations reducer/actions

--- cvat-ui/src/reducers/root.reducer.ts
+++ cvat-ui/src/reducers/root.reducer.ts
@@ -4,10 +4,12 @@ import authContext from './auth.reducer';
 import tasks from './tasks.reducer';
 import tasksFilter from './tasks-filter.reducer';
 import server from './server.reducer';
+import annotations from './annotations.reducer';
 
 export default combineReducers({
   authContext,
   tasks,
   tasksFilter,
   server,
+  annotations,
 });
