827396144369b281943091830931df243a2fd9b0
Artyom Zankevich <zankevich.artyom@gmail.com>
2019-09-02 15:38:32 +0300

    Add global errors handler reducer

--- cvat-ui/src/reducers/root.reducer.ts
+++ cvat-ui/src/reducers/root.reducer.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers, AnyAction } from 'redux';
 
 import authContext from './auth.reducer';
 import tasks from './tasks.reducer';
@@ -7,6 +7,20 @@ import tasksFilter from './tasks-filter.reducer';
 import server from './server.reducer';
 import annotations from './annotations.reducer';
 
+
+// INFO: global errors handler reducer
+const errorMessage = (state = null, action: AnyAction) => {
+  const { type, payload } = action;
+
+  if (type === 'RESET_ERROR_MESSAGE') {
+    return null;
+  } else if (type.endsWith('ERROR')) {
+    return payload;
+  }
+
+  return state;
+}
+
 export default combineReducers({
   authContext,
   tasks,
@@ -14,4 +28,5 @@ export default combineReducers({
   tasksFilter,
   server,
   annotations,
+  errorMessage,
 });
