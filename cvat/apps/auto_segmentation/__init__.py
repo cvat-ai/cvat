7b1a1dfbfcb4cdbcc303174f895a7d10f6aa6820
mohammad reza <malekabbasi@meam.ir>
2019-09-02 12:53:06 +0300

    just cloned tf_annotation folder

---
+++ cvat/apps/auto_segmentation/__init__.py
@@ -0,0 +1,9 @@
+
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from cvat.settings.base import JS_3RDPARTY
+
+JS_3RDPARTY['dashboard'] = JS_3RDPARTY.get('dashboard', []) + ['tf_annotation/js/dashboardPlugin.js']
+
---
+++ cvat/apps/auto_segmentation/admin.py
@@ -0,0 +1,9 @@
+
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from django.contrib import admin
+
+# Register your models here.
+
