6d681e40224217fe3f8738ea6c0dffac5242f2bb
Shannon <shannonj112@gmail.com>
2019-10-02 16:01:52 -0400

    moved cors-header in MIDDLEWARE to above django.middleware.common.CommonMiddleWare

--- backend/cvat-develop/cvat/settings/base.py
+++ backend/cvat-develop/cvat/settings/base.py
@@ -169,13 +169,13 @@ if os.getenv('DJANGO_LOG_VIEWER_HOST'):
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'dj_pagination.middleware.PaginationMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 # Cross-Origin Resource Sharing settings for CVAT UI
