92206ba6adc0d1363b13a648988071b443807672
Boris Sekachev <boris.sekachev@yandex.ru>
2019-10-18 17:27:15 +0300

    Containers&components header refactored

--- cvat-ui/src/containers/header/header.tsx
+++ cvat-ui/src/containers/header/header.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { RouteComponentProps } from 'react-router';
-import { withRouter } from 'react-router-dom';
-
-import {
-    Layout,
-    Radio,
-    Icon,
-    Button,
-    Menu,
-    Modal,
-} from 'antd';
-import Text from 'antd/lib/typography/Text';
 
 import { logoutAsync } from '../../actions/auth-actions';
-
 import { CombinedState } from '../../reducers/root-reducer';
-import { AuthState } from '../../reducers/interfaces';
+
+import HeaderComponent from '../../components/header/header';
 
 interface StateToProps {
-    auth: AuthState;
+    username: string;
+    logoutError: any;
 }
 
 interface DispatchToProps {
@@ -28,7 +17,8 @@ interface DispatchToProps {
 
 function mapStateToProps(state: CombinedState): StateToProps {
     return {
-        auth: state.auth,
+        username: state.auth.user.username,
+        logoutError: state.auth.logoutError,
     };
 }
 
@@ -38,75 +28,17 @@ function mapDispatchToProps(dispatch: any): DispatchToProps {
     }
 }
 
-type HeaderProps = StateToProps & DispatchToProps & RouteComponentProps;
-
-function CVATHeaderContainer(props: HeaderProps) {
-    const cvatLogo = () => (<img src="/assets/cvat-logo.svg"/>);
-    const backLogo = () => (<img src="/assets/icon-playcontrol-previous.svg"/>);
-    const userLogo = () => (<img src="/assets/icon-account.svg" />);
-    const { username } = props.auth.user;
-    const { pathname } = props.location;
-    const { logoutError } = props.auth;
-
-    if (logoutError) {
-        Modal.error({
-            title: 'Could not logout',
-            content: `${logoutError.toString()}`,
-        });
-    }
-
-    let activeTab = null;
-
-    if (pathname === '/tasks') {
-        activeTab = 'tasks';
-    } else if (pathname === '/models') {
-        activeTab = 'models';
-    }
-
+function HeaderContainer(props: StateToProps & DispatchToProps) {
     return (
-        <Layout.Header className='cvat-header'>
-            <div className='left-header'>
-                <Icon className='cvat-logo-icon' component={cvatLogo}/>
-                <Icon className='cvat-back-icon' component={backLogo}/>
-
-                <Radio.Group size='default' value={activeTab} className='cvat-header-buttons'>
-                    <Radio.Button value='tasks'onChange={
-                        () => props.history.push('/tasks')
-                    }> Tasks </Radio.Button>
-                    <Radio.Button value='models' onChange={
-                        () => props.history.push('/models')
-                    }> Models </Radio.Button>
-                    <Button className='header-button' type='link' onClick={
-                        () => window.open('/analytics/app/kibana', '_blank')
-                    }> Analytics </Button>
-                </Radio.Group>
-            </div>
-            <div className='right-header'>
-                <Button className='header-button' type='link' onClick={
-                        () => window.open('https://github.com/opencv/cvat', '_blank')
-                }> <Icon type='github' /> GitHub </Button>
-                <Button className='header-button' type='link' onClick={
-                        () => window.open('/documentation/user_guide.html', '_blank')
-                }> Help </Button>
-                <Menu className='cvat-header-menu' subMenuCloseDelay={0.1} mode='horizontal'>
-                    <Menu.SubMenu title={
-                        <span>
-                            <Icon className='cvat-header-user-icon' component={userLogo} />
-                            <span>
-                                <Text strong> {username} </Text>
-                                <Icon className='cvat-header-menu-icon' component={backLogo} />
-                            </span>
-                        </span>
-                    }>
-                        <Menu.Item onClick={props.logout}>Logout</Menu.Item>
-                    </Menu.SubMenu>
-                </Menu>
-            </div>
-        </Layout.Header>
+        <HeaderComponent
+            onLogout={props.logout}
+            username={props.username}
+            logoutError={props.logoutError ? props.logoutError.toString() : ''}
+        />
     );
 }
 
-export default withRouter(connect(
+export default connect(
     mapStateToProps,
     mapDispatchToProps,
-)(CVATHeaderContainer));
+)(HeaderContainer);
