2dc32114b1de822adae2bc2c41489b2f6a0d38ee
yurygoru <yury.gorunov@intel.com>
2019-11-06 10:40:01 +0000

    fix problems with image play idle

--- cvat-data/src/js/unzip_imgs.js
+++ cvat-data/src/js/unzip_imgs.js
@@ -1,14 +1,11 @@
 const JSZip = require('jszip');
 
-
-
 self.onmessage = function (e) {
 
     const zip = new JSZip();
     const start =  e.data.start;
     const end =  e.data.end;
     const block =  e.data.block;
-
     
     zip.loadAsync(block).then((_zip) => {
         fileMapping = {};
@@ -25,7 +22,7 @@ self.onmessage = function (e) {
             _zip.file(relativePath).async('blob').then((fileData) => {
                 const reader = new FileReader();
                 reader.onload = (function(i, event){
-                    postMessage({fileName : relativePath, index : fileIndex, data: reader.result, isEnd : i === inverseUnzippedFilesCount < start});
+                    postMessage({fileName : relativePath, index : fileIndex, data: reader.result, isEnd : inverseUnzippedFilesCount <= start});
                     inverseUnzippedFilesCount --;
                     if (inverseUnzippedFilesCount < start){
                         self.close();
