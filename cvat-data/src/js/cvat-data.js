025506ea71edb47aa7cbb6ac0230134230dd85e9
Andrey Zhavoronkov <andrey.zhavoronkov@intel.com>
2019-11-19 16:13:56 +0300

    added cropImage function

--- cvat-data/src/js/cvat-data.js
+++ cvat-data/src/js/cvat-data.js
@@ -165,72 +165,24 @@ class FrameProvider {
         this._blocks[chunkNumber] = "loading";
     }
 
-    YCbCrToRGBA (y, cb, cr, width, height) {
-        const rgba = new Uint8ClampedArray(width * height * 4).fill(255);
-        const w = ((width + 15) >> 4) << 4;
-        const w2 = w >> 1;
+    cropImage(imageBuffer, imageWidth, imageHeight, x_offset, y_offset, width, height) {
+        if (x_offset + width === imageWidth &&
+            y_offset + height === imageHeight) {
+                return new Uint8ClampedArray(imageBuffer);
+        }
+        const source = new Uint32Array(imageBuffer);
+        const buffer = new ArrayBuffer(width * height * 4);
+        const rgbaInt32 = new Uint32Array(buffer);
+        const rgbaInt8Clamped = new Uint8ClampedArray(buffer);
 
-        let yIndex1 = 0;
-        let yIndex2 = w;
-        const yNext2Lines = w + (w - width);
-
-        let cIndex = 0;
-        const cNextLine = w2 - (width >> 1);
-
-        let rgbaIndex1 = 0;
-        let rgbaIndex2 = width * 4;
-        const rgbaNext2Lines = width * 4;
-
-        const cols = width >> 1;
-        const rows = height >> 1;
-
-        let ccb = 0;
-        let ccr = 0;
-        let r = 0;
-        let g = 0;
-        let b = 0;
-
-        for (let row = 0; row < rows; row++) {
-            for (let col = 0; col < cols; col++) {
-                ccb = cb[cIndex];
-                ccr = cr[cIndex];
-                cIndex++;
-
-                r = (ccb + ((ccb * 103) >> 8)) - 179;
-                g = ((ccr * 88) >> 8) - 44 + ((ccb * 183) >> 8) - 91;
-                b = (ccr + ((ccr * 198) >> 8)) - 227;
-
-                // Line 1
-                const y1 = y[yIndex1++];
-                const y2 = y[yIndex1++];
-                rgba[rgbaIndex1] = y1 + r;
-                rgba[rgbaIndex1 + 1] = y1 - g;
-                rgba[rgbaIndex1 + 2] = y1 + b;
-                rgba[rgbaIndex1 + 4] = y2 + r;
-                rgba[rgbaIndex1 + 5] = y2 - g;
-                rgba[rgbaIndex1 + 6] = y2 + b;
-                rgbaIndex1 += 8;
-
-                // Line 2
-                const y3 = y[yIndex2++];
-                const y4 = y[yIndex2++];
-                rgba[rgbaIndex2] = y3 + r;
-                rgba[rgbaIndex2 + 1] = y3 - g;
-                rgba[rgbaIndex2 + 2] = y3 + b;
-                rgba[rgbaIndex2 + 4] = y4 + r;
-                rgba[rgbaIndex2 + 5] = y4 - g;
-                rgba[rgbaIndex2 + 6] = y4 + b;
-                rgbaIndex2 += 8;
-            }
-
-            yIndex1 += yNext2Lines;
-            yIndex2 += yNext2Lines;
-            rgbaIndex1 += rgbaNext2Lines;
-            rgbaIndex2 += rgbaNext2Lines;
-            cIndex += cNextLine;
+        let out = 0;
+        for (let row = y_offset; row < height; row++) {
+            const start = row * imageWidth + x_offset;
+            rgbaInt32.set(source.subarray(start, start + width), out);
+            out += width;
         }
 
-        return rgba;
+        return rgbaInt8Clamped;
     }
 
     async startDecode() {
@@ -256,25 +208,29 @@ class FrameProvider {
             const worker = new Worker('/static/engine/js/Decoder.js');
 
             let index = start;
-            var t0 = performance.now();
-            worker.onmessage = e => {
-                if (e.data.consoleLog) {
+
+            worker.onmessage = (e) => {
+                if (e.data.consoleLog) { // ignore initialization message
                   return;
                 }
-                var t1 = performance.now();
-                console.log(`Call to decode frame ${index} took ` + (t1 - t0) + " milliseconds.")
-                t0 = t1;
 
-                this._frames[index] = new Uint8ClampedArray(e.data.buf);
+                this._frames[index] = this.cropImage(e.data.buf, e.data.width, e.data.height, 0, 0, this._width, this._height);
                 this._decodingBlocks[`${start}:${end}`].resolveCallback(index);
                 if (index === end) {
                     this._decodeThreadCount--;
-                    // console.log("stop decoding " + event.data.start + " to " + event.data.end + " frames");
-                    // console.log(this._decodeThreadCount);
                     delete this._decodingBlocks[`${start}:${end}`];
                     worker.terminate();
                 }
                 index++;
+            };
+
+            worker.onerror = (e) => {
+                console.log(['ERROR: Line ', e.lineno, ' in ', e.filename, ': ', e.message].join(''));
+                worker.terminate();
+                this._decodeThreadCount--;
+                // console.log(this._decodeThreadCount);
+                this._decodingBlocks[`${start}:${end}`].rejectCallback();
+                delete this._decodingBlocks[`${start}:${end}`];
             };
 
             worker.postMessage({
