4ad58b33433a890598f2a3fce32df34d49dfc496
Boris Sekachev <boris.sekachev@yandex.ru>
2019-11-29 14:49:23 +0300

    Fixed default variables

--- cvat-ui/src/core.ts
+++ cvat-ui/src/core.ts
@@ -5,9 +5,9 @@ const cvat: any = _cvat;
 const protocol = typeof (process.env.REACT_APP_API_PROTOCOL) === 'undefined'
     ? 'http' : process.env.REACT_APP_API_PROTOCOL;
 const host = typeof (process.env.REACT_APP_API_HOST) === 'undefined'
-    ? 'http' : process.env.REACT_APP_API_HOST;
+    ? 'localhost' : process.env.REACT_APP_API_HOST;
 const port = typeof (process.env.REACT_APP_API_PORT) === 'undefined'
-    ? 'http' : process.env.REACT_APP_API_PORT;
+    ? '7000' : process.env.REACT_APP_API_PORT;
 
 cvat.config.backendAPI = `${protocol}://${host}:${port}/api/v1`;
 
