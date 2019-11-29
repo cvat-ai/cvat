59d587915823545f22ae83e286cddb1f2bf05342
Boris Sekachev <boris.sekachev@yandex.ru>
2019-11-29 13:29:41 +0300

    Better check for env variables

--- cvat-ui/src/core.ts
+++ cvat-ui/src/core.ts
@@ -2,9 +2,12 @@ import _cvat from '../../cvat-core/src/api';
 
 const cvat: any = _cvat;
 
-const protocol = process.env.REACT_APP_API_PROTOCOL || 'http';
-const host = process.env.REACT_APP_API_HOST || 'localhost';
-const port = process.env.REACT_APP_API_PORT || '7000';
+const protocol = 'REACT_APP_API_PROTOCOL' in process.env
+    ? process.env.REACT_APP_API_PROTOCOL : 'http';
+const host = 'REACT_APP_API_HOST' in process.env
+    ? process.env.REACT_APP_API_HOST : 'localhost';
+const port = 'REACT_APP_API_PORT' in process.env
+    ? process.env.REACT_APP_API_PORT : '7000';
 
 cvat.config.backendAPI = `${protocol}://${host}:${port}/api/v1`;
 
