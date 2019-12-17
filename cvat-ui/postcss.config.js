5ee01b8099d05c4f9d9aa8870a26249dde778afb
Boris Sekachev <boris.sekachev@yandex.ru>
2019-12-17 13:10:07 +0300

    Sass for CVAT, less for Antd, added autoprefixer and css polyfills

---
+++ cvat-ui/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: false,
+    plugins: {
+        'postcss-preset-env': {
+            browsers: '> 2%', // https://github.com/browserslist/browserslist
+        },
+    },
+};
