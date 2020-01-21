3a6d9dffb7802f0cd27456fa5f634f5bfe7ac470
Zhiltsov Max <zhiltsov.max35@gmail.com>
2020-01-21 17:57:40 +0300

    Add images dir extractor

---
+++ datumaro/datumaro/components/extractors/images_dir.py
@@ -0,0 +1,54 @@
+
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from collections import OrderedDict
+import os
+import os.path as osp
+
+from datumaro.components.extractor import DatasetItem, Extractor
+from datumaro.util.image import lazy_image
+
+
+class CvatImagesDirExtractor(Extractor):
+    _SUPPORTED_FORMATS = ['.png', '.jpg']
+
+    def __init__(self, url):
+        super().__init__()
+
+        items = []
+        for (dirpath, _, filenames) in os.walk(url):
+            for name in filenames:
+                path = osp.join(dirpath, name)
+                if self._is_image(path):
+                    item_id = osp.splitext(name)[0]
+                    item = DatasetItem(id=item_id, image=lazy_image(path))
+                    items.append((item.id, item))
+
+        items = sorted(items, key=lambda e: e[0])
+        items = OrderedDict(items)
+        self._items = items
+
+        self._subsets = None
+
+    def __iter__(self):
+        for item in self._items.values():
+            yield item
+
+    def __len__(self):
+        return len(self._items)
+
+    def subsets(self):
+        return self._subsets
+
+    def get(self, item_id, subset=None, path=None):
+        if path or subset:
+            raise KeyError()
+        return self._items[item_id]
+
+    def _is_image(self, path):
+        for ext in self._SUPPORTED_FORMATS:
+            if osp.isfile(path) and path.endswith(ext):
+                return True
+        return False
