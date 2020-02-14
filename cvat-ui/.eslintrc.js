572994a2ae09069ea09a3abffef8b1d6d3e5d957
Nikita Glazov <nglazov@gmail.com>
2020-02-14 15:38:59 +0300

    Update eslint in cvat-ui

--- cvat-ui/.eslintrc.js
+++ cvat-ui/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'parserOptions': {
         'parser': '@typescript-eslint/parser',
         'ecmaVersion': 6,
+        'project': './tsconfig.json',
     },
     'plugins': [
         '@typescript-eslint',
