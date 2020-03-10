a65072a030bb2006e673698f4c721538afeb52bd
Nikita Manovich <nikita.manovich@intel.com>
2020-03-10 15:31:07 +0300

    Sorted requirements, added a comment, removed nosec for exec.

--- datumaro/datumaro/plugins/openvino_launcher.py
+++ datumaro/datumaro/plugins/openvino_launcher.py
@@ -23,7 +23,7 @@ class InterpreterScript:
             script = f.read()
 
         context = {}
-        exec(script, context, context) # nosec
+        exec(script, context, context)
 
         process_outputs = context['process_outputs']
         assert callable(process_outputs)
@@ -48,6 +48,9 @@ class OpenVinoLauncher(Launcher):
     @staticmethod
     def _check_instruction_set(instruction):
         return instruction == str.strip(
+            # Let's ignore a warning from bandit about using shell=True.
+            # In this case it isn't a security issue and we use some
+            # shell features like pipes.
             subprocess.check_output(
                 'lscpu | grep -o "{}" | head -1'.format(instruction),
                 shell=True).decode('utf-8') # nosec
