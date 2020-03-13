5274072b796c6c5c6b233b077e9dc1b281cb637c
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2020-03-13 13:24:28 +0300

    Fix labelme filenames

--- cvat/apps/annotation/labelme.py
+++ cvat/apps/annotation/labelme.py
@@ -107,13 +107,13 @@ def dump_frame_anno(frame_annotation):
     return ET.tostring(root_elem, encoding='unicode', pretty_print=True)
 
 def dump_as_labelme_annotation(file_object, annotations):
+    import os.path as osp
     from zipfile import ZipFile, ZIP_DEFLATED
 
     with ZipFile(file_object, 'w', compression=ZIP_DEFLATED) as output_zip:
         for frame_annotation in annotations.group_by_frame():
             xml_data = dump_frame_anno(frame_annotation)
-            filename = frame_annotation.name
-            filename = filename[ : filename.rfind('.')] + '.xml'
+            filename = osp.splitext(frame_annotation.name)[0] + '.xml'
             output_zip.writestr(filename, xml_data)
 
 def parse_xml_annotations(xml_data, annotations, input_zip):
