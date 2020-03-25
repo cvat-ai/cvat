73a7a7fd79f8dc5adbbe4fcf3446d3b5ab764ec4
Boris Sekachev <boris.sekachev@yandex.ru>
2020-03-25 19:02:08 +0300

    Fixed resize on right mouse button

--- cvat-canvas/src/typescript/canvasView.ts
+++ cvat-canvas/src/typescript/canvasView.ts
@@ -1208,11 +1208,7 @@ export class CanvasViewImpl implements CanvasView, Listener {
 
         let shapeSizeElement: ShapeSizeElement | null = null;
         let resized = false;
-        (shape as any).resize().on('resizestart', (e: any): void => {
-            if (e.detail.event.detail.event.button === 2) {
-                e.preventDefault();
-                return;
-            }
+        (shape as any).resize().on('resizestart', (): void => {
             this.mode = Mode.RESIZE;
             if (state.shapeType === 'rectangle') {
                 shapeSizeElement = displayShapeSize(this.adoptedContent, this.adoptedText);
