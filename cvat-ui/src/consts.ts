dd4a0884da15d0d6b5760fba97fccdc0f539c198
Boris Sekachev <boris.sekachev@yandex.ru>
2020-04-01 00:26:36 +0300

    Special behaviour for the attribute value __undefined__

---
+++ cvat-ui/src/consts.ts
@@ -0,0 +1,9 @@
+// Copyright (C) 2019-2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+const UNDEFINED_ATTRIBUTE_VALUE = '__undefined__';
+
+export default {
+    UNDEFINED_ATTRIBUTE_VALUE,
+};
