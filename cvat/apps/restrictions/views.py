5490f06f59a805d69b3cc283677d1bc8ad7089aa
Andrey Zhavoronkov <andrey.zhavoronkov@intel.com>
2020-04-23 19:48:16 +0300

    fix

--- cvat/apps/restrictions/views.py
+++ cvat/apps/restrictions/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-
+from drf_yasg.utils import swagger_auto_schema
 
 from cvat.apps.restrictions.serializers import UserAgreementSerializer
 
