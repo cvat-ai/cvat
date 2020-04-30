ee6deba30404264085b98bf0e24ad3684330f6fc
Andrey Zhavoronkov <41117609+azhavoro@users.noreply.github.com>
2020-04-30 15:18:35 +0300

    Fixed 'Open task' button doesn't work (#1474)

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -187,19 +187,7 @@ MIDDLEWARE = [
     'dj_pagination.middleware.PaginationMiddleware',
 ]
 
-# Cross-Origin Resource Sharing settings for CVAT UI
-UI_SCHEME = os.environ.get('UI_SCHEME', 'http')
-UI_HOST = os.environ.get('UI_HOST', 'localhost')
-UI_PORT = os.environ.get('UI_PORT', '3000')
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = [UI_HOST]
-UI_URL = '{}://{}'.format(UI_SCHEME, UI_HOST)
-
-if UI_PORT and UI_PORT != '80':
-    UI_URL += ':{}'.format(UI_PORT)
-
-CORS_ORIGIN_WHITELIST = [UI_URL]
-CORS_REPLACE_HTTPS_REFERER = True
+UI_URL = ''
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
