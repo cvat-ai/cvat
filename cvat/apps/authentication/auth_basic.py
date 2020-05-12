5f2dcbd6d7665f2a52b9b51876ab0de1d7eff266
Andrey Zhavoronkov <andrey.zhavoronkov@intel.com>
2020-05-12 12:04:36 +0300

    allow to specify default group for the new user

--- cvat/apps/authentication/auth_basic.py
+++ cvat/apps/authentication/auth_basic.py
@@ -3,10 +3,20 @@
 # SPDX-License-Identifier: MIT
 from . import AUTH_ROLE
 from django.conf import settings
+from importlib import import_module
+
+def _import_role(role):
+    assert isinstance(role, str)
+    package, role_name = role.rsplit('.AUTH_ROLE.', 1)
+    module = getattr(import_module(package), 'AUTH_ROLE')
+    return getattr(module, role_name)
 
 def create_user(sender, instance, created, **kwargs):
     from django.contrib.auth.models import Group
 
     if instance.is_superuser and instance.is_staff:
         db_group = Group.objects.get(name=AUTH_ROLE.ADMIN)
+        instance.groups.add(db_group)
+    for group in settings.DJANGO_AUTH_DEFAULT_GROUPS:
+        db_group = Group.objects.get(name=_import_role(group))
         instance.groups.add(db_group)
--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -230,6 +230,7 @@ WSGI_APPLICATION = 'cvat.wsgi.application'
 
 # Django Auth
 DJANGO_AUTH_TYPE = 'BASIC'
+DJANGO_AUTH_DEFAULT_GROUPS = []
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 AUTH_LOGIN_NOTE = '<p>Have not registered yet? <a href="/auth/register">Register here</a>.</p>'
