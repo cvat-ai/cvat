ea751d5d2a3a3f7246601a94182c6da92dbdb3ae
Ubuntu <rush@rush.io5wjidgvxtebp1qtnd0go0geh.dx.internal.cloudapp.net>
2020-06-03 20:20:37 +0000

    check if username exists before creating new one

--- default_superuser.py
+++ default_superuser.py
@@ -1,8 +1,10 @@
 import os
 
 from django.contrib.auth.models import User
-u = User(username=os.getenv("DJANGO_SUPERUSER_USERNAME","onepaneladmin"))
-u.set_password(os.getenv("DJANGO_SUPERUSER_PASSWORD","onepaneladmin"))
-u.is_superuser = True
-u.is_staff = True
-u.save()
+if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME","onepaneladmin")).exists():
+
+	u = User(username=os.getenv("DJANGO_SUPERUSER_USERNAME","onepaneladmin"))
+	u.set_password(os.getenv("DJANGO_SUPERUSER_PASSWORD","onepaneladmin"))
+	u.is_superuser = True
+	u.is_staff = True
+	u.save()
