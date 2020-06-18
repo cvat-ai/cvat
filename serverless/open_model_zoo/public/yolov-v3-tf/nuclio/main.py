9490d1ab4c6c3abc3a754611d282b1e3a8587596
Nikita Manovich <nikita.manovich@intel.com>
2020-06-18 15:50:30 +0300

    Read labels spec from function.yaml.

--- serverless/open_model_zoo/public/yolov-v3-tf/nuclio/main.py
+++ serverless/open_model_zoo/public/yolov-v3-tf/nuclio/main.py
@@ -5,7 +5,7 @@ import io
 from model_loader import ModelLoader
 import numpy as np
 from math import exp
-
+import yaml
 
 def init_context(context):
     context.logger.info("Init context...  0%")
@@ -13,88 +13,9 @@ def init_context(context):
     model_bin = "/opt/nuclio/open_model_zoo/public/yolo-v3-tf/FP32/yolo-v3-tf.bin"
     model_handler = ModelLoader(model_xml, model_bin)
     setattr(context.user_data, 'model_handler', model_handler)
-    labels = {
-        0: "person",
-        1: "bicycle",
-        2: "car",
-        3: "motorbike",
-        4: "aeroplane",
-        5: "bus",
-        6: "train",
-        7: "truck",
-        8: "boat",
-        9: "traffic light",
-        10: "fire hydrant",
-        11: "stop sign",
-        12: "parking meter",
-        13: "bench",
-        14: "bird",
-        15: "cat",
-        16: "dog",
-        17: "horse",
-        18: "sheep",
-        19: "cow",
-        20: "elephant",
-        21: "bear",
-        22: "zebra",
-        23: "giraffe",
-        24: "backpack",
-        25: "umbrella",
-        26: "handbag",
-        27: "tie",
-        28: "suitcase",
-        29: "frisbee",
-        30: "skis",
-        31: "snowboard",
-        32: "sports ball",
-        33: "kite",
-        34: "baseball bat",
-        35: "baseball glove",
-        36: "skateboard",
-        37: "surfboard",
-        38: "tennis racket",
-        39: "bottle",
-        40: "wine glass",
-        41: "cup",
-        42: "fork",
-        43: "knife",
-        44: "spoon",
-        45: "bowl",
-        46: "banana",
-        47: "apple",
-        48: "sandwich",
-        49: "orange",
-        50: "broccoli",
-        51: "carrot",
-        52: "hot dog",
-        53: "pizza",
-        54: "donut",
-        55: "cake",
-        56: "chair",
-        57: "sofa",
-        58: "pottedplant",
-        59: "bed",
-        60: "diningtable",
-        61: "toilet",
-        62: "tvmonitor",
-        63: "laptop",
-        64: "mouse",
-        65: "remote",
-        66: "keyboard",
-        67: "cell phone",
-        68: "microwave",
-        69: "oven",
-        70: "toaster",
-        71: "sink",
-        72: "refrigerator",
-        73: "book",
-        74: "clock",
-        75: "vase",
-        76: "scissors",
-        77: "teddy bear",
-        78: "hair drier",
-        79: "toothbrush"
-    }
+    functionconfig = yaml.safe_load(open("/opt/nuclio/function.yaml"))
+    labels_spec = functionconfig['metadata']['annotations']['spec']
+    labels = {item['id']: item['name'] for item in json.loads(labels_spec)}
     setattr(context.user_data, "labels", labels)
     context.logger.info("Init context...100%")
 
