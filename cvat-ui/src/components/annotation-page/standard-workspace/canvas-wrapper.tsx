4065bb36b0cbdb4cacb2cb4bc05533770cd17707
Boris Sekachev <boris.sekachev@yandex.ru>
2020-06-19 10:23:03 +0300

    Fixed AAM bug: #1750

--- cvat-ui/src/components/annotation-page/standard-workspace/canvas-wrapper.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/canvas-wrapper.tsx
@@ -618,14 +618,12 @@ export default class CanvasWrapperComponent extends React.PureComponent<Props> {
         const {
             annotations,
             frameData,
-            frameAngle,
             canvasInstance,
         } = this.props;
 
         if (frameData !== null) {
             canvasInstance.setup(frameData, annotations
                 .filter((e) => e.objectType !== ObjectType.TAG));
-            canvasInstance.rotate(frameAngle);
         }
     }
 
