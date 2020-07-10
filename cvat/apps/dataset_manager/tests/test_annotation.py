75027cb346e3b2e77b848ef236c6518b13e96c6a
Zhiltsov Max <zhiltsov.max35@gmail.com>
2020-07-10 10:39:32 +0300

    Add interpolation smoke tests

--- cvat/apps/dataset_manager/tests/test_annotation.py
+++ cvat/apps/dataset_manager/tests/test_annotation.py
@@ -8,7 +8,7 @@ from unittest import TestCase
 
 
 class TrackManagerTest(TestCase):
-    def test_single_point_interpolation(self):
+    def test_point_interpolation(self):
         track = {
             "frame": 0,
             "label_id": 0,
@@ -28,6 +28,96 @@ class TrackManagerTest(TestCase):
                     "attributes": [],
                     "points": [3.0, 4.0, 5.0, 6.0],
                     "type": "points",
+                    "occluded": False,
+                    "outside": True
+                },
+            ]
+        }
+
+        interpolated = TrackManager.get_interpolated_shapes(track, 0, 2)
+
+        self.assertEqual(len(interpolated), 3)
+
+    def test_polygon_interpolation(self):
+        track = {
+            "frame": 0,
+            "label_id": 0,
+            "group": None,
+            "attributes": [],
+            "shapes": [
+                {
+                    "frame": 0,
+                    "points": [1.0, 2.0, 3.0, 4.0, 5.0, 2.0],
+                    "type": "polygon",
+                    "occluded": False,
+                    "outside": False,
+                    "attributes": []
+                },
+                {
+                    "frame": 2,
+                    "attributes": [],
+                    "points": [3.0, 4.0, 5.0, 6.0, 7.0, 6.0, 4.0, 5.0],
+                    "type": "polygon",
+                    "occluded": False,
+                    "outside": True
+                },
+            ]
+        }
+
+        interpolated = TrackManager.get_interpolated_shapes(track, 0, 2)
+
+        self.assertEqual(len(interpolated), 3)
+
+    def test_bbox_interpolation(self):
+        track = {
+            "frame": 0,
+            "label_id": 0,
+            "group": None,
+            "attributes": [],
+            "shapes": [
+                {
+                    "frame": 0,
+                    "points": [1.0, 2.0, 3.0, 4.0],
+                    "type": "rectangle",
+                    "occluded": False,
+                    "outside": False,
+                    "attributes": []
+                },
+                {
+                    "frame": 2,
+                    "attributes": [],
+                    "points": [3.0, 4.0, 5.0, 6.0],
+                    "type": "rectangle",
+                    "occluded": False,
+                    "outside": True
+                },
+            ]
+        }
+
+        interpolated = TrackManager.get_interpolated_shapes(track, 0, 2)
+
+        self.assertEqual(len(interpolated), 3)
+
+    def test_line_interpolation(self):
+        track = {
+            "frame": 0,
+            "label_id": 0,
+            "group": None,
+            "attributes": [],
+            "shapes": [
+                {
+                    "frame": 0,
+                    "points": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+                    "type": "polyline",
+                    "occluded": False,
+                    "outside": False,
+                    "attributes": []
+                },
+                {
+                    "frame": 2,
+                    "attributes": [],
+                    "points": [3.0, 4.0, 5.0, 6.0],
+                    "type": "polyline",
                     "occluded": False,
                     "outside": True
                 },
