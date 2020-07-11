5cc94fa3425c7034b4ea1ff2c7800ffdb85c57bf
Ilya Kochankov <fakafafakafa@mail.ru>
2020-07-11 10:42:42 +0300

    Rewrite yolo tests

--- datumaro/tests/test_yolo_format.py
+++ datumaro/tests/test_yolo_format.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 from datumaro.components.extractor import (Extractor, DatasetItem,
     AnnotationType, Bbox, LabelCategories,
 )
-from datumaro.components.project import Project
+from datumaro.components.project import Project, Dataset
 from datumaro.plugins.yolo_format.importer import YoloImporter
 from datumaro.plugins.yolo_format.converter import YoloConverter
 from datumaro.util.image import Image, save_image
@@ -15,9 +15,7 @@ from datumaro.util.test_utils import TestDir, compare_datasets
 
 class YoloFormatTest(TestCase):
     def test_can_save_and_load(self):
-        class TestExtractor(Extractor):
-            def __iter__(self):
-                return iter([
+        source_dataset = Dataset.from_iterable([
                     DatasetItem(id=1, subset='train', image=np.ones((8, 8, 3)),
                         annotations=[
                             Bbox(0, 2, 4, 2, label=2),
@@ -37,18 +35,12 @@ class YoloFormatTest(TestCase):
                             Bbox(0, 2, 4, 2, label=6),
                             Bbox(0, 7, 3, 2, label=7),
                         ]),
-                ])
-
-            def categories(self):
-                label_categories = LabelCategories()
-                for i in range(10):
-                    label_categories.add('label_' + str(i))
-                return {
-                    AnnotationType.label: label_categories,
-                }
+                ], categories={
+                    AnnotationType.label: LabelCategories.from_iterable(
+                        'label_' + str(i) for i in range(10)),
+                })
 
         with TestDir() as test_dir:
-            source_dataset = TestExtractor()
 
             YoloConverter(save_images=True)(source_dataset, test_dir)
             parsed_dataset = YoloImporter()(test_dir).make_dataset()
@@ -56,27 +48,19 @@ class YoloFormatTest(TestCase):
             compare_datasets(self, source_dataset, parsed_dataset)
 
     def test_can_save_dataset_with_image_info(self):
-        class TestExtractor(Extractor):
-            def __iter__(self):
-                return iter([
+        source_dataset = Dataset.from_iterable([
                     DatasetItem(id=1, subset='train',
                         image=Image(path='1.jpg', size=(10, 15)),
                         annotations=[
                             Bbox(0, 2, 4, 2, label=2),
                             Bbox(3, 3, 2, 3, label=4),
                         ]),
-                ])
-
-            def categories(self):
-                label_categories = LabelCategories()
-                for i in range(10):
-                    label_categories.add('label_' + str(i))
-                return {
-                    AnnotationType.label: label_categories,
-                }
+                ], categories={
+                    AnnotationType.label: LabelCategories.from_iterable(
+                        'label_' + str(i) for i in range(10)),
+                })
 
         with TestDir() as test_dir:
-            source_dataset = TestExtractor()
 
             YoloConverter()(source_dataset, test_dir)
 
@@ -87,27 +71,19 @@ class YoloFormatTest(TestCase):
             compare_datasets(self, source_dataset, parsed_dataset)
 
     def test_can_load_dataset_with_exact_image_info(self):
-        class TestExtractor(Extractor):
-            def __iter__(self):
-                return iter([
+        source_dataset = Dataset.from_iterable([
                     DatasetItem(id=1, subset='train',
                         image=Image(path='1.jpg', size=(10, 15)),
                         annotations=[
                             Bbox(0, 2, 4, 2, label=2),
                             Bbox(3, 3, 2, 3, label=4),
                         ]),
-                ])
-
-            def categories(self):
-                label_categories = LabelCategories()
-                for i in range(10):
-                    label_categories.add('label_' + str(i))
-                return {
-                    AnnotationType.label: label_categories,
-                }
+                ], categories={
+                    AnnotationType.label: LabelCategories.from_iterable(
+                        'label_' + str(i) for i in range(10)),
+                })
 
         with TestDir() as test_dir:
-            source_dataset = TestExtractor()
 
             YoloConverter()(source_dataset, test_dir)
 
@@ -117,24 +93,20 @@ class YoloFormatTest(TestCase):
             compare_datasets(self, source_dataset, parsed_dataset)
 
     def test_relative_paths(self):
-        class TestExtractor(Extractor):
-            def __iter__(self):
-                return iter([
+        source_dataset = Dataset.from_iterable([
                     DatasetItem(id='1', subset='train',
                         image=np.ones((4, 2, 3))),
                     DatasetItem(id='subdir1/1', subset='train',
                         image=np.ones((2, 6, 3))),
                     DatasetItem(id='subdir2/1', subset='train',
                         image=np.ones((5, 4, 3))),
-                ])
-
-            def categories(self):
-                return { AnnotationType.label: LabelCategories() }
+                ], categories={
+                    AnnotationType.label: LabelCategories(),
+                })
 
         for save_images in {True, False}:
             with self.subTest(save_images=save_images):
                 with TestDir() as test_dir:
-                    source_dataset = TestExtractor()
 
                     YoloConverter(save_images=save_images)(
                         source_dataset, test_dir)
@@ -150,26 +122,19 @@ class YoloImporterTest(TestCase):
         self.assertTrue(YoloImporter.detect(DUMMY_DATASET_DIR))
 
     def test_can_import(self):
-        class DstExtractor(Extractor):
-            def __iter__(self):
-                return iter([
+        expected_dataset = Dataset.from_iterable([
                     DatasetItem(id=1, subset='train',
                         image=np.ones((10, 15, 3)),
                         annotations=[
                             Bbox(0, 2, 4, 2, label=2),
                             Bbox(3, 3, 2, 3, label=4),
                         ]),
-                ])
-
-            def categories(self):
-                label_categories = LabelCategories()
-                for i in range(10):
-                    label_categories.add('label_' + str(i))
-                return {
-                    AnnotationType.label: label_categories,
-                }
+                ], categories={
+                    AnnotationType.label: LabelCategories.from_iterable(
+                        'label_' + str(i) for i in range(10)),
+                })
 
         dataset = Project.import_from(DUMMY_DATASET_DIR, 'yolo') \
             .make_dataset()
 
-        compare_datasets(self, DstExtractor(), dataset)
+        compare_datasets(self, expected_dataset, dataset)
