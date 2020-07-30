42e3d0202b170e3a25a67b24cbea2badf27db885
Mike Peven <mpeven@gmail.com>
2020-07-30 15:07:59 -0400

    Increase the rate of allowed requests

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/hour',
+        'anon': '100/minute',
     },
 }
 
