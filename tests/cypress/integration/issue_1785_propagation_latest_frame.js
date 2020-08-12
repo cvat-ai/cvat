29b580cf135f1dc493731dd3cda11de8d17ee329
Dmitry Kruchinin <dmitryx.kruchinin@intel.com>
2020-08-12 15:11:35 +0300

    Applying comments.
    Code refactoring.

--- tests/cypress/integration/issue_1785_propagation_latest_frame.js
+++ tests/cypress/integration/issue_1785_propagation_latest_frame.js
@@ -16,43 +16,36 @@ context('Check propagation work from the latest frame', () => {
     const images = [`image_${issueId}_1.png`,
                     `image_${issueId}_2.png`,
                     `image_${issueId}_3.png`]
-    const width = '800'
-    const height = '800'
-    const posX=10
-    const posY=10
+    const width = 800
+    const height = 800
+    const posX = 10
+    const posY = 10
     const color = 'gray'
     const archiveName = `images_issue_${issueId}.zip`
     const archivePath = `cypress/fixtures/${archiveName}`
     const imagesFolder = `cypress/fixtures/image_issue_${issueId}`
-    const lastElement = images[images.length - 1];
+    const lastElement = images[images.length - 1]
 
     before(() => {
         cy.visit('auth/login')
         cy.login()
-        cy.get('[type="submit"]').click()
         for (let img of images) {
             cy.imageGenerator(imagesFolder, img, width, height, color, posX, posY, labelName)
         }
         cy.wait(2000)
         cy.createZipArchive(imagesFolder, archivePath)
         cy.createAnnotationTask(taskName, labelName, attrName, textDefaultValue, archiveName)
+        cy.openTaskJob(taskName)
     })
 
-    describe(`Open task "${taskName}"`, () => {
-        it('The annotation task present in the list', () => {
-            cy.contains('strong', taskName)
-            .parent()
-            .parent()
-            .parent()
-            .contains('a', 'Open').click()
-        })
-        it('Open a job', () => {
-            cy.contains('a', 'Job #').click()
-            cy.url().should('include', '/jobs')
-        })
+    describe(`Testing issue "${issueId}"`, () => {
         it('Go to the last frame', () => {
-            cy.get(':nth-child(7) > svg > path', {timeout:5000}).click()
-            cy.contains('span', lastElement)
+            cy.get('.cvat-annotation-header')
+            .find(':nth-child(7)')
+            .click()
+            cy.get('.cvat-player-controls')
+            .find('.ant-typography')
+            .should('contain.text', lastElement)
         })
         it('Create a shape', () => {
             cy.createShape(309, 431, 616, 671)
@@ -60,7 +53,9 @@ context('Check propagation work from the latest frame', () => {
         it('Try to propagate', () => {
             cy.get('#cvat_canvas_shape_1').trigger('mousemove')
             cy.get('body').type('{ctrl}b')
-            cy.contains('button', 'Yes').click()
+            cy.get('.ant-modal-content')
+            .find('.ant-btn-primary')
+            .click()
             cy.get('.ant-notification-notice').should('not.exist')
         })
     })
