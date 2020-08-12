3aebacc6430d5e44701cd242eb0cdb4caaad5429
Dmitry Kruchinin <dmitryx.kruchinin@intel.com>
2020-08-12 12:50:49 +0300

    Appying comments.

--- tests/cypress/integration/issue_1919_check_text_attr.js
+++ tests/cypress/integration/issue_1919_check_text_attr.js
@@ -8,56 +8,47 @@
 
 context('Check label attribute changes', () => {
 
-    const issueId='1919'
-    const labelName=`Issue ${issueId}`
-    const taskName=`New annotation task for ${labelName}`
-    const attrName=`Attr for ${labelName}`
-    const textDefaultValue='Some default value for type Text'
-    const image=`image_${issueId}.png`
+    const issueId = '1919'
+    const labelName = `Issue ${issueId}`
+    const taskName = `New annotation task for ${labelName}`
+    const attrName = `Attr for ${labelName}`
+    const textDefaultValue = 'Some default value for type Text'
+    const image = `image_${issueId}.png`
     const newLabelAttrValue = 'New attribute value'
-    const width='800'
-    const height='800'
-    const posX=10
-    const posY=10
-    const color='gray'
+    const width = 800
+    const height = 800
+    const posX = 10
+    const posY = 10
+    const color = 'gray'
 
     before(() => {
         cy.visit('auth/login')
         cy.login()
-        cy.get('[type="submit"]').click()
         cy.imageGenerator('cypress/fixtures', image, width, height, color, posX, posY, labelName)
         cy.createAnnotationTask(taskName, labelName, attrName, textDefaultValue, image)
+        cy.openTaskJob(taskName)
+        cy.createShape(309, 431, 616, 671)
     })
 
-    describe(`Open task "${taskName}"`, () => {
-        it('The annotation task present in the list', () => {
-            cy.contains('strong', taskName)
-            .parent()
-            .parent()
-            .parent()
-            .contains('a', 'Open').click()
-        })
-        it('Open a job', () => {
-            cy.contains('a', 'Job #').click()
-            cy.url().should('include', '/jobs')
-        })
-        it('Create a shape', () => {
-            cy.createShape(309, 431, 616, 671)
-        })
-        it('Open object menu on the created shape', () => {
+    describe(`Testing issue "${issueId}"`, () => {
+        it('Open object menu', () => {
             cy.get('#cvat_canvas_shape_1').trigger('mousemove').rightclick()
         })
         it('Open object menu details', () => {
-            cy.get('.cvat-canvas-context-menu > [style="display: flex; margin-bottom: 1px;"] > #cvat-objects-sidebar-state-item-1 > .ant-row > .ant-collapse > .ant-collapse-item > .ant-collapse-header > span')
-            .should('contain', 'Details')
+            cy.get('.cvat-canvas-context-menu')
+            .contains('Details')
             .click()
         })
         it('Clear field of text attribute and write new value', () => {
-            cy.get('.cvat-canvas-context-menu > [style="display: flex; margin-bottom: 1px;"] > #cvat-objects-sidebar-state-item-1 > .ant-row > .ant-collapse > .ant-collapse-item > .ant-collapse-content > .ant-collapse-content-box > .ant-row-flex > .ant-col-16 > .ant-input')
-            .clear().type(newLabelAttrValue)
+            cy.get('.cvat-canvas-context-menu')
+            .find('.cvat-object-item-text-attribute')
+            .should('have.value', textDefaultValue)
+            .clear()
+            .type(newLabelAttrValue)
         })
         it('Check what value of right panel is changed too', () => {
-            cy.get('.cvat-objects-sidebar-states-list > [style="display: flex; margin-bottom: 1px;"] > #cvat-objects-sidebar-state-item-1 > .ant-row > .ant-collapse > .ant-collapse-item > .ant-collapse-content > .ant-collapse-content-box > .ant-row-flex > .ant-col-16 > .ant-input')
+            cy.get('#cvat-objects-sidebar-state-item-1')
+            .find('.cvat-object-item-text-attribute')
             .should('have.value', newLabelAttrValue)
         })
     })
