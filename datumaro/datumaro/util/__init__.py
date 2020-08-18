75172d90297955f9c3d084ede0e130bbbf640dea
Zhiltsov Max <zhiltsov.max35@gmail.com>
2020-08-18 17:46:26 +0300

    fix merge

--- datumaro/datumaro/util/__init__.py
+++ datumaro/datumaro/util/__init__.py
@@ -89,13 +89,5 @@ def str_to_bool(s):
     else:
         raise ValueError("Can't convert value '%s' to bool" % s)
 
-def ensure_cls(c):
-    def converter(arg):
-        if isinstance(arg, c):
-            return arg
-        else:
-            return c(**arg)
-    return converter
-
 def filter_dict(d, exclude_keys):
     return { k: v for k, v in d.items() if k not in exclude_keys }
