38acc99f277e54b9b0d28500f76576dc9fceb71a
Boris Sekachev <boris.sekachev@yandex.ru>
2020-09-10 07:27:47 +0300

    Added automatic annotation progress to the task view (#2148)
    * Added automatic annotation progress to the task view
    
    * Updated changelog & version
    
    Co-authored-by: Nikita Manovich <nikita.manovich@intel.com>

--- cvat-ui/src/containers/task-page/details.tsx
+++ cvat-ui/src/containers/task-page/details.tsx
@@ -7,10 +7,8 @@ import { connect } from 'react-redux';
 
 import DetailsComponent from 'components/task-page/details';
 import { updateTaskAsync } from 'actions/tasks-actions';
-import {
-    Task,
-    CombinedState,
-} from 'reducers/interfaces';
+import { cancelInferenceAsync } from 'actions/models-actions';
+import { Task, CombinedState, ActiveInference } from 'reducers/interfaces';
 
 interface OwnProps {
     task: Task;
@@ -18,26 +16,32 @@ interface OwnProps {
 
 interface StateToProps {
     registeredUsers: any[];
+    activeInference: ActiveInference | null;
     installedGit: boolean;
 }
 
 interface DispatchToProps {
+    cancelAutoAnnotation(): void;
     onTaskUpdate: (taskInstance: any) => void;
 }
 
-function mapStateToProps(state: CombinedState): StateToProps {
+function mapStateToProps(state: CombinedState, own: OwnProps): StateToProps {
     const { list } = state.plugins;
 
     return {
         registeredUsers: state.users.users,
         installedGit: list.GIT_INTEGRATION,
+        activeInference: state.models.inferences[own.task.instance.id] || null,
     };
 }
 
 
-function mapDispatchToProps(dispatch: any): DispatchToProps {
+function mapDispatchToProps(dispatch: any, own: OwnProps): DispatchToProps {
     return {
         onTaskUpdate: (taskInstance: any): void => dispatch(updateTaskAsync(taskInstance)),
+        cancelAutoAnnotation(): void {
+            dispatch(cancelInferenceAsync(own.task.instance.id));
+        },
     };
 }
 
@@ -46,7 +50,9 @@ function TaskPageContainer(props: StateToProps & DispatchToProps & OwnProps): JS
     const {
         task,
         installedGit,
+        activeInference,
         registeredUsers,
+        cancelAutoAnnotation,
         onTaskUpdate,
     } = props;
 
@@ -55,8 +61,10 @@ function TaskPageContainer(props: StateToProps & DispatchToProps & OwnProps): JS
             previewImage={task.preview}
             taskInstance={task.instance}
             installedGit={installedGit}
-            onTaskUpdate={onTaskUpdate}
             registeredUsers={registeredUsers}
+            activeInference={activeInference}
+            onTaskUpdate={onTaskUpdate}
+            cancelAutoAnnotation={cancelAutoAnnotation}
         />
     );
 }
