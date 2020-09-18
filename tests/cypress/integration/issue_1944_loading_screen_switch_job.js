b5b872fa97cecfb97c5d406cdd6efb095b312130
Dmitry Kruchinin <33020454+dvkruchinin@users.noreply.github.com>
2020-09-18 09:59:48 +0300

    Cypress test to check task parameters frame (#2164)
    * Cypress test to check if parameters "startFrame", "stopFrame", "frameStep" works as expected
    
    * Apply comments.
    
    Co-authored-by: Dmitry Kruchinin <dmitryx.kruchinin@intel.com>

--- tests/cypress/integration/issue_1944_loading_screen_switch_job.js
+++ tests/cypress/integration/issue_1944_loading_screen_switch_job.js
@@ -27,7 +27,10 @@ context('Being able to return to the job list for a task and start a new job wit
     const archivePath = `cypress/fixtures/${archiveName}`
     const imagesFolder = `cypress/fixtures/image_issue_${issueId}`
     const directoryToArchive = imagesFolder
-    const multiJobs = true
+    const advancedConfigurationParams = {
+        multiJobs: true,
+        segmentSize: 1
+    }
 
     before(() => {
         cy.visit('auth/login')
@@ -40,7 +43,8 @@ context('Being able to return to the job list for a task and start a new job wit
 
     describe(`Testing issue "${issueId}"`, () => {
         it('Create a multijob task', () => {
-            cy.createAnnotationTask(taskName, labelName, attrName, textDefaultValue, archiveName, multiJobs)
+            cy.createAnnotationTask(taskName, labelName, attrName, textDefaultValue, archiveName,
+                null, advancedConfigurationParams)
         })
         it('Open the task. Open first job', () => {
             cy.openTaskJob(taskName)
