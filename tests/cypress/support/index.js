68a12d50e6b516ac9e268bdb40783f47d5c6f6f3
Dmitry Kruchinin <dmitryx.kruchinin@intel.com>
2020-09-21 12:20:25 +0300

    Disable creating nyc output before implementing the coverage measurement.

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -19,21 +19,19 @@ before(() => {
     }
 })
 
-afterEach(() => {
-    // save coverage after each test
-    // because the entire "window" object is about
-    // to be recycled by Cypress before next test
-    if (Cypress.browser.name === 'chrome') {
-        cy.window().then(win => {
-            if (win.__coverage__) {
-                cy.task('combineCoverage', win.__coverage__)
-            }
-        })
-    }
-})
+// Will need to uncomment this code when implementing coverage measurement.
+// afterEach(() => {
+//     if (Cypress.browser.name === 'chrome') {
+//         cy.window().then(win => {
+//             if (win.__coverage__) {
+//                 cy.task('combineCoverage', win.__coverage__)
+//             }
+//         })
+//     }
+// })
 
-after(() => {
-    if (Cypress.browser.name === 'chrome') {
-        cy.task('coverageReportPrepare')
-    }
-})
+// after(() => {
+//     if (Cypress.browser.name === 'chrome') {
+//         cy.task('coverageReportPrepare')
+//     }
+// })
