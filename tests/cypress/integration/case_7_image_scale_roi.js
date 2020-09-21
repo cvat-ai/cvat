4a43988edd7ba9094c641e3480b3f436f49303f6
Dmitry Kruchinin <dmitryx.kruchinin@intel.com>
2020-09-21 15:22:40 +0300

    Cypress test to check if image was scaled to ROI

---
+++ tests/cypress/integration/case_7_image_scale_roi.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/// <reference types="cypress" />
+
+context('Check if image was scaled to ROI', () => {
+
+    const caseId = '7'
+    const labelName = `Case ${caseId}`
+    const taskName = `New annotation task for ${labelName}`
+    const attrName = `Attr for ${labelName}`
+    const textDefaultValue = 'Some default value for type Text'
+    const imageFileName = `image_${labelName.replace(' ', '_').toLowerCase()}`
+    const image = `${imageFileName}.png`
+    const width = 800
+    const height = 800
+    const posX = 10
+    const posY = 10
+    const color = 'gray'
+
+    before(() => {
+        cy.visit('auth/login')
+        cy.login()
+        cy.imageGenerator('cypress/fixtures', image, width, height, color, posX, posY, labelName)
+        cy.createAnnotationTask(taskName, labelName, attrName, textDefaultValue, image)
+        cy.openTaskJob(taskName)
+    })
+
+    describe(`Testing "${labelName}"`, () => {
+        it('Create ROI', () => {
+            cy.get('.cvat-resize-control')
+            .click()
+            cy.get('.cvat-canvas-container')
+            .trigger('mousedown', 309, 431, {which: 1})
+            .trigger('mousemove', 616, 671)
+            .trigger('mouseup', 616, 671)
+        })
+        it('Image scaled to ROI', () => {
+            cy.get('#cvat_canvas_background')
+            .should('have.attr', 'style').and('contain', 'scale(3.2817') //Chrome: scale(3.28173), Firefox: scale(3.28172)
+        })
+    })
+})
