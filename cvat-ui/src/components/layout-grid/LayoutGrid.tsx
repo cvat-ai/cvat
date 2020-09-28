a81a5af042d26fbae9e8d36811a950077a48d962
Vitaliy Nishukov <vnishukov@gmail.com>
2020-09-28 21:06:49 +0300

    Layout grid

---
+++ cvat-ui/src/components/layout-grid/LayoutGrid.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+
+export default function LayoutGrid(): React.ReactPortal | null {
+    const [showGrid, setShowGrid] = useState(false);
+
+    const listener = (e: KeyboardEvent): void => {
+        if (e.key === 'g' && e.ctrlKey) {
+            setShowGrid(!showGrid);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('keyup', listener);
+        return () => {
+            window.removeEventListener('keyup', listener);
+        };
+    }, [showGrid]);
+
+    const portalContent: JSX.Element | null = showGrid ? <div className='grid' /> : null;
+    return ReactDOM.createPortal(portalContent, document.getElementById('layout-grid') as HTMLElement);
+}
