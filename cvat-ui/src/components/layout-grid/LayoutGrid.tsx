03de9bbdf81d5bb2d29fabf33cee84585231f18d
Vitaliy Nishukov <vnishukov@gmail.com>
2020-09-28 21:14:43 +0300

    License headers

--- cvat-ui/src/components/layout-grid/LayoutGrid.tsx
+++ cvat-ui/src/components/layout-grid/LayoutGrid.tsx
@@ -1,3 +1,7 @@
+// Copyright (C) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
