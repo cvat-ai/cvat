e7cf1579362d4e2f7dd0d3e65b77e174ce34a83b
Dmitry Kruchinin <33020454+dvkruchinin@users.noreply.github.com>
2020-10-06 10:47:35 +0300

    CVAT-UI: measure coverage. (#2196)
    * Try measure coverage
    
    * Return first logic to runing a tests and measure coverage.
    
    * Prepare to measure coverage.
    
    Temporary disable run Cypress test via Firefox.
    
    * Disable creating nyc output before implementing the coverage measurement.
    
    * Change plugin for nyc to babel.
    
    Add to eclude folders:
    3rdparty
    tests
    
    Disable run test via Firefox.
    
    * Update travis.yaml
    
    Co-authored-by: Dmitry Kruchinin <dmitryx.kruchinin@intel.com>

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -8,14 +8,45 @@
 
 const {imageGenerator} = require('../plugins/imageGenerator/addPlugin')
 const {createZipArchive} = require('../plugins/createZipArchive/addPlugin')
+const istanbul = require('istanbul-lib-coverage')
+const { join } = require('path')
+const { existsSync, mkdirSync, writeFileSync } = require('fs')
+const execa = require('execa')
 
 module.exports = (on) => {
+    let coverageMap = istanbul.createCoverageMap({})
+    const outputFolder = '../.nyc_output'
+    const nycFilename = join(outputFolder, 'out.json')
+
+    if (!existsSync(outputFolder)) {
+      mkdirSync(outputFolder)
+      console.log('created folder %s for output coverage', outputFolder)
+    }
+
     on('task', {imageGenerator})
     on('task', {createZipArchive})
     on('task', {
         log(message) {
             console.log(message)
             return null
+        }
+    })
+    on('task', {
+        /**
+         * Combines coverage information from single test
+         * with previously collected coverage.
+         */
+        combineCoverage (coverage) {
+          coverageMap.merge(coverage)
+          return null
+        },
+
+        /**
+         * Saves coverage information as a JSON file and calls
+         */
+        coverageReportPrepare () {
+          writeFileSync(nycFilename, JSON.stringify(coverageMap, null, 2))
+          return null
         }
     })
 }
