71dc16aa4a85529401f14fa83e6013a0536722e6
Vitaliy Nishukov <vnishukov@gmail.com>
2020-10-13 19:35:25 +0300

    Test fix

--- cvat-core/tests/api/tasks.js
+++ cvat-core/tests/api/tasks.js
@@ -10,19 +10,19 @@
 */
 
 // Setup mock for a server
-jest.mock("../../src/server-proxy", () => {
-    const mock = require("../mocks/server-proxy.mock");
+jest.mock('../../src/server-proxy', () => {
+    const mock = require('../mocks/server-proxy.mock');
     return mock;
 });
 
 // Initialize api
-window.cvat = require("../../src/api");
+window.cvat = require('../../src/api');
 
-const { Task } = require("../../src/session");
+const { Task } = require('../../src/session');
 
 // Test cases
-describe("Feature: get a list of tasks", () => {
-    test("get all tasks", async () => {
+describe('Feature: get a list of tasks', () => {
+    test('get all tasks', async () => {
         const result = await window.cvat.tasks.get();
         expect(Array.isArray(result)).toBeTruthy();
         expect(result).toHaveLength(6);
@@ -31,7 +31,7 @@ describe("Feature: get a list of tasks", () => {
         }
     });
 
-    test("get a task by an id", async () => {
+    test('get a task by an id', async () => {
         const result = await window.cvat.tasks.get({
             id: 3,
         });
@@ -41,7 +41,7 @@ describe("Feature: get a list of tasks", () => {
         expect(result[0].id).toBe(3);
     });
 
-    test("get a task by an unknown id", async () => {
+    test('get a task by an unknown id', async () => {
         const result = await window.cvat.tasks.get({
             id: 50,
         });
@@ -49,60 +49,59 @@ describe("Feature: get a list of tasks", () => {
         expect(result).toHaveLength(0);
     });
 
-    test("get a task by an invalid id", async () => {
+    test('get a task by an invalid id', async () => {
         expect(
             window.cvat.tasks.get({
-                id: "50",
-            })
+                id: '50',
+            }),
         ).rejects.toThrow(window.cvat.exceptions.ArgumentError);
     });
 
-    test("get tasks by filters", async () => {
+    test('get tasks by filters', async () => {
         const result = await window.cvat.tasks.get({
-            mode: "interpolation",
+            mode: 'interpolation',
         });
         expect(Array.isArray(result)).toBeTruthy();
         expect(result).toHaveLength(3);
         for (const el of result) {
             expect(el).toBeInstanceOf(Task);
-            expect(el.mode).toBe("interpolation");
+            expect(el.mode).toBe('interpolation');
         }
     });
 
-    test("get tasks by invalid filters", async () => {
+    test('get tasks by invalid filters', async () => {
         expect(
             window.cvat.tasks.get({
-                unknown: "5",
-            })
+                unknown: '5',
+            }),
         ).rejects.toThrow(window.cvat.exceptions.ArgumentError);
     });
 
-    test("get task by name, status and mode", async () => {
+    test('get task by name, status and mode', async () => {
         const result = await window.cvat.tasks.get({
-            mode: "interpolation",
-            status: "annotation",
-            name: "Test Task",
+            mode: 'interpolation',
+            status: 'annotation',
+            name: 'Test Task',
         });
         expect(Array.isArray(result)).toBeTruthy();
         expect(result).toHaveLength(1);
         for (const el of result) {
             expect(el).toBeInstanceOf(Task);
-            expect(el.mode).toBe("interpolation");
-            expect(el.status).toBe("annotation");
-            expect(el.name).toBe("Test Task");
+            expect(el.mode).toBe('interpolation');
+            expect(el.status).toBe('annotation');
+            expect(el.name).toBe('Test Task');
         }
     });
 });
 
-describe("Feature: save a task", () => {
-    test("save some changed fields in a task", async () => {
+describe('Feature: save a task', () => {
+    test('save some changed fields in a task', async () => {
         let result = await window.cvat.tasks.get({
             id: 2,
         });
 
-        result[0].bugTracker = "newBugTracker";
-        result[0].zOrder = true;
-        result[0].name = "New Task Name";
+        result[0].bugTracker = 'newBugTracker';
+        result[0].name = 'New Task Name';
 
         result[0].save();
 
@@ -110,12 +109,11 @@ describe("Feature: save a task", () => {
             id: 2,
         });
 
-        expect(result[0].bugTracker).toBe("newBugTracker");
-        expect(result[0].zOrder).toBe(true);
-        expect(result[0].name).toBe("New Task Name");
+        expect(result[0].bugTracker).toBe('newBugTracker');
+        expect(result[0].name).toBe('New Task Name');
     });
 
-    test("save some new labels in a task", async () => {
+    test('save some new labels in a task', async () => {
         let result = await window.cvat.tasks.get({
             id: 2,
         });
@@ -125,11 +123,11 @@ describe("Feature: save a task", () => {
             name: "My boss's car",
             attributes: [
                 {
-                    default_value: "false",
-                    input_type: "checkbox",
+                    default_value: 'false',
+                    input_type: 'checkbox',
                     mutable: true,
-                    name: "parked",
-                    values: ["false"],
+                    name: 'parked',
+                    values: ['false'],
                 },
             ],
         });
@@ -142,45 +140,43 @@ describe("Feature: save a task", () => {
         });
 
         expect(result[0].labels).toHaveLength(labelsLength + 1);
-        const appendedLabel = result[0].labels.filter(
-            (el) => el.name === "My boss's car"
-        );
+        const appendedLabel = result[0].labels.filter((el) => el.name === "My boss's car");
         expect(appendedLabel).toHaveLength(1);
         expect(appendedLabel[0].attributes).toHaveLength(1);
-        expect(appendedLabel[0].attributes[0].name).toBe("parked");
-        expect(appendedLabel[0].attributes[0].defaultValue).toBe("false");
+        expect(appendedLabel[0].attributes[0].name).toBe('parked');
+        expect(appendedLabel[0].attributes[0].defaultValue).toBe('false');
         expect(appendedLabel[0].attributes[0].mutable).toBe(true);
-        expect(appendedLabel[0].attributes[0].inputType).toBe("checkbox");
+        expect(appendedLabel[0].attributes[0].inputType).toBe('checkbox');
     });
 
-    test("save new task without an id", async () => {
+    test('save new task without an id', async () => {
         const task = new window.cvat.classes.Task({
-            name: "New Task",
+            name: 'New Task',
             labels: [
                 {
                     name: "My boss's car",
                     attributes: [
                         {
-                            default_value: "false",
-                            input_type: "checkbox",
+                            default_value: 'false',
+                            input_type: 'checkbox',
                             mutable: true,
-                            name: "parked",
-                            values: ["false"],
+                            name: 'parked',
+                            values: ['false'],
                         },
                     ],
                 },
             ],
-            bug_tracker: "bug tracker value",
+            bug_tracker: 'bug tracker value',
             image_quality: 50,
         });
 
         const result = await task.save();
-        expect(typeof result.id).toBe("number");
+        expect(typeof result.id).toBe('number');
     });
 });
 
-describe("Feature: delete a task", () => {
-    test("delete a task", async () => {
+describe('Feature: delete a task', () => {
+    test('delete a task', async () => {
         let result = await window.cvat.tasks.get({
             id: 3,
         });
