64889e34ad4b27c078269f7307b6a52257da217f
Dmitry Kalinin <dmitry.kalinin4+github@gmail.com>
2020-10-23 15:26:47 +0300

    Fixed migrations

--- cvat/apps/engine/migrations/0033_projects_adjastment.py
+++ cvat/apps/engine/migrations/0033_projects_adjastment.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('engine', '0031_auto_20201011_0220'),
+        ('engine', '0032_remove_task_z_order'),
     ]
 
     operations = [
