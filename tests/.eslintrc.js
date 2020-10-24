1c14dafae4360ae4dcac7735d09968d037bc147a
Boris Sekachev <boris.sekachev@yandex.ru>
2020-10-24 20:37:16 +0300

    Added .eslintrc.js for tests (#2355)

---
+++ tests/.eslintrc.js
@@ -0,0 +1,10 @@
+// Copyright (C) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+module.exports = {
+    env: {
+        'cypress/globals': true,
+    },
+    plugins: ['cypress']
+};
