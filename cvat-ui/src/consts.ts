856003eba469324964ae773cbcb0c1e20e4ccbb8
Boris Sekachev <boris.sekachev@yandex.ru>
2020-10-28 13:12:38 +0300

    Updated internal links in the tool

--- cvat-ui/src/consts.ts
+++ cvat-ui/src/consts.ts
@@ -4,18 +4,18 @@
 
 const UNDEFINED_ATTRIBUTE_VALUE = '__undefined__';
 const NO_BREAK_SPACE = '\u00a0';
-const CHANGELOG_URL = 'https://github.com/opencv/cvat/blob/develop/CHANGELOG.md';
-const LICENSE_URL = 'https://github.com/opencv/cvat/blob/develop/LICENSE';
+const CHANGELOG_URL = 'https://github.com/openvinotoolkit/cvat/blob/develop/CHANGELOG.md';
+const LICENSE_URL = 'https://github.com/openvinotoolkit/cvat/blob/develop/LICENSE';
 const GITTER_URL = 'https://gitter.im/opencv-cvat';
 const GITTER_PUBLIC_URL = 'https://gitter.im/opencv-cvat/public';
 const FORUM_URL = 'https://software.intel.com/en-us/forums/intel-distribution-of-openvino-toolkit';
-const GITHUB_URL = 'https://github.com/opencv/cvat';
+const GITHUB_URL = 'https://github.com/openvinotoolkit/cvat';
 const GITHUB_IMAGE_URL =
-    'https://raw.githubusercontent.com/opencv/cvat/develop/cvat/apps/documentation/static/documentation/images/cvat.jpg';
+    'https://raw.githubusercontent.com/openvinotoolkit/cvat/develop/cvat/apps/documentation/static/documentation/images/cvat.jpg';
 const SHARE_MOUNT_GUIDE_URL =
-    'https://github.com/opencv/cvat/blob/master/cvat/apps/documentation/installation.md#share-path';
+    'https://github.com/openvinotoolkit/cvat/blob/master/cvat/apps/documentation/installation.md#share-path';
 const NUCLIO_GUIDE =
-    'https://github.com/opencv/cvat/blob/develop/cvat/apps/documentation/installation.md#semi-automatic-and-automatic-annotation';
+    'https://github.com/openvinotoolkit/cvat/blob/develop/cvat/apps/documentation/installation.md#semi-automatic-and-automatic-annotation';
 const CANVAS_BACKGROUND_COLORS = ['#ffffff', '#f1f1f1', '#e5e5e5', '#d8d8d8', '#CCCCCC', '#B3B3B3', '#999999'];
 const NEW_LABEL_COLOR = '#b3b3b3';
 
