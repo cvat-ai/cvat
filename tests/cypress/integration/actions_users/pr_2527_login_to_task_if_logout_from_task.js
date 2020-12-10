9d82bb43f1ab6872e3743362513f05233ef9e89b
Dmitriy Oparin <dmitriyx.oparin@intel.com>
2020-12-10 22:31:33 +0300

    added step login with token

--- tests/cypress/integration/actions_users/pr_2527_login_to_task_if_logout_from_task.js
+++ tests/cypress/integration/actions_users/pr_2527_login_to_task_if_logout_from_task.js
@@ -8,39 +8,55 @@ import { taskName } from '../../support/const';
 
 context('Login to task if logout from task', () => {
     const prId = '2527';
-    let idTask;
-
-    function loginToTask(username = Cypress.env('user'), password = Cypress.env('password')) {
-        cy.get('[placeholder="Username"]').type(username);
-        cy.get('[placeholder="Password"]').type(password);
-        cy.get('[type="submit"]').click();
-        cy.url()
-            .should('include', `/tasks/${idTask}`)
-            .and('not.include', '/auth/login/');
-    }
-
-    function logoutFromTask(username = Cypress.env('user')) {
-        cy.get('.cvat-right-header').within(() => {
-            cy.get('.cvat-header-menu-dropdown').should('have.text', username).trigger('mouseover', { which: 1 });
-        });
-        cy.get('span[aria-label="logout"]').click();
-        cy.url().should('include', `/auth/login/?next=/tasks/${idTask}`);
-    }
+    let taskId;
 
     before(() => {
         cy.openTask(taskName);
 
         // get id task
         cy.url().then((link) => {
-            idTask = Number(link.split('/').slice(-1)[0]);
+            taskId = Number(link.split('/').slice(-1)[0]);
         });
     });
 
     describe(`Testing pr "${prId}"`, () => {
-        it('Logout and login', () => {
-            logoutFromTask();
-            loginToTask();
-            cy.contains('.cvat-task-details-task-name', `${taskName}`).should('exist');
+        it('Logout and login via GUI', () => {
+            // logout from task
+            cy.get('.cvat-right-header').within(() => {
+                cy.get('.cvat-header-menu-dropdown').should('have.text', Cypress.env('user')).trigger('mouseover', { which: 1 });
+            });
+            cy.get('span[aria-label="logout"]').click();
+            cy.url().should('include', `/auth/login/?next=/tasks/${taskId}`);
+
+            // login to task
+            cy.get('[placeholder="Username"]').type(Cypress.env('user'));
+            cy.get('[placeholder="Password"]').type(Cypress.env('password'));
+            cy.get('[type="submit"]').click();
+            cy.url()
+                .should('include', `/tasks/${taskId}`)
+                .and('not.include', '/auth/login/');
+            cy.contains('.cvat-task-details-task-name', `${taskName}`).should('be.visible');
+        });
+
+        it('Logout and login via token', () => {
+            cy.logout();
+
+            // get token and login to task
+            cy.request({
+                method: 'POST',
+                url: '/api/v1/auth/login',
+                body: {
+                    username: Cypress.env('user'),
+                    email: Cypress.env('email'),
+                    password: Cypress.env('password'),
+                },
+            }).then(async (responce) => {
+                responce = await responce['headers']['set-cookie'];
+                const csrfToken = responce[0].match(/csrftoken=\w+/)[0].replace('csrftoken=', '');
+                const sessionId = responce[1].match(/sessionid=\w+/)[0].replace('sessionid=', '');
+                cy.visit(`/login-with-token/${sessionId}/${csrfToken}?next=/tasks/${taskId}`)
+                cy.contains('.cvat-task-details-task-name', `${taskName}`).should('be.visible');
+            });
         });
     });
 });
