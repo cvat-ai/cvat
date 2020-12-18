5003915a2304c485fa4e7ec9f7b8aaf6a8fa4b0c
Kruchinin <dmitryx.kruchinin@intel.com>
2020-12-18 11:39:11 +0300

    Finish write the test.

--- tests/cypress/integration/actions_tasks_objects/case_35_search_task_feature.js
+++ tests/cypress/integration/actions_tasks_objects/case_35_search_task_feature.js
@@ -14,6 +14,14 @@ context('Check if the image is scaled and then fitted', () => {
             cy.server().route('GET', '/api/v1/tasks**').as('searchTask');
             cy.get('.cvat-task-page-search-task').type(`${taskName.substring(0, 3)}{Enter}`);
             cy.wait('@searchTask').its('status').should('equal', 200);
+            cy.contains('.cvat-item-task-name', taskName).should('exist');
+        });
+
+        it('Type to task search field any another text does not contain the name of the task. The task should not exist.', () => {
+            cy.server().route('GET', '/api/v1/tasks**').as('searchTask');
+            cy.get('.cvat-task-page-search-task').type('121212{Enter}');
+            cy.wait('@searchTask').its('status').should('equal', 200);
+            cy.contains('.cvat-item-task-name', taskName).should('not.exist');
         });
     });
 });
