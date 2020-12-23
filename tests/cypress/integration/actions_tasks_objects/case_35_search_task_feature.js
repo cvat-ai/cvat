817ac4568163608a27b3b865c74dc840776a93bb
Kruchinin <dmitryx.kruchinin@intel.com>
2020-12-23 16:26:27 +0300

    Add some options for search

--- tests/cypress/integration/actions_tasks_objects/case_35_search_task_feature.js
+++ tests/cypress/integration/actions_tasks_objects/case_35_search_task_feature.js
@@ -9,19 +9,21 @@ import { taskName } from '../../support/const';
 context('Search task feature.', () => {
     const caseId = '35';
 
-    describe(`Testing case "${caseId}"`, () => {
-        it('Type to task search field full name task, part of the task name. The task should exist.', () => {
-            cy.server().route('GET', '/api/v1/tasks**').as('searchTask');
-            cy.get('.cvat-task-page-search-task').type(`${taskName.substring(0, 3)}{Enter}`);
-            cy.wait('@searchTask').its('status').should('equal', 200);
-            cy.contains('.cvat-item-task-name', taskName).should('exist');
-        });
+    function searchTask(option, result) {
+        cy.server().route('GET', '/api/v1/tasks**').as('searchTask');
+        cy.get('.cvat-task-page-search-task').find('[placeholder="Search"]').clear().type(`${option}{Enter}`);
+        cy.wait('@searchTask').its('status').should('equal', 200);
+        cy.contains('.cvat-item-task-name', taskName).should(result);
+    }
 
-        it('Type to task search field any another text does not contain the name of the task. The task should not exist.', () => {
-            cy.server().route('GET', '/api/v1/tasks**').as('searchTask');
-            cy.get('.cvat-task-page-search-task').type('121212{Enter}');
-            cy.wait('@searchTask').its('status').should('equal', 200);
-            cy.contains('.cvat-item-task-name', taskName).should('not.exist');
+    describe(`Testing case "${caseId}"`, () => {
+        it('Type to task search some field and check result.', () => {
+            searchTask(`${taskName.substring(0, 3)}`, 'exist');
+            searchTask('121212', 'not.exist');
+            searchTask(`owner: ${Cypress.env('user')}`, 'exist');
+            searchTask('mode: annotation', 'exist');
+            searchTask('status: annotation', 'exist');
+            searchTask(`mode: interpolation AND owner: ${Cypress.env('user')}`, 'not.exist');
         });
     });
 });
