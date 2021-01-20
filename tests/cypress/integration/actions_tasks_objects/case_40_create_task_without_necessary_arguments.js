88754db3dfb963c3995a5b057f4fe43fa3ee87ad
dvkruchinin <dvkruchinin@gmail.com>
2021-01-20 09:26:31 +0300

    Add the test.

---
+++ tests/cypress/integration/actions_tasks_objects/case_40_create_task_without_necessary_arguments.js
@@ -0,0 +1,37 @@
+// Copyright (C) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+/// <reference types="cypress" />
+
+context('Try to create a task without necessary arguments.', () => {
+    const caseId = '40';
+    const labelName = `Case ${caseId}`;
+    const taskName = `New annotation task for ${labelName}`;
+    const attrName = `Attr for ${labelName}`;
+    const textDefaultValue = 'Some default value for type Text';
+    const imagesCount = 1;
+    const imageFileName = `image_${labelName.replace(' ', '_').toLowerCase()}`;
+    const width = 800;
+    const height = 800;
+    const posX = 10;
+    const posY = 10;
+    const color = 'gray';
+    const archiveName = `${imageFileName}.zip`;
+    const archivePath = `cypress/fixtures/${archiveName}`;
+    const imagesFolder = `cypress/fixtures/${imageFileName}`;
+    const directoryToArchive = imagesFolder;
+
+    before(() => {
+        cy.visit('auth/login');
+        cy.login();
+        cy.imageGenerator(imagesFolder, imageFileName, width, height, color, posX, posY, labelName, imagesCount);
+        cy.createZipArchive(directoryToArchive, archivePath);
+    });
+
+    describe(`Testing "${labelName}"`, () => {
+        it('Go to create task page', () => {
+            cy.log('121212');
+        });
+    });
+});
