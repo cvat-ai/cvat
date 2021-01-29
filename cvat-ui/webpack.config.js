6bf4f59b6055fd197aa6ea2761adbacb15f81624
Vitaliy Nishukov <vnishukov@gmail.com>
2021-01-29 16:39:22 +0300

    CHANGELOG, version increment, babel nullish-coalescing-operator

--- cvat-ui/webpack.config.js
+++ cvat-ui/webpack.config.js
@@ -45,6 +45,7 @@ module.exports = {
                         plugins: [
                             '@babel/plugin-proposal-class-properties',
                             '@babel/plugin-proposal-optional-chaining',
+                            '@babel/plugin-proposal-nullish-coalescing-operator',
                             [
                                 'import',
                                 {
