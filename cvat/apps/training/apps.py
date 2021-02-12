a940a989a4dd72fa61f169610e84fbb856d5351a
dmitry <dmitry.agapov@intel.com>
2021-02-12 17:24:43 +0300

    fix

---
+++ cvat/apps/training/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class TrainingConfig(AppConfig):
+    name = 'cvat.apps.training'
+
+    def ready(self):
+        assert 'Hello'
+        # Required to define signals in application
+        import cvat.apps.training.signals
+        # Required in order to silent "unused-import" in pyflake
+        assert cvat.apps.training.signals
---
+++ cvat/apps/training/views.py
@@ -0,0 +1,50 @@
+from cacheops import cache, CacheMiss
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from cvat.apps.training.jobs import save_frame_prediction_to_cache_job, save_prediction_server_status_to_cache_job
+
+
+class PredictView(viewsets.ViewSet):
+    @swagger_auto_schema(method='get', operation_summary='Returns prediction for image')
+    @action(detail=False, methods=['GET'], url_path='frame')
+    def predict_image(self, request):
+        frame = self.request.query_params.get('frame')
+        task_id = self.request.query_params.get('task')
+        if not task_id:
+            return Response(data='query param "task" empty or not provided', status=status.HTTP_400_BAD_REQUEST)
+        if not frame:
+            return Response(data='query param "frame" empty or not provided', status=status.HTTP_400_BAD_REQUEST)
+        cache_key = f'predict_image_{task_id}_{frame}'
+        try:
+            resp = cache.get(cache_key)
+        except CacheMiss:
+            save_frame_prediction_to_cache_job.delay(cache_key, task_id=task_id,
+                                                     frame=frame)
+            resp = {
+                'status': 'queued',
+            }
+            cache.set(cache_key=cache_key, data=resp, timeout=60)
+
+        return Response(resp)
+
+    @swagger_auto_schema(method='get',
+                         operation_summary='Returns information of the tasks of the project with the selected id')
+    @action(detail=False, methods=['GET'], url_path='status')
+    def predict_status(self, request):
+        project_id = self.request.query_params.get('project')
+        if not project_id:
+            return Response(data='query param "project" empty or not provided', status=status.HTTP_400_BAD_REQUEST)
+        cache_key = f'predict_status_{project_id}'
+        try:
+            resp = cache.get(cache_key)
+        except CacheMiss:
+            save_prediction_server_status_to_cache_job.delay(cache_key, cvat_project_id=project_id)
+            resp = {
+                'status': 'queued',
+            }
+            cache.set(cache_key=cache_key, data=resp, timeout=60)
+
+        return Response(resp)
