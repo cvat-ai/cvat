abcdba916f3a1ad9cf688cd454a6c39ca832d416
dvkruchinin <dvkruchinin@gmail.com>
2021-02-24 08:58:31 +0300

    Cypress. Fix case 5 for Firefox.

--- tests/cypress/integration/actions_tasks_objects/case_5_image_rotate.js
+++ tests/cypress/integration/actions_tasks_objects/case_5_image_rotate.js
@@ -10,7 +10,8 @@ context('Check if the image is rotated', () => {
     const caseId = '5';
 
     function imageRotate(direction = 'anticlockwise') {
-        cy.get('.cvat-rotate-canvas-control').trigger('mouseover');
+        cy.get('.cvat-rotate-canvas-control').trigger('mouseover').wait(500);
+        cy.get('.cvat-rotate-canvas-popover-visible').should('exist');
         if (direction === 'clockwise') {
             cy.get('.cvat-rotate-canvas-controls-right').click();
         } else {
@@ -31,7 +32,8 @@ context('Check if the image is rotated', () => {
             .then(($styles) => {
                 scaleAfter = Number($styles.match(/scale\((\d\.\d+)\)/m)[1]);
                 cy.expect(scaleBefore).to.be.greaterThan(scaleAfter);
-                cy.get('#cvat_canvas_content').dblclick();
+                cy.get('#cvat_canvas_content').dblclick().wait(500);
+                cy.get('.cvat-rotate-canvas-popover-visible').should('not.exist');
                 cy.get('#cvat_canvas_background').should('have.attr', 'style').and('contain', scaleBefore);
             });
     }
