4d999e3d5196130aaa70480eeae1f1e426496c4e
Boris Sekachev <boris.sekachev@intel.com>
2021-03-30 13:42:22 +0300

    Added tooltip for project search

--- cvat-ui/src/components/tasks-page/top-bar.tsx
+++ cvat-ui/src/components/tasks-page/top-bar.tsx
@@ -9,9 +9,8 @@ import { PlusOutlined } from '@ant-design/icons';
 import Button from 'antd/lib/button';
 import Input from 'antd/lib/input';
 import Text from 'antd/lib/typography/Text';
-import Paragraph from 'antd/lib/typography/Paragraph';
 
-import CVATTooltip from 'components/common/cvat-tooltip';
+import SearchTooltip from 'components/search-tooltip/search-tooltip';
 
 interface VisibleTopBarProps {
     onSearch: (value: string) => void;
@@ -28,63 +27,7 @@ export default function TopBarComponent(props: VisibleTopBarProps): JSX.Element 
             <Row justify='center' align='middle'>
                 <Col md={11} lg={9} xl={8} xxl={7}>
                     <Text className='cvat-title'>Tasks</Text>
-                    <CVATTooltip
-                        overlayClassName='cvat-tasks-search-tooltip'
-                        title={(
-                            <>
-                                <Paragraph>
-                                    <Text strong>owner: admin</Text>
-                                    <Text>
-                                        all tasks created by the user who has the substring
-                                        <q>admin</q>
-                                        in their username
-                                    </Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text strong>assignee: employee</Text>
-                                    <Text>
-                                        all tasks which are assigned to a user who has the substring
-                                        <q>admin</q>
-                                        in their username
-                                    </Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text strong>name: training</Text>
-                                    <Text>
-                                        all tasks with the substring
-                                        <q>training</q>
-                                        in its name
-                                    </Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text strong>mode: annotation</Text>
-                                    <Text>
-                                        annotation tasks are tasks with images, interpolation tasks are tasks with
-                                        videos
-                                    </Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text strong>status: annotation</Text>
-                                    <Text>annotation, validation, or completed</Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text strong>id: 5</Text>
-                                    <Text>the task with id 5</Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text>
-                                        Filters can be combined (to the exclusion of id) using the keyword AND. Example:
-                                        <Text type='warning'>
-                                            <q>mode: interpolation AND owner: admin</q>
-                                        </Text>
-                                    </Text>
-                                </Paragraph>
-                                <Paragraph>
-                                    <Text type='success'>Search within all the string fields by default</Text>
-                                </Paragraph>
-                            </>
-                        )}
-                    >
+                    <SearchTooltip instance='task'>
                         <Input.Search
                             className='cvat-task-page-search-task'
                             defaultValue={searchValue}
@@ -92,7 +35,7 @@ export default function TopBarComponent(props: VisibleTopBarProps): JSX.Element 
                             size='large'
                             placeholder='Search'
                         />
-                    </CVATTooltip>
+                    </SearchTooltip>
                 </Col>
                 <Col md={{ span: 11 }} lg={{ span: 9 }} xl={{ span: 8 }} xxl={{ span: 7 }}>
                     <Button
