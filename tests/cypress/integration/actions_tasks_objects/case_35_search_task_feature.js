f04fcc2ccba1e392232ee3292d27fe40d4c08df0
dvkruchinin <dvkruchinin@gmail.com>
2021-04-01 16:48:03 +0300

    Update test to check task search tooltip

--- tests/cypress/integration/actions_tasks_objects/case_35_search_task_feature.js
+++ tests/cypress/integration/actions_tasks_objects/case_35_search_task_feature.js
@@ -16,12 +16,33 @@ context('Search task feature.', () => {
         cy.contains('.cvat-item-task-name', taskName).should(result);
     }
 
+    before(() => {
+        cy.openTask(taskName);
+        cy.assignTaskToUser(Cypress.env('user'));
+        cy.goToTaskList();
+    });
+
+    after(() => {
+        cy.goToTaskList();
+        cy.openTask(taskName);
+        cy.assignTaskToUser('');
+    });
+
     describe(`Testing case "${caseId}"`, () => {
-        it('Type to task search some field and check result.', () => {
+        it('Tooltip task filter contain all the possible options.', () => {
+            cy.get('.cvat-task-page-search-task').trigger('mouseover');
+            cy.get('.cvat-tasks-search-tooltip').should('be.visible').invoke('text').then(($tooltipText) => {
+                for (const i of ['owner: admin', 'assignee: employee', 'name: training', 'mode: annotation', 'status: annotation', 'id: 5']) {
+                    expect($tooltipText).contain(i);
+                }
+            });
+        });
+
+        it('Type to task search some filter and check result.', () => {
             searchTask(`${taskName.substring(0, 3)}`, 'exist');
             searchTask('121212', 'not.exist');
             searchTask(`owner: ${Cypress.env('user')}`, 'exist');
-            searchTask('mode: annotation', 'exist');
+            searchTask(`mode: annotation AND assignee: ${Cypress.env('user')}`, 'exist');
             searchTask('status: annotation', 'exist');
             searchTask(`mode: interpolation AND owner: ${Cypress.env('user')}`, 'not.exist');
         });
