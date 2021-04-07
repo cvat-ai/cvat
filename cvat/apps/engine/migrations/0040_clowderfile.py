5a5868a098a3d142855c664573e1f406b8fc126b
Andrei Misikhin <andrei.misikhin@syngenta.com>
2021-04-07 12:58:46 +0300

    Fixed migration numbers

--- cvat/apps/engine/migrations/0040_clowderfile.py
+++ cvat/apps/engine/migrations/0040_clowderfile.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('engine', '0038_manifest'),
+        ('engine', '0039_auto_training'),
     ]
 
     operations = [
