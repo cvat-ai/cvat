9caff79ec71ca72b71b83500dbd4ec8a4eb3dc6d
Maya <maya17grd@gmail.com>
2021-04-16 02:29:21 +0300

    Update migration dependency

--- cvat/apps/engine/migrations/0039_auto_20210414_2110.py
+++ cvat/apps/engine/migrations/0039_auto_20210414_2110.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('engine', '0038_manifest'),
+        ('engine', '0039_auto_training'),
     ]
 
     operations = [
