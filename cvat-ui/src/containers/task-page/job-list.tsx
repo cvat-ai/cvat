38e592aa6c171253519c5626e1881bf53b960846
André Aquilina <andre.aquilina@dysismedical.com>
2021-04-22 16:00:39 +0100

    Add dispatch function to job list container.

--- cvat-ui/src/containers/task-page/job-list.tsx
+++ cvat-ui/src/containers/task-page/job-list.tsx
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Intel Corporation
+// Copyright (C) 2021 Intel Corporation
 //
 // SPDX-License-Identifier: MIT
 
@@ -6,7 +6,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import JobListComponent from 'components/task-page/job-list';
-import { updateJobAsync } from 'actions/tasks-actions';
+import { updateJobAsync, updateCurrentJobsPage } from 'actions/tasks-actions';
 import { Task } from 'reducers/interfaces';
 
 interface OwnProps {
@@ -15,18 +15,27 @@ interface OwnProps {
 
 interface DispatchToProps {
     onJobUpdate(jobInstance: any): void;
+    setCurrentJobsPage(taskID: number, pagenumber: number): void;
 }
 
 function mapDispatchToProps(dispatch: any): DispatchToProps {
     return {
         onJobUpdate: (jobInstance: any): void => dispatch(updateJobAsync(jobInstance)),
+        setCurrentJobsPage: (taskID: number, pageNumber: number): void =>
+            dispatch(updateCurrentJobsPage(taskID, pageNumber)),
     };
 }
 
 function TaskPageContainer(props: DispatchToProps & OwnProps): JSX.Element {
-    const { task, onJobUpdate } = props;
+    const { task, onJobUpdate, setCurrentJobsPage } = props;
 
-    return <JobListComponent taskInstance={task.instance} onJobUpdate={onJobUpdate} />;
+    return (
+        <JobListComponent
+            taskInstance={task.instance}
+            onJobUpdate={onJobUpdate}
+            setCurrentJobsPage={setCurrentJobsPage}
+        />
+    );
 }
 
 export default connect(null, mapDispatchToProps)(TaskPageContainer);
