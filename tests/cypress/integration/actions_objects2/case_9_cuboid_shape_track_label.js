bdb30e04dffa2af6ec2908d56418f16e76c6c6d3
dvkruchinin <dvkruchinin@gmail.com>
2021-05-05 10:06:54 +0300

    step 1

--- tests/cypress/integration/actions_objects2/case_9_cuboid_shape_track_label.js
+++ tests/cypress/integration/actions_objects2/case_9_cuboid_shape_track_label.js
@@ -75,6 +75,8 @@ context('Actions on Cuboid', () => {
         fourthX: createCuboidShape4Points.fourthX,
         fourthY: createCuboidShape4Points.fourthY + 150,
     };
+    let svgJsPolygonId = [];
+    let svgJsCircleId = [];
 
     before(() => {
         cy.openTask(taskName);
@@ -85,15 +87,48 @@ context('Actions on Cuboid', () => {
     describe(`Testing case "${caseId}"`, () => {
         it('Draw a Cuboid shape in two ways (From rectangle, by 4 points)', () => {
             cy.createCuboid(createCuboidShape2Points);
-            cy.createCuboid(createCuboidShape4Points);
+            cy.get('polygon').then(($polygon) => {
+                for (let i = 0; i < $polygon.length; i++) {
+                    if ($polygon[i].id.match(/^SvgjsPolygon\d+$/)) {
+                        svgJsPolygonId.push($polygon[i].id);
+                    }
+                }
+                // cy.get(`#${svgJsPolygonId[1]}`).invoke('attr', 'points').then(($svgjsPolygonPointsCoords) => {
+                //     cy.log($svgjsPolygonPointsCoords.split(' '))
+                // });
+                // cy.get('.cvat-canvas-container').trigger('mousemove', 300, 400);
+                // cy.get('#cvat_canvas_shape_1').should('have.class', 'cvat_canvas_shape_activated')
+                // cy.get('.cvat-canvas-container').trigger('mouseenter').trigger('mousedown', 330, 400, {button: 0})
+                // cy.get('text').should('have.class', 'cvat_canvas_hidden')
+                // cy.get('.cvat-canvas-container').trigger('mousemove', 500, 500).click(500, 500);
+            });
+            cy.get('.cvat-canvas-container').trigger('mousemove', 300, 400);
+            cy.get('#cvat_canvas_shape_1').should('have.class', 'cvat_canvas_shape_activated')
+            cy.get('circle').then(($circle) => {
+                for (let i = 0; i < $circle.length; i++) {
+                    if ($circle[i].id.match(/^SvgjsCircle\d+$/)) {
+                        svgJsCircleId.push($circle[i].id);
+                    }
+                }
+            });
+            cy.get('.cvat-canvas-container')
+                .trigger('mouseenter', 360, 340)
+                .trigger('mousedown', 360, 340, {button: 0})
+                .trigger('mousemove', 360, 240)
+                .trigger('mouseup', 360, 240)
+                .trigger('mouseenter', 360, 340)
+                .trigger('mousedown', 360, 340, {button: 0})
+                .trigger('mousemove', 400, 340)
+                .trigger('mouseup', 400, 340);
+            // cy.createCuboid(createCuboidShape4Points);
         });
-        it('Draw a Cuboid track in two ways (From rectangle, by 4 points)', () => {
-            cy.createCuboid(createCuboidTrack2Points);
-            cy.createCuboid(createCuboidTrack4Points);
-        });
-        it('Draw a new Cuboid shape in two ways (From rectangle, by 4 points) with second label', () => {
-            cy.createCuboid(createCuboidShape2PointsNewLabel);
-            cy.createCuboid(createCuboidShape4PointsNewLabel);
-        });
+        // it('Draw a Cuboid track in two ways (From rectangle, by 4 points)', () => {
+        //     cy.createCuboid(createCuboidTrack2Points);
+        //     cy.createCuboid(createCuboidTrack4Points);
+        // });
+        // it('Draw a new Cuboid shape in two ways (From rectangle, by 4 points) with second label', () => {
+        //     cy.createCuboid(createCuboidShape2PointsNewLabel);
+        //     cy.createCuboid(createCuboidShape4PointsNewLabel);
+        // });
     });
 });
