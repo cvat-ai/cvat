9417635ff09d7bc831ad8de4ec5f3122279f0a3a
dvkruchinin <dvkruchinin@gmail.com>
2021-05-24 10:26:27 +0300

    Revert case 64

--- tests/cypress/integration/canvas3d_functionality/case_64_canvas3d_functionality_cuboid.js
+++ tests/cypress/integration/canvas3d_functionality/case_64_canvas3d_functionality_cuboid.js
@@ -25,7 +25,6 @@ context('Canvas 3D functionality. Add cuboid.', () => {
             cy.get('.cvat-draw-cuboid-control').trigger('mouseover');
             cy.get('.cvat-draw-shape-popover').find('button').click();
             cy.get('.cvat-canvas3d-perspective').dblclick();
-            cy.wait(1000);
             cy.get('.cvat-canvas3d-perspective').screenshot('canvas3d_perspective_after_add_cuboid'); // The cuboid displayed
             cy.compareImagesAndCheckResult(
                 `${screenshotsPath}/canvas3d_perspective_before_all.png`,
@@ -33,17 +32,46 @@ context('Canvas 3D functionality. Add cuboid.', () => {
             );
         });
 
+        it('Cuboid interaction by mouse.', () => {
+            // The cuboid does not have time to change color so we make the mouse movement 3 times.
+            cy.get('.cvat-canvas3d-perspective')
+                .trigger('mousemove', 500, 200)
+                .trigger('mousemove', 400, 200)
+                .trigger('mousemove', 300, 200); // The cuboid should change a color after movement cursor from it
+            cy.get('.cvat-canvas3d-perspective').screenshot('canvas3d_perspective_after_cursor_movements_from_cuboid');
+            cy.compareImagesAndCheckResult(
+                `${screenshotsPath}/canvas3d_perspective_after_add_cuboid.png`,
+                `${screenshotsPath}/canvas3d_perspective_after_cursor_movements_from_cuboid.png`,
+            );
+
+            // Move cursor to the cuboid. The cuboid does not have time to change color so we make the mouse movement 3 times
+            cy.get('.cvat-canvas3d-perspective').trigger('mousemove').trigger('mousemove').trigger('mousemove');
+            cy.get('.cvat-canvas3d-perspective').screenshot('canvas3d_perspective_after_cursor_movements_to_cuboid');
+            // The cuboid changed a color
+            cy.compareImagesAndCheckResult(
+                `${screenshotsPath}/canvas3d_perspective_after_cursor_movements_from_cuboid.png`,
+                `${screenshotsPath}/canvas3d_perspective_after_cursor_movements_to_cuboid.png`,
+            );
+        });
+
         it('Top/side/front views should be changed afer dblclick on the cuboid on perspective view because of the drawing of the cuboid.', () => {
-            // On the top/side/front view the cuboid should change a color also.
+            cy.get('.cvat-canvas3d-perspective').dblclick(); // Dblclick on the cuboid
+            // On the perspective view the cuboid should change a color also.
+            cy.get('.cvat-canvas3d-perspective').screenshot('canvas3d_perspective_after_dblclick_on_cuboid');
+            cy.compareImagesAndCheckResult(
+                `${screenshotsPath}/canvas3d_perspective_after_cursor_movements_to_cuboid.png`,
+                `${screenshotsPath}/canvas3d_perspective_after_dblclick_on_cuboid.png`,
+            );
+
             ['topview', 'sideview', 'frontview'].forEach((view) => {
                 cy.get(`.cvat-canvas3d-${view}`)
                     .find('.cvat-canvas3d-fullsize')
-                    .screenshot(`canvas3d_${view}_add_cuboid`);
+                    .screenshot(`canvas3d_${view}_show_cuboid`);
             });
             [
-                ['canvas3d_topview_before_all.png', 'canvas3d_topview_add_cuboid.png'],
-                ['canvas3d_sideview_before_all.png', 'canvas3d_sideview_add_cuboid.png'],
-                ['canvas3d_frontview_before_all.png', 'canvas3d_frontview_add_cuboid.png'],
+                ['canvas3d_topview_before_all.png', 'canvas3d_topview_show_cuboid.png'],
+                ['canvas3d_sideview_before_all.png', 'canvas3d_sideview_show_cuboid.png'],
+                ['canvas3d_frontview_before_all.png', 'canvas3d_frontview_show_cuboid.png'],
             ].forEach(([viewBefore, viewAfter]) => {
                 cy.compareImagesAndCheckResult(`${screenshotsPath}/${viewBefore}`, `${screenshotsPath}/${viewAfter}`);
             });
