da5a25dddd4fb57bbf866abad9273c8da2fe07f7
Maya <maya17grd@gmail.com>
2021-06-16 13:01:27 +0300

    Update create page: using functional component

--- cvat-ui/src/components/create-cloud-storage-page/create-cloud-storage-content.tsx
+++ cvat-ui/src/components/create-cloud-storage-page/create-cloud-storage-content.tsx
@@ -2,8 +2,9 @@
 //
 // SPDX-License-Identifier: MIT
 
-import React from 'react';
-import { RouteComponentProps } from 'react-router'; // useHistory
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Row, Col } from 'antd/lib/grid';
 import Button from 'antd/lib/button';
@@ -11,78 +12,58 @@ import Form, { FormInstance } from 'antd/lib/form';
 import Select from 'antd/lib/select';
 import Input from 'antd/lib/input';
 import TextArea from 'antd/lib/input/TextArea';
+import notification from 'antd/lib/notification';
+import { CombinedState } from 'reducers/interfaces';
+import { createCloudStorageAsync } from 'actions/cloud-storage-actions';
 import { ProviderType, CredentialsType } from '../enums';
 
-export interface CreateCloudStorageData {
-    providerType: ProviderType | null;
-    credentialsType: CredentialsType | null;
-    resource: string | null;
-    displayName: string | null;
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function CreateCloudStorageContent(props:any): JSX.Element {
+    const formRef = React.createRef<FormInstance>();
+    const [providerType, setProviderType] = useState<ProviderType | null>(null);
+    const [credentialsType, setCredentialsType] = useState<CredentialsType | null>(null);
+    const dispatch = useDispatch();
+    const newCloudStorageId = useSelector((state: CombinedState) => state.cloudStorages.activities.creates.id);
 
-interface Props {
-    onCreate: (data: CreateCloudStorageData) => void;
-    status: string;
-    cloudStorageId: number | null;
-}
+    useEffect(() => {
+        if (Number.isInteger(newCloudStorageId)) {
+            // Clear form
+            if (formRef.current) {
+                formRef.current.resetFields();
+            }
 
-interface State {
-    providerType: ProviderType | null | string;
-    displayName: string;
-    resource: string;
-    credentialsType: null | CredentialsType | string;
-}
+            notification.info({
+                message: 'The cloud storage has been created',
+                className: 'cvat-notification-create-cloud-storage-success',
+            });
+        }
+    }, [newCloudStorageId]);
 
-const defaultState = {
-    providerType: null,
-    credentialsType: null,
-    resource: '',
-    displayName: '',
-};
-
-class CreateCloudStorageContent extends React.PureComponent<Props & RouteComponentProps, State> {
-    formRef = React.createRef<FormInstance>();
-
-    public constructor(props: Props & RouteComponentProps) {
-        super(props);
-        this.state = { ...defaultState };
-    }
-
-    onSumbit = async (): Promise<void> => {
+    const onSumbit = async (): Promise<void> => {
         let cloudStorageData: Record<string, any> = {};
-        if (this.formRef.current) {
-            const formValues = await this.formRef.current.validateFields();
+        if (formRef.current) {
+            const formValues = await formRef.current.validateFields();
             cloudStorageData = {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 ...cloudStorageData,
                 ...formValues,
             };
+            dispatch(createCloudStorageAsync(cloudStorageData));
         }
     };
 
-    onReset = (): void => {
-        this.formRef.current?.resetFields();
+    const onReset = (): void => {
+        formRef.current?.resetFields();
     };
 
-    private selectProviderType = (newProviderType: ProviderType | null | string): void => {
-        const curentState = this.state;
-        this.setState({
-            ...curentState,
-            providerType: newProviderType,
-            credentialsType: null,
-        });
+    // TODO: change this to user hook
+    const handleProviderType = (providerValue: ProviderType): void => {
+        setProviderType(providerValue);
+        setCredentialsType(null);
+        // FIXME: need to clean Credentials type value when change provider value
     };
 
-    private selectCredentialsType = (newCredentialsType: CredentialsType | null | string): void => {
-        const currentState = this.state;
-        this.setState({
-            ...currentState,
-            credentialsType: newCredentialsType,
-        });
-    };
-
-    private renderCredentialsBlok(): JSX.Element {
-        const { credentialsType, providerType } = this.state;
+    const credentialsBlok = (): JSX.Element => {
+        // const { credentialsType, providerType } = this.state;
         let credentials;
         switch (providerType && credentialsType) {
             case ProviderType.AWS_S3_BUCKET && CredentialsType.TEMP_KEY_SECRET_KEY_TOKEN_SET: {
@@ -119,8 +100,6 @@ class CreateCloudStorageContent extends React.PureComponent<Props & RouteCompone
                         <Form.Item
                             label='Account name'
                             name='accountName'
-                            tooltip='Storage account names must be between 3 and 24 characters in length
-                                and may contain numbers and lowercase letters only.'
                             rules={[{ required: true, message: 'Please input your account name' }]}
                         >
                             <Input.Password minLength={3} maxLength={24} />
@@ -128,8 +107,6 @@ class CreateCloudStorageContent extends React.PureComponent<Props & RouteCompone
                         <Form.Item
                             label='SAS token'
                             name='sasToken'
-                            tooltip='shared access signature used to authority to Blob Storage Account.
-                                See more https://docs.microsoft.com/bs-latn-ba/azure/storage/common/storage-sas-overview?toc=/azure/storage/blobs/toc.json'
                             rules={[{ required: true, message: 'Please input your SAS token' }]}
                         >
                             <Input.Password maxLength={40} />
@@ -145,119 +122,128 @@ class CreateCloudStorageContent extends React.PureComponent<Props & RouteCompone
         }
 
         return credentials;
-    }
+    };
 
-    private renderAWSS3Configuration(): JSX.Element {
-        return (
-            <>
-                <Form.Item
-                    label='Bucket name'
-                    name='bucketName'
-                    rules={[{ required: true, message: 'Please, input bucket name' }]}
+    const AWSS3Configuration = (): JSX.Element => (
+        <>
+            <Form.Item
+                label='Bucket name'
+                name='bucketName'
+                rules={[{ required: true, message: 'Please, input bucket name' }]}
+            >
+                <Input maxLength={63} />
+            </Form.Item>
+            <Form.Item
+                label='Credentials type'
+                name='credentialsType'
+                rules={[{ required: true, message: 'Please, select credentials type' }]}
+            >
+                <Select
+                    onSelect={(value: CredentialsType) => setCredentialsType(value)}
                 >
-                    <Input maxLength={63} />
-                </Form.Item>
-                <Form.Item
-                    label='Credentials type'
-                    name='credentialsType'
-                    rules={[{ required: true, message: 'Please, select credentials type' }]}
-                >
-                    <Select defaultValue='select credentials type' onSelect={this.selectCredentialsType}>
-                        <Select.Option value={CredentialsType.TEMP_KEY_SECRET_KEY_TOKEN_SET}>
-                            {CredentialsType.TEMP_KEY_SECRET_KEY_TOKEN_SET}
-                        </Select.Option>
-                        <Select.Option value={CredentialsType.ANONYMOUS_ACCESS}>
-                            {CredentialsType.ANONYMOUS_ACCESS}
-                        </Select.Option>
-                    </Select>
-                </Form.Item>
-                {this.renderCredentialsBlok()}
-            </>
-        );
-    }
+                    <Select.Option value={CredentialsType.TEMP_KEY_SECRET_KEY_TOKEN_SET}>
+                        {CredentialsType.TEMP_KEY_SECRET_KEY_TOKEN_SET}
+                    </Select.Option>
+                    <Select.Option value={CredentialsType.ANONYMOUS_ACCESS}>
+                        {CredentialsType.ANONYMOUS_ACCESS}
+                    </Select.Option>
+                </Select>
+            </Form.Item>
+            {credentialsBlok()}
+        </>
+    );
 
-    private renderAzureBlobStorageConfiguration(): JSX.Element {
-        return (
-            <>
-                <Form.Item
-                    label='Container name'
-                    name='containerName'
-                    rules={[{ required: true, message: 'Please, input container name' }]}
+    const AzureBlobStorageConfiguration = (): JSX.Element => (
+        <>
+            <Form.Item
+                label='Container name'
+                name='containerName'
+                rules={[{ required: true, message: 'Please, input container name' }]}
+            >
+                <Input maxLength={63} />
+            </Form.Item>
+            <Form.Item
+                label='Credentials type'
+                name='credentialsType'
+                rules={[{ required: true, message: 'Please, select credentials type' }]}
+            >
+                <Select
+                    onSelect={(value: CredentialsType) => setCredentialsType(value)}
                 >
-                    <Input maxLength={63} />
-                </Form.Item>
-                <Form.Item
-                    label='Credentials type'
-                    name='credentialsType'
-                    rules={[{ required: true, message: 'Please, select credentials type' }]}
+                    <Select.Option value={CredentialsType.ACCOUNT_NAME_TOKEN_PAIR}>
+                        {CredentialsType.ACCOUNT_NAME_TOKEN_PAIR}
+                    </Select.Option>
+                </Select>
+            </Form.Item>
+
+            {credentialsBlok()}
+        </>
+    );
+
+    return (
+        <Row justify='start' align='middle' className='cvat-create-cloud-storage-content'>
+            <Col span={24}>
+                <Form
+                    layout='horizontal'
+                    ref={formRef}
                 >
-                    <Select defaultValue='select credentials type' onSelect={this.selectCredentialsType}>
-                        <Select.Option value={CredentialsType.ACCOUNT_NAME_TOKEN_PAIR}>
-                            {CredentialsType.ACCOUNT_NAME_TOKEN_PAIR}
-                        </Select.Option>
-                    </Select>
-                </Form.Item>
-                {/* TODO: need to change selected value on credentials
-                type to "default" when provider type value changes */}
-                {this.renderCredentialsBlok()}
-            </>
-        );
-    }
-
-    public render(): JSX.Element {
-        const { providerType } = this.state;
-
-        return (
-            <Row justify='start' align='middle' className='cvat-create-cloud-storage-content'>
-                <Col span={24}>
-                    <Form layout='horizontal' ref={this.formRef}>
-                        <Form.Item
-                            label='Display name'
-                            tooltip='What name will be used for display?'
-                            name='displayName'
-                            rules={[{ required: true, message: 'Please, input your display name' }]}
-                        >
-                            <Input maxLength={63} />
-                        </Form.Item>
-                        <Form.Item label='Description' name='description' labelAlign='left'>
-                            <TextArea autoSize={{ minRows: 2, maxRows: 5 }} placeholder='Add storage description' />
-                        </Form.Item>
-                        <Form.Item
-                            label='Provider'
-                            name='provider'
-                            rules={[{ required: true, message: 'Please, select provider' }]}
-                        >
-                            <Select defaultValue='select provider type' onSelect={this.selectProviderType}>
-                                <Select.Option value={ProviderType.AWS_S3_BUCKET}>
-                                    {ProviderType.AWS_S3_BUCKET}
-                                </Select.Option>
-                                <Select.Option value={ProviderType.AZURE_BLOB_CONTAINER}>
-                                    {ProviderType.AZURE_BLOB_CONTAINER}
-                                </Select.Option>
-                            </Select>
-                        </Form.Item>
-                        {providerType === ProviderType.AWS_S3_BUCKET ? this.renderAWSS3Configuration() : null}
-                        {providerType === ProviderType.AZURE_BLOB_CONTAINER ?
-                            this.renderAzureBlobStorageConfiguration() :
-                            null}
-                    </Form>
-                </Col>
-                <Col span={6} offset={18}>
-                    <Button htmlType='button' onClick={this.onReset} className='cvat-create-cloud-storage-reset-button'>
-                        Cansel
-                    </Button>
-                    <Button
-                        type='primary'
-                        htmlType='submit'
-                        onClick={this.onSumbit}
-                        className='cvat-create-cloud-storage-submit-button'
+                    <Form.Item
+                        label='Display name'
+                        // tooltip='What name will be used for display?'
+                        name='displayName'
+                        rules={[{ required: true, message: 'Please, input your display name' }]}
                     >
-                        Submit
-                    </Button>
-                </Col>
-            </Row>
-        );
-    }
+                        <Input maxLength={63} />
+                    </Form.Item>
+                    <Form.Item
+                        label='Description'
+                        name='description'
+                        labelAlign='left'
+                    >
+                        <TextArea
+                            autoSize={{ minRows: 2, maxRows: 5 }}
+                            placeholder='Add storage description'
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label='Provider'
+                        name='provider'
+                        rules={[{ required: true, message: 'Please, select provider' }]}
+                    >
+                        <Select
+                            onSelect={(value:ProviderType) => handleProviderType(value)}
+                        >
+                            <Select.Option value={ProviderType.AWS_S3_BUCKET}>
+                                {ProviderType.AWS_S3_BUCKET}
+                            </Select.Option>
+                            <Select.Option value={ProviderType.AZURE_BLOB_CONTAINER}>
+                                {ProviderType.AZURE_BLOB_CONTAINER}
+                            </Select.Option>
+                        </Select>
+                    </Form.Item>
+                    {providerType === ProviderType.AWS_S3_BUCKET && AWSS3Configuration()}
+                    {providerType === ProviderType.AZURE_BLOB_CONTAINER && AzureBlobStorageConfiguration()}
+                </Form>
+            </Col>
+            <Col span={6} offset={18}>
+                <Button
+                    htmlType='button'
+                    onClick={onReset}
+                    className='cvat-create-cloud-storage-reset-button'
+                >
+                    Cansel
+                </Button>
+                <Button
+                    type='primary'
+                    htmlType='submit'
+                    onClick={onSumbit}
+                    className='cvat-create-cloud-storage-submit-button'
+                >
+                    Submit
+                </Button>
+            </Col>
+        </Row>
+    );
 }
 
 export default withRouter(CreateCloudStorageContent);
