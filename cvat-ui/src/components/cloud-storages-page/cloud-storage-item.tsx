de10bb18262b5503c110ac97db6be293ec9ec8fd
Boris Sekachev <boris.sekachev@intel.com>
2021-06-16 21:27:41 +0300

    Implemented menu & deleting

--- cvat-ui/src/components/cloud-storages-page/cloud-storage-item.tsx
+++ cvat-ui/src/components/cloud-storages-page/cloud-storage-item.tsx
@@ -2,25 +2,70 @@
 //
 // SPDX-License-Identifier: MIT
 
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+import { CloudSyncOutlined, MoreOutlined } from '@ant-design/icons';
 import Card from 'antd/lib/card';
 import Meta from 'antd/lib/card/Meta';
 import Paragraph from 'antd/lib/typography/Paragraph';
 import Text from 'antd/lib/typography/Text';
+import Button from 'antd/lib/button';
+import Dropdown from 'antd/lib/dropdown';
+import Menu from 'antd/lib/menu';
+import Modal from 'antd/lib/modal';
 import moment from 'moment';
 
-import { CloudSyncOutlined } from '@ant-design/icons';
-import { CloudStorage } from 'reducers/interfaces';
+import { CloudStorage, CombinedState } from 'reducers/interfaces';
+import { deleteCloudStorageAsync } from 'actions/cloud-storage-actions';
 
 interface Props {
     cloudStorageInstance: CloudStorage;
 }
 
+// TODO: fix owner [server]
+// TODO: implement status
+// TODO: implement editing
+// TODO: implement preview
+
 export default function CloudStorageItemComponent(props: Props): JSX.Element {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
     const { cloudStorageInstance } = props;
     const {
         id, displayName, preview, provider, owner, createdDate, updatedDate,
     } = cloudStorageInstance;
+
+    const deletes = useSelector((state: CombinedState) => state.cloudStorages.activities.deletes);
+    const deleted = cloudStorageInstance.id in deletes ? deletes[cloudStorageInstance.id] : false;
+
+    const style: React.CSSProperties = {};
+
+    if (deleted) {
+        style.pointerEvents = 'none';
+        style.opacity = 0.5;
+    }
+
+    const onEdit = useCallback(() => {
+        history.push(`/cloudstorages/edit/${id}`);
+    }, []);
+
+    const onDelete = useCallback(() => {
+        Modal.confirm({
+            title: 'Please, confirm your action',
+            content: `You are going to remove the cloudstorage "${displayName}". Continue?`,
+            className: 'cvat-delete-cloud-storage-modal',
+            onOk: () => {
+                dispatch(deleteCloudStorageAsync(cloudStorageInstance));
+            },
+            okButtonProps: {
+                type: 'primary',
+                danger: true,
+            },
+            okText: 'Delete',
+        });
+    }, [cloudStorageInstance]);
 
     return (
         <Card
@@ -34,6 +79,7 @@ export default function CloudStorageItemComponent(props: Props): JSX.Element {
                 )
             }
             size='small'
+            style={style}
             className='cvat-cloud-storage-item'
         >
             <Meta
@@ -63,6 +109,21 @@ export default function CloudStorageItemComponent(props: Props): JSX.Element {
                             <Text type='secondary'>Status: </Text>
                             <Text type='warning'>Not implemented</Text>
                         </Paragraph>
+                        <Dropdown
+                            overlay={(
+                                <Menu className='cvat-project-actions-menu'>
+                                    <Menu.Item onClick={onEdit}>Edit</Menu.Item>
+                                    <Menu.Item onClick={onDelete}>Delete</Menu.Item>
+                                </Menu>
+                            )}
+                        >
+                            <Button
+                                className='cvat-cloud-storage-item-menu-button'
+                                type='link'
+                                size='large'
+                                icon={<MoreOutlined />}
+                            />
+                        </Dropdown>
                     </>
                 )}
             />
