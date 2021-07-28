f655ae8d2e8a464d2ca99d7fae49586301f1aaa3
dvkruchinin <dvkruchinin@gmail.com>
2021-07-28 16:47:48 +0300

    Added steps to the test.

--- tests/cypress/integration/actions_tasks2/case_101_opencv_basic_actions.js
+++ tests/cypress/integration/actions_tasks2/case_101_opencv_basic_actions.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2021 Intel Corporation
+// Copyright (C) 2021 Intel Corporation
 //
 // SPDX-License-Identifier: MIT
 
@@ -6,22 +6,35 @@
 
 import { taskName, labelName } from '../../support/const';
 
-context('Canvas color feature', () => {
-    const caseId = '100';
+context('OpenCV. Intelligent cissors. Basic actions.', () => {
+    const caseId = '101';
     const createOpencvShape = {
         labelName: labelName,
         pointsMap: [
             { x: 200, y: 200 },
             { x: 250, y: 200 },
-            { x: 200, y: 250 },
-            { x: 250, y: 300 },
+            { x: 300, y: 250 },
+            { x: 350, y: 300 },
             { x: 300, y: 350 },
+        ],
+    };
+    const newLabel = `${createOpencvShape.labelName} new`
+    const createOpencvShapeSecondLabel = {
+        labelName: newLabel,
+        pointsMap: [
+            { x: 300, y: 200 },
+            { x: 350, y: 200 },
+            { x: 400, y: 250 },
+            { x: 450, y: 300 },
+            { x: 400, y: 350 },
         ],
     };
     const keyCodeN = 78;
 
     before(() => {
-        cy.openTaskJob(taskName);
+        cy.openTask(taskName);
+        cy.addNewLabel(newLabel);
+        cy.openJob();
     });
 
     describe(`Testing case "${caseId}"`, () => {
@@ -32,18 +45,25 @@ context('Canvas color feature', () => {
             cy.get('.cvat-opencv-drawing-tool').should('exist').and('be.visible');
         });
 
-        it('Create a shape via "Intelligent cissors".', () => {
+        it('Create a shape with "Intelligent cissors".', () => {
             cy.opencvCreateShape(createOpencvShape);
+        });
+
+        it('Create a shape with "Intelligent cissors".', () => {
+            cy.opencvCreateShape(createOpencvShapeSecondLabel);
+        });
+
+        // Waiting for fix https://github.com/openvinotoolkit/cvat/issues/3474
+        it.skip('Redraw the shape created with "Intelligent cissors".', () => {
             cy.get('#cvat_canvas_shape_1')
                 .trigger('mousemove', {scrollBehavior: false})
                 .trigger('mouseover', {scrollBehavior: false})
                 .should('have.class', 'cvat_canvas_shape_activated');
             cy.get('body').trigger('keydown', { keyCode: keyCodeN, shiftKey: true }).trigger('keyup');
-            cy.get('.cvat-draw-polygon-control').should('have.attr', 'tabindex');
             cy.get('.cvat-tools-control').should('have.attr', 'tabindex');
             createOpencvShape.pointsMap.forEach((el) => {
                 cy.get('.cvat-canvas-container')
-                    .click(el.x + 100, el.y + 50)
+                    .click(el.x + 150, el.y + 50)
             });
             cy.get('body').trigger('keydown', { keyCode: keyCodeN }).trigger('keyup');
         });
