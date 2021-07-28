2794b21f1ec219db28d7f86ec06f081be7912b73
dvkruchinin <dvkruchinin@gmail.com>
2021-07-28 14:55:42 +0300

    check redraw

--- tests/cypress/integration/actions_tasks2/case_101_opencv_basic_actions.js
+++ tests/cypress/integration/actions_tasks2/case_101_opencv_basic_actions.js
@@ -13,9 +13,12 @@ context('Canvas color feature', () => {
         pointsMap: [
             { x: 200, y: 200 },
             { x: 250, y: 200 },
-            { x: 250, y: 250 },
+            { x: 200, y: 250 },
+            { x: 250, y: 300 },
+            { x: 300, y: 350 },
         ],
     };
+    const keyCodeN = 78;
 
     before(() => {
         cy.openTaskJob(taskName);
@@ -31,6 +34,18 @@ context('Canvas color feature', () => {
 
         it('Create a shape via "Intelligent cissors".', () => {
             cy.opencvCreateShape(createOpencvShape);
+            cy.get('#cvat_canvas_shape_1')
+                .trigger('mousemove', {scrollBehavior: false})
+                .trigger('mouseover', {scrollBehavior: false})
+                .should('have.class', 'cvat_canvas_shape_activated');
+            cy.get('body').trigger('keydown', { keyCode: keyCodeN, shiftKey: true }).trigger('keyup');
+            cy.get('.cvat-draw-polygon-control').should('have.attr', 'tabindex');
+            cy.get('.cvat-tools-control').should('have.attr', 'tabindex');
+            createOpencvShape.pointsMap.forEach((el) => {
+                cy.get('.cvat-canvas-container')
+                    .click(el.x + 100, el.y + 50)
+            });
+            cy.get('body').trigger('keydown', { keyCode: keyCodeN }).trigger('keyup');
         });
     });
 });
