1f5f6f23ce2917467d5d7b90ac4a3f2ce7e98f98
dvkruchinin <dvkruchinin@gmail.com>
2021-07-29 13:42:45 +0300

    Added some classes

--- cvat-ui/src/components/export-dataset/export-dataset-modal.tsx
+++ cvat-ui/src/components/export-dataset/export-dataset-modal.tsx
@@ -72,6 +72,7 @@ function ExportDatasetModal(): JSX.Element {
             message: 'Dataset export started',
             description: `Dataset export was started for ${instanceType} #${instance?.id}. ` +
                 'Download will start automaticly as soon as the dataset is ready.',
+            className: `cvat-notification-notice-export-${instanceType}-start`,
         });
     }, [instance?.id, instance instanceof core.classes.Project]);
 
@@ -81,6 +82,7 @@ function ExportDatasetModal(): JSX.Element {
             visible={modalVisible}
             onCancel={closeModal}
             onOk={() => form.submit()}
+            className={`cvat-modal-export-${instanceType}-dataset`}
         >
             <Form
                 name='Export dataset'
