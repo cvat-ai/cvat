c340b3bbdeca65758ea0d8f41b08253502cf9f0a
dvkruchinin <dvkruchinin@gmail.com>
2021-08-04 19:07:14 +0300

    Try to getting a clipboard text

--- tests/cypress/integration/actions_tasks/case_102_create_link_shape_frame.js
+++ tests/cypress/integration/actions_tasks/case_102_create_link_shape_frame.js
@@ -26,9 +26,16 @@ context('Create a link for shape, frame.', () => {
 
     describe(`Testing case "${caseId}"`, () => {
         it('Create a link for the shape.', () => {
+            cy.wrap(Cypress.automation('remote:debugger:protocol', {
+                command: 'Browser.grantPermissions',
+                params: {
+                    permissions: ['clipboardReadWrite', 'clipboardSanitizedWrite'],
+                    origin: window.location.origin,
+                },
+            }));
             cy.get('#cvat-objects-sidebar-state-item-1').find('[aria-label="more"]').trigger('mouseover');
             cy.get('#cvat_canvas_shape_1').should('have.class', 'cvat_canvas_shape_activated')
-            cy.get('.cvat-object-item-menu').last().should('be.visible').contains('button', 'Create object URL').realClick();
+            cy.get('.cvat-object-item-menu').last().should('be.visible').contains('button', 'Create object URL').trigger('mouseover').realClick();
             cy.task('getClipboard').then(($clipboard) => {
                 const url = $clipboard;
                 cy.task('log', `URL: ${url}`);
