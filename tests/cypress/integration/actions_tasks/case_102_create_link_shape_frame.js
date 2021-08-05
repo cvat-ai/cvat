4b7c4bc1eb15983c03ef01b56a8bcacdf6e6229d
dvkruchinin <dvkruchinin@gmail.com>
2021-08-05 08:22:47 +0300

    Added the step for creation link for a frame. Added asserts

--- tests/cypress/integration/actions_tasks/case_102_create_link_shape_frame.js
+++ tests/cypress/integration/actions_tasks/case_102_create_link_shape_frame.js
@@ -25,16 +25,37 @@ context('Create a link for shape, frame.', () => {
     });
 
     describe(`Testing case "${caseId}"`, () => {
-        it('Create a link for the shape.', () => {
+        it('Create a link for a shape, for a frame.', () => {
             cy.window().then(win => {
-                cy.stub(win, 'prompt').returns(win.prompt).as('copyToClipboardPrompt');
+                cy.stub(win, 'prompt').returns(win.prompt).as('copyToClipboardPromptShape');
             });
             cy.get('#cvat-objects-sidebar-state-item-1').find('[aria-label="more"]').trigger('mouseover');
             cy.get('#cvat_canvas_shape_1').should('have.class', 'cvat_canvas_shape_activated')
             cy.get('.cvat-object-item-menu').last().should('be.visible').contains('button', 'Create object URL').click();
-            cy.get('@copyToClipboardPrompt').should('be.called');
-            cy.get('@copyToClipboardPrompt').then(prompt => {
-                cy.task('log', `URL: ${prompt.args[0][1]}`);
+            cy.get('@copyToClipboardPromptShape').should('be.called');
+            cy.get('@copyToClipboardPromptShape').then(prompt => {
+                const url = prompt.args[0][1];
+                expect(url).include('frame=');
+                expect(url).include('type=');
+                expect(url).include('serverID=');
+                cy.visit(url);
+                cy.get('.cvat-canvas-container').should('be.visible');
+                cy.get('#cvat_canvas_shape_1').should('be.visible');
+            });
+
+            cy.window().then(win => {
+                cy.stub(win, 'prompt').returns(win.prompt).as('copyToClipboardPromptFrame');
+            });
+            cy.get('.cvat-player-frame-url-icon').click();
+            cy.get('@copyToClipboardPromptFrame').should('be.called');
+            cy.get('@copyToClipboardPromptFrame').then(prompt => {
+                const url = prompt.args[0][1];
+                expect(url).include('frame=');
+                expect(url).not.include('type=');
+                expect(url).not.include('serverID=');
+                cy.visit(url);
+                cy.get('.cvat-canvas-container').should('be.visible');
+                cy.get('#cvat_canvas_shape_1').should('be.visible');
             });
         });
     });
