950e722899599792716e011d926582cefe778685
Boris Sekachev <boris.sekachev@yandex.ru>
2021-08-17 15:25:20 +0300

    Popover visibility reworked

--- cvat-ui/src/components/annotation-page/standard-workspace/controls-side-bar/handle-popover-visibility.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/controls-side-bar/handle-popover-visibility.tsx
@@ -7,7 +7,6 @@ import Popover, { PopoverProps } from 'antd/lib/popover';
 
 export default function withVisibilityHandling(WrappedComponent: typeof Popover, popoverType: string) {
     return (props: PopoverProps): JSX.Element => {
-        const [initialized, setInitialized] = useState<boolean>(false);
         const [visible, setVisible] = useState<boolean>(false);
         const { overlayClassName, ...rest } = props;
         const overlayClassNames = typeof overlayClassName === 'string' ? overlayClassName.split(/\s+/) : [];
@@ -19,31 +18,21 @@ export default function withVisibilityHandling(WrappedComponent: typeof Popover,
             overlayClassNames.push(visiblePopoverClassName);
         }
 
-        const callback = (event: Event): void => {
-            if ((event as AnimationEvent).animationName === 'antZoomBigIn') {
-                setVisible(true);
-            }
-        };
-
         return (
             <WrappedComponent
                 {...rest}
                 trigger={visible ? 'click' : 'hover'}
                 overlayClassName={overlayClassNames.join(' ').trim()}
+                afterVisibleChange={(_visible: boolean) => {
+                    setVisible(_visible);
+                }}
                 onVisibleChange={(_visible: boolean) => {
-                    if (!_visible) {
-                        setVisible(false);
-                    } else {
+                    if (_visible) {
                         // Hide other popovers
                         const element = window.document.getElementsByClassName(`${popoverClassName}`)[0];
                         if (element) {
                             element.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
                         }
-                    }
-                    if (!initialized) {
-                        const self = window.document.getElementsByClassName(`cvat-${popoverType}-popover`)[0];
-                        self?.addEventListener('animationend', callback);
-                        setInitialized(true);
                     }
                 }}
             />
