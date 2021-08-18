bd004b2fb5da80ae3fa0738081bcc19420cb6823
Andrey Zhavoronkov <andrey.zhavoronkov@intel.com>
2021-08-18 11:12:36 +0300

    fixed comments

--- cvat-ui/src/components/login-page/intel-footer-drawer.tsx
+++ cvat-ui/src/components/login-page/intel-footer-drawer.tsx
@@ -9,36 +9,20 @@ import { isPublic } from 'utils/enviroment';
 
 import consts from 'consts';
 
-function FooterDrawer(): JSX.Element {
+function FooterDrawer(): JSX.Element | null {
     const { Footer } = Layout;
     const { INTEL_TERMS_OF_USE_URL, INTEL_COOKIES_URL, INTEL_PRIVACY_URL } = consts;
 
-    return (
-        <>
-            {isPublic() && (
-                <Footer style={{ textAlign: 'center', borderTop: '1px solid #e8e8e8' }}>
-                    © Intel Corporation |
-                    <a target='_blank' rel='noopener noreferrer' href={INTEL_TERMS_OF_USE_URL}>
-                        {' '}
-                        Terms of Use
-                        {' '}
-                    </a>
-                    |
-                    <a target='_blank' rel='noopener noreferrer' href={INTEL_COOKIES_URL}>
-                        {' '}
-                        Cookies
-                        {' '}
-                    </a>
-                    |
-                    <a target='_blank' rel='noopener noreferrer' href={INTEL_PRIVACY_URL}>
-                        {' '}
-                        Privacy
-                        {' '}
-                    </a>
-                </Footer>
-            )}
-        </>
-    );
+    return isPublic() ? (
+        <Footer style={{ textAlign: 'center', borderTop: '1px solid #e8e8e8' }}>
+            © Intel Corporation |
+            <a target='_blank' rel='noopener noreferrer' href={INTEL_TERMS_OF_USE_URL}> Terms of Use </a>
+            |
+            <a target='_blank' rel='noopener noreferrer' href={INTEL_COOKIES_URL}> Cookies </a>
+            |
+            <a target='_blank' rel='noopener noreferrer' href={INTEL_PRIVACY_URL}> Privacy </a>
+        </Footer>
+    ) : null;
 }
 
 export default FooterDrawer;
