1cff0915d43d9910cfac89fe054981bde092879d
Maya <maya17grd@gmail.com>
2021-08-30 13:59:28 +0300

    Update migrtaions

--- cvat/apps/engine/migrations/0041_auto_20210827_0258.py
+++ cvat/apps/engine/migrations/0041_auto_20210827_0258.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('engine', '0041_auto_20210813_0853'),
+        ('engine', '0040_cloud_storage'),
     ]
 
     operations = [
