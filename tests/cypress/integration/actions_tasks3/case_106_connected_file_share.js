745af704e3b63cb981f6faf67f6c837788bd1022
dvkruchinin <dvkruchinin@gmail.com>
2021-10-04 14:04:11 +0300

    Continue writing the test

--- tests/cypress/integration/actions_tasks3/case_106_connected_file_share.js
+++ tests/cypress/integration/actions_tasks3/case_106_connected_file_share.js
@@ -8,20 +8,16 @@ context('Connected file share.', () => {
     const caseId = '106';
     const taskName = `Case ${caseId}`;
     const labelName = taskName;
-
-    const imagesCount = 3;
-    const imageFileName = `image_${labelName.replace(' ', '_').toLowerCase()}`;
-    const width = 800;
-    const height = 800;
-    const posX = 10;
-    const posY = 10;
-    const color = 'gray';
-    const imagesFolder = `cypress/fixtures/${imageFileName}`;
+    let stdoutToList;
 
     before(() => {
-        cy.imageGenerator(imagesFolder, imageFileName, width, height, color, posX, posY, labelName, imagesCount);
         cy.visit('auth/login');
         cy.login();
+    });
+
+    after(() => {
+        cy.goToTaskList();
+        cy.deleteTask(taskName);
     });
 
     describe(`Testing case "${caseId}"`, () => {
@@ -30,7 +26,31 @@ context('Connected file share.', () => {
             cy.get('#name').type(taskName);
             cy.addNewLabel(labelName);
             cy.contains('[role="tab"]', 'Connected file share').click();
-            cy.get('.cvat-share-tree').should('be.visible');
+            cy.get('.cvat-share-tree').should('be.visible').within(() => {
+                cy.get('[aria-label="plus-square"]').click();
+                cy.get('[title]').should('have.length', 4) // Also "root"
+                cy.exec('docker exec -i cvat /bin/bash -c "ls ~/share"').then((command) => {
+                    stdoutToList = command.stdout.split('\n');
+                    // [image_case_106_1.png, image_case_106_2.png, image_case_106_3.png]
+                    expect (stdoutToList.length).to.be.eq(3);
+                    stdoutToList.forEach((el) => {
+                        cy.get(`[title="${el}"]`).should('exist');
+                        // Click on the checkboxes
+                        cy.get(`[title="${el}"]`).prev().click().should('have.attr', 'class').and('contain', 'checked');
+                    });
+                });
+            });
+            cy.contains('button', 'Submit').click();
+            cy.get('.cvat-notification-create-task-success').should('exist').find('button').click();
+            cy.get('.cvat-task-details').should('exist');
+            cy.openJob();
+            cy.get('.cvat-player-filename-wrapper').then((playerFilenameWrapper) => {
+                for (let el = 0; el < stdoutToList.length; el++) {
+                    cy.get(playerFilenameWrapper).should('have.text', stdoutToList[el]);
+                    cy.checkFrameNum(el)
+                    cy.get('.cvat-player-next-button').click().trigger('mouseout');
+                }
+            });
         });
     });
 });
