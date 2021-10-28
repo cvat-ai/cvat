653d270604fc40d0377638344c8e8e70c1750db7
Troy Carlson <troycarlson4@gmail.com>
2021-10-28 01:05:16 +0000

    Move a few config values to environment variables

--- cvat/apps/voxel/commands/sync_labels.py
+++ cvat/apps/voxel/commands/sync_labels.py
@@ -1,14 +1,10 @@
-import datetime
 import io
 import os
 from django.conf import settings
 from cvat.apps.voxel.commands.voxel_command import VoxelCommand
-from google.cloud import firestore
 from google.cloud import storage
 from google.oauth2 import service_account
 from cvat.apps.dataset_manager.formats.cvat import dump_as_cvat_interpolation
-
-_GCP_PROJECT = "sodium-carving-227300"
 
 
 class SyncLabels(VoxelCommand):
@@ -17,41 +13,25 @@ class SyncLabels(VoxelCommand):
     def __init__(self, task_id, video_uuid):
         self.task_id = task_id
         self.video_uuid = self._sanitize_video_uuid(video_uuid)
-        # Note: this file needs to be available in the prod Docker container
-        key_path = '{}/voxel_keys/sodium-carving-227300-6d23b84328c2.json'.format(
-            os.getcwd())
-        self.credentials = service_account.Credentials.from_service_account_file(
-            key_path)
+        # NOTE: this file needs to be available in the prod Docker container
+        key_filename = os.getenv("VOXEL_KEY_FILENAME", "missing_env_var_VOXEL_KEY_FILENAME")
+        key_path = f"{os.getcwd()}/voxel_keys/{key_filename}"
+        self.credentials = (
+            service_account.Credentials.from_service_account_file(key_path)
+        )
 
     def _sanitize_video_uuid(self, video_uuid):
         """Sanitize UUIDs since it most likely comes from CVAT task name."""
         # Strip file extensions
-        return video_uuid.strip().split('.')[0]
-
-    def _sync_to_firestore(self, cvat_xml):
-        # TODO: Convert CVAT XML to our JSON format
-        # This may be better as a cloud function so we don't have to duplicate
-        # this logic between CVAT and voxelsafety repositories.
-        #     convertor = Convertor(self.video_uuid, xml=cvat_xml)
-        #     convertor.parse()
-        #     internal_json = convertor.get_label_json()
-        db = firestore.Client(
-            credentials=self.credentials, project=_GCP_PROJECT)
-        collection = settings.VOXEL_LABEL_FIRESTORE_COLLECTION
-        doc_ref = db.collection(collection).document(self.video_uuid)
-        doc_ref.set({
-            'cvat_task_id': self.task_id,
-            'updated_at': datetime.datetime.utcnow(),
-            'cvat_xml': cvat_xml,
-            # 'internal_json': internal_json,
-        }, merge=True)
+        return video_uuid.strip().split(".")[0]
 
     def _sync_cvat_xml_to_gcs(self, cvat_xml):
         """Pushes CVAT XML to Google Cloud Storage bucket."""
+        project = os.getenv("VOXEL_GCP_PROJECT", "missing_env_var_VOXEL_GCP_PROJECT")
         client = storage.Client(
-            credentials=self.credentials, project=_GCP_PROJECT)
+            credentials=self.credentials, project=project)
         bucket = client.bucket(settings.VOXEL_LABEL_BUCKET_NAME)
-        blob_name = '{}.xml'.format(self.video_uuid)
+        blob_name = f"{self.video_uuid}.xml"
         blob = bucket.blob(blob_name)
         blob.upload_from_string(cvat_xml, content_type='text/xml')
 
@@ -60,4 +40,3 @@ class SyncLabels(VoxelCommand):
         task_annotation.export(xml_stream, dump_as_cvat_interpolation)
         cvat_xml = xml_stream.getvalue()
         self._sync_cvat_xml_to_gcs(cvat_xml)
-        # self._sync_to_firestore(cvat_xml)
