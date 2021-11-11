f48bcf120f208848c224ae896edcf4d920d68fac
dvkruchinin <dvkruchinin@gmail.com>
2021-11-11 13:25:08 +0300

    Update test for checking issue 3810

--- tests/cypress/integration/actions_projects_models/case_103_project_export.js
+++ tests/cypress/integration/actions_projects_models/case_103_project_export.js
@@ -8,24 +8,32 @@ import { projectName, labelName } from '../../support/const_project';
 
 context('Export project dataset.', { browser: '!firefox' }, () => {
     const caseID = 103;
-    const taskName = `Task case ${caseID}`;
-    const attrName = `Attr for ${labelName}`;
-    const textDefaultValue = 'Some value for type Text';
+    const task = {
+        name: `Case ${caseID}`,
+        label: 'Tree',
+        attrName: 'Kind',
+        attrValue: 'Oak',
+        nameSecond: `Case ${caseID} second`,
+        labelSecond: 'Car',
+        attrNameSecons: 'Color',
+        attrValueSecond: 'Red',
+        multiAttrParams: false,
+        advancedConfigurationParams: false,
+        forProject: true,
+        attachToProject: false,
+    };
     const imagesCount = 1;
-    const imageFileName = `image_${taskName.replace(/\s+/g, '_').toLowerCase()}`;
+    const imageFileName = `image_${task.name.replace(' ', '_').toLowerCase()}`;
     const width = 800;
     const height = 800;
     const posX = 10;
     const posY = 10;
-    const color = 'white';
+    const color = 'gray';
     const archiveName = `${imageFileName}.zip`;
     const archivePath = `cypress/fixtures/${archiveName}`;
     const imagesFolder = `cypress/fixtures/${imageFileName}`;
     const directoryToArchive = imagesFolder;
-    const advancedConfigurationParams = false;
-    const forProject = true;
-    const attachToProject = false;
-    const multiAttrParams = false;
+
     let projectID = '';
 
     function getProjectID() {
@@ -37,21 +45,44 @@ context('Export project dataset.', { browser: '!firefox' }, () => {
             });
     }
 
+    function checkCounTasksInXML(projectParams, expectedCount) {
+        cy.exportProject(projectParams);
+        cy.waitForDownload();
+        cy.unpackZipArchive(`cypress/fixtures/${projectParams.archiveCustomeName}.zip`);
+        cy.readFile('cypress/fixtures/annotations.xml').should('exist').then((xml) => {
+            const tasks = Cypress.$(Cypress.$.parseXML(xml)).find('task').find('name');
+            expect(tasks.length).to.be.eq(expectedCount);
+        });
+    }
+
     before(() => {
         cy.imageGenerator(imagesFolder, imageFileName, width, height, color, posX, posY, labelName, imagesCount);
         cy.createZipArchive(directoryToArchive, archivePath);
         cy.openProject(projectName);
         getProjectID(projectName);
         cy.createAnnotationTask(
-            taskName,
-            labelName,
-            attrName,
-            textDefaultValue,
+            task.nameSecond,
+            task.labelSecond,
+            task.attrNameSecond,
+            task.attrValueSecond,
             archiveName,
-            multiAttrParams,
-            advancedConfigurationParams,
-            forProject,
-            attachToProject,
+            task.multiAttrParams,
+            task.advancedConfigurationParams,
+            task.forProject,
+            task.attachToProject,
+            projectName,
+        );
+        cy.openProject(projectName);
+        cy.createAnnotationTask(
+            task.name,
+            task.label,
+            task.attrName,
+            task.attrValue,
+            archiveName,
+            task.multiAttrParams,
+            task.advancedConfigurationParams,
+            task.forProject,
+            task.attachToProject,
             projectName,
         );
     });
@@ -95,9 +126,12 @@ context('Export project dataset.', { browser: '!firefox' }, () => {
                 dumpType: 'CVAT for images',
                 archiveCustomeName: 'export_project_annotation',
             };
-            cy.exportProject(exportAnnotationsRenameArchive);
-            cy.waitForDownload();
-            cy.unpackZipArchive(`cypress/fixtures/${exportAnnotationsRenameArchive.archiveCustomeName}.zip`);
+            // Check issue 3810
+            checkCounTasksInXML(exportAnnotationsRenameArchive, 2);
+            cy.openProject(projectName);
+            cy.deleteTask(task.nameSecond);
+            cy.goToProjectsList();
+            checkCounTasksInXML(exportAnnotationsRenameArchive, 1);
         });
     });
 });
