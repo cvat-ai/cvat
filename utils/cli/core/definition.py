4bf74fd7c688c8ba0634d0aa0943ba31b9659d73
Maya <maya17grd@gmail.com>
2021-11-24 11:07:12 +0300

    Update cli

--- utils/cli/core/definition.py
+++ utils/cli/core/definition.py
@@ -208,6 +208,13 @@ task_create_parser.add_argument(
     action='store_false',
     help='''set the option to use the cache (default: %(default)s)'''
 )
+task_create_parser.add_argument(
+    '--sorting-method',
+    default='DEFAULT',
+    choices=['DEFAULT', 'NATIVE', 'CUSTOM', 'RANDOM', 'REVERSED'],
+    help='''data soring method (default: %(default)s)'''
+)
+
 #######################################################################
 # Delete
 #######################################################################
