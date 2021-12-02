712433544505cf5838711a247a6cf9ec1f74b630
dvkruchinin <dvkruchinin@gmail.com>
2021-12-02 11:59:57 +0300

    Add dummy per CS. Writing new test.

--- tests/cypress/integration/actions_tasks3/case_109_dummy_cloud_storage.js
+++ tests/cypress/integration/actions_tasks3/case_109_dummy_cloud_storage.js
@@ -4,55 +4,45 @@
 
 /// <reference types="cypress" />
 
-const { cloudStoragesDummyData } = require('../../../../cvat-core/tests/mocks/dummy-data.mock');
+const {
+    cloudStoragesDummyDataGoogleStorage,
+    cloudStoragesDummyDataAzureContainer,
+    cloudStoragesDummyDataAzureBucket
+} = require('../../support/dummy-data');
 
 context('Cloud storage.', () => {
     const caseId = '109';
 
-    before(() => {
+    beforeEach(() => {
         cy.visit('auth/login');
         cy.login();
     });
 
-    describe(`Testing case "${caseId}"`, () => {
-        it('Create dummy cloud storages without previwe and status.', () => {
-            cy.intercept('GET', 'api/v1/cloudstorages?page_size=12&page=1', cloudStoragesDummyData).as('createCS');
+    // afterEach(() => {
+    //     cy.logout();
+    // });
 
+    describe(`Testing case "${caseId}"`, () => {
+        it('Create dummy Google cloud storage.', () => {
+            cy.intercept('GET', 'api/v1/cloudstorages?page_size=12&page=1', cloudStoragesDummyDataGoogleStorage).as('createCS');
             cy.contains('.cvat-header-button', 'Cloud Storages').should('be.visible').click();
             cy.wait('@createCS').its('response.statusCode').should('eq', 200);
 
-            cy.get('.cvat-cloud-storage-item-empty-preview').should('have.length', 3);
+            cy.get('.cvat-cloud-storage-item-empty-preview').should('have.length', 1);
 
             // Check and close notifications
-            cy.get('.cvat-notification-notice-fetch-cloud-storage-status-failed')
-                .should('have.length', 3)
-                .find('[data-icon="close"]')
-                .click({ multiple: true });
-            cy.get('.cvat-notification-notice-fetch-cloud-storage-preview-failed')
-                .should('have.length', 3)
-                .find('[data-icon="close"]')
-                .click({ multiple: true });
+            cy.get('.cvat-notification-notice-fetch-cloud-storage-status-failed').should('have.length', 1);
+            cy.closeNotification('.cvat-notification-notice-fetch-cloud-storage-status-failed');
+            cy.get('.cvat-notification-notice-fetch-cloud-storage-preview-failed').should('have.length', 1);
+            cy.closeNotification('.cvat-notification-notice-fetch-cloud-storage-preview-failed');
 
-            cy.get('.cvat-cloud-storage-item').should('have.length', 3).then((items) => {
+            cy.get('.cvat-cloud-storage-item').should('have.length', 1).then((items) => {
                 for (let i = 0; i < items.length; i++) {
                     expect(items[i].innerText).contain('Status: Error');
                 }
                 expect(items[0].innerText).contain('#3: Demo GCS');
                 expect(items[0].innerText).contain('Provider: GOOGLE_CLOUD_STORAGE');
-
-                expect(items[1].innerText).contain('#2: Demonstration container');
-                expect(items[1].innerText).contain('Provider: AZURE_CONTAINER');
-
-                expect(items[2].innerText).contain('#1: Demonstration bucket');
-                expect(items[2].innerText).contain('Provider: AWS_S3_BUCKET');
             });
-        });
-
-        it('Set status and preview for Google cloud storage.', () => {
-            cy.intercept('GET', 'api/v1/cloudstorages?page_size=12&page=1', cloudStoragesDummyData).as('createCS');
-
-            cy.contains('.cvat-header-button', 'Cloud Storages').should('be.visible').click();
-            cy.wait('@createCS').its('response.statusCode').should('eq', 200);
 
             cy.intercept('GET', 'api/v1/cloudstorages/3/status', 'NOT_FOUND').as('gcsStatus');
             cy.intercept(
