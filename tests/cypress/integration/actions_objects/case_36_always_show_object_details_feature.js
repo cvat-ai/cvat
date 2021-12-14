159b4681fb7b4a3c32d8260eff15a680a5037777
dvkruchinin <dvkruchinin@gmail.com>
2021-12-14 09:30:11 +0300

    Revert changes in the case 36

--- tests/cypress/integration/actions_objects/case_36_always_show_object_details_feature.js
+++ tests/cypress/integration/actions_objects/case_36_always_show_object_details_feature.js
@@ -6,12 +6,12 @@
 
 import { taskName, labelName } from '../../support/const';
 
-context('Always show object details feature. Text size/position.', () => {
+context('Always show object details feature', () => {
     const caseId = '36';
     const rectangleShape2Points = {
         points: 'By 2 Points',
         type: 'Shape',
-        labelName,
+        labelName: labelName,
         firstX: 400,
         firstY: 100,
         secondX: 500,
@@ -20,7 +20,7 @@ context('Always show object details feature. Text size/position.', () => {
     const polygonTrack = {
         reDraw: false,
         type: 'Track',
-        labelName,
+        labelName: labelName,
         pointsMap: [
             { x: 400, y: 300 },
             { x: 500, y: 300 },
@@ -74,63 +74,6 @@ context('Always show object details feature. Text size/position.', () => {
             });
             cy.closeSettings();
             checkShowDetails('be.visible', 'be.visible');
-        });
-
-        // Check text font size
-        it('Text font size.', () => {
-            cy.get('.cvat_canvas_text').should('have.attr', 'style', 'font-size: 14px;');
-            cy.openSettings();
-            cy.get('.cvat-workspace-settings-text-size')
-                .find('input')
-                .should('have.attr', 'value', '14')
-                .clear()
-                .type('16')
-                .should('have.attr', 'value', '16');
-            cy.closeSettings();
-            cy.get('.cvat_canvas_text').should('have.attr', 'style', 'font-size: 16px;');
-        });
-
-        it('Text position.', () => {
-            const rectTextCoords = [];
-            const rectTextCoordsCenter = [];
-            const polygonTextCoords = [];
-            const polygonTextCoordsCenter = [];
-
-            // rectangle
-            cy.get('.cvat_canvas_text').first().then((rectText) => {
-                rectTextCoords.push(rectText.attr('x'));
-                rectTextCoords.push(rectText.attr('y'));
-            });
-
-            // polygon
-            cy.get('.cvat_canvas_text').last().then((rectText) => {
-                polygonTextCoords.push(rectText.attr('x'));
-                polygonTextCoords.push(rectText.attr('y'));
-            });
-
-            cy.openSettings();
-            cy.get('.cvat-workspace-settings-text-position')
-                .find('[title="Auto"]')
-                .click();
-            cy.get('.ant-select-dropdown')
-                .not('.ant-select-dropdown-hidden')
-                .find('[title="Center"]')
-                .click();
-            cy.closeSettings();
-
-            // rectangle
-            cy.get('.cvat_canvas_text').first().then((rectText) => {
-                rectTextCoordsCenter.push(rectText.attr('x'));
-                rectTextCoordsCenter.push(rectText.attr('y'));
-                expect(rectTextCoordsCenter).not.deep.equal(rectTextCoords);
-            });
-
-            // polygon
-            cy.get('.cvat_canvas_text').last().then((rectText) => {
-                polygonTextCoordsCenter.push(rectText.attr('x'));
-                polygonTextCoordsCenter.push(rectText.attr('y'));
-                expect(polygonTextCoordsCenter).not.deep.equal(polygonTextCoords);
-            });
         });
     });
 });
