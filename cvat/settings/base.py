cc072a80f48d255ff88ea4d7bd11e771329e3d50
Nikita Manovich <nikita.manovich@intel.com>
2021-12-14 09:48:19 +0300

    Fix file upload in case of TUS protocol with content type application/offset+octet-stream

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -135,6 +135,12 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'cvat.apps.engine.parsers.TusUploadParser',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         'cvat.apps.iam.permissions.IsMemberInOrganization',
