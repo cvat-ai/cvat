d19a1de7778bdc887cfca882d25ea26cc5bfbb3d
kirill-sizov <kirill.sizov@intel.com>
2021-12-22 17:43:51 +0300

    Update users api tests

--- tests/rest_api/test_0001_users_api.py
+++ tests/rest_api/test_0001_users_api.py
@@ -2,34 +2,57 @@
 #
 # SPDX-License-Identifier: MIT
 
-import os
-from http import HTTPStatus
-import requests
 import json
-from .utils import config
+import os.path as osp
+from http import HTTPStatus
+
+import pytest
 from deepdiff import DeepDiff
 
+from .utils.config import ASSETS_DIR, get_response
+
+@pytest.fixture(scope='module')
+def users():
+    with open(osp.join(ASSETS_DIR, 'users.json')) as f:
+        return json.load(f)
 
 def test_non_admin_cannot_see_others():
     for username in ['dummy1', 'worker1', 'user1', 'business1']:
-        response = requests.get(config.get_api_url('users'), auth=(username, config.USER_PASS))
+        response = get_response(username, 'users')
+
         assert response.status_code == HTTPStatus.OK
         assert response.json()['count'] == 1
 
-def test_admin_can_see_all_others():
-    response = requests.get(config.get_api_url('users'), auth=('admin2', config.USER_PASS))
-    assert response.status_code == HTTPStatus.OK
-    with open(os.path.join(config.ASSETS_DIR, 'users.json')) as f:
-        data = json.load(f)
-        assert response.json()['count'] == data['count']
+def test_non_members_cannot_see_list_of_members():
+    non_member = 'user2'
+    response = get_response(non_member, 'users', org='org1')
 
-def test_everybody_can_see_self():
-    with open(os.path.join(config.ASSETS_DIR, 'users.json')) as f:
-        data = json.load(f)['results']
-        users = {user['username']:user for user in data}
+    assert response.status_code == HTTPStatus.FORBIDDEN
+
+def test_admin_can_see_all_others(users):
+    response = get_response('admin2', 'users')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()['count'] == users['count']
+
+def test_everybody_can_see_self(users):
+    users = {user['username']: user for user in users['results']}
 
     for username in ['dummy1', 'worker1', 'user1', 'business1', 'admin1']:
-        response = requests.get(config.get_api_url('users/self'), auth=(username, config.USER_PASS))
+        response = get_response(username, 'users/self')
+
         assert response.status_code == HTTPStatus.OK
         assert DeepDiff(users[username], response.json(), ignore_order=True,
             exclude_paths="root['last_login']") == {}
+
+def test_all_members_can_see_list_of_members(users):
+    available_fields = ['url', 'id', 'username', 'first_name', 'last_name']
+    org1_members = ['user1', 'worker1', 'worker2', 'business1']
+    users = [dict(filter(lambda row: row[0] in available_fields, user.items()))
+        for user in users['results'] if user['username'] in org1_members]
+
+    for username in org1_members:
+        response = get_response(username, 'users', org='org1')
+
+        assert response.status_code == HTTPStatus.OK
+        assert DeepDiff(users, response.json()['results'], ignore_order=True) == {}
--- tests/rest_api/utils/config.py
+++ tests/rest_api/utils/config.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 import os
+import requests
 
 ROOT_DIR = os.path.dirname(__file__)
 ASSETS_DIR = os.path.join(ROOT_DIR, '..', 'assets')
@@ -12,3 +13,6 @@ BASE_URL = 'http://localhost:8080/api/v1/'
 
 def get_api_url(endpoint, **kwargs):
     return BASE_URL + endpoint + '?' + '&'.join([f'{k}={v}' for k,v in kwargs.items()])
+
+def get_response(username, endpoint, **kwargs):
+    return requests.get(get_api_url(endpoint, **kwargs), auth=(username, USER_PASS))
