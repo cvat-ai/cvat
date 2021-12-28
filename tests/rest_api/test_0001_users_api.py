4fb8fecce94f26859d16c79848308fb1973c71d7
kirill-sizov <kirill.sizov@intel.com>
2021-12-28 15:26:39 +0300

    Update Users tests

--- tests/rest_api/test_0001_users_api.py
+++ tests/rest_api/test_0001_users_api.py
@@ -2,34 +2,55 @@
 #
 # SPDX-License-Identifier: MIT
 
-import os
-from http import HTTPStatus
-import requests
 import json
-from .utils import config
+import os.path as osp
+from http import HTTPStatus
+
+import pytest
 from deepdiff import DeepDiff
 
+from .utils.config import ASSETS_DIR, get_method
+
+@pytest.fixture(scope='module')
+def users():
+    with open(osp.join(ASSETS_DIR, 'users.json')) as f:
+        return json.load(f)
 
 def test_non_admin_cannot_see_others():
-    for username in ['dummy1', 'worker1', 'user1', 'business1']:
-        response = requests.get(config.get_api_url('users'), auth=(username, config.USER_PASS))
+    for username in ['business1', 'dummy1', 'user1', 'worker1']:
+        response = get_method(username, 'users')
+
         assert response.status_code == HTTPStatus.OK
         assert response.json()['count'] == 1
 
-def test_admin_can_see_all_others():
-    response = requests.get(config.get_api_url('users'), auth=('admin2', config.USER_PASS))
+def test_non_members_cannot_see_list_of_members():
+    response = get_method('user2', 'users', org='org1')
+    assert response.status_code == HTTPStatus.FORBIDDEN
+
+def test_admin_can_see_all_others(users):
+    response = get_method('admin2', 'users')
+
     assert response.status_code == HTTPStatus.OK
-    with open(os.path.join(config.ASSETS_DIR, 'users.json')) as f:
-        data = json.load(f)
-        assert response.json()['count'] == data['count']
+    assert response.json()['count'] == users['count']
 
-def test_everybody_can_see_self():
-    with open(os.path.join(config.ASSETS_DIR, 'users.json')) as f:
-        data = json.load(f)['results']
-        users = {user['username']:user for user in data}
+def test_everybody_can_see_self(users):
+    users = {user['username']: user for user in users['results']}
 
-    for username in ['dummy1', 'worker1', 'user1', 'business1', 'admin1']:
-        response = requests.get(config.get_api_url('users/self'), auth=(username, config.USER_PASS))
+    for username in ['admin1', 'business1', 'dummy1', 'user1', 'worker1']:
+        response = get_method(username, 'users/self')
+
         assert response.status_code == HTTPStatus.OK
         assert DeepDiff(users[username], response.json(), ignore_order=True,
             exclude_paths="root['last_login']") == {}
+
+def test_all_members_can_see_list_of_members(users):
+    available_fields = ['url', 'id', 'username', 'first_name', 'last_name']
+    org1_members = ['business1', 'user1', 'worker1', 'worker2']
+    users = [dict(filter(lambda row: row[0] in available_fields, user.items()))
+        for user in users['results'] if user['username'] in org1_members]
+
+    for username in org1_members:
+        response = get_method(username, 'users', org='org1')
+
+        assert response.status_code == HTTPStatus.OK
+        assert DeepDiff(users, response.json()['results'], ignore_order=True) == {}
