83ea83691f7334944319d146f5581ffdb769cc60
kirill-sizov <kirill.sizov@intel.com>
2021-12-28 15:26:09 +0300

    Add patch method

--- tests/rest_api/utils/config.py
+++ tests/rest_api/utils/config.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 import os
+import requests
 
 ROOT_DIR = os.path.dirname(__file__)
 ASSETS_DIR = os.path.join(ROOT_DIR, '..', 'assets')
@@ -12,3 +13,9 @@ BASE_URL = 'http://localhost:8080/api/v1/'
 
 def get_api_url(endpoint, **kwargs):
     return BASE_URL + endpoint + '?' + '&'.join([f'{k}={v}' for k,v in kwargs.items()])
+
+def get_method(username, endpoint, **kwargs):
+    return requests.get(get_api_url(endpoint, **kwargs), auth=(username, USER_PASS))
+
+def patch_method(username, endpoint, data, **kwargs):
+    return requests.patch(get_api_url(endpoint, **kwargs), data, auth=(username, USER_PASS))
