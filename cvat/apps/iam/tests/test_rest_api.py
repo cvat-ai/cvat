abcf3c64fd1ea7c8581c65b5182a29d83a78449e
karthik <gkraja@mindkosh.com>
2021-12-29 12:39:03 +0530

    Add test case for user registration when email confirmation is manadatory

--- cvat/apps/iam/tests/test_rest_api.py
+++ cvat/apps/iam/tests/test_rest_api.py
@@ -2,8 +2,18 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
-from allauth.account import app_settings as allauth_settings
 from django.test import override_settings
+from cvat.apps.iam.urls import urlpatterns as iam_url_patterns
+from django.urls import path, re_path
+from allauth.account.views import ConfirmEmailView, EmailVerificationSentView
+
+
+urlpatterns = iam_url_patterns + [
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(),
+            name='account_confirm_email'),
+    path('register/account-email-verification-sent', EmailVerificationSentView.as_view(),
+         name='account_email_verification_sent'),
+]
 
 
 class AccountTests(APITestCase):
@@ -22,16 +32,17 @@ class AccountTests(APITestCase):
                                          'username': 'test_username', 'email': 'test_email@test.com',
                                          'email_verification_required': False, 'key': user_token.key})
 
-    # @override_settings(ACCOUNT_AUTHENTICATION_METHOD='username', ACCOUNT_CONFIRM_EMAIL_ON_GET=True,
-    #                    ACCOUNT_EMAIL_REQUIRED=True, ACCOUNT_EMAIL_VERIFICATION='mandatory',
-    #                    EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend')
-    # def test_register_account_with_email_verification(self):
-    #     url = reverse('rest_register')
-    #     data = {'first_name': 'test_first', 'last_name': 'test_last', 'username': 'test_username',
-    #             'email': 'test_email@test.com', 'password1': '$Test357Test%', 'password2': '$Test357Test%',
-    #             'confirmations': []}
-    #     response = self.client.post(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(response.data, {'first_name': 'test_first', 'last_name': 'test_last',
-    #                                      'username': 'test_username', 'email': 'test_email@test.com',
-    #                                      'email_verification_required': True, 'key': None})
+    @override_settings(ACCOUNT_AUTHENTICATION_METHOD='username', ACCOUNT_CONFIRM_EMAIL_ON_GET=True,
+                       ACCOUNT_EMAIL_REQUIRED=True, ACCOUNT_EMAIL_VERIFICATION='mandatory',
+                       EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend',
+                       ACCOUNT_EMAIL_CONFIRMATION_HMAC=True, ROOT_URLCONF=__name__)
+    def test_register_account_with_email_verification(self):
+        url = reverse('rest_register')
+        data = {'first_name': 'test_first', 'last_name': 'test_last', 'username': 'test_username',
+                'email': 'test_email@test.com', 'password1': '$Test357Test%', 'password2': '$Test357Test%',
+                'confirmations': []}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {'first_name': 'test_first', 'last_name': 'test_last',
+                                         'username': 'test_username', 'email': 'test_email@test.com',
+                                         'email_verification_required': True, 'key': None})
