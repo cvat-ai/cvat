Enabled rotation, redesigned drawing method, fixed typos

cvat/apps/dataset_manager/formats/transformations.py
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: MIT
 
 import math
+import cv2
 import numpy as np
 from itertools import chain
 from pycocotools import mask as mask_utils
-from PIL import Image, ImageDraw
 
 from datumaro.components.extractor import ItemTransform
 import datumaro.components.annotation as datum_annotation
@@ -42,10 +42,11 @@ class EllipsesToMasks:
         cx, cy, rightX, topY = ellipse.points
         rx = rightX - cx
         ry = cy - topY
-        shape = (cx - rx, cy - ry, cx + rx, cy + ry)
-        mask = Image.new('1', (img_w, img_h))
-        drawer = ImageDraw.Draw(mask)
-        drawer.ellipse(shape, fill = 1)
-        rle = mask_utils.encode(np.asfortranarray(mask))
+        center = (round(cx), round(cy))
+        axis = (round(rx), round(ry))
+        angle = ellipse.rotation
+        mat = np.zeros((img_h, img_w), dtype=np.uint8)
+        cv2.ellipse(mat, center, axis, angle, 0, 360, 255, thickness=-1)
+        rle = mask_utils.encode(np.asfortranarray(mat))
         return datum_annotation.RleMask(rle=rle, label=ellipse.label, z_order=ellipse.z_order,
             attributes=ellipse.attributes, group=ellipse.group)