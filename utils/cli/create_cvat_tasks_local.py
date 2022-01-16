b2c221d1d97c7b86d990790959467bcd3532db60
Alexander Ivkin <a@ivkin.dev>
2022-01-16 11:22:42 -0500

    Extracted credentials

--- utils/cli/create_cvat_tasks_local.py
+++ utils/cli/create_cvat_tasks_local.py
@@ -5,7 +5,7 @@ from cli import run
 
 dir = sys.argv[1]
 project_id = f"{sys.argv[2]}"
-credentials = 'admin:admin'
+credentials = sys.argv[3] if len(sys.argv) > 3 else 'admin:admin'
 
 labels = [{"name": "frisbee", "color": "#f68e83", "attributes":[]}];
 for filename in sorted(os.listdir(dir)):
