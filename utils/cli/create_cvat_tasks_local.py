f38e21b84c8fe8636d5d5f5fbafaaa323535377e
Alexander Ivkin <a@ivkin.dev>
2022-01-16 11:19:39 -0500

    Extracted credentials

--- utils/cli/create_cvat_tasks_local.py
+++ utils/cli/create_cvat_tasks_local.py
@@ -5,8 +5,9 @@ from cli import run
 
 dir = sys.argv[1]
 project_id = f"{sys.argv[2]}"
+credentials = 'admin:admin'
 
 labels = [{"name": "frisbee", "color": "#f68e83", "attributes":[]}];
 for filename in sorted(os.listdir(dir)):
      filepath = os.path.join(dir, filename)
-     run(['--auth', 'vsevolod:Boston21', '--server-host', 'localhost', '--server-port', '8080', 'create', '--labels', json.dumps(labels), '--project_id', project_id, '--image_quality', '100', filename, 'local', filepath])
+     run(['--auth', credentials, '--server-host', 'localhost', '--server-port', '8080', 'create', '--labels', json.dumps(labels), '--project_id', project_id, '--image_quality', '100', filename, 'local', filepath])
