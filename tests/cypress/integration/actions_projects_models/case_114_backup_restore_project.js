9938cb8f74d7b1062f95d95f880643b8efc20f07
dvkruchinin <dmitryx.kruchinin@intel.com>
2022-01-20 12:26:57 +0300

    Activate check for 3d task

--- tests/cypress/integration/actions_projects_models/case_114_backup_restore_project.js
+++ tests/cypress/integration/actions_projects_models/case_114_backup_restore_project.js
@@ -111,8 +111,7 @@ context('Backup, restore a project.', { browser: '!firefox' }, () => {
     });
 });
 
-// FIXME: Activate after solving the issue 4105
-context.skip('Backup, restore a project with a 3D task.', { browser: '!firefox' }, () => {
+context('Backup, restore a project with a 3D task.', { browser: '!firefox' }, () => {
     const archiveName3d = '../../cypress/integration/canvas3d_functionality/assets/test_canvas3d.zip';
 
     const cuboidCreationParams = {
