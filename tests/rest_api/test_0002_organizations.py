6a00eb95ba37206ab33697a9fc285fc9765ecfe7
kirill.sizov <kirill.sizov@intel.com>
2022-01-24 12:05:03 +0300

    Update organizations tests

--- tests/rest_api/test_0002_organizations.py
+++ tests/rest_api/test_0002_organizations.py
@@ -6,7 +6,6 @@ import os
 from http import HTTPStatus
 import json
 import pytest
-from tests.rest_api.conftest import roles_by_org
 from .utils.config import get_method, ASSETS_DIR
 from deepdiff import DeepDiff
 
@@ -17,31 +16,31 @@ def compare_organizations(org_id, response):
     DeepDiff(org, response.json())
 
 class TestGetOrganizations:
-    _ORG = 1
+    _ORG = 2
 
-    def test_admin1_get_organization_id_1():
-        response = get_method('admin1', 'organizations/1')
+    def _test_can_see_organization(self, user, endpoint, **kwargs):
+        response = get_method(user, endpoint, **kwargs)
+
         assert response.status_code == HTTPStatus.OK
 
-    def test_user1_get_organization_id_1():
-        response = get_method('user1', 'organizations/1')
-        assert response.status_code == HTTPStatus.OK
+    def _test_cannot_see_organization(self, user, endpoint, **kwargs):
+        response = get_method(user, endpoint, **kwargs)
 
-    @pytest.mark.parametrize('privilege', 'role', 'is_allow', [
-        ('admin',    None,         True),
-        ('user',     None,         False),
-        ('business', None,         False),
-        ('worker',   None,         False),
-        (None,       'owner',      True),
-        (None,       'maintainer', True),
-        (None,       'worker',     True),
-        (None,       'supervisor', True),
+        assert response.status_code == HTTPStatus.FORBIDDEN
+
+    @pytest.mark.parametrize('privilege, role, org, is_allow', [
+        ('admin',    None,         None, True),
+        ('user',     None,         1,    False),
+        ('business', None,         1,    False),
+        ('worker',   None,         1,    False),
+        (None,       'owner',      2,    True),
+        (None,       'maintainer', 2,    True),
+        (None,       'worker',     2,    True),
+        (None,       'supervisor', 2,    True),
     ])
-    def test_can_see_get_spicific_organization(self, privilege, role, is_allow, find_user):
-        user = find_user(privilege=privilege, org=self._ORG, role=role)['username']
+    def test_can_see_specific_organization(self, privilege, role, org, is_allow, find_users):
+        user = find_users(privilege=privilege, role=role, org=org)[0]
         if is_allow:
-            self._test_can_see(user, f'organization/{self._ORG}')
+            self._test_can_see_organization(user['username'], f'organizations/{self._ORG}')
         else:
-            self._test_cannot_see(user, f'organization/{self._ORG}')
-
-
+            self._test_cannot_see_organization(user['username'], f'organizations/{self._ORG}')
