76cad65cb16db69d6da02280c5685ac4cbafd111
kirill-sizov <kirill.sizov@intel.com>
2022-01-28 13:56:48 +0300

    Test organizations: declare data for patch method as fixtures

--- tests/rest_api/test_0002_organizations.py
+++ tests/rest_api/test_0002_organizations.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 from http import HTTPStatus
+from urllib import request
 import pytest
 from .utils.config import get_method, patch_method
 from deepdiff import DeepDiff
@@ -37,6 +38,17 @@ class TestGetOrganizations:
 class TestPatchOrganizations:
     _ORG = 2
 
+    @pytest.fixture(scope='class')
+    def request_data(self):
+        return {'slug': 'new', 'name': 'new', 'description': 'new',
+            'contact': {'email': 'new@cvat.org'}}
+
+    @pytest.fixture(scope='class')
+    def expected_data(self, organizations, request_data):
+        data = organizations(self._ORG).copy()
+        data.update(request_data)
+        return data
+
     @pytest.mark.parametrize('privilege, role, is_member, is_allow', [
         ('admin',    None,         None,  True),
         ('user',     None,         False, False),
@@ -48,21 +60,17 @@ class TestPatchOrganizations:
         (None,       'supervisor', True,  False),
     ])
     def test_can_update_specific_organization(self, privilege, role, is_member,
-            is_allow, find_users, organizations):
+            is_allow, find_users, request_data, expected_data):
         exclude_org = None if is_member else self._ORG
         org = self._ORG if is_member else None
         user = find_users(privilege=privilege, role=role, org=org,
             exclude_org=exclude_org)[0]['username']
 
-        data = {'slug': 'new', 'name': 'new', 'description': 'new'}
-        expected_data = organizations(self._ORG).copy()
-        expected_data.update(data)
-        response = patch_method(user, f'organizations/{self._ORG}', data)
-
+        response = patch_method(user, f'organizations/{self._ORG}', request_data)
 
         if is_allow:
             assert response.status_code == HTTPStatus.OK
-            assert DeepDiff(expected_data,
-                response.json(), exclude_paths="root['updated_date']") == {}
+            assert DeepDiff(expected_data, response.json(),
+                exclude_paths="root['updated_date']") == {}
         else:
             assert response.status_code != HTTPStatus.OK
