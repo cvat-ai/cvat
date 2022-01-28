661156efdf1bd081d51f2510d2966ba92dcfaa19
kirill.sizov <kirill.sizov@intel.com>
2022-01-28 11:51:41 +0300

    Test organizations: finish tests for GET method

--- tests/rest_api/test_0002_organizations.py
+++ tests/rest_api/test_0002_organizations.py
@@ -5,19 +5,10 @@
 from http import HTTPStatus
 import pytest
 from .utils.config import get_method
+from deepdiff import DeepDiff
 
 class TestGetOrganizations:
     _ORG = 2
-
-    def _test_can_see_organization(self, user, endpoint, **kwargs):
-        response = get_method(user, endpoint, **kwargs)
-
-        assert response.status_code == HTTPStatus.OK
-
-    def _test_cannot_see_organization(self, user, endpoint, **kwargs):
-        response = get_method(user, endpoint, **kwargs)
-
-        assert response.status_code == HTTPStatus.NOT_FOUND
 
     @pytest.mark.parametrize('privilege, role, is_member, is_allow', [
         ('admin',    None,         None,  True),
@@ -29,12 +20,16 @@ class TestGetOrganizations:
         (None,       'worker',     True,  True),
         (None,       'supervisor', True,  True),
     ])
-    def test_can_see_specific_organization(self, privilege, role, is_member, is_allow, find_users):
+    def test_can_see_specific_organization(self, privilege, role, is_member,
+            is_allow, find_users, organizations):
         exclude_org = None if is_member else self._ORG
         org = self._ORG if is_member else None
         user = find_users(privilege=privilege, role=role, org=org,
-            exclude_org=exclude_org)[0]
+            exclude_org=exclude_org)[0]['username']
+
+        response = get_method(user, f'organizations/{self._ORG}')
         if is_allow:
-            self._test_can_see_organization(user['username'], f'organizations/{self._ORG}')
+            assert response.status_code == HTTPStatus.OK
+            assert DeepDiff(organizations(self._ORG), response.json()) == {}
         else:
-            self._test_cannot_see_organization(user['username'], f'organizations/{self._ORG}')
+            assert response.status_code == HTTPStatus.NOT_FOUND
