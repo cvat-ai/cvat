43eabf87cfe46ca3dfff2ccb604e65bbdf0a5578
kirill.sizov <kirill.sizov@intel.com>
2022-01-28 18:20:27 +0300

    Dump objects: prettify code

--- tests/rest_api/utils/dump_objects.py
+++ tests/rest_api/utils/dump_objects.py
@@ -1,14 +1,10 @@
-import os
-import requests
+import os.path as osp
+from config import get_method, ASSETS_DIR
 import json
-import config
 
-with requests.Session() as session:
-    session.auth = ('admin1', config.USER_PASS)
-
-    for obj in ['user', 'project', 'task', 'job', 'organization', 'membership',
-        'invitation']:
-        response = session.get(f'http://localhost:8080/api/v1/{obj}s?page_size=all')
-        with open(os.path.join(config.ASSETS_DIR, f'{obj}s.json'), 'w') as f:
-            json.dump(response.json(), f, indent=2, sort_keys=True)
+for obj in ['user', 'project', 'task', 'job', 'organization', 'membership',
+    'invitation']:
+    response = get_method('admin1', obj, page_size='all')
+    with open(osp.join(ASSETS_DIR, f'{obj}s.json'), 'w') as f:
+        json.dump(response.json(), f, indent=2, sort_keys=True)
 
