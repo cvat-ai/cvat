9bbf0deaae0d9f94c94484d5d5c7e2cbad2ec7b1
Maya <maya17grd@gmail.com>
2022-02-17 12:32:36 +0300

    Fix env

--- tests/rest_api/utils/config.py
+++ tests/rest_api/utils/config.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-import os
 import os.path as osp
 import requests
 
@@ -11,8 +10,6 @@ ASSETS_DIR = osp.abspath(osp.join(ROOT_DIR, '..', 'assets'))
 # Suppress the warning from Bandit about hardcoded passwords
 USER_PASS = '!Q@W#E$R' # nosec
 BASE_URL = 'http://localhost:8080/api/'
-
-os.environ['TESTING'] = 'True'
 
 def get_api_url(endpoint, **kwargs):
     return BASE_URL + endpoint + '?' + '&'.join([f'{k}={v}' for k,v in kwargs.items()])
