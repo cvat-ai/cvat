333819859d55779cc44c74a2c63078ba5d5a92ff
kwjung <jelsominaa@gmail.com>
2022-03-04 23:46:11 +0900

    fixed: Permission error occured when accessing the comments of a specific issue - #4416

--- cvat/apps/iam/permissions.py
+++ cvat/apps/iam/permissions.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Intel Corporation
+# Copyright (C) 2022 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 
@@ -959,7 +959,8 @@ class IssuePermission(OpenPolicyAgentPermission):
             'create': 'create@job',
             'destroy': 'delete',
             'partial_update': 'update',
-            'retrieve': 'view'
+            'retrieve': 'view',
+            'comments': 'view'
         }.get(view.action, None)]
 
     def get_resource(self):
