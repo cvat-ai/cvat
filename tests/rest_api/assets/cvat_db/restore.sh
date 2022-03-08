7ca1c799be5b1622d2a4fa989efeca62056c4e63
kirill-sizov <kirill.sizov@intel.com>
2022-03-08 19:21:47 +0300

    Restore by only one command

--- tests/rest_api/assets/cvat_db/restore.sh
+++ tests/rest_api/assets/cvat_db/restore.sh
@@ -1,7 +1,5 @@
-terminate="SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'cvat' AND pid <> pg_backend_pid();"
-drop="DROP DATABASE IF EXISTS $2;"
-create="CREATE DATABASE $2 WITH TEMPLATE $1;"
+restore="SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'cvat' AND pid <> pg_backend_pid();\
+DROP DATABASE IF EXISTS $2;\
+CREATE DATABASE $2 WITH TEMPLATE $1;"
 
-echo $terminate | psql -U root -d postgres
-echo $drop | psql -U root -d postgres
-echo $create | psql -U root -d postgres
+echo $restore | psql -U root -d postgres
