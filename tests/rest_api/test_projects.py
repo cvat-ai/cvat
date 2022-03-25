d8003753df8066f5bd67bffc2d61c5d19c3c88a2
Irina Kovaleva <irinax.kovaleva@intel.com>
2022-03-25 14:29:49 +0300

    [WIP] REST API accessibility tests

--- tests/rest_api/test_projects.py
+++ tests/rest_api/test_projects.py
@@ -12,14 +12,19 @@ class TestGetProjects:
     def _find_by_owner(self, projects, users, is_project_staff, is_org_member, is_owner, has_membership):
         for p in projects:
             for u in users:
-                if is_owner:
+                if is_owner and not has_membership:
                     if is_project_staff(u['id'], p['id']):
                         return u['username'], p['id']
-                elif has_membership:
-                    if is_org_member(u['id'], p['organization']):
-                        return u['username'], p['id']
-                else:
-                    if not is_project_staff(u['id'], p['id']):
+                if has_membership and not is_owner:
+                    if p['organization'] is not None:
+                        if is_org_member(u['id'], p['organization']):
+                            return u['username'], p['id']
+                if is_owner and has_membership:
+                    if is_project_staff(u['id'], p['id']):
+                        if is_org_member(u['id'], p['organization']):
+                            return u['username'], p['id']
+                if not is_owner and not has_membership:
+                    if not is_project_staff(u['id'], p['id']) and not is_org_member(u['id'], p['organization']):
                         return u['username'], p['id']
 
     def _test_response_200(self, username, project_id):
@@ -32,20 +37,16 @@ class TestGetProjects:
         response = get_method(username, f'projects/{project_id}')
         assert response.status_code == HTTPStatus.FORBIDDEN
 
-    # Admin can see any project even he has no ownerships for this project (GET /projects/{id}).
-    # Admin is a owner of the project, or a membership of the project organization. Or not
-    @pytest.mark.parametrize('is_owner, has_membership', [
-        (True, False), (False, True), (False, False)
-    ])
-    def _test_project_admin_accessibility(self, users, projects, find_users, is_project_staff, is_org_member, is_owner, has_membership,):
+    # [sandbox] Admin can see any project even he has no ownerships for this project (GET /projects/{id}).
+    def test_project_admin_accessibility(self, users, projects, find_users, is_project_staff, is_org_member):
         admins = find_users(privilege='admin')
 
-        admin_project = self._find_by_owner(projects, admins, is_project_staff, is_org_member, is_owner, has_membership)
+        admin_project = self._find_by_owner(projects, admins, is_project_staff, is_org_member, False, False)
         assert admin_project is not None
         self._test_response_200(*admin_project)
 
     # [sandbox] Project owner or project assignee can see project (GET /projects/{id}).
-    def _test_project_owner_accessibility(self, users, projects, find_users, is_project_staff):
+    def test_project_owner_accessibility(self, users, projects, find_users, is_project_staff):
 
         for p in projects:
             if p['owner'] is not None:
@@ -61,7 +62,7 @@ class TestGetProjects:
 
     # [sandbox] Non-admin user cannot see project if this user is not project owner or project assignee
     # [sandbox] (GET /projects/{id}).
-    def _test_user_cannot_see_project(self, users, projects, find_users, is_project_staff, is_org_member):
+    def test_user_cannot_see_project(self, users, projects, find_users, is_project_staff, is_org_member):
         non_admins = find_users(exclude_privilege='admin')
 
         user_not_in_project = self._find_by_owner(projects, non_admins, is_project_staff, is_org_member, False, False)
@@ -71,7 +72,7 @@ class TestGetProjects:
     # [organization] cannot see project if this member it’s not project owner
     # or project assignee (GET /projects/{id}).
     @pytest.mark.parametrize('role', ('supervisor', 'worker'))
-    def _test_if_supervisor_or_worker_cannot_see_project(self, users, projects, find_users, is_project_staff, is_org_member, role):
+    def test_if_supervisor_or_worker_cannot_see_project(self, users, projects, find_users, is_project_staff, is_org_member, role):
         non_admins = find_users(role=role, exclude_privilege='admin')
         assert non_admins is not None
 
@@ -82,7 +83,7 @@ class TestGetProjects:
     # [organization]  can see any project even he has not any ownerships for this project
     # or project assignee (GET /projects/{id}).
     @pytest.mark.parametrize('role', ('maintainer', 'owner'))
-    def _test_if_maintainer_or_owner_can_see_project(self, users, projects, find_users, is_project_staff,
+    def test_if_maintainer_or_owner_can_see_project(self, users, projects, find_users, is_project_staff,
                                                         is_org_member, role):
         non_admins = find_users(role=role, exclude_privilege='admin')
         assert non_admins is not None
