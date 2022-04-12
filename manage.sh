7ce8edacc28dea8b3b96ae2aa5db823a12efd0b1
farid <farid@retechlabs.com>
2022-04-13 00:18:23 +0600

    REB3-7385 Manage wrapper script for docker.

---
+++ manage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -z "$DOCKER_COMPOSE_FILE" ]; then DOCKER_COMPOSE_FILE="./rebotics/docker-compose.yml"; fi
+
+SERVICE_NAME=cvat
+CONTAINER_ID=$(docker-compose -f $DOCKER_COMPOSE_FILE ps -q $SERVICE_NAME)
+
+if [[ -z "$CONTAINER_ID" || -z "$(docker ps -q --no-trunc | grep "$CONTAINER_ID")" ]]; then
+  docker-compose -f $DOCKER_COMPOSE_FILE run --entrypoint "python" $SERVICE_NAME manage.py "$@"
+else
+  docker-compose -f $DOCKER_COMPOSE_FILE exec $SERVICE_NAME python manage.py "$@"
+fi
