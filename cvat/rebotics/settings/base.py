ef016edf3724980163c61d823d4dda0e90121ad9
farid <farid@retechlabs.com>
2022-04-14 16:31:14 +0600

    REB3-7385 Added custom storage backend from gallery.

--- cvat/rebotics/settings/base.py
+++ cvat/rebotics/settings/base.py
@@ -10,4 +10,5 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=3600',
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'cvat.rebotics.storage.CustomAWSMediaStorage'
