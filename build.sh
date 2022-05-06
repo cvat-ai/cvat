257200b309f0b467e6d6f94f7dc23d97cf1837ba
farid <farid@retechlabs.com>
2022-05-06 13:56:10 +0600

    REB3-7385 Moved opa to our versioning instead of copying its version.

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ if [ -z $VERSION ]; then
 fi
 
 OPA_VERSION=0.34.2-rootless  # fixed, rarely changes
-docker build -f rebotics/Dockerfile.opa --build-arg OPA_VERSION=${OPA_VERSION} -t retechlabs/rebotics-cvat-opa:${OPA_VERSION} .
+docker build -f rebotics/Dockerfile.opa --build-arg OPA_VERSION=${OPA_VERSION} -t retechlabs/rebotics-cvat-opa:${VERSION} .
 docker build -f rebotics/Dockerfile -t retechlabs/rebotics-cvat:${VERSION} .
