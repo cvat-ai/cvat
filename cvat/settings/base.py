48efa1d5af865bf844424ce656a9f5af9b196b33
farid <farid@retechlabs.com>
2022-05-17 13:43:05 +0600

    REB3-7385 Added localhost and 127.0.0.1 to default allowed hosts.

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -27,7 +27,8 @@ from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = str(Path(__file__).parents[2])
 
-ALLOWED_HOSTS = ['*cvat.rebotics.net', '*cvat.rebotics.cn'] + os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*cvat.rebotics.net', '*cvat.rebotics.cn', 'localhost', '127.0.0.1']\
+                + os.environ.get('ALLOWED_HOSTS', '').split(',')
 PUBLIC_DOMAIN_NAME = os.environ.get('PUBLIC_DOMAIN_NAME')
 if PUBLIC_DOMAIN_NAME:
     ALLOWED_HOSTS += [PUBLIC_DOMAIN_NAME]
