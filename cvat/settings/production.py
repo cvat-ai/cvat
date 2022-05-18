dfa53189c426baace63fc216555689d7db0ead46
farid <farid@retechlabs.com>
2022-05-18 14:45:19 +0600

    REB3-7385 Added SECURE_PROXY_SSL_HEADER as a possible fix.

--- cvat/settings/production.py
+++ cvat/settings/production.py
@@ -13,3 +13,12 @@ INSTALLED_APPS += [
 # Django-sendfile:
 # https://github.com/moggers87/django-sendfile2
 SENDFILE_BACKEND = 'django_sendfile.backends.xsendfile'
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# possible fix for admin csrf error.
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://*cvat.rebotics.net',
+#     'https://*cvat.rebotics.cn',
+# ]
