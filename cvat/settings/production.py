d22a974b21b51d3ca04bb8c3bc01c09f999ecee5
Malik Sulaimanov <amywoodehy@gmail.com>
2022-05-19 15:47:25 +0600

    Add initialization into production settings

--- cvat/settings/production.py
+++ cvat/settings/production.py
@@ -4,6 +4,28 @@
 
 from .base import *
 
+
+try:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn="https://acc5a0b8c5f14f379c6aaa5baba2dc76@o29828.ingest.sentry.io/6420408",
+        integrations=[DjangoIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+except Exception as exc:
+    print(exc)
+
+
 DEBUG = bool(int(os.getenv('DJANGO_DEBUG', 0)))
 
 INSTALLED_APPS += [
