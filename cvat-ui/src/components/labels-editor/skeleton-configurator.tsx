727c9cb2fd3569755141d22db0a1492332489891
Boris Sekachev <sekachev.bs@gmail.com>
2022-06-16 14:23:00 +0300

    Redesigned as ES6 component

--- cvat-ui/src/components/labels-editor/skeleton-configurator.tsx
+++ cvat-ui/src/components/labels-editor/skeleton-configurator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Row, Col } from 'antd/lib/grid';
 import Upload from 'antd/lib/upload';
 import Button from 'antd/lib/button';
@@ -10,52 +10,57 @@ import {
     EllipseIcon, PointIcon, PolygonIcon, RectangleIcon,
 } from 'icons';
 
-function SkeletonConfigurator(): JSX.Element {
-    const [activeTool, setActiveTool] = useState<'point' | 'join' | 'delete'>('point');
-    const [image, setImage] = useState<RcFile | null>(null);
-    const [imageSetupFlag, setImageSetupFlag] = useState<boolean>(false);
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const svgRef = useRef<SVGSVGElement>(null);
+interface State {
+    activeTool: 'point' | 'join' | 'delete';
+    image: RcFile | null;
+}
 
-    useEffect(() => {
-        if (canvasRef.current && svgRef.current) {
-            const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
-                const [canvasEntry] = entries;
-                (canvasEntry.target as HTMLCanvasElement).style.height = `${canvasEntry.target.clientWidth}px`;
-                (canvasEntry.target as HTMLCanvasElement).height = canvasEntry.target.clientWidth;
-                (canvasEntry.target as HTMLCanvasElement).width = canvasEntry.target.clientWidth;
-                (svgRef.current as SVGSVGElement).style.width = `${canvasEntry.target.clientWidth}px`;
-                (svgRef.current as SVGSVGElement).style.height = `${canvasEntry.target.clientWidth}px`;
-                setImageSetupFlag(false);
-            });
-            observer.observe(canvasRef.current);
-            return () => {
-                observer.unobserve(canvasRef.current as HTMLCanvasElement);
-                observer.disconnect();
-            };
+export default class SkeletonConfigurator extends React.PureComponent<{}, State> {
+    private canvasRef: React.RefObject<HTMLCanvasElement>;
+    private svgRef: React.RefObject<SVGSVGElement>;
+    private canvasResizeObserver: ResizeObserver;
+
+    public constructor(props: {}) {
+        super(props);
+        this.state = {
+            activeTool: 'point',
+            image: null,
+        };
+
+        this.canvasRef = React.createRef<HTMLCanvasElement>();
+        this.svgRef = React.createRef<SVGSVGElement>();
+        this.canvasResizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+            const [canvasEntry] = entries;
+            (canvasEntry.target as HTMLCanvasElement).style.height = `${canvasEntry.target.clientWidth}px`;
+            (canvasEntry.target as HTMLCanvasElement).height = canvasEntry.target.clientWidth;
+            (canvasEntry.target as HTMLCanvasElement).width = canvasEntry.target.clientWidth;
+            if (this.svgRef.current) {
+                (this.svgRef.current as SVGSVGElement).style.width = `${canvasEntry.target.clientWidth}px`;
+                (this.svgRef.current as SVGSVGElement).style.height = `${canvasEntry.target.clientWidth}px`;
+            }
+            this.setCanvasBackground();
+        });
+    }
+
+    public componentDidMount(): void {
+        const { canvasRef } = this;
+        if (canvasRef.current) {
+            this.canvasResizeObserver.observe(canvasRef.current);
         }
+    }
 
-        return () => {};
-    }, [canvasRef.current, svgRef.current]);
+    public componentDidUpdate(): void {
 
-    useEffect(() => {
-        function clickEventListener(): void {
+    }
 
-        }
+    public componentWillUnmount(): void {
+        this.canvasResizeObserver.disconnect();
+    }
 
-        if (svgRef.current) {
-            svgRef.current.addEventListener('click', clickEventListener);
-
-            return () => {
-                (svgRef.current as SVGSVGElement).removeEventListener('click', clickEventListener);
-            };
-        }
-
-        return () => {};
-    }, [svgRef.current]);
-
-    useEffect(() => {
-        if (image && canvasRef.current && !imageSetupFlag) {
+    private setCanvasBackground(): void {
+        const { canvasRef } = this;
+        const { image } = this.state;
+        if (image && canvasRef.current) {
             const url = URL.createObjectURL(image);
             const img = new Image();
             img.onload = () => {
@@ -78,148 +83,152 @@ function SkeletonConfigurator(): JSX.Element {
                                 img, (canvasWidth - width * dh) / 2, 0, width * dh, height * dh,
                             );
                         }
-                        setImageSetupFlag(true);
                     }
                 }
             };
             img.src = url;
         }
-    }, [image, canvasRef.current, imageSetupFlag]);
+    }
 
-    return (
-        <Row className='cvat-skeleton-configurator'>
-            <Col span={16}>
-                <canvas ref={canvasRef} className='cvat-skeleton-configurator-canvas' />
-                <svg width={100} height={100} ref={svgRef} className='cvat-skeleton-configurator-svg' />
-            </Col>
-            <Col span={7} offset={1}>
-                <div className='cvat-skeleton-configurator-image-dragger'>
-                    <Upload.Dragger
-                        showUploadList={false}
-                        beforeUpload={(file: RcFile) => {
-                            if (!file.type.startsWith('image/')) {
-                                notification.error({
-                                    message: `File must be an image. Got mime type: ${file.type}`,
-                                });
-                            }
-                            setImage(file);
-                            return false;
-                        }}
-                    >
-                        <p className='ant-upload-drag-icon'>
-                            <PictureOutlined />
-                        </p>
-                        <p className='ant-upload-text'>Click or drag an image to this area</p>
-                    </Upload.Dragger>
-                </div>
-                <Row justify='space-between' className='cvat-skeleton-configurator-shape-buttons'>
-                    <Col span={5}>
-                        <Button
-                            type={activeTool === 'point' ? 'dashed' : 'default'}
-                            className='cvat-skeleton-configurator-point-button'
-                            size='large'
-                            shape='round'
-                            icon={<Icon component={PointIcon} />}
-                            onClick={() => setActiveTool('point')}
-                        />
-                    </Col>
-                    <Col span={5}>
-                        <Button
-                            className='cvat-skeleton-configurator-rect-button'
-                            size='large'
-                            shape='round'
-                            disabled
-                            title='Not implemented'
-                            icon={<Icon component={RectangleIcon} />}
-                        />
-                    </Col>
-                    <Col span={5}>
-                        <Button
-                            className='cvat-skeleton-configurator-ellipse-button'
-                            size='large'
-                            shape='round'
-                            disabled
-                            title='Not implemented'
-                            icon={<Icon component={EllipseIcon} />}
-                        />
-                    </Col>
-                    <Col span={5}>
-                        <Button
-                            className='cvat-skeleton-configurator-polygon-button'
-                            size='large'
-                            shape='round'
-                            disabled
-                            title='Not implemented'
-                            icon={<Icon component={PolygonIcon} />}
-                        />
-                    </Col>
-                </Row>
-                <Row justify='space-between' className='cvat-skeleton-configurator-action-buttons'>
-                    <Button
-                        type={activeTool === 'join' ? 'dashed' : 'default'}
-                        onClick={() => setActiveTool('join')}
-                    >
-                        Join
-                    </Button>
-                    <Button
-                        type={activeTool === 'delete' ? 'dashed' : 'default'}
-                        onClick={() => setActiveTool('delete')}
-                        danger
-                    >
-                        Delete
-                    </Button>
-                </Row>
-                <Row justify='space-between' className='cvat-skeleton-configurator-svg-buttons'>
-                    <Button
-                        type='default'
-                        onClick={() => {
-                            if (svgRef.current) {
-                                const text = svgRef.current.innerHTML;
-                                const blob = new Blob([text], { type: 'image/svg+xml;charset=utf-8' });
-                                const url = URL.createObjectURL(blob);
-                                const anchor = window.document.getElementById('downloadAnchor');
-                                if (anchor) {
-                                    (anchor as HTMLAnchorElement).href = url;
-                                    (anchor as HTMLAnchorElement).click();
-                                    setTimeout(() => {
-                                        URL.revokeObjectURL(url);
-                                    });
-                                }
-                            }
-                        }}
-                    >
-                        Save
-                    </Button>
-                    <Upload
-                        showUploadList={false}
-                        beforeUpload={(file: RcFile) => {
-                            file.text().then((result) => {
-                                const tmpSvg = window.document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-                                // eslint-disable-next-line
-                                tmpSvg.innerHTML = result;
-                                let isSVG = true;
-                                for (let c = tmpSvg.childNodes, i = c.length; i--;) {
-                                    isSVG = isSVG && c[i].nodeType === 1;
-                                }
+    public render(): JSX.Element {
+        const { canvasRef, svgRef } = this;
+        const { activeTool } = this.state;
 
-                                if (isSVG && svgRef.current) {
-                                    // eslint-disable-next-line
-                                    svgRef.current.innerHTML = tmpSvg.innerHTML;
-                                } else {
+        return (
+            <Row className='cvat-skeleton-configurator'>
+                <Col span={16}>
+                    <canvas ref={canvasRef} className='cvat-skeleton-configurator-canvas' />
+                    <svg width={100} height={100} ref={svgRef} className='cvat-skeleton-configurator-svg' />
+                </Col>
+                <Col span={7} offset={1}>
+                    <div className='cvat-skeleton-configurator-image-dragger'>
+                        <Upload.Dragger
+                            showUploadList={false}
+                            beforeUpload={(file: RcFile) => {
+                                if (!file.type.startsWith('image/')) {
                                     notification.error({
-                                        message: 'Wrong skeleton structure',
+                                        message: `File must be an image. Got mime type: ${file.type}`,
                                     });
                                 }
-                            });
-                            return false;
-                        }}
-                    >
-                        <Button type='default'>Upload</Button>
-                    </Upload>
-                </Row>
-            </Col>
-        </Row>
-    );
-}
+                                this.setState({ image: file }, () => {
+                                    this.setCanvasBackground();
+                                });
+                                return false;
+                            }}
+                        >
+                            <p className='ant-upload-drag-icon'>
+                                <PictureOutlined />
+                            </p>
+                            <p className='ant-upload-text'>Click or drag an image to this area</p>
+                        </Upload.Dragger>
+                    </div>
+                    <Row justify='space-between' className='cvat-skeleton-configurator-shape-buttons'>
+                        <Col span={5}>
+                            <Button
+                                type={activeTool === 'point' ? 'dashed' : 'default'}
+                                className='cvat-skeleton-configurator-point-button'
+                                size='large'
+                                shape='round'
+                                icon={<Icon component={PointIcon} />}
+                                onClick={() => this.setState({ activeTool: 'point' })}
+                            />
+                        </Col>
+                        <Col span={5}>
+                            <Button
+                                className='cvat-skeleton-configurator-rect-button'
+                                size='large'
+                                shape='round'
+                                disabled
+                                title='Not implemented'
+                                icon={<Icon component={RectangleIcon} />}
+                            />
+                        </Col>
+                        <Col span={5}>
+                            <Button
+                                className='cvat-skeleton-configurator-ellipse-button'
+                                size='large'
+                                shape='round'
+                                disabled
+                                title='Not implemented'
+                                icon={<Icon component={EllipseIcon} />}
+                            />
+                        </Col>
+                        <Col span={5}>
+                            <Button
+                                className='cvat-skeleton-configurator-polygon-button'
+                                size='large'
+                                shape='round'
+                                disabled
+                                title='Not implemented'
+                                icon={<Icon component={PolygonIcon} />}
+                            />
+                        </Col>
+                    </Row>
+                    <Row justify='space-between' className='cvat-skeleton-configurator-action-buttons'>
+                        <Button
+                            type={activeTool === 'join' ? 'dashed' : 'default'}
+                            onClick={() => this.setState({ activeTool: 'join' })}
+                        >
+                            Join
+                        </Button>
+                        <Button
+                            type={activeTool === 'delete' ? 'dashed' : 'default'}
+                            onClick={() => this.setState({ activeTool: 'delete' })}
+                            danger
+                        >
+                            Delete
+                        </Button>
+                    </Row>
+                    <Row justify='space-between' className='cvat-skeleton-configurator-svg-buttons'>
+                        <Button
+                            type='default'
+                            onClick={() => {
+                                if (svgRef.current) {
+                                    const text = svgRef.current.innerHTML;
+                                    const blob = new Blob([text], { type: 'image/svg+xml;charset=utf-8' });
+                                    const url = URL.createObjectURL(blob);
+                                    const anchor = window.document.getElementById('downloadAnchor');
+                                    if (anchor) {
+                                        (anchor as HTMLAnchorElement).href = url;
+                                        (anchor as HTMLAnchorElement).click();
+                                        setTimeout(() => {
+                                            URL.revokeObjectURL(url);
+                                        });
+                                    }
+                                }
+                            }}
+                        >
+                            Save
+                        </Button>
+                        <Upload
+                            showUploadList={false}
+                            beforeUpload={(file: RcFile) => {
+                                file.text().then((result) => {
+                                    const tmpSvg = window.document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                                    // eslint-disable-next-line
+                                    tmpSvg.innerHTML = result;
+                                    let isSVG = true;
+                                    for (let c = tmpSvg.childNodes, i = c.length; i--;) {
+                                        isSVG = isSVG && c[i].nodeType === 1;
+                                    }
 
-export default React.memo(SkeletonConfigurator);
+                                    if (isSVG && svgRef.current) {
+                                        // eslint-disable-next-line
+                                        svgRef.current.innerHTML = tmpSvg.innerHTML;
+                                    } else {
+                                        notification.error({
+                                            message: 'Wrong skeleton structure',
+                                        });
+                                    }
+                                });
+                                return false;
+                            }}
+                        >
+                            <Button type='default'>Upload</Button>
+                        </Upload>
+                    </Row>
+                </Col>
+            </Row>
+        );
+    }
+}
