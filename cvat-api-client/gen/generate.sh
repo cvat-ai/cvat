7248a7f1de5b59a9837e0aa3d3203cf888e3ec02
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2022-06-24 19:00:32 +0300

    add generator config

---
+++ cvat-api-client/gen/generate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VERSION="2.0-alpha"
+LIB_NAME="cvat_api_client"
+DST_DIR="."
+TEMPLATE_DIR="gen"
+
+# Pass template dir here
+# https://github.com/OpenAPITools/openapi-generator/issues/8420
+docker run --rm -v $PWD:/local openapitools/openapi-generator-cli generate \
+    -t /local/${TEMPLATE_DIR}/templates/openapi-generator/ \
+    -i /local/schema/schema.yml \
+    --config /local/${TEMPLATE_DIR}/generator-config.yml \
+    -g python \
+    -o /local/${DST_DIR}/
+sudo chown -R "$(id -u)":"$(id -g)" ${DST_DIR}/
+
+sed -e "s|{{packageVersion}}|$\{VERSION\}|g" > ${DST_DIR}/${LIB_NAME}/version.py
+cp -r ${TEMPLATE_DIR}/templates/requirements/ ${DST_DIR}/
