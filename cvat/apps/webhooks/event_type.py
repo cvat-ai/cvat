75ad895c3e2530d2307b5b6e81e8a8027a2808c3
kirill-sizov <sizow.k.d@gmail.com>
2022-08-02 18:16:49 +0300

    extend event type choice

--- cvat/apps/webhooks/event_type.py
+++ cvat/apps/webhooks/event_type.py
@@ -6,6 +6,10 @@ class EventTypeChoice(str, Enum):
     TASK_UPDATED = "task_updated"
     TASK_DELETED = "task_deleted"
 
+    JOB_CREATED = "job_created"
+    JOB_UPDATED = "job_updated"
+    JOB_DELETED = "job_deleted"
+
     @classmethod
     def choices(cls):
         return [(e.value, e.value) for e in cls]
