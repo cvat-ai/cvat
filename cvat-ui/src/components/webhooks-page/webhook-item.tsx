6013825c12ef6bcfad47e0c86cb13287fbb8fe36
Boris <sekachev.bs@gmail.com>
2022-08-12 14:58:33 +0300

    Implemented deleting

--- cvat-ui/src/components/webhooks-page/webhook-item.tsx
+++ cvat-ui/src/components/webhooks-page/webhook-item.tsx
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 import './styles.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import moment from 'moment';
 import { Col, Row } from 'antd/lib/grid';
@@ -14,12 +14,14 @@ import Text from 'antd/lib/typography/Text';
 import { MoreOutlined } from '@ant-design/icons';
 
 import { groupEvents } from 'components/setup-webhook-pages/setup-webhook-content';
+import { Modal } from 'antd';
 
 export interface WebhookItemProps {
     webhookInstance: any;
 }
 
 function WebhookItem(props: WebhookItemProps): JSX.Element | null {
+    const [isRemoved, setIsRemoved] = useState<boolean>(false);
     const history = useHistory();
     const { webhookInstance } = props;
     const {
@@ -32,7 +34,7 @@ function WebhookItem(props: WebhookItemProps): JSX.Element | null {
 
     const statusClassName = 'cvat-webhook-status-unavailable';
     return (
-        <Row className='cvat-webhooks-list-item'>
+        <Row className='cvat-webhooks-list-item' style={isRemoved ? { opacity: 0.5, pointerEvents: 'none' } : {}}>
             <Col>
                 <svg height='32' width='32' className={statusClassName}>
                     <circle cx='16' cy='11' r='4' strokeWidth='0' />
@@ -78,7 +80,7 @@ function WebhookItem(props: WebhookItemProps): JSX.Element | null {
                     <Col>
                         <Dropdown overlay={() => (
                             <Menu>
-                                <Menu.Item key='qwerq'>
+                                <Menu.Item>
                                     <a
                                         href={`/webhooks/${id}`}
                                         onClick={(e: React.MouseEvent) => {
@@ -90,7 +92,19 @@ function WebhookItem(props: WebhookItemProps): JSX.Element | null {
                                         Edit
                                     </a>
                                 </Menu.Item>
-                                <Menu.Item key='wqwe'>
+                                <Menu.Item
+                                    onClick={() => {
+                                        Modal.confirm({
+                                            title: 'Are you sure you want to remove the hook?',
+                                            content: 'It will stop notificating the specified URL about listed events',
+                                            onOk: () => {
+                                                webhookInstance.delete().then(() => {
+                                                    setIsRemoved(true);
+                                                });
+                                            },
+                                        });
+                                    }}
+                                >
                                     Delete
                                 </Menu.Item>
                             </Menu>
