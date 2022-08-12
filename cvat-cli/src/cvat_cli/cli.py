5aaa32361e61a2a3290de353d11c0c5c5e9d0d79
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2022-08-12 18:23:27 +0300

    Fix linter error

--- cvat-cli/src/cvat_cli/cli.py
+++ cvat-cli/src/cvat_cli/cli.py
@@ -10,8 +10,8 @@ from typing import Dict, List, Sequence, Tuple
 
 import tqdm
 from cvat_sdk import Client, models
-from cvat_sdk.core.types import ResourceType
 from cvat_sdk.core.helpers import TqdmProgressReporter
+from cvat_sdk.core.types import ResourceType
 
 
 class CLI:
