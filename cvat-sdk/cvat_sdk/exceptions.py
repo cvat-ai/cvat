1b663281fe43fd8863dc47530384210b754c3231
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2022-08-12 17:35:04 +0300

    fix linter error

--- cvat-sdk/cvat_sdk/exceptions.py
+++ cvat-sdk/cvat_sdk/exceptions.py
@@ -3,9 +3,11 @@
 # SPDX-License-Identifier: MIT
 
 # pylint: disable=unused-import
-from cvat_sdk.api_client.exceptions import OpenApiException
-from cvat_sdk.api_client.exceptions import ApiAttributeError
-from cvat_sdk.api_client.exceptions import ApiTypeError
-from cvat_sdk.api_client.exceptions import ApiValueError
-from cvat_sdk.api_client.exceptions import ApiKeyError
-from cvat_sdk.api_client.exceptions import ApiException
+from cvat_sdk.api_client.exceptions import (
+    ApiAttributeError,
+    ApiException,
+    ApiKeyError,
+    ApiTypeError,
+    ApiValueError,
+    OpenApiException,
+)
