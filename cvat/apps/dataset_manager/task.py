bec3543abc7edbd3dbf113304c2224ec1daf84d0
yasakova-anastasia <yasakova_anastasiya@mail.ru>
2022-08-31 12:45:33 +0300

    Removed useless changes

--- cvat/apps/dataset_manager/task.py
+++ cvat/apps/dataset_manager/task.py
@@ -426,21 +426,15 @@ class JobAnnotation:
         )
 
         shapes = {}
-        elements = {}
         for db_shape in db_shapes:
             self._extend_attributes(db_shape.labeledshapeattributeval_set,
                 self.db_attributes[db_shape.label_id]["all"].values())
-            db_shape['elements'] = []
 
+            db_shape.elements = []
             if db_shape.parent is None:
                 shapes[db_shape.id] = db_shape
             else:
-                if db_shape.parent not in elements:
-                    elements[db_shape.parent] = []
-                elements[db_shape.parent].append(db_shape)
-
-        for shape_id, shape_elements in elements.items():
-            shapes[shape_id].elements = shape_elements
+                shapes[db_shape.parent].elements.append(db_shape)
 
         serializer = serializers.LabeledShapeSerializer(list(shapes.values()), many=True)
         self.ir_data.shapes = serializer.data
@@ -499,7 +493,6 @@ class JobAnnotation:
         )
 
         tracks = {}
-        elements = {}
         for db_track in db_tracks:
             db_track["trackedshape_set"] = _merge_table_rows(db_track["trackedshape_set"], {
                 'trackedshapeattributeval_set': [
@@ -524,17 +517,12 @@ class JobAnnotation:
                 # by previous shape attribute values (not default values)
                 self._extend_attributes(db_shape["trackedshapeattributeval_set"], default_attribute_values)
                 default_attribute_values = db_shape["trackedshapeattributeval_set"]
-            db_track['elements'] = []
 
+            db_track.elements = []
             if db_track.parent is None:
                 tracks[db_track.id] = db_track
             else:
-                if db_track.parent not in elements:
-                    elements[db_track.parent] = []
-                elements[db_track.parent].append(db_track)
-
-        for track_id, track_elements in elements.items():
-            tracks[track_id].elements = track_elements
+                tracks[db_track.parent].elements.append(db_track)
 
         serializer = serializers.LabeledTrackSerializer(list(tracks.values()), many=True)
         self.ir_data.tracks = serializer.data
