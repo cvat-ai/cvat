a82419671822e8fd9132bdbacb1f8f9dbe2dccdb
Rodrigo Berriel <rodrigo.berriel@gmail.com>
2022-09-05 11:08:56 -0300

    Add support for Oracle OCI Buckets (#4876)

--- cvat/apps/engine/serializers.py
+++ cvat/apps/engine/serializers.py
@@ -1150,8 +1150,8 @@ class CloudStorageReadSerializer(serializers.ModelSerializer):
 class CloudStorageWriteSerializer(serializers.ModelSerializer):
     owner = BasicUserSerializer(required=False)
     session_token = serializers.CharField(max_length=440, allow_blank=True, required=False)
-    key = serializers.CharField(max_length=20, allow_blank=True, required=False)
-    secret_key = serializers.CharField(max_length=40, allow_blank=True, required=False)
+    key = serializers.CharField(max_length=40, allow_blank=True, required=False)
+    secret_key = serializers.CharField(max_length=44, allow_blank=True, required=False)
     key_file = serializers.FileField(required=False)
     account_name = serializers.CharField(max_length=24, allow_blank=True, required=False)
     manifests = ManifestSerializer(many=True, default=[])
