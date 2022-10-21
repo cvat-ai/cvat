2311b10b800a5c564af3af547472541e9b9b7cde
Anastasia Yasakova <yasakova_anastasiya@mail.ru>
2022-10-21 12:52:36 +0300

    Fix an issue with Imagenet dataset import (#4861)

--- tests/python/rest_api/test_projects.py
+++ tests/python/rest_api/test_projects.py
@@ -458,6 +458,23 @@ class TestImportExportDatasetProject:
             )
             assert response.status == HTTPStatus.ACCEPTED
 
+    def test_can_import_export_dataset_with_imagenet_format(self):
+        # https://github.com/opencv/cvat/issues/4850
+        username = "admin1"
+        format_name = "ImageNet 1.0"
+        project_id = 4
+
+        response = self._test_export_project(username, project_id, format_name)
+
+        tmp_file = io.BytesIO(response.data)
+        tmp_file.name = "dataset.zip"
+
+        import_data = {
+            "dataset_file": tmp_file,
+        }
+
+        self._test_import_project(username, project_id, format_name, import_data)
+
 
 @pytest.mark.usefixtures("restore_db_per_function")
 class TestPatchProjectLabel:
