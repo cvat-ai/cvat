88ea69dc20bfdc830abb049ea3d7f560908a87b4
Boris Sekachev <sekachev.bs@gmail.com>
2022-10-24 11:33:34 -0700

    Fixed import

--- cvat-core/src/api-implementation.ts
+++ cvat-core/src/api-implementation.ts
@@ -3,7 +3,7 @@
 //
 // SPDX-License-Identifier: MIT
 
-const config = require('./config');
+const config = require('./config').default;
 
 (() => {
     const PluginRegistry = require('./plugins').default;
