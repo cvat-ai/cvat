86241a56a4af74dcc4ea99299ebb72e0bb16f002
inpv <inpv@protonmail.ch>
2022-10-31 22:21:54 +0300

    - Refactor code for the nightly cvat-sdk build's models - Add admin user/pass discovery for api_client config params

--- tests/python/rest_api/test_load_register.py
+++ tests/python/rest_api/test_load_register.py
@@ -9,18 +9,24 @@ from locust.stats import stats_printer, stats_history
 
 from shared.utils.config import BASE_URL, USER_PASS
 from cvat_sdk.api_client import ApiClient, Configuration, exceptions
-from cvat_sdk.api_client.models import RestrictedRegisterRequest
+from cvat_sdk.api_client.models import RegisterSerializerExRequest
 from pprint import pprint
+
+
+@pytest.fixture(scope='function', autouse=True)
+def get_admin_user(admin_user: str):
+    # getting the admin user from session fixture
+    return admin_user
 
 
 class PerformTask(locust.SequentialTaskSet):
 
     @locust.task
-    @pytest.mark.usefixtures("restore_db_per_class")
     def perform_random_user_register_task(self):
-        # Initializing Faker for name generation
+        # Initializing Faker for name/email generation
         fake = Faker()
         username = fake.first_name()
+        email = fake.ascii_email()
 
         # Setting a random passwd
         password_length = random.randrange(13, 24)
@@ -28,53 +34,56 @@ class PerformTask(locust.SequentialTaskSet):
 
         # Setting the config for client auth
         config = Configuration(host=LoadUser.host,
-                               username="YOUR_USERNAME",
-                               password="YOUR_PASSWORD")
+                               username=get_admin_user(),
+                               password=USER_PASS)
 
         # Launching the client with randomized data
         with ApiClient(configuration=config) as api_client:
             try:
                 # forming the request body
-                register_request = RestrictedRegisterRequest(username=username,
-                                                             password1=passwd,
-                                                             password2=passwd
-                                                             )
-                # registering the request
+                register_request = RegisterSerializerExRequest(username=username,
+                                                               password1=passwd,
+                                                               password2=passwd,
+                                                               email=email
+                                                               )
+                # sending the request
                 (data, response) = api_client.auth_api.create_register(register_request)
-                pprint(data.username)
+                pprint(data)
             except exceptions.ApiException as e:
                 print("Exception when calling AuthApi.create_register: %s\n" % e)
 
 
 class LoadUser(locust.HttpUser):
 
-    tasks = [PerformTask]  # a list of tasks to perform
+    tasks = [PerformTask.perform_random_user_register_task]  # a list of tasks to perform
     wait_time = locust.between(1, 3)  # how long to wait before a single task is performed
     host = BASE_URL
 
 
-def test_load_register():
-    # setup Environment and Runner
-    env = Environment(user_classes=[LoadUser])
-    env.create_local_runner()
+class TestLoadRegister:
+    @staticmethod
+    def test_load_register():
+        # setup Environment and Runner
+        env = Environment(user_classes=[LoadUser])
+        env.create_local_runner()
 
-    # start a greenlet that periodically outputs the current stats
-    gevent.spawn(stats_printer(env.stats))
+        # start a greenlet that periodically outputs the current stats
+        gevent.spawn(stats_printer(env.stats))
 
-    # start a greenlet that save current stats to history
-    gevent.spawn(stats_history, env.runner)
+        # start a greenlet that save current stats to history
+        gevent.spawn(stats_history, env.runner)
 
-    # start the test with params
-    env.runner.start(5000, spawn_rate=20)
+        # start the test with params
+        env.runner.start(5000, spawn_rate=20)
 
-    # in 10 seconds stop the runner
-    gevent.spawn_later(10, lambda: env.runner.quit())
+        # in 10 seconds stop the runner
+        gevent.spawn_later(10, lambda: env.runner.quit())
 
-    # wait for the greenlets
-    env.runner.greenlet.join()
+        # wait for the greenlets
+        env.runner.greenlet.join()
 
-    assert env.stats.total.avg_response_time < 5  # testing average response time
-    assert env.stats.total.num_failures == 0  # testing for 0 failures
+        assert env.stats.total.avg_response_time < 5  # testing average response time
+        assert env.stats.total.num_failures == 0  # testing for 0 failures
 
-    # stop the web server for good measures
-    env.runner.quit()
+        # stop the web server for good measures
+        env.runner.quit()
