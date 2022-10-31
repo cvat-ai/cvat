a139600fd74181afdba7f884f84670a53e6ce010
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2022-10-31 19:48:12 +0600

    Update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -13,28 +13,45 @@ function makePattern(extension) {
 module.exports = (stagedFiles) => {
     const eslintExtensions = ['ts', 'tsx', 'js'].map(makePattern);
     const scssExtensions = ['scss'].map(makePattern);
+    const pythonExtensions = ['py'].map(makePattern);
     const eslintFiles = micromatch(stagedFiles, eslintExtensions);
     const scssFiles = micromatch(stagedFiles, scssExtensions);
+    const pythonFiles = micromatch(stagedFiles, pythonExtensions);
 
-    const tests = containsInPath('/tests/cypress', eslintFiles);
+    const uiTests = containsInPath('/tests/cypress', eslintFiles);
     const cvatData = containsInPath('/cvat-data/', eslintFiles);
     const cvatCore = containsInPath('/cvat-core/src', eslintFiles);
     const cvatCanvas = containsInPath('/cvat-canvas/', eslintFiles);
     const cvatCanvas3d = containsInPath('/cvat-canvas3d/', eslintFiles);
     const cvatUI = containsInPath('/cvat-ui/', eslintFiles);
+    const cvatSdk = containsInPath('/cvat-sdk', pythonFiles);
+    const cvatCli = containsInPath('/cvat-cli', pythonFiles);
+    const pythonTests = containsInPath('/tests/python', pythonFiles);
 
-    const mapping = {};
+    const tasks = []; // [command, file list] pairs
+    tasks.push(['npx stylelint --fix ', scssFiles.join(' ')]);
+    tasks.push(['yarn run precommit:cvat-tests -- ', uiTests.join(' ')]);
+    tasks.push(['yarn run precommit:cvat-ui -- ', cvatUI.join(' ')]);
+    tasks.push(['yarn run precommit:cvat-data -- ', cvatData.join(' ')]);
+    tasks.push(['yarn run precommit:cvat-core -- ', cvatCore.join(' ')]);
+    tasks.push(['yarn run precommit:cvat-canvas -- ', cvatCanvas.join(' ')]);
+    tasks.push(['yarn run precommit:cvat-canvas3d -- ', cvatCanvas3d.join(' ')]);
+
+    // If different components use different Black/isort configs,
+    // we need to run Black/isort for each python component group separately.
+    // Otherwise, they all will use the same config. This will mess up
+    // the current package detection for different modules, leading
+    // to incorrect formatting of module imports.
+    tasks.push(['black', cvatSdk.join(' ')]);
+    tasks.push(['black', cvatCli.join(' ')]);
+    tasks.push(['black', pythonTests.join(' ')]);
+    tasks.push(['isort', cvatSdk.join(' ')]);
+    tasks.push(['isort', cvatCli.join(' ')]);
+    tasks.push(['isort', pythonTests.join(' ')]);
+    tasks.push(['pylint', pythonFiles.join(' ')]);
+
     const commands = [];
-    mapping['npx stylelint --fix '] = scssFiles.join(' ');
-    mapping['yarn run precommit:cvat-tests -- '] = tests.join(' ');
-    mapping['yarn run precommit:cvat-ui -- '] = cvatUI.join(' ');
-    mapping['yarn run precommit:cvat-data -- '] = cvatData.join(' ');
-    mapping['yarn run precommit:cvat-core -- '] = cvatCore.join(' ');
-    mapping['yarn run precommit:cvat-canvas -- '] = cvatCanvas.join(' ');
-    mapping['yarn run precommit:cvat-canvas3d -- '] = cvatCanvas3d.join(' ');
-
-    for (const command of Object.keys(mapping)) {
-        const files = mapping[command];
+    for (const [command, files] of tasks) {
         if (files.length) {
             commands.push(`${command} ${files}`);
         }
