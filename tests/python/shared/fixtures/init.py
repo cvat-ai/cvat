ecf5061713352d0ef94e7eaa201863bdb771953a
kirill-sizov <sizow.k.d@gmail.com>
2022-11-01 12:11:05 +0200

    Fix CVAT_VERSION for CI

--- tests/python/shared/fixtures/init.py
+++ tests/python/shared/fixtures/init.py
@@ -218,7 +218,7 @@ def start_services(rebuild=False):
         )
 
     _run(
-        f"docker-compose -p {PREFIX} "
+        f"CVAT_VERSION=dev docker-compose -p {PREFIX} "
         + "--env-file "
         + osp.join(CVAT_ROOT_DIR, "tests", "python", "webhook_receiver", ".env")
         + f" -f {' -f '.join(DC_FILES)} up -d "
@@ -264,7 +264,7 @@ def services(request):
 
         if stop:
             _run(
-                f"docker-compose -p {PREFIX} "
+                f"CVAT_VERSION=dev docker-compose -p {PREFIX} "
                 + "--env-file "
                 + osp.join(CVAT_ROOT_DIR, "tests", "python", "webhook_receiver", ".env")
                 + f" -f {' -f '.join(DC_FILES)} down -v",
