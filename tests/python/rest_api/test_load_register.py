60b9bccda71d6df497777f711e5ff0d7b9c2b55b
inpv <inpv@protonmail.ch>
2022-11-01 20:46:02 +0300

    - Recreate fixture data to get an admin user (fixtures unsupported by locust) - Add more registration params

--- tests/python/rest_api/test_load_register.py
+++ tests/python/rest_api/test_load_register.py
@@ -1,31 +1,81 @@
-import gevent
-import locust
-import pytest
+import os
 import random
 import secrets
 from faker import Faker
-from locust.env import Environment
-from locust.stats import stats_printer, stats_history
-
-from shared.utils.config import BASE_URL, USER_PASS
-from cvat_sdk.api_client import ApiClient, Configuration, exceptions
-from cvat_sdk.api_client.models import RegisterSerializerExRequest
 from pprint import pprint
 
+import json
+import gevent
+import locust
+from locust.env import Environment
+from locust.stats import stats_printer, stats_history
+from cvat_sdk.api_client import ApiClient, Configuration, exceptions
+from cvat_sdk.api_client.models import RegisterSerializerExRequest
 
-@pytest.fixture(scope='function', autouse=True)
-def get_admin_user(admin_user: str):
-    # getting the admin user from session fixture
-    return admin_user
+from shared.utils.config import BASE_URL, USER_PASS, ASSETS_DIR
+
+
+class AdminUserNotFoundInDbException(Exception):
+    def __init__(self):
+        print("Can't find any admin user in the test DB")
+
+
+class Container:
+    """
+    We have to recreate the fixtures' data generation,
+    since locust doesn't support using pytest tests as tasks yet
+    """
+    def __init__(self, data, key="id"):
+        self.raw_data = data
+        self.map_data = {obj[key]: obj for obj in data}
+
+    @property
+    def raw(self):
+        return self.raw_data
+
+    @property
+    def map(self):
+        return self.map_data
+
+    def __iter__(self):
+        return iter(self.raw_data)
+
+    def __len__(self):
+        return len(self.raw_data)
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return self.raw_data[key]
+        return self.map_data[key]
+
+
+class GetUser:
+    """
+    We have to recreate the fixtures' data generation,
+    since locust doesn't support using pytest tests as tasks yet
+    """
+    @staticmethod
+    def get_admin_user():
+        # getting the user from local db
+        with open(os.path.join(ASSETS_DIR, "users.json")) as f:
+            container = Container(json.load(f)["results"])
+
+            for user in container:
+                if user["is_superuser"] and user["is_active"]:  # getting the admin user
+                    return user["username"]
+
+            raise AdminUserNotFoundInDbException()
 
 
 class PerformTask(locust.SequentialTaskSet):
 
     @locust.task
     def perform_random_user_register_task(self):
-        # Initializing Faker for name/email generation
+        # Initializing Faker for data generation
         fake = Faker()
-        username = fake.first_name()
+        username = fake.first_name()  # needs to add 'username already exists' error handling
+        first_name = username
+        last_name = fake.last_name()
         email = fake.ascii_email()
 
         # Setting a random passwd
@@ -33,8 +83,9 @@ class PerformTask(locust.SequentialTaskSet):
         passwd = secrets.token_urlsafe(password_length)
 
         # Setting the config for client auth
+        # Needs further dehardcoding
         config = Configuration(host=LoadUser.host,
-                               username=get_admin_user(),
+                               username=GetUser.get_admin_user(),
                                password=USER_PASS)
 
         # Launching the client with randomized data
@@ -44,10 +95,12 @@ class PerformTask(locust.SequentialTaskSet):
                 register_request = RegisterSerializerExRequest(username=username,
                                                                password1=passwd,
                                                                password2=passwd,
-                                                               email=email
+                                                               email=email,
+                                                               first_name=first_name,
+                                                               last_name=last_name
                                                                )
                 # sending the request
-                (data, response) = api_client.auth_api.create_register(register_request)
+                (data) = api_client.auth_api.create_register(register_request)
                 pprint(data)
             except exceptions.ApiException as e:
                 print("Exception when calling AuthApi.create_register: %s\n" % e)
@@ -55,7 +108,7 @@ class PerformTask(locust.SequentialTaskSet):
 
 class LoadUser(locust.HttpUser):
 
-    tasks = [PerformTask.perform_random_user_register_task]  # a list of tasks to perform
+    tasks = [PerformTask]  # a list of tasks to perform
     wait_time = locust.between(1, 3)  # how long to wait before a single task is performed
     host = BASE_URL
 
