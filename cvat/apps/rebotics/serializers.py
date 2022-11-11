37a32ed86827989e2ff7b5a7129fcaaac4b0f232
farid <farid@retechlabs.com>
2022-11-11 16:36:15 +0600

    REB3-8814 [WIP] Importing cvat data in imggal format.

--- cvat/apps/rebotics/serializers.py
+++ cvat/apps/rebotics/serializers.py
@@ -9,22 +9,23 @@ from cvat.rebotics.utils import fix_between
 import exceptions
 
 
-# TODO: replace models to cvat models.
+class BaseImportSerializer(serializers.Serializer):
 
-class DetectionImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.DetectionImage
-        fields = [
-            'id',
-            'image',
-            'preview',
-            'training',
-        ]
+    def create(self, **kwargs):
+        raise NotImplementedError('Creating export data is not allowed')
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError('Updating export data is not allowed')
 
 
-class DetectionAnnotationImportSerializer(serializers.Serializer):
-    """Supporting serializer, used by DetectionImageImportSerializer to implement datastructure of annotation"""
+class ImportResponseSerializer(BaseImportSerializer):
+    id = ...        # image id in cvat.
+    image = ...     # image link
+    preview = ...   # preview link
+    training = ...  # ?
 
+
+class ImportAnnotationSerializer(BaseImportSerializer):
     lowerx = serializers.FloatField()
     lowery = serializers.FloatField()
     upperx = serializers.FloatField()
@@ -34,93 +35,28 @@ class DetectionAnnotationImportSerializer(serializers.Serializer):
     points = serializers.CharField(max_length=255, required=False, allow_null=True)
     type = serializers.CharField(max_length=255, required=False, allow_null=True)
 
-    def update(self, instance, validated_data):
-        raise NotImplementedError('Updating export data is not allowed')
-
-    def create(self, **kwargs):
-        raise NotImplementedError('Creating export data is not allowed')
+    # fix coordinates somewhere so they are not out of bounds.
+    # fix_between(), etc.
 
 
-class DetectionAnnotationImportPriceTagSerializer(DetectionAnnotationImportSerializer):
+class ImportPriceTagSerializer(ImportAnnotationSerializer):
     upc = serializers.CharField(max_length=128, allow_blank=True, allow_null=True, required=False)
 
 
-class DetectionImageImportSerializer(serializers.Serializer):
-    """Serializer, used to accept data from retailer instances"""
-
-    items = serializers.ListSerializer(child=DetectionAnnotationImportSerializer())
+class ImportSerializer(BaseImportSerializer):
+    items = serializers.ListSerializer(child=ImportAnnotationSerializer())
     image = serializers.URLField()
     export_by = serializers.CharField(required=False, allow_null=True)
     planogram_title = serializers.CharField(required=False, allow_null=True)
     retailer_codename = serializers.CharField(required=False, allow_null=True)
     processing_action_id = serializers.IntegerField(required=False, allow_null=True)
     price_tags = serializers.ListSerializer(
-        child=DetectionAnnotationImportPriceTagSerializer(),
+        child=ImportPriceTagSerializer(),
         required=False,
         allow_null=True
     )
 
-    def create(self, validated_data):
-        detection_image = models.DetectionImage.objects.create(
-            image=self.__get_image_from_url(validated_data['image']),
-            exported_by=validated_data.get('export_by'),
-            planogram_title=validated_data.get('planogram_title'),
-            retailer=validated_data.get('retailer_codename'),
-            scan_id=validated_data.get('processing_action_id')
-        )
-
-        width = detection_image.image.width
-        height = detection_image.image.height
-
-        items_to_create = []
-        for item in validated_data['items']:
-            points = item.get('points')
-            item_type = item.get('type', RECTANGLE)
-            label, created = models.DetectionClass.objects.get_or_create(code=item['upc'])
-
-            if not label.title or label.title != item['label']:
-                label.title = item['label']
-                label.save()
-
-            items_to_create.append(models.DetectionAnnotation(
-                image=detection_image,
-                detection_class=label,
-                lowerx=fix_between(item['lowerx'], 0, width),
-                lowery=fix_between(item['lowery'], 0, height),
-                upperx=fix_between(item['upperx'], 0, width),
-                uppery=fix_between(item['uppery'], 0, height),
-                points=points,
-                type=item_type
-            ))
-
-        price_tag_items = validated_data.get('price_tags', [])
-        price_tag_label, created = models.DetectionClass.objects.get_or_create(code='All PRICE TAGS')
-        if not price_tag_label.title:
-            price_tag_label.title = 'All PRICE TAGS'
-            price_tag_label.save()
-
-        for price_tag in price_tag_items:
-            lowerx = fix_between(price_tag['lowerx'], 0, width)
-            lowery = fix_between(price_tag['lowery'], 0, height)
-            upperx = fix_between(price_tag['upperx'], 0, width)
-            uppery = fix_between(price_tag['uppery'], 0, height)
-
-            items_to_create.append(models.DetectionAnnotation(
-                image=detection_image,
-                detection_class=price_tag_label,
-                lowerx=lowerx,
-                lowery=lowery,
-                upperx=upperx,
-                uppery=uppery
-            ))
-
-        models.DetectionAnnotation.objects.bulk_create(items_to_create)
-
-        return detection_image
-
-    def update(self, instance, validated_data):
-        raise NotImplementedError('Updating export data is not allowed')
-
+    # may need it.
     @staticmethod
     def __get_image_from_url(image_url):
         image_response = requests.get(image_url, verify=False)
--- cvat/apps/rebotics/views.py
+++ cvat/apps/rebotics/views.py
@@ -4,24 +4,25 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .admin_auth import RetailerInAdminAuthentication
-from .serializers import DetectionImageSerializer, DetectionImageImportSerializer
+from .serializers import ImportSerializer, ImportResponseSerializer
 
 
 class ImportTrainingDataViewSet(viewsets.GenericViewSet):
     authentication_classes = [RetailerInAdminAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated, ]
-    serializer_class = DetectionImageImportSerializer
+    serializer_class = ImportSerializer
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
-        detection_images = self._perform_multiple_create(serializer)
+        data = self._import_task(serializer)
 
-        detection_image_serializer = DetectionImageSerializer(instance=detection_images, many=True)
-        return Response(detection_image_serializer.data)
+        response_serializer = ImportResponseSerializer(instance=data, many=True)
+        return Response(response_serializer.data)
 
-    def _perform_multiple_create(self, serializer):
-        detection_images = []
-        for validated_item in serializer.validated_data:
-            detection_images.append(serializer.child.create(validated_item))
-        return detection_images
+    def _import_task(self, serializer):
+        # get images from serializer and create into task.
+        # 1) convert images from imggal json format to cvat data
+        # 2) import as a task.
+        # 3) return task.data
+        pass
