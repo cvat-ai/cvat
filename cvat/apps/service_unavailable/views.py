cb880cecef779980896b4f8152edcf3769dd981d
Andrey Zhavoronkov <andrey@cvat.ai>
2022-11-18 07:26:48 +0300

    docs

--- cvat/apps/service_unavailable/views.py
+++ cvat/apps/service_unavailable/views.py
@@ -15,7 +15,9 @@ class DBFailureView(APIView):
 
     @staticmethod
     def _get_response():
-        return Response('Some message some message some message',
+        return Response("It seems the backend service can't connect to the database. \
+            If you upgraded from version 2.3.0 or earlier, see the Upgrade Guide section \
+            in the online documentation, you may need to do some manual actions.",
             status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
     def get(self, request):
