f192bb7193252586cdb5ff04b7cf0f507d9234b3
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2022-12-01 17:32:16 +0600

    Add backward compatibility for python

--- cvat/apps/lambda_manager/tests/test_lambda.py
+++ cvat/apps/lambda_manager/tests/test_lambda.py
@@ -6,7 +6,7 @@
 import json
 from collections import OrderedDict
 from io import BytesIO
-from typing import Optional
+from typing import Dict, Optional
 from unittest import mock, skip
 import os
 
@@ -995,7 +995,7 @@ class Issue4996_Cases(_LambdaTestCaseBase):
     # We need to check that job assignee can call functions in the assigned jobs
     # This requires to pass the job id in the call request.
 
-    def _create_org(self, *, owner: int, members: dict[int, str] = None) -> dict:
+    def _create_org(self, *, owner: int, members: Dict[int, str] = None) -> dict:
         org = self._post_request('/api/organizations', user=owner, data={
             "slug": "testorg",
             "name": "test Org",
