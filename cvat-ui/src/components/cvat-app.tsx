a3b4f97f9a806edbce5905ec1250892c5a70774e
Maria Khrustaleva <maya17grd@gmail.com>
2022-12-10 21:16:51 +0100

    Improve social authentication (#5349)

--- cvat-ui/src/components/cvat-app.tsx
+++ cvat-ui/src/components/cvat-app.tsx
@@ -19,6 +19,7 @@ import 'antd/dist/antd.css';
 import LogoutComponent from 'components/logout-component';
 import LoginPageContainer from 'containers/login-page/login-page';
 import LoginWithTokenComponent from 'components/login-with-token/login-with-token';
+import LoginWithSocialAppComponent from 'components/login-with-social-app/login-with-social-app';
 import RegisterPageContainer from 'containers/register-page/register-page';
 import ResetPasswordPageConfirmComponent from 'components/reset-password-confirm-page/reset-password-confirm-page';
 import ResetPasswordPageComponent from 'components/reset-password-page/reset-password-page';
@@ -501,6 +502,11 @@ class CVATApplication extends React.PureComponent<CVATAppProps & RouteComponentP
                             exact
                             path='/auth/login-with-token/:token'
                             component={LoginWithTokenComponent}
+                        />
+                        <Route
+                            exact
+                            path='/auth/login-with-social-app/'
+                            component={LoginWithSocialAppComponent}
                         />
                         <Route exact path='/auth/password/reset' component={ResetPasswordPageComponent} />
                         <Route
