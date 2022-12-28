cb7a3fc823330874aa3fc88a22d0bbf826a5fc53
kirill-sizov <sizow.k.d@gmail.com>
2022-12-28 15:03:53 +0200

    test limitations: add base tests for limitation endpoint

---
+++ tests/python/rest_api/test_limitations.py
@@ -0,0 +1,39 @@
+from http import HTTPStatus
+import pytest
+from shared.utils.config import get_method, patch_method, delete_method
+
+@pytest.mark.usefixtures("restore_db_per_function")
+class TestUpdateLimitations:
+    def test_can_update_org_limitation(self):
+        response = get_method("admin1", "limitations", org_id=1)
+        assert response.status_code == HTTPStatus.OK
+        limitation = response.json()[0]
+
+        data = {"projects": 99}
+        response = patch_method("admin1", f"limitations/{limitation['id']}", data)
+        assert response.status_code == HTTPStatus.OK
+
+        response = get_method("admin1", "limitations", org_id=1)
+        assert response.status_code == HTTPStatus.OK
+        limitation = response.json()[0]
+
+        assert all([
+            value == limitation[key]
+            for key, value in data.items()
+        ])
+
+
+    def test_cannot_delete_org_that_has_paid_plan(self):
+        org_id = 1
+
+        response = get_method("admin1", "limitations", org_id=org_id)
+        assert response.status_code == HTTPStatus.OK
+        limitation = response.json()[0]
+
+        data = {"type": "paid", "projects": 99}
+        response = patch_method("admin1", f"limitations/{limitation['id']}", data)
+        assert response.status_code == HTTPStatus.OK
+
+        response = delete_method("admin1", f"organizations/{org_id}")
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
