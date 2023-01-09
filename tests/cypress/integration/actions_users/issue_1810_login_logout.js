0be74b0bb5010794a258edee1e4e052a3132c482
Maria Khrustaleva <maria@cvat.ai>
2023-01-09 13:17:49 +0100

    Check that icon was received from the server

--- tests/cypress/integration/actions_users/issue_1810_login_logout.js
+++ tests/cypress/integration/actions_users/issue_1810_login_logout.js
@@ -107,6 +107,12 @@ context('When clicking on the Logout button, get the user session closed.', () =
                 expect(socialAuthMethods).length.gt(0);
                 cy.visit('auth/login');
 
+                cy.get('.cvat-social-authentication-icon').should('have.length', socialAuthMethods.length).within((items) => {
+                    for (const item of items) {
+                        expect(item.children.length).to.be.equal(1); // check that icon was received from the server
+                    }
+                });
+
                 for (const provider of socialAuthMethods) {
                     let username = '';
                     cy.get(`.cvat-social-authentication-${provider}`).should('be.visible').click();
