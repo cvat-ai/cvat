c498fa5d619e8c50ab932468c51733b079824c58
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2023-01-10 00:03:08 +0200

    Update tests

--- cvat/apps/engine/tests/test_rest_api_3D.py
+++ cvat/apps/engine/tests/test_rest_api_3D.py
@@ -24,6 +24,8 @@ from cvat.apps.engine.media_extractors import ValidateDimension
 from cvat.apps.dataset_manager.task import TaskAnnotation
 from datumaro.util.test_utils import TestDir
 
+from cvat.apps.engine.tests.utils import get_paginated_collection
+
 CREATE_ACTION = "create"
 UPDATE_ACTION = "update"
 DELETE_ACTION = "delete"
@@ -140,8 +142,10 @@ class _DbTestBase(APITestCase):
 
     def _get_jobs(self, task_id):
         with ForceLogin(self.admin, self.client):
-            response = self.client.get("/api/tasks/{}/jobs".format(task_id))
-        return response.data
+            values = get_paginated_collection(lambda page:
+                self.client.get("/api/tasks/{}/jobs?page={}".format(task_id, page))
+            )
+        return values
 
     def _get_request(self, path, user):
         with ForceLogin(user, self.client):
