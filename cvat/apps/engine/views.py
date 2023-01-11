ee55e11c70650c1d3f2f67b47c94e56504e0d977
Boris Sekachev <sekachev.bs@gmail.com>
2023-01-11 06:51:07 -0800

    Use commonpath

--- cvat/apps/engine/views.py
+++ cvat/apps/engine/views.py
@@ -670,22 +670,6 @@ class ProjectViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
 
         return response
 
-def find_common_path(paths):
-    reference = paths[0].split(os.path.sep)
-    truncate_from = len(reference)
-
-    if len(paths):
-        for idx, fragment in enumerate(reference):
-            thesame = True
-            for path in paths[1:]:
-                fragments = path.split(os.path.sep)
-                thesame = fragment == fragments[idx] if idx < len(fragments) else False
-                if not thesame:
-                    truncate_from = idx
-                    break
-
-    return os.path.sep.join(reference[0:truncate_from])
-
 class DataChunkGetter:
     def __init__(self, data_type, data_num, data_quality, task_dim):
         possible_data_type_values = ('chunk', 'frame', 'preview', 'context_image')
@@ -753,7 +737,7 @@ class DataChunkGetter:
                             return Response(data='No context image related to the frame',
                                 status=status.HTTP_404_NOT_FOUND)
 
-                        common_path = find_common_path(list(map(lambda x: str(x.path), image.related_files.all())))
+                        common_path = os.path.commonpath(list(map(lambda x: str(x.path), image.related_files.all())))
                         for i in image.related_files.all():
                             path = os.path.realpath(str(i.path))
                             name = os.path.relpath(str(i.path), common_path)
