4016223fa84fb820a214240d1bc8a779f4668c27
Maria Khrustaleva <maria@cvat.ai>
2023-01-31 16:42:29 +0100

    Update signal receiver

--- cvat/apps/iam/signals.py
+++ cvat/apps/iam/signals.py
@@ -4,10 +4,11 @@
 
 from django.conf import settings
 from django.contrib.auth.models import User, Group
+from django.contrib.sites.shortcuts import get_current_site
 from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from allauth.account.adapter import get_adapter
-from allauth.account.signals import user_signed_up
+from allauth.account.signals import user_signed_up, email_confirmed
 from allauth.account.utils import user_email
 from allauth.account import app_settings as allauth_settings
 
@@ -66,12 +67,49 @@ def register_signals(app_config):
         # has an admin group.
         django_auth_ldap.backend.populate_user.connect(create_user)
 
-@receiver(user_signed_up, dispatch_uid='send_welcome_email_after_sign_up')
-def send_welcome_email(request, user, **kwargs):
+@receiver([user_signed_up, email_confirmed], dispatch_uid='send_welcome_email_after_sign_up_or_email_verification')
+def send_welcome_email(request, **kwargs):
+    """
+        Send welcome email in 2 cases:
+        - CVAT server is configured with MANDATORY email verification and the user has confirmed the email (handle email_confirmed signal)
+        - CVAT server is configured with OPTIONAL or NONE email verification policy and the user has signed up (handle user_signed_up signal)
+        signal user_signed_up provides: request, user
+        signal email_confirmed provides: request, email_address
+    """
+
+    def _is_mandatory_email_verification():
+        return (
+            settings.ACCOUNT_EMAIL_VERIFICATION
+            == allauth_settings.EmailVerificationMethod.MANDATORY
+        )
+
+    def _define_user_and_email():
+        if (user := kwargs.get('user')):
+            return user, user_email(user)
+        else:
+            email_instance = kwargs['email_address']
+            return email_instance.user, email_instance.email
+
+    user, email = _define_user_and_email()
     adapter = get_adapter(request)
-    email = user_email(user)
-    if email and settings.ACCOUNT_EMAIL_VERIFICATION != allauth_settings.EmailVerificationMethod.NONE:
+
+    send_email = bool(
+        email
+        and (
+            (
+                not _is_mandatory_email_verification()
+                and 'register' in request.path
+            )
+            or (
+                _is_mandatory_email_verification() and 'account-confirm-email' in request.path
+            )
+        )
+    )
+    if send_email:
         context={
             'email': email,
+            'request': request,
+            'user': user,
+            'current_site': get_current_site(request)
         }
         adapter.send_mail(template_prefix='account/email/welcome', email=email, context=context)
