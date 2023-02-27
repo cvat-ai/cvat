c8dcb17d5978effd65d05b4cdb18bad6e42d3376
Maria Khrustaleva <maria@cvat.ai>
2023-02-28 00:46:21 +0100

    Fix schema && add test

--- tests/python/rest_api/test_resource_import_export.py
+++ tests/python/rest_api/test_resource_import_export.py
@@ -11,46 +11,64 @@ from typing import Any, Dict, TypeVar
 
 import pytest
 
-from shared.utils.config import get_method, post_method
-from shared.utils.s3 import make_client
+from abc import ABC, abstractstaticmethod
+from shared.utils.config import get_method, post_method, make_api_client
+from shared.utils.s3 import make_client as make_s3_client
+from shared.utils.azure import make_client as make_azure_blob_container_client
+from shared.utils.config import AZURE_CONTAINER, AZURE_TOKEN, AZURE_ACCOUNT_NAME
 
 T = TypeVar("T")
 
 FILENAME_TEMPLATE = "cvat/{}/{}.zip"
-FORMAT = "COCO 1.0"
+EXPORT_FORMAT = "CVAT for images 1.1"
+IMPORT_FORMAT = "CVAT 1.1"
 
 
-# https://docs.pytest.org/en/7.1.x/example/markers.html#marking-whole-classes-or-modules
-pytestmark = [pytest.mark.with_external_services]
-
-
-def _make_custom_resource_params(obj: str, resource: str, cloud_storage_id: int) -> Dict[str, Any]:
-    params = {
+def _make_custom_resource_params(resource: str, obj: str, cloud_storage_id: int) -> Dict[str, Any]:
+    return {
         "filename": FILENAME_TEMPLATE.format(obj, resource),
         "location": "cloud_storage",
         "cloud_storage_id": cloud_storage_id,
         "use_default_location": False,
     }
-    if resource != "backup":
-        params["format"] = FORMAT
-    return params
 
 
-def _make_default_resource_params(obj: str, resource: str) -> Dict[str, Any]:
-    params = {
+def _make_default_resource_params(resource: str, obj: str) -> Dict[str, Any]:
+    return {
         "filename": FILENAME_TEMPLATE.format(obj, resource),
         "use_default_location": True,
     }
+
+
+def _make_export_resource_params(
+    resource: str, is_default: bool = True, **kwargs
+) -> Dict[str, Any]:
+    func = _make_default_resource_params if is_default else _make_custom_resource_params
+    params = func(resource, **kwargs)
     if resource != "backup":
-        params["format"] = FORMAT
+        params["format"] = EXPORT_FORMAT
     return params
 
 
-class _S3ResourceTest:
+def _make_import_resource_params(
+    resource: str, is_default: bool = True, **kwargs
+) -> Dict[str, Any]:
+    func = _make_default_resource_params if is_default else _make_custom_resource_params
+    params = func(resource, **kwargs)
+    if resource != "backup":
+        params["format"] = IMPORT_FORMAT
+    return params
+
+
+class _CloudStorageResourceTest(ABC):
+    @abstractstaticmethod
+    def _make_client():
+        pass
+
     @pytest.fixture(autouse=True)
-    def setup(self, admin_user: str):
-        self.user = admin_user
-        self.s3_client = make_client()
+    def setup(self):
+        self.user = "admin2"
+        self.client = self._make_client()
         self.exit_stack = ExitStack()
         with self.exit_stack:
             yield
@@ -62,12 +80,12 @@ class _S3ResourceTest:
             bucket = storage["resource"]
 
             # check that file doesn't exist on the bucket
-            assert not self.s3_client.file_exists(bucket, filename)
+            assert not self.client.file_exists(bucket=bucket, filename=filename)
 
             func(*args, **kwargs)
 
             # check that file exists on the bucket
-            assert self.s3_client.file_exists(bucket, filename)
+            assert self.client.file_exists(bucket=bucket, filename=filename)
 
         return wrapper
 
@@ -92,6 +110,9 @@ class _S3ResourceTest:
             response = post_method(user, url, data=None, **kwargs)
             status = response.status_code
 
+        annotations = get_method(user, url).json()
+        assert len(annotations["shapes"])
+
     def _import_backup_from_cloud_storage(self, obj_id, obj, *, user, **kwargs):
         url = f"{obj}/backup"
         response = post_method(user, url, data=None, **kwargs)
@@ -113,6 +134,21 @@ class _S3ResourceTest:
             response = get_method(user, url, action="import_status")
             status = response.status_code
 
+    def _import_resource(self, cloud_storage: Dict[str, Any], resource_type: str, *args, **kwargs):
+        methods = {
+            "annotations": self._import_annotations_from_cloud_storage,
+            "dataset": self._import_dataset_from_cloud_storage,
+            "backup": self._import_backup_from_cloud_storage,
+        }
+
+        org_id = cloud_storage["organization"]
+        if org_id:
+            kwargs.setdefault("org_id", org_id)
+
+        kwargs.setdefault("user", self.user)
+
+        return methods[resource_type](*args, **kwargs)
+
     def _export_resource(self, cloud_storage: Dict[str, Any], *args, **kwargs):
         org_id = cloud_storage["organization"]
         if org_id:
@@ -126,14 +162,22 @@ class _S3ResourceTest:
         export_callback(*args, **kwargs)
 
         self.exit_stack.callback(
-            self.s3_client.remove_file,
+            self.client.remove_file,
             bucket=cloud_storage["resource"],
             filename=kwargs["filename"],
         )
 
 
+class _S3ResourceTest(_CloudStorageResourceTest):
+    @staticmethod
+    def _make_client():
+        return make_s3_client()
+
+
+# https://docs.pytest.org/en/7.1.x/example/markers.html#marking-whole-classes-or-modules
+@pytest.mark.with_external_services
 @pytest.mark.usefixtures("restore_db_per_class")
-class TestExportResource(_S3ResourceTest):
+class TestExportResourceToS3(_S3ResourceTest):
     @pytest.mark.parametrize("cloud_storage_id", [3])
     @pytest.mark.parametrize(
         "obj_id, obj, resource",
@@ -152,7 +196,9 @@ class TestExportResource(_S3ResourceTest):
         self, cloud_storage_id, obj_id, obj, resource, cloud_storages
     ):
         cloud_storage = cloud_storages[cloud_storage_id]
-        kwargs = _make_custom_resource_params(obj, resource, cloud_storage_id)
+        kwargs = _make_export_resource_params(
+            resource, is_default=False, obj=obj, cloud_storage_id=cloud_storage_id
+        )
 
         self._export_resource(cloud_storage, obj_id, obj, resource, **kwargs)
 
@@ -190,29 +236,15 @@ class TestExportResource(_S3ResourceTest):
             task_id = jobs[obj_id]["task_id"]
             cloud_storage_id = tasks[task_id]["target_storage"]["cloud_storage_id"]
         cloud_storage = cloud_storages[cloud_storage_id]
-        kwargs = _make_default_resource_params(obj, resource)
+        kwargs = _make_export_resource_params(resource, obj=obj)
 
         self._export_resource(cloud_storage, obj_id, obj, resource, **kwargs)
 
 
+@pytest.mark.with_external_services
 @pytest.mark.usefixtures("restore_db_per_function")
 @pytest.mark.usefixtures("restore_cvat_data")
-class TestImportResource(_S3ResourceTest):
-    def _import_resource(self, cloud_storage: Dict[str, Any], resource_type: str, *args, **kwargs):
-        methods = {
-            "annotations": self._import_annotations_from_cloud_storage,
-            "dataset": self._import_dataset_from_cloud_storage,
-            "backup": self._import_backup_from_cloud_storage,
-        }
-
-        org_id = cloud_storage["organization"]
-        if org_id:
-            kwargs.setdefault("org_id", org_id)
-
-        kwargs.setdefault("user", self.user)
-
-        return methods[resource_type](*args, **kwargs)
-
+class TestImportResourceFromS3(_S3ResourceTest):
     @pytest.mark.parametrize("cloud_storage_id", [3])
     @pytest.mark.parametrize(
         "obj_id, obj, resource",
@@ -228,8 +260,12 @@ class TestImportResource(_S3ResourceTest):
         self, cloud_storage_id, obj_id, obj, resource, cloud_storages
     ):
         cloud_storage = cloud_storages[cloud_storage_id]
-        kwargs = _make_custom_resource_params(obj, resource, cloud_storage_id)
-        export_kwargs = _make_custom_resource_params(obj, resource, cloud_storage_id)
+        kwargs = _make_import_resource_params(
+            resource, is_default=False, obj=obj, cloud_storage_id=cloud_storage_id
+        )
+        export_kwargs = _make_export_resource_params(
+            resource, is_default=False, obj=obj, cloud_storage_id=cloud_storage_id
+        )
         self._export_resource(cloud_storage, obj_id, obj, resource, **export_kwargs)
 
         self._import_resource(cloud_storage, resource, obj_id, obj, **kwargs)
@@ -264,7 +300,69 @@ class TestImportResource(_S3ResourceTest):
             cloud_storage_id = tasks[task_id]["source_storage"]["cloud_storage_id"]
         cloud_storage = cloud_storages[cloud_storage_id]
 
-        kwargs = _make_default_resource_params(obj, resource)
+        export_kwargs = _make_export_resource_params(resource, obj=obj)
+        import_kwargs = _make_import_resource_params(resource, obj=obj)
+        self._export_resource(cloud_storage, obj_id, obj, resource, **export_kwargs)
+        self._import_resource(cloud_storage, resource, obj_id, obj, **import_kwargs)
+
+
+class _AzureResourceTest(_CloudStorageResourceTest):
+
+    SPEC = {
+        "provider_type": "AZURE_CONTAINER",
+        "resource": AZURE_CONTAINER,
+        "display_name": "Azure Container",
+        "credentials_type": "ACCOUNT_NAME_TOKEN_PAIR",
+        "session_token": AZURE_TOKEN,
+        "account_name": AZURE_ACCOUNT_NAME,
+        "description": "Some description",
+        "manifests": [
+            "manifest.jsonl",
+        ],
+    }
+
+    @staticmethod
+    def _make_client():
+        return make_azure_blob_container_client()
+
+
+@pytest.mark.skipif(
+    not bool(AZURE_CONTAINER and AZURE_TOKEN and AZURE_ACCOUNT_NAME),
+    reason="Azure credentials were not found",
+)
+@pytest.mark.usefixtures("restore_db_per_class")
+@pytest.mark.usefixtures("restore_cvat_data")
+class TestExportImportAnnotationsToAzure(_AzureResourceTest):
+    @pytest.mark.parametrize("obj, obj_id, resource", [("tasks", 11, "annotations")])
+    def test_export_resource_to_azure_cloud_storage(self, obj: str, obj_id: int, resource: str):
+        with make_api_client(self.user) as api_client:
+            (cloud_storage, response) = api_client.cloudstorages_api.create(self.SPEC)
+            assert response.status == HTTPStatus.CREATED
+
+        kwargs = _make_export_resource_params(
+            resource, is_default=False, obj=obj, cloud_storage_id=cloud_storage["id"]
+        )
         self._export_resource(cloud_storage, obj_id, obj, resource, **kwargs)
 
-        self._import_resource(cloud_storage, resource, obj_id, obj, **kwargs)
+
+@pytest.mark.skipif(
+    not bool(AZURE_CONTAINER and AZURE_TOKEN and AZURE_ACCOUNT_NAME),
+    reason="Azure credentials were not found",
+)
+@pytest.mark.usefixtures("restore_db_per_class")
+@pytest.mark.usefixtures("restore_cvat_data")
+class TestImportAnnotationsFromAzure(_AzureResourceTest):
+    @pytest.mark.parametrize("obj, obj_id, resource", [("tasks", 11, "annotations")])
+    def test_import_resource_from_cloud_storage(self, obj: str, obj_id: int, resource: str):
+        with make_api_client(self.user) as api_client:
+            (cloud_storage, response) = api_client.cloudstorages_api.create(self.SPEC)
+            assert response.status == HTTPStatus.CREATED
+
+        export_kwargs = _make_export_resource_params(
+            resource, is_default=False, obj=obj, cloud_storage_id=cloud_storage["id"]
+        )
+        import_kwargs = _make_import_resource_params(
+            resource, is_default=False, obj=obj, cloud_storage_id=cloud_storage["id"]
+        )
+        self._export_resource(cloud_storage, obj_id, obj, resource, **export_kwargs)
+        self._import_resource(cloud_storage, resource, obj_id, obj, **import_kwargs)
---
+++ tests/python/shared/utils/azure.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2023 CVAT.ai Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from azure.storage.blob import BlobServiceClient
+from azure.core.exceptions import HttpResponseError
+
+from shared.utils.config import AZURE_TOKEN, AZURE_ACCOUNT_NAME
+
+
+class AzureBlobContainerClient:
+    def __init__(self, account_name: str, sas_token: str) -> None:
+        self.service_client = BlobServiceClient(
+            account_url="{}.blob.core.windows.net".format(account_name), credential=sas_token
+        )
+
+    def create_file(self, bucket: str, filename: str, data: bytes = b""):
+        self.service_client.get_container_client(bucket).upload_blob(name=filename, data=data)
+
+    def remove_file(self, bucket: str, filename: str, **kwargs):
+        self.service_client.get_container_client(bucket).delete_blob(blob=filename)
+
+    def file_exists(self, bucket: str, filename: str, **kwargs) -> bool:
+        try:
+            blob_client = self.service_client.get_container_client(bucket).get_blob_client(filename)
+            blob_client.get_blob_properties()
+        except HttpResponseError:
+            return False
+        return True
+
+    def download_fileobj(self, bucket: str, key: str) -> bytes:
+        storage_stream_downloader = self.service_client.get_container_client(bucket).download_blob(
+            blob=key
+        )
+        return storage_stream_downloader.content_as_bytes(max_concurrency=3)
+
+
+def make_client() -> AzureBlobContainerClient:
+    return AzureBlobContainerClient(account_name=AZURE_ACCOUNT_NAME, sas_token=AZURE_TOKEN)
