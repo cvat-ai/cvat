05a6601f70542ec23c5abd356cccbcd950c8488a
Kirill Lakhov <kirill.9992@gmail.com>
2023-03-15 11:14:38 +0300

    Disabled premium features notification (#5853)

--- cvat-ui/src/components/cvat-app.tsx
+++ cvat-ui/src/components/cvat-app.tsx
@@ -75,7 +75,6 @@ import EmailConfirmationPage from './email-confirmation-pages/email-confirmed';
 import EmailVerificationSentPage from './email-confirmation-pages/email-verification-sent';
 import IncorrectEmailConfirmationPage from './email-confirmation-pages/incorrect-email-confirmation';
 import CreateModelPage from './create-model-page/create-model-page';
-import PremiumFeaturesModal from './premium-features-modal/premium-features-modal';
 
 interface CVATAppProps {
     loadFormats: () => void;
@@ -399,8 +398,6 @@ class CVATApplication extends React.PureComponent<CVATAppProps & RouteComponentP
             isModelPluginActive,
         } = this.props;
 
-        const { CVAT_BILLING_URL } = appConfig;
-
         const { healthIinitialized, backendIsHealthy } = this.state;
 
         const notRegisteredUserInitialized = (userInitialized && (user == null || !user.isVerified));
@@ -496,7 +493,6 @@ class CVATApplication extends React.PureComponent<CVATAppProps & RouteComponentP
                                     <ExportBackupModal />
                                     <ImportDatasetModal />
                                     <ImportBackupModal />
-                                    { CVAT_BILLING_URL && <PremiumFeaturesModal /> }
                                     {/* eslint-disable-next-line */}
                                     <a id='downloadAnchor' target='_blank' style={{ display: 'none' }} download />
                                 </Layout.Content>
