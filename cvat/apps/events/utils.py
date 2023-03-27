9050adb9e2f816c076606d4c87dca82b00f02d82
Andrey Zhavoronkov <andrey@cvat.ai>
2023-03-27 21:57:55 +0300

    remove locks

--- cvat/apps/events/utils.py
+++ cvat/apps/events/utils.py
@@ -2,10 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-from threading import Lock
-
 _caches = {}
-_locks = {}
 
 class DeleteCache():
     def __init__(self, cache_id):
@@ -16,28 +13,23 @@ class DeleteCache():
             Issue: {},
             Comment: {},
         })
-        self._lock = _locks.setdefault(cache_id, Lock())
 
     def set(self, instance_class, instance_id, value):
-        with self._lock:
-            self._cache[instance_class][instance_id] = value
+        self._cache[instance_class][instance_id] = value
 
     def pop(self, instance_class, instance_id, default=None):
-        with self._lock:
-            if instance_class in self._cache and \
-                instance_id in self._cache[instance_class]:
-                return self._cache[instance_class].pop(instance_id, default)
+        if instance_class in self._cache and \
+            instance_id in self._cache[instance_class]:
+            return self._cache[instance_class].pop(instance_id, default)
 
     def has_key(self, instance_class, instance_id):
-        with self._lock:
-            if instance_class in self._cache and \
-                instance_id in self._cache[instance_class]:
-                return True
-            return False
+        if instance_class in self._cache and \
+            instance_id in self._cache[instance_class]:
+            return True
+        return False
 
     def clear(self):
-        with self._lock:
-            self._cache.clear()
+        self._cache.clear()
 
 def get_cache():
     from .handlers import request_id
