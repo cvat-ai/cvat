fe78c28fc1cc74e37a38fe07e7e62dc1cb387bff
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2023-03-27 23:19:22 +0300

    Fix formatting script

--- dev/format_python_code.sh
+++ dev/format_python_code.sh
@@ -6,10 +6,10 @@ PYTHON="${PYTHON:-python3}"
 BLACK="${PYTHON} -m black"
 ISORT="${PYTHON} -m isort"
 
-${BLACK} --version >/dev/null 2>&1 && {
+[ ${BLACK} --version >/dev/null 2>&1 ] && {
     echo "black is not found, please check it is installed"; exit 1;
 }
-${ISORT} --version >/dev/null 2>&1 && {
+[ ${ISORT} --version >/dev/null 2>&1 ] && {
     echo "isort is not found, please check it is installed"; exit 1;
 }
 
