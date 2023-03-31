d588aa6bc1a2e7bd74cce08f800186209578cac6
Nikita Manovich <nikita@cvat.ai>
2023-03-31 18:26:51 +0300

    Removed some unnecessary configuration params for Silk

--- cvat/settings/development.py
+++ cvat/settings/development.py
@@ -67,11 +67,3 @@ SILKY_MAX_REQUEST_BODY_SIZE = 1024
 SILKY_MAX_RESPONSE_BODY_SIZE = 1024
 SILKY_IGNORE_PATHS = ['/admin', '/documentation', '/django-rq', '/auth']
 SILKY_MAX_RECORDED_REQUESTS = 10**4
-def SILKY_INTERCEPT_FUNC(request):
-    # Ignore all requests which try to get a frame (too many of them)
-    if request.method == 'GET' and '/frames/' in request.path:
-        return False
-
-    return True
-
-SILKY_INTERCEPT_FUNC = SILKY_INTERCEPT_FUNC
