d8a0d106d6bcff403781bf9363d664ee655d9df3
kirill-sizov <sizow.k.d@gmail.com>
2023-04-07 10:15:31 +0300

    fix tests

--- tests/python/shared/fixtures/init.py
+++ tests/python/shared/fixtures/init.py
@@ -357,7 +357,7 @@ def local_start(start, stop, dumpdb, cleanup, rebuild, cvat_root_dir, cvat_db_di
         pytest.exit("All testing containers are stopped", returncode=0)
 
     if (
-        not any(set(running_containers) & {f"{PREFIX}_cvat_server_1", f"{PREFIX}_cvat_db_1"})
+        not any(set(running_containers()) & {f"{PREFIX}_cvat_server_1", f"{PREFIX}_cvat_db_1"})
         or rebuild
     ):
         start_services(dc_files, rebuild, cvat_root_dir)
