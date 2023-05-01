94a0b512ffa35da3f45a2c787e5e3d6ca0e9c261
Boris Sekachev <sekachev.bs@gmail.com>
2023-05-01 18:30:16 +0300

    Fixed UI tests

--- tests/cypress/e2e/actions_tasks3/case_118_multi_tasks.js
+++ tests/cypress/e2e/actions_tasks3/case_118_multi_tasks.js
@@ -86,20 +86,17 @@ context('Create mutli tasks.', () => {
         });
 
         it('Trying to create a tasks with images from the shared storage', () => {
+            cy.intercept('GET', '/api/server/share?**').as('shareRequest');
             cy.contains('[role="tab"]', 'Connected file share').click();
-            cy.get('.cvat-share-tree')
-                .should('be.visible')
+            cy.wait('@shareRequest').then((interception) => {
+                expect(interception.response.body
+                    .sort((a, b) => a.name.localeCompare(b.name)))
+                    .to.deep.equal(expectedTopLevel);
+            });
+            cy.get('.cvat-remote-browser-table-wrapper')
+                .should('exist')
                 .within(() => {
-                    cy.intercept('GET', '/api/server/share?**').as('shareRequest');
-                    cy.get('[aria-label="plus-square"]').click();
-                    cy.wait('@shareRequest').then((interception) => {
-                        expect(interception.response.body
-                            .sort((a, b) => a.name.localeCompare(b.name)))
-                            .to.deep.equal(expectedTopLevel);
-                    });
-                    cy.get('[title="images"]').parent().within(() => {
-                        cy.get('[aria-label="plus-square"]').click();
-                    });
+                    cy.get('button').contains('images').click();
                     cy.wait('@shareRequest').then((interception) => {
                         expect(interception.response.body
                             .sort((a, b) => a.name.localeCompare(b.name)))
@@ -107,8 +104,8 @@ context('Create mutli tasks.', () => {
                     });
                     expectedImagesList.forEach((el) => {
                         const { name } = el;
-                        cy.get(`[title="${name}"]`).parent().within(() => {
-                            cy.get('.ant-tree-checkbox').click().should('have.attr', 'class').and('contain', 'checked');
+                        cy.get('.ant-table-cell').contains(name).parent().within(() => {
+                            cy.get('.ant-checkbox-input').click();
                         });
                     });
                 });
@@ -144,20 +141,17 @@ context('Create mutli tasks.', () => {
         });
 
         it('Trying to create a tasks with videos from the shared storage', () => {
+            cy.intercept('GET', '/api/server/share?**').as('shareRequest');
             cy.contains('[role="tab"]', 'Connected file share').click();
-            cy.get('.cvat-share-tree')
-                .should('be.visible')
+            cy.wait('@shareRequest').then((interception) => {
+                expect(interception.response.body
+                    .sort((a, b) => a.name.localeCompare(b.name)))
+                    .to.deep.equal(expectedTopLevel);
+            });
+            cy.get('.cvat-remote-browser-table-wrapper')
+                .should('exist')
                 .within(() => {
-                    cy.intercept('GET', '/api/server/share?**').as('shareRequest');
-                    cy.get('[aria-label="plus-square"]').click();
-                    cy.wait('@shareRequest').then((interception) => {
-                        expect(interception.response.body
-                            .sort((a, b) => a.name.localeCompare(b.name)))
-                            .to.deep.equal(expectedTopLevel);
-                    });
-                    cy.get('[title="videos"]').parent().within(() => {
-                        cy.get('[aria-label="plus-square"]').click();
-                    });
+                    cy.get('button').contains('videos').click();
                     cy.wait('@shareRequest').then((interception) => {
                         expect(interception.response.body
                             .sort((a, b) => a.name.localeCompare(b.name)))
@@ -165,8 +159,8 @@ context('Create mutli tasks.', () => {
                     });
                     expectedVideosList.forEach((el) => {
                         const { name } = el;
-                        cy.get(`[title="${name}"]`).parent().within(() => {
-                            cy.get('.ant-tree-checkbox').click().should('have.attr', 'class').and('contain', 'checked');
+                        cy.get('.ant-table-cell').contains(name).parent().within(() => {
+                            cy.get('.ant-checkbox-input').click();
                         });
                     });
                 });
