ac4e56f2bddc59a20d17689d72267a14a8e18a68
klakhov <kirill.9992@gmail.com>
2023-05-19 14:20:17 +0300

    added quality reports hint

--- cvat-ui/src/components/task-analytics-page/quality/task-quality-component.tsx
+++ cvat-ui/src/components/task-analytics-page/quality/task-quality-component.tsx
@@ -9,6 +9,7 @@ import {
     Job, JobType, Task,
 } from 'cvat-core-wrapper';
 import { Row } from 'antd/lib/grid';
+import Text from 'antd/lib/typography/Text';
 import JobItem from 'components/job-item/job-item';
 import { useDispatch, useSelector } from 'react-redux';
 import { getQualityReportsAsync } from 'actions/analytics-actions';
@@ -43,11 +44,16 @@ function TaskQualityComponent(props: Props): JSX.Element {
             <Row>
                 <MeanQuality task={task} />
             </Row>
-            <Row style={{ marginTop: '16px' }} gutter={16}>
+            <Row gutter={16}>
                 <GtConflicts task={task} />
                 <Issues task={task} />
             </Row>
-            <Row style={{ marginTop: '16px' }}>
+            <Row>
+                <Text type='secondary' className='cvat-task-quality-reports-hint'>
+                    Quality reports are not computed unless the GT job is in the accepted state.
+                </Text>
+            </Row>
+            <Row>
                 <JobItem job={gtJob} onJobUpdate={onJobUpdate} />
             </Row>
             <Row>
