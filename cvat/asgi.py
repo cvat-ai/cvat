0a291a55cd55b37ea8fd4c5f28183b81d29ddf0b
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2023-06-01 17:21:00 +0300

    Implement asgi server debugging

--- cvat/asgi.py
+++ cvat/asgi.py
@@ -14,8 +14,28 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.core.handlers.asgi import ASGIHandler
+
+import cvat.utils.remote_debugger as debug
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cvat.settings.{}" \
     .format(os.environ.get("DJANGO_CONFIGURATION", "development")))
 
 application = get_asgi_application()
+
+
+if debug.is_debugging_enabled():
+    class DebuggerApp(ASGIHandler):
+        """
+        Support for VS code debugger
+        """
+
+        def __init__(self) -> None:
+            super().__init__()
+            self.__debugger = debug.RemoteDebugger()
+
+        async def handle(self, *args, **kwargs):
+            self.__debugger.attach_current_thread()
+            return await super().handle(*args, **kwargs)
+
+    application = DebuggerApp()
