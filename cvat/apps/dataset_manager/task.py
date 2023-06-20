cvat-ai/cvat

commit 9a600f3fa86d05d394da17e0d54711632c03ae43

Honey pot server (#6204)
The server part of #6039 

- Added support for Ground Truth jobs in a task
- Added support for job creation and removal (only Ground Truth jobs can
  be created or removed in a task)
- Added a component to autocompute quality metrics for a task
- Added tests
- Fixed https://github.com/opencv/cvat/issues/5971 (both parts - the
  outside problem and the manifest problem, the manifest part fix is also
  available in #6216)

Co-authored-by: klakhov <kirill.9992@gmail.com>
Co-authored-by: Roman Donchenko <roman@cvat.ai>
Co-authored-by: Kirill Sizov <kirill.sizov@cvat.ai>

cvat/apps/dataset_manager/task.py
@@ -19,10 +19,11 @@ from cvat.apps.engine import models, serializers
 from cvat.apps.engine.plugins import plugin_decorator
 from cvat.apps.profiler import silk_profile
 
-from .annotation import AnnotationIR, AnnotationManager
-from .bindings import JobData, TaskData, CvatImportError
-from .formats.registry import make_exporter, make_importer
-from .util import bulk_create
+from cvat.apps.dataset_manager.annotation import AnnotationIR, AnnotationManager
+from cvat.apps.dataset_manager.bindings import TaskData, JobData, CvatImportError
+from cvat.apps.dataset_manager.formats.registry import make_exporter, make_importer
+from cvat.apps.dataset_manager.util import add_prefetch_fields, bulk_create
+
 
 class dotdict(OrderedDict):
     """dot.notation access to dictionary attributes"""
@@ -73,18 +74,48 @@ def _merge_table_rows(rows, keys_for_merge, field_id):
     return list(merged_rows.values())
 
 class JobAnnotation:
-    def __init__(self, pk, is_prefetched=False):
-        if is_prefetched:
-            self.db_job = models.Job.objects.select_related('segment__task') \
-                .select_for_update().get(id=pk)
-        else:
-            self.db_job = models.Job.objects.prefetch_related(
-                'segment',
-                'segment__task',
-                Prefetch('segment__task__data', queryset=models.Data.objects.select_related('video').prefetch_related(
+    @classmethod
+    def add_prefetch_info(cls, queryset):
+        assert issubclass(queryset.model, models.Job)
+
+        label_qs = add_prefetch_fields(models.Label.objects.all(), [
+            'skeleton',
+            'parent',
+            'attributespec_set',
+        ])
+        label_qs = JobData.add_prefetch_info(label_qs)
+
+        return queryset.select_related(
+            'segment',
+            'segment__task',
+        ).prefetch_related(
+            'segment__task__owner',
+            'segment__task__assignee',
+            'segment__task__project__owner',
+            'segment__task__project__assignee',
+
+            Prefetch('segment__task__data',
+                queryset=models.Data.objects.select_related('video').prefetch_related(
                     Prefetch('images', queryset=models.Image.objects.order_by('frame'))
-                ))
-            ).get(pk=pk)
+            )),
+
+            Prefetch('segment__task__label_set', queryset=label_qs),
+            Prefetch('segment__task__project__label_set', queryset=label_qs),
+        )
+
+    def __init__(self, pk, *, is_prefetched=False, queryset=None):
+        if queryset is None:
+            queryset = self.add_prefetch_info(models.Job.objects).all()
+
+        if is_prefetched:
+            self.db_job: models.Job = queryset.select_related(
+                'segment__task'
+            ).select_for_update().get(id=pk)
+        else:
+            try:
+                self.db_job: models.Job = next(job for job in queryset if job.pk == int(pk))
+            except StopIteration as ex:
+                raise models.Job.DoesNotExist from ex
 
         db_segment = self.db_job.segment
         self.start_frame = db_segment.start_frame
@@ -660,7 +691,9 @@ class TaskAnnotation:
         ).get(id=pk)
 
         # Postgres doesn't guarantee an order by default without explicit order_by
-        self.db_jobs = models.Job.objects.select_related("segment").filter(segment__task_id=pk).order_by('id')
+        self.db_jobs = models.Job.objects.select_related("segment").filter(
+            segment__task_id=pk, type=models.JobType.ANNOTATION.value,
+        ).order_by('id')
         self.ir_data = AnnotationIR(self.db_task.dimension)
 
     def reset(self):
@@ -711,6 +744,9 @@ class TaskAnnotation:
         self.reset()
 
         for db_job in self.db_jobs:
+            if db_job.type != models.JobType.ANNOTATION:
+                continue
+
             annotation = JobAnnotation(db_job.id, is_prefetched=True)
             annotation.init_from_db()
             if annotation.ir_data.version > self.ir_data.version:

cvat/apps/dataset_manager/util.py
@@ -3,10 +3,14 @@
 #
 # SPDX-License-Identifier: MIT
 
+from copy import deepcopy
+from typing import Sequence
 import inspect
 import os, os.path as osp
 import zipfile
+
 from django.conf import settings
+from django.db.models import QuerySet
 
 
 def current_function_name(depth=1):
@@ -35,3 +39,25 @@ def bulk_create(db_model, objects, flt_param):
             return db_model.objects.bulk_create(objects)
 
     return []
+
+def is_prefetched(queryset: QuerySet, field: str) -> bool:
+    return field in queryset._prefetch_related_lookups
+
+def add_prefetch_fields(queryset: QuerySet, fields: Sequence[str]) -> QuerySet:
+    for field in fields:
+        if not is_prefetched(queryset, field):
+            queryset = queryset.prefetch_related(field)
+
+    return queryset
+
+def deepcopy_simple(v):
+    # Default deepcopy is very slow
+
+    if isinstance(v, dict):
+        return {k: deepcopy_simple(vv) for k, vv in v.items()}
+    elif isinstance(v, (list, tuple, set)):
+        return type(v)(deepcopy_simple(vv) for vv in v)
+    elif isinstance(v, (int, float, str, bool)) or v is None:
+        return v
+    else:
+        return deepcopy(v)

cvat/apps/engine/migrations/0070_add_job_type_created_date.py
@@ -1,0 +1,54 @@
+import cvat.apps.engine.models
+from django.db import migrations, models
+import django.utils.timezone
+
+
+def add_created_date_to_existing_jobs(apps, schema_editor):
+    Job = apps.get_model("engine", "Job")
+
+    jobs = Job.objects.prefetch_related('segment__task').all()
+    for job in jobs:
+        task = job.segment.task
+        job.created_date = task.created_date
+
+    Job.objects.bulk_update(jobs, fields=['created_date'])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('engine', '0069_auto_20230608_1915'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='job',
+            name='type',
+            field=models.CharField(choices=[('annotation', 'ANNOTATION'), ('ground_truth', 'GROUND_TRUTH')], default='annotation', max_length=32),
+        ),
+        migrations.AddField(
+            model_name='segment',
+            name='frames',
+            field=cvat.apps.engine.models.IntArrayField(blank=True, default=''),
+        ),
+        migrations.AddField(
+            model_name='segment',
+            name='type',
+            field=models.CharField(choices=[('range', 'RANGE'), ('specific_frames', 'SPECIFIC_FRAMES')], default='range', max_length=32),
+        ),
+
+        migrations.AddField(
+            model_name='job',
+            name='created_date',
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now, null=True),
+            preserve_default=False,
+        ),
+        migrations.RunPython(
+            code=add_created_date_to_existing_jobs,
+        ),
+        migrations.AlterField(
+            model_name='job',
+            name='created_date',
+            field=models.DateTimeField(auto_now_add=True),
+        ),
+    ]

cvat/apps/engine/task.py
@@ -171,11 +171,7 @@ def _save_task_to_db(db_task: models.Task, *, job_file_mapping: Optional[JobFile
 
         db_job = models.Job(segment=db_segment)
         db_job.save()
-
-        job_path = db_job.get_dirname()
-        if os.path.isdir(job_path):
-            shutil.rmtree(job_path)
-        os.makedirs(job_path)
+        db_job.make_dirs()
 
     db_task.data.save()
     db_task.save()

commit 97eb6a969a7fa53981ffac1c9d5d44b0ead66a3d

Optimize DB requests (#6340)
Optimizes several DB request memory issues, introduced in #6204 

- Optimized memory use in job annotation retrieval
- Optimized migration

cvat/apps/dataset_manager/task.py
@@ -22,7 +22,7 @@ from cvat.apps.profiler import silk_profile
 from cvat.apps.dataset_manager.annotation import AnnotationIR, AnnotationManager
 from cvat.apps.dataset_manager.bindings import TaskData, JobData, CvatImportError
 from cvat.apps.dataset_manager.formats.registry import make_exporter, make_importer
-from cvat.apps.dataset_manager.util import add_prefetch_fields, bulk_create
+from cvat.apps.dataset_manager.util import add_prefetch_fields, bulk_create, get_cached
 
 
 class dotdict(OrderedDict):
@@ -105,17 +105,14 @@ class JobAnnotation:
 
     def __init__(self, pk, *, is_prefetched=False, queryset=None):
         if queryset is None:
-            queryset = self.add_prefetch_info(models.Job.objects).all()
+            queryset = self.add_prefetch_info(models.Job.objects)
 
         if is_prefetched:
             self.db_job: models.Job = queryset.select_related(
                 'segment__task'
             ).select_for_update().get(id=pk)
         else:
-            try:
-                self.db_job: models.Job = next(job for job in queryset if job.pk == int(pk))
-            except StopIteration as ex:
-                raise models.Job.DoesNotExist from ex
+            self.db_job: models.Job = get_cached(queryset, pk=int(pk))
 
         db_segment = self.db_job.segment
         self.start_frame = db_segment.start_frame

cvat/apps/dataset_manager/util.py
@@ -10,7 +10,7 @@ import os, os.path as osp
 import zipfile
 
 from django.conf import settings
-from django.db.models import QuerySet
+from django.db import models
 
 
 def current_function_name(depth=1):
@@ -40,15 +40,34 @@ def bulk_create(db_model, objects, flt_param):
 
     return []
 
-def is_prefetched(queryset: QuerySet, field: str) -> bool:
+def is_prefetched(queryset: models.QuerySet, field: str) -> bool:
     return field in queryset._prefetch_related_lookups
 
-def add_prefetch_fields(queryset: QuerySet, fields: Sequence[str]) -> QuerySet:
+def add_prefetch_fields(queryset: models.QuerySet, fields: Sequence[str]) -> models.QuerySet:
     for field in fields:
         if not is_prefetched(queryset, field):
             queryset = queryset.prefetch_related(field)
 
     return queryset
+
+def get_cached(queryset: models.QuerySet, pk: int) -> models.Model:
+    """
+    Like regular queryset.get(), but checks for the cached values first
+    instead of just making a request.
+    """
+
+    # Read more about caching insights:
+    # https://www.mattduck.com/2021-01-django-orm-result-cache.html
+    # The field is initialized on accessing the query results, eg. on iteration
+    if getattr(queryset, '_result_cache'):
+        result = next((obj for obj in queryset if obj.pk == pk), None)
+    else:
+        result = None
+
+    if result is None:
+        result = queryset.get(id=pk)
+
+    return result
 
 def deepcopy_simple(v):
     # Default deepcopy is very slow

cvat/apps/engine/migrations/0070_add_job_type_created_date.py
@@ -11,7 +11,7 @@ def add_created_date_to_existing_jobs(apps, schema_editor):
         task = job.segment.task
         job.created_date = task.created_date
 
-    Job.objects.bulk_update(jobs, fields=['created_date'])
+    Job.objects.bulk_update(jobs, fields=['created_date'], batch_size=500)
 
 
 class Migration(migrations.Migration):

cvat/apps/quality_control/quality_reports.py
@@ -2241,10 +2241,9 @@ class QualityReportUpdateManager:
             except Task.DoesNotExist:
                 return
 
-            # Try to use shared queryset to minimize DB requests
-            job_queryset = Job.objects.prefetch_related("segment")
-            job_queryset = JobDataProvider.add_prefetch_info(job_queryset)
-            job_queryset = job_queryset.filter(segment__task_id=task_id).all()
+            # Try to use a shared queryset to minimize DB requests
+            job_queryset = Job.objects.select_related("segment")
+            job_queryset = job_queryset.filter(segment__task_id=task_id)
 
             # The GT job could have been removed during scheduling, so we need to check it exists
             gt_job: Job = next(
@@ -2257,6 +2256,14 @@ class QualityReportUpdateManager:
             # - task updated (the gt job, frame set or labels changed) -> everything is computed
             # - job updated -> job report is computed
             #   old reports can be reused in this case (need to add M-1 relationship in reports)
+
+            # Add prefetch data to the shared queryset
+            # All the jobs / segments share the same task, so we can load it just once.
+            # We reuse the same object for better memory use (OOM is possible otherwise).
+            # Perform manual "join", since django can't do this.
+            gt_job = JobDataProvider.add_prefetch_info(job_queryset).get(id=gt_job.id)
+            for job in job_queryset:
+                job.segment.task = gt_job.segment.task
 
             # Preload all the data for the computations
             # It must be done in a single transaction and before all the remaining computations