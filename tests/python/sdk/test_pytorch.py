cvat-ai/cvat

commit 4fc494f4b8ca43b073174b696111ac14edf000ae

Remove collections in endpoints (#5662)
This PR is focused on server performance optimization for requests with
collections.
- Collections with user-controlled size are removed from server output
and replaced
with structures providing links to the matching paginated endpoints and
summary info:
  - /api/projects/{id}.tasks -> /api/tasks?project_id={id}
- /api/tasks/{id}.segments -> /api/jobs?task_id={id}: .start_frame,
.stop_frame
  - /api/jobs/{id}.issues -> /api/issues?job_id={id}
  - /api/issues/{id}.comments -> /api/comments?issue_id={id}
- /api/projects | tasks | jobs/{id}.labels -> /api/labels?
project_id|task_id|job_id={id}
- Added Label-related endpoints:
  - /api/labels (GET)
  - /api/labels/{id} (GET, PATCH, DELETE)
    - PATCH and DELETE both cannot be used for sublabels
- Added and updated tests
  - Removed unused test server data
  - Added several new cases with skeletons and ownerships
- Fixed possible problems with duplicate labels appearing on renaming

cvat-sdk/cvat_sdk/pytorch/caching.py
@@ -151,7 +151,7 @@ class _CacheManagerOnline(CacheManager):
         task_json_path = self.task_json_path(task.id)
 
         try:
-            saved_task = self.load_model(task_json_path, models.TaskRead)
+            saved_task = self.load_model(task_json_path, _OfflineTaskModel)
         except Exception:
             self._logger.info(f"Task {task.id} is not yet cached or the cache is corrupted")
 
@@ -166,7 +166,7 @@ class _CacheManagerOnline(CacheManager):
                 shutil.rmtree(task_dir)
 
         task_dir.mkdir(exist_ok=True, parents=True)
-        self.save_model(task_json_path, task._model)
+        self.save_model(task_json_path, _OfflineTaskModel.from_entity(task))
 
     def ensure_task_model(
         self,
@@ -224,7 +224,8 @@ class _CacheManagerOnline(CacheManager):
 class _CacheManagerOffline(CacheManager):
     def retrieve_task(self, task_id: int) -> Task:
         self._logger.info(f"Retrieving task {task_id} from cache...")
-        return Task(self._client, self.load_model(self.task_json_path(task_id), models.TaskRead))
+        cached_model = self.load_model(self.task_json_path(task_id), _OfflineTaskModel)
+        return _OfflineTaskProxy(self._client, cached_model, cache_manager=self)
 
     def ensure_task_model(
         self,
@@ -250,26 +251,71 @@ class _CacheManagerOffline(CacheManager):
 
 
 @define
-class _OfflineProjectModel(_CacheObjectModel):
-    api_model: models.IProjectRead
-    task_ids: List[int]
+class _OfflineTaskModel(_CacheObjectModel):
+    api_model: models.ITaskRead
+    labels: List[models.ILabel]
 
     def dump(self) -> _CacheObject:
         return {
             "model": to_json(self.api_model),
-            "tasks": self.task_ids,
+            "labels": to_json(self.labels),
         }
 
     @classmethod
     def load(cls, obj: _CacheObject):
         return cls(
-            api_model=obj["model"],
+            api_model=models.TaskRead._from_openapi_data(**obj["model"]),
+            labels=[models.Label._from_openapi_data(**label) for label in obj["labels"]],
+        )
+
+    @classmethod
+    def from_entity(cls, entity: Task):
+        return cls(
+            api_model=entity._model,
+            labels=entity.get_labels(),
+        )
+
+
+class _OfflineTaskProxy(Task):
+    def __init__(
+        self, client: Client, cached_model: _OfflineTaskModel, *, cache_manager: CacheManager
+    ) -> None:
+        super().__init__(client, cached_model.api_model)
+        self._offline_model = cached_model
+        self._cache_manager = cache_manager
+
+    def get_labels(self) -> List[models.ILabel]:
+        return self._offline_model.labels
+
+
+@define
+class _OfflineProjectModel(_CacheObjectModel):
+    api_model: models.IProjectRead
+    task_ids: List[int]
+    labels: List[models.ILabel]
+
+    def dump(self) -> _CacheObject:
+        return {
+            "model": to_json(self.api_model),
+            "tasks": self.task_ids,
+            "labels": to_json(self.labels),
+        }
+
+    @classmethod
+    def load(cls, obj: _CacheObject):
+        return cls(
+            api_model=models.ProjectRead._from_openapi_data(**obj["model"]),
             task_ids=obj["tasks"],
+            labels=[models.Label._from_openapi_data(**label) for label in obj["labels"]],
         )
 
     @classmethod
     def from_entity(cls, entity: Project):
-        return cls(api_model=entity._model, task_ids=[t.id for t in entity.get_tasks()])
+        return cls(
+            api_model=entity._model,
+            task_ids=[t.id for t in entity.get_tasks()],
+            labels=entity.get_labels(),
+        )
 
 
 class _OfflineProjectProxy(Project):
@@ -282,6 +328,9 @@ class _OfflineProjectProxy(Project):
 
     def get_tasks(self) -> List[Task]:
         return [self._cache_manager.retrieve_task(t) for t in self._offline_model.task_ids]
+
+    def get_labels(self) -> List[models.ILabel]:
+        return self._offline_model.labels
 
 
 _CACHE_MANAGER_CLASSES: Mapping[UpdatePolicy, Type[CacheManager]] = {

tests/python/sdk/test_pytorch.py
@@ -83,7 +83,7 @@ class TestTaskVisionDataset:
             data_params={"chunk_size": 3},
         )
 
-        self.label_ids = sorted(l.id for l in self.task.labels)
+        self.label_ids = sorted(l.id for l in self.task.get_labels())
 
         self.task.update_annotations(
             models.PatchedLabeledDataRequest(
@@ -222,7 +222,7 @@ class TestTaskVisionDataset:
         assert isinstance(dataset[0][1], PIL.Image.Image)
 
     def test_custom_label_mapping(self):
-        label_name_to_id = {label.name: label.id for label in self.task.labels}
+        label_name_to_id = {label.name: label.id for label in self.task.get_labels()}
 
         dataset = cvatpt.TaskVisionDataset(
             self.client,
@@ -275,7 +275,7 @@ class TestProjectVisionDataset:
                 ],
             )
         )
-        self.label_ids = sorted(l.id for l in self.project.labels)
+        self.label_ids = sorted(l.id for l in self.project.get_labels())
 
         subsets = ["Train", "Test", "Val"]
         num_images_per_task = 3
@@ -356,7 +356,7 @@ class TestProjectVisionDataset:
         self._test_filtering(include_subsets={self.tasks[1].subset, self.tasks[2].subset})
 
     def test_custom_label_mapping(self):
-        label_name_to_id = {label.name: label.id for label in self.project.labels}
+        label_name_to_id = {label.name: label.id for label in self.project.get_labels()}
 
         dataset = cvatpt.ProjectVisionDataset(
             self.client, self.project.id, label_name_to_index={"person": 123, "car": 456}

commit 65999f025de3667b5f0aa4c707bd8e42787e64f5

PyTorch adapter: fix loading tasks that have already been cached
This was broken in 4fc494f4.

Add a test to cover this case.

cvat-sdk/cvat_sdk/pytorch/caching.py
@@ -159,7 +159,7 @@ class _CacheManagerOnline(CacheManager):
             if task_dir.exists():
                 shutil.rmtree(task_dir)
         else:
-            if saved_task.updated_date < task.updated_date:
+            if saved_task.api_model.updated_date < task.updated_date:
                 self._logger.info(
                     f"Task {task.id} has been updated on the server since it was cached; purging the cache"
                 )

tests/python/sdk/test_pytorch.py
@@ -7,7 +7,8 @@ import itertools
 import os
 from logging import Logger
 from pathlib import Path
-from typing import Tuple
+from typing import Container, Tuple
+from urllib.parse import urlparse
 
 import pytest
 from cvat_sdk import Client, models
@@ -43,11 +44,18 @@ def _common_setup(
         api_client.configuration.logger[k] = logger
 
 
-def _disable_api_requests(monkeypatch: pytest.MonkeyPatch) -> None:
-    def disabled_request(*args, **kwargs):
-        raise RuntimeError("Disabled!")
+def _restrict_api_requests(
+    monkeypatch: pytest.MonkeyPatch, allow_paths: Container[str] = ()
+) -> None:
+    original_request = RESTClientObject.request
 
-    monkeypatch.setattr(RESTClientObject, "request", disabled_request)
+    def restricted_request(self, method, url, *args, **kwargs):
+        parsed_url = urlparse(url)
+        if parsed_url.path in allow_paths:
+            return original_request(self, method, url, *args, **kwargs)
+        raise RuntimeError("Disallowed!")
+
+    monkeypatch.setattr(RESTClientObject, "request", restricted_request)
 
 
 @pytest.mark.skipif(cvatpt is None, reason="PyTorch dependencies are not installed")
@@ -243,7 +251,7 @@ class TestTaskVisionDataset:
 
         fresh_samples = list(dataset)
 
-        _disable_api_requests(monkeypatch)
+        _restrict_api_requests(monkeypatch)
 
         dataset = cvatpt.TaskVisionDataset(
             self.client,
@@ -254,6 +262,44 @@ class TestTaskVisionDataset:
         cached_samples = list(dataset)
 
         assert fresh_samples == cached_samples
+
+    def test_update(self, monkeypatch: pytest.MonkeyPatch):
+        dataset = cvatpt.TaskVisionDataset(
+            self.client,
+            self.task.id,
+        )
+
+        # Recreating the dataset should only result in minimal requests.
+        _restrict_api_requests(
+            monkeypatch, allow_paths={f"/api/tasks/{self.task.id}", "/api/labels"}
+        )
+
+        dataset = cvatpt.TaskVisionDataset(
+            self.client,
+            self.task.id,
+        )
+
+        assert dataset[5][1].annotations.tags[0].label_id == self.label_ids[0]
+
+        # After an update, the annotations should be redownloaded.
+        monkeypatch.undo()
+
+        self.task.update_annotations(
+            models.PatchedLabeledDataRequest(
+                tags=[
+                    models.LabeledImageRequest(
+                        id=dataset[5][1].annotations.tags[0].id, frame=5, label_id=self.label_ids[1]
+                    ),
+                ]
+            )
+        )
+
+        dataset = cvatpt.TaskVisionDataset(
+            self.client,
+            self.task.id,
+        )
+
+        assert dataset[5][1].annotations.tags[0].label_id == self.label_ids[1]
 
 
 @pytest.mark.skipif(cvatpt is None, reason="PyTorch dependencies are not installed")
@@ -398,7 +444,7 @@ class TestProjectVisionDataset:
 
         fresh_samples = list(dataset)
 
-        _disable_api_requests(monkeypatch)
+        _restrict_api_requests(monkeypatch)
 
         dataset = cvatpt.ProjectVisionDataset(
             self.client,