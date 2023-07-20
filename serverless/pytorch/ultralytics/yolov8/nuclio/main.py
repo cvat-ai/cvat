35538eb1cfb6231e96fd05bd1bacffacd7a0ce0b
Hardik Dava <39372750+hardikdava@users.noreply.github.com>
2023-07-20 12:23:31 +0200

    Update main.py

--- serverless/pytorch/ultralytics/yolov8/nuclio/main.py
+++ serverless/pytorch/ultralytics/yolov8/nuclio/main.py
@@ -5,19 +5,14 @@ import io
 from ultralytics import YOLO
 import supervision as sv
 
-
 def init_context(context):
     context.logger.info("Init context...  0%")
-
+    # Change model path for custom model
     model_path = "yolov8m.pt"
-
     model = YOLO(model_path)
-
     # Read the DL model
     context.user_data.model = model
-
     context.logger.info("Init context...100%")
-
 
 def handler(context, event):
     context.logger.info("Run yolo-v8 model")
@@ -26,22 +21,17 @@ def handler(context, event):
     threshold = float(data.get("threshold", 0.5))
     context.user_data.model.conf = threshold
     image = Image.open(buf)
-
     yolo_results = context.user_data.model(image, conf=threshold)[0]
     labels = yolo_results.names
     detections = sv.Detections.from_yolov8(yolo_results)
-
     detections = detections[detections.confidence > threshold]
     boxes = detections.xyxy
     conf = detections.confidence
     class_ids = detections.class_id
-
-    results = []
     
+    results = []    
     if boxes.shape[0] > 0:
-
-        for label, score, box in zip(class_ids, conf, boxes):
-          
+        for label, score, box in zip(class_ids, conf, boxes):          
             xtl = int(box[0])
             ytl = int(box[1])
             xbr = int(box[2])
