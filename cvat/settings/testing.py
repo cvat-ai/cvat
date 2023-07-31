64ee4495187f17579efb4b14743f2250e6486666
Andrey Zhavoronkov <andrey@cvat.ai>
2023-07-31 13:47:19 +0300

    Using a separate event cache file for each process

--- cvat/settings/testing.py
+++ cvat/settings/testing.py
@@ -18,11 +18,6 @@ BASE_DIR = _temp_dir.name
 DATA_ROOT = os.path.join(BASE_DIR, 'data')
 os.makedirs(DATA_ROOT, exist_ok=True)
 
-EVENTS_LOCAL_DB = os.path.join(DATA_ROOT, 'events.db')
-os.makedirs(DATA_ROOT, exist_ok=True)
-if not os.path.exists(EVENTS_LOCAL_DB):
-    open(EVENTS_LOCAL_DB, 'w').close()
-
 MEDIA_DATA_ROOT = os.path.join(DATA_ROOT, 'data')
 os.makedirs(MEDIA_DATA_ROOT, exist_ok=True)
 
