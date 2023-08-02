4bbfb7fbdbcddaadc7d892d677c284a78769a70c
skall <skall@sewerai.com>
2023-08-02 18:04:18 -0500

    realign

--- cvat-ui/src/containers/annotation-page/top-bar/annotation-menu.tsx
+++ cvat-ui/src/containers/annotation-page/top-bar/annotation-menu.tsx
@@ -45,7 +45,7 @@ function mapStateToProps(state: CombinedState): StateToProps {
                 instance: jobInstance,
                 instance: { stopFrame },
             },
-        }
+        },
     } = state;
 
     return {
