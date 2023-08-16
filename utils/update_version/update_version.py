1dad2a5394174720997f1fb1a8300935e1e139e4
Roman Donchenko <roman@cvat.ai>
2023-08-16 13:29:25 +0300

    update_version.py: factor out common code in replacement rules

--- utils/update_version/update_version.py
+++ utils/update_version/update_version.py
@@ -1,21 +1,20 @@
 import argparse
+import functools
+import re
 from dataclasses import dataclass
 from pathlib import Path
-import re
+from typing import Callable, Match, Pattern
 
 
 SUCCESS_CHAR = '\u2714'
 FAIL_CHAR = '\u2716'
 
 CVAT_VERSION_PATTERN = re.compile(r'VERSION\s*=\s*\((\d+),\s*(\d*),\s*(\d+),\s*[\',\"](\w+)[\',\"],\s*(\d+)\)')
-COMPOSE_VERSION_PATTERN = re.compile(r'(\$\{CVAT_VERSION:-)([\w.]+)(\})')
-HELM_VERSION_PATTERN = re.compile(r'(^    image: cvat/(?:ui|server)\n    tag: )([\w.]+)', re.M)
 
 REPO_ROOT_DIR = Path(__file__).resolve().parents[2]
 
-CVAT_INIT_PY_PATH = REPO_ROOT_DIR / 'cvat/__init__.py'
-COMPOSE_FILE_PATH = REPO_ROOT_DIR / 'docker-compose.yml'
-HELM_VALUES_PATH = REPO_ROOT_DIR / 'helm-chart/values.yaml'
+CVAT_INIT_PY_REL_PATH = 'cvat/__init__.py'
+CVAT_INIT_PY_PATH = REPO_ROOT_DIR / CVAT_INIT_PY_REL_PATH
 
 @dataclass()
 class Version:
@@ -32,6 +31,8 @@ class Version:
         return f"({self.major}, {self.minor}, {self.patch}, '{self.prerelease}', {self.prerelease_number})"
 
     def compose_repr(self):
+        if self.prerelease != 'final':
+            return 'dev'
         return f'v{self.major}.{self.minor}.{self.patch}'
 
     def increment_prerelease_number(self) -> None:
@@ -81,68 +82,49 @@ class Version:
         self.minor = 0
         self._set_default_patch()
 
-def update_compose_config(new_version: Version) -> None:
-    compose_text = COMPOSE_FILE_PATH.read_text()
+@dataclass(frozen=True)
+class ReplacementRule:
+    rel_path: str
+    pattern: Pattern[str]
+    replacement: Callable[[Version, Match[str]], str]
 
-    if new_version.prerelease == 'final':
-        new_version_repr = new_version.compose_repr()
-    else:
-        new_version_repr = 'dev'
+    def apply(self, new_version: Version) -> None:
+        path = REPO_ROOT_DIR / self.rel_path
+        text = path.read_text()
 
-    match = re.search(COMPOSE_VERSION_PATTERN, compose_text)
-    if not match:
-        raise RuntimeError('Cannot match version pattern')
+        new_text, num_replacements = self.pattern.subn(
+            functools.partial(self.replacement, new_version), text)
 
-    if match[2] != new_version_repr:
-        compose_text = re.sub(COMPOSE_VERSION_PATTERN, f'\\g<1>{new_version_repr}\\g<3>', compose_text)
-        COMPOSE_FILE_PATH.write_text(compose_text)
+        if not num_replacements:
+            raise RuntimeError('Cannot match version pattern')
 
-        print(f'{SUCCESS_CHAR} {COMPOSE_FILE_PATH} was updated. {match[2]} -> {new_version_repr}\n')
+        if text == new_text:
+            print(f'{SUCCESS_CHAR} {self.rel_path}: no need to update.')
+        else:
+            path.write_text(new_text)
+            print(f'{SUCCESS_CHAR} {self.rel_path}: updated.')
 
-    else:
-        print(f'{SUCCESS_CHAR} {COMPOSE_FILE_PATH} no need to update.')
+REPLACEMENT_RULES = [
+    ReplacementRule(CVAT_INIT_PY_REL_PATH, CVAT_VERSION_PATTERN,
+        lambda v, m: f'VERSION = {v.cvat_repr()}'),
 
-def update_cvat_version(old_version: str, new_version: Version) -> None:
-    version_text = CVAT_INIT_PY_PATH.read_text()
+    ReplacementRule('docker-compose.yml',
+        re.compile(r'(\$\{CVAT_VERSION:-)([\w.]+)(\})'),
+        lambda v, m: m[1] + v.compose_repr() + m[3]),
 
-    new_version_str = f'VERSION = {new_version.cvat_repr()}'
-    version_text = version_text.replace(old_version, new_version_str)
+    ReplacementRule('helm-chart/values.yaml',
+        re.compile(r'(^    image: cvat/(?:ui|server)\n    tag: )([\w.]+)', re.M),
+        lambda v, m: m[1] + v.compose_repr()),
+]
 
-    CVAT_INIT_PY_PATH.write_text(version_text)
-
-    print(f'{SUCCESS_CHAR} {CVAT_INIT_PY_PATH} was updated. {old_version} -> {new_version_str}\n')
-
-def update_helm_version(new_version: Version) -> None:
-    helm_values_text = HELM_VALUES_PATH.read_text()
-
-    if new_version.prerelease == 'final':
-        new_version_repr = new_version.compose_repr()
-    else:
-        new_version_repr = 'dev'
-
-    match = re.search(HELM_VERSION_PATTERN, helm_values_text)
-    if not match:
-        raise RuntimeError('Cannot match version pattern')
-
-    if match[2] != new_version_repr:
-        helm_values_text = re.sub(HELM_VERSION_PATTERN, f'\\g<1>{new_version_repr}', helm_values_text, 2)
-        HELM_VALUES_PATH.write_text(helm_values_text)
-
-        print(f'{SUCCESS_CHAR} {HELM_VALUES_PATH} was updated. {match[2]} -> {new_version_repr}\n')
-
-    else:
-        print(f'{SUCCESS_CHAR} {HELM_VALUES_PATH} no need to update.')
-
-
-def get_current_version() -> 'tuple[str, Version]':
+def get_current_version() -> Version:
     version_text = CVAT_INIT_PY_PATH.read_text()
 
     match = re.search(CVAT_VERSION_PATTERN, version_text)
     if not match:
         raise RuntimeError(f'Failed to find version in {CVAT_INIT_PY_PATH}')
 
-    version = Version(int(match[1]), int(match[2]), int(match[3]), match[4], int(match[5]))
-    return match[0], version
+    return Version(int(match[1]), int(match[2]), int(match[3]), match[4], int(match[5]))
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Bump CVAT version')
@@ -166,7 +148,7 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    version_str, version = get_current_version()
+    version = get_current_version()
 
     if args.current:
         print(version)
@@ -195,9 +177,8 @@ def main() -> None:
 
     print(f'{SUCCESS_CHAR} Bump version to {version}\n')
 
-    update_cvat_version(version_str, version)
-    update_compose_config(version)
-    update_helm_version(version)
+    for rule in REPLACEMENT_RULES:
+        rule.apply(version)
 
 if __name__ == '__main__':
     main()
