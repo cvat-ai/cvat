9b1ab3e2542202f75c9cd97c8637a24524584f99
Boris Sekachev <sekachev.bs@gmail.com>
2023-08-22 19:34:36 +0300

    Fixed invisible progress when open task

--- cvat-ui/src/components/task-page/task-page.tsx
+++ cvat-ui/src/components/task-page/task-page.tsx
@@ -6,17 +6,18 @@
 import './styles.scss';
 import React, { useEffect, useRef, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'antd/lib/grid';
 import Spin from 'antd/lib/spin';
 import Result from 'antd/lib/result';
 import notification from 'antd/lib/notification';
 
+import { getInferenceStatusAsync } from 'actions/models-actions';
 import { getCore, Task, Job } from 'cvat-core-wrapper';
 import JobListComponent from 'components/task-page/job-list';
 import ModelRunnerModal from 'components/model-runner-modal/model-runner-dialog';
 import CVATLoadingSpinner from 'components/common/loading-spinner';
 import MoveTaskModal from 'components/move-task-modal/move-task-modal';
-import { useSelector } from 'react-redux';
 import { CombinedState } from 'reducers';
 import TopBarComponent from './top-bar';
 import DetailsComponent from './details';
@@ -26,13 +27,14 @@ const core = getCore();
 function TaskPageComponent(): JSX.Element {
     const history = useHistory();
     const id = +useParams<{ id: string }>().id;
-
+    const dispatch = useDispatch();
     const [taskInstance, setTaskInstance] = useState<Task | null>(null);
     const [fetchingTask, setFetchingTask] = useState(true);
     const [updatingTask, setUpdatingTask] = useState(false);
     const mounted = useRef(false);
 
     const deletes = useSelector((state: CombinedState) => state.tasks.activities.deletes);
+    dispatch(getInferenceStatusAsync());
 
     const receieveTask = (): void => {
         if (Number.isInteger(id)) {
