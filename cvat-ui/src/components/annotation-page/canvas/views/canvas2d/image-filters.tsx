14931851e6c7a97b187e40fefa5ec26756e68f4e
klakhov <kirill.9992@gmail.com>
2023-09-04 13:29:27 +0300

    removed rgb sliders

--- cvat-ui/src/components/annotation-page/canvas/views/canvas2d/image-filters.tsx
+++ cvat-ui/src/components/annotation-page/canvas/views/canvas2d/image-filters.tsx
@@ -6,7 +6,6 @@ import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'antd/lib/grid';
 import { CombinedState } from 'reducers';
-import Checkbox, { CheckboxChangeEvent } from 'antd/lib/checkbox';
 import Text from 'antd/lib/typography/Text';
 import Slider from 'antd/lib/slider';
 
@@ -22,16 +21,25 @@ import './image-setups.scss';
 
 export default function ImageFilters(): JSX.Element {
     const dispatch = useDispatch();
-    const [gamma, setGamma] = useState<number[]>([1.5, 1.5, 1.5]);
+    const [gamma, setGamma] = useState<number>(1);
     const filters = useSelector((state: CombinedState) => state.settings.imageProcessing.filters);
     const gammaFilterActive = filterActive(filters, ImageFilterAlias.GAMMA_CORRECTION);
     const onChangeGamma = useCallback((newGamma) => {
         setGamma(newGamma);
-        if (gammaFilterActive) {
+        if (newGamma === 1) {
+            if (gammaFilterActive) {
+                dispatch(removeImageFilter(ImageFilterAlias.GAMMA_CORRECTION));
+            }
+        } else if (gammaFilterActive) {
             dispatch(setupImageFilter(
                 ImageFilterAlias.GAMMA_CORRECTION,
-                { gamma: newGamma },
+                { gamma: [newGamma, newGamma, newGamma] },
             ));
+        } else {
+            dispatch(addImageFilter({
+                modifier: new GammaCorrection({ gamma: [newGamma, newGamma, newGamma] }),
+                alias: ImageFilterAlias.GAMMA_CORRECTION,
+            }));
         }
     }, [filters]);
 
@@ -43,63 +51,17 @@ export default function ImageFilters(): JSX.Element {
                 <Col span={24}>
                     <Row className='cvat-image-setups-gamma'>
                         <Col span={6}>
-                            <Text className='cvat-text-color'> Gamma </Text>
-                        </Col>
-                        <Col>
-                            <Checkbox
-                                checked={gammaFilterActive}
-                                onChange={(e: CheckboxChangeEvent) => {
-                                    if (e.target.checked) {
-                                        dispatch(addImageFilter({
-                                            modifier: new GammaCorrection({ gamma }),
-                                            alias: ImageFilterAlias.GAMMA_CORRECTION,
-                                        }));
-                                    } else {
-                                        dispatch(removeImageFilter(ImageFilterAlias.GAMMA_CORRECTION));
-                                    }
-                                }}
-                            />
+                            <Text className='cvat-text-color'> Gamma: </Text>
                         </Col>
                     </Row>
                     <Row>
-                        <Col span={6}>
-                            <Text>Red:</Text>
-                        </Col>
                         <Col span={12}>
                             <Slider
                                 min={0.2}
                                 max={2.2}
-                                value={gamma[0]}
+                                value={gamma}
                                 step={0.01}
-                                onChange={(value) => onChangeGamma([value, gamma[1], gamma[2]])}
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={6}>
-                            <Text>Green:</Text>
-                        </Col>
-                        <Col span={12}>
-                            <Slider
-                                min={0.2}
-                                max={2.2}
-                                value={gamma[1]}
-                                step={0.01}
-                                onChange={(value) => onChangeGamma([gamma[0], value, gamma[2]])}
-                            />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col span={6}>
-                            <Text>Blue:</Text>
-                        </Col>
-                        <Col span={12}>
-                            <Slider
-                                min={0.2}
-                                max={2.2}
-                                value={gamma[2]}
-                                step={0.01}
-                                onChange={(value) => onChangeGamma([gamma[0], gamma[1], value])}
+                                onChange={(value) => onChangeGamma(value)}
                             />
                         </Col>
                     </Row>
