c635bca6a71e24deedab9c1c05a6fde5db60d4cf
Maria Khrustaleva <maria@cvat.ai>
2023-09-08 13:43:06 +0200

    Add is_active to list_display

--- cvat/apps/iam/admin.py
+++ cvat/apps/iam/admin.py
@@ -8,6 +8,7 @@ from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.utils.translation import gettext_lazy as _
 
 class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_active", "is_staff")
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
