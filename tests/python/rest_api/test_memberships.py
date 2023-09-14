b717622e54012426148ac3856a996b829a50c1b2
Maria Khrustaleva <maria@cvat.ai>
2023-09-14 14:30:20 +0200

    Add REST API tests to remove users from organization

--- tests/python/rest_api/test_memberships.py
+++ tests/python/rest_api/test_memberships.py
@@ -7,9 +7,10 @@ from http import HTTPStatus
 
 import pytest
 from cvat_sdk.api_client.api_client import ApiClient, Endpoint
+from cvat_sdk.api_client.model.membership_read import MembershipRead
 from deepdiff import DeepDiff
 
-from shared.utils.config import get_method, patch_method
+from shared.utils.config import get_method, make_api_client, patch_method
 
 from .utils import CollectionSimpleFilterTestBase
 
@@ -117,3 +118,73 @@ class TestPatchMemberships:
             self._test_can_change_membership(user, membership_id, new_role)
         else:
             self._test_cannot_change_membership(user, membership_id, new_role)
+
+
+@pytest.mark.usefixtures("restore_db_per_function")
+class TestDeleteMemberships:
+    _ORG = 1
+
+    def _get_user_membership(self, user: str, api_client: ApiClient) -> MembershipRead:
+        (memberships, _) = api_client.memberships_api.list(user=user, org_id=self._ORG)
+        assert 1 == memberships["count"]
+        return memberships["results"][0]
+
+    def _test_delete_membership(
+        self,
+        active_user: str,
+        user_to_be_deleted: str,
+        is_allow: bool,
+    ) -> None:
+        expected_status = HTTPStatus.NO_CONTENT if is_allow else HTTPStatus.FORBIDDEN
+
+        with make_api_client(active_user) as api_client:
+            membership = self._get_user_membership(user_to_be_deleted, api_client)
+            (_, response) = api_client.memberships_api.destroy(membership.id, _check_status=False)
+            assert response.status == expected_status
+
+    @pytest.mark.parametrize(
+        "role, is_allow",
+        [
+            ("worker", True),
+            ("supervisor", True),
+            ("maintainer", True),
+            ("owner", False),
+        ],
+    )
+    def test_member_can_leave_organization(self, role, is_allow, find_users):
+        user = find_users(role=role, org=self._ORG)[0]["username"]
+
+        self._test_delete_membership(user, user, is_allow)
+
+    @pytest.mark.parametrize(
+        "active_role, desired_role, is_allow",
+        [
+            ("worker", "worker", False),
+            ("worker", "supervisor", False),
+            ("worker", "maintainer", False),
+            ("worker", "owner", False),
+            ("supervisor", "worker", False),
+            ("supervisor", "supervisor", False),
+            ("supervisor", "maintainer", False),
+            ("supervisor", "owner", False),
+            ("maintainer", "worker", True),
+            ("maintainer", "supervisor", True),
+            ("maintainer", "maintainer", False),
+            ("maintainer", "owner", False),
+            ("owner", "worker", True),
+            ("owner", "supervisor", True),
+            ("owner", "maintainer", True),
+        ],
+    )
+    def test_member_can_exclude_another_organization_member(
+        self,
+        active_role: str,
+        desired_role: str,
+        is_allow: bool,
+        find_users,
+    ):
+        active_user = find_users(role=active_role, org=self._ORG)[0]["username"]
+        desired_user = find_users(role=desired_role, org=self._ORG, exclude_username=active_user)[
+            0
+        ]["username"]
+        self._test_delete_membership(active_user, desired_user, is_allow)
