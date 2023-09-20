bceece0a96aa0736e4a69a51cff6fe1877c03ea4
skall <skall@sewerai.com>
2023-09-20 14:09:41 -0500

    fix deploy command

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 docker-compose down
-CVAT_HOST=ec2-44-234-209-83.us-west-2.compute.amazonaws.com DOCKER_BUILDKIT=1 docker compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml up -d --build
+CVAT_HOST=ec2-44-234-209-83.us-west-2.compute.amazonaws.com DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml up -d --build
 
 
