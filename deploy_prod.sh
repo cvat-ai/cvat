0c959bd51b17e6d79f397755a909875f36159e27
skall <skall@sewerai.com>
2023-09-20 14:08:42 -0500

    post-merge fixes

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 docker-compose down
-DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml build
-CVAT_HOST=ec2-44-234-209-83.us-west-2.compute.amazonaws.com docker-compose up -d
+CVAT_HOST=ec2-44-234-209-83.us-west-2.compute.amazonaws.com DOCKER_BUILDKIT=1 docker compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml up -d --build
+
 
