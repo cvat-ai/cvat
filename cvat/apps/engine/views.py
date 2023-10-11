3268628e3153298e10b25418d05d4e434259fcc5
Maria Khrustaleva <maria@cvat.ai>
2023-10-11 10:17:24 +0200

    Migrate to new version rq

--- cvat/apps/engine/views.py
+++ cvat/apps/engine/views.py
@@ -2988,7 +2988,7 @@ def _export_annotations(db_instance, rq_id, request, format_name, action, callba
             rq_job.delete()
         else:
             if rq_job.is_finished:
-                file_path = rq_job.return_value
+                file_path = rq_job.return_value()
                 if action == "download" and osp.exists(file_path):
                     rq_job.delete()
 
