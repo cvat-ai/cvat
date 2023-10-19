2933a93c9b2f328b59b2242604d6ddeb966dc087
klakhov <kirill.9992@gmail.com>
2023-10-19 13:35:43 +0300

    updated ignore patterns

--- cvat-ui/.eslintrc.js
+++ cvat-ui/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ignorePatterns: [
         '.eslintrc.js',
         'webpack.config.js',
-        'src/assets/opencv_4.5.4_dev.js',
+        'src/assets/opencv*.js',
         'node_modules/**',
         'dist/**',
     ],
