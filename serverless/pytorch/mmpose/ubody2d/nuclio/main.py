da2285a52524feebd926255a4a16618ae3db5c1e
Boris Sekachev <boris@cvat.ai>
2023-10-30 14:22:47 +0200

    Rewritten with clsas

--- serverless/pytorch/mmpose/ubody2d/nuclio/main.py
+++ serverless/pytorch/mmpose/ubody2d/nuclio/main.py
@@ -5,47 +5,27 @@ import yaml
 import numpy as np
 from PIL import Image
 
-from mmpose.apis import inference_topdown
-from mmpose.apis import init_model as init_pose_estimator
-from mmdet.apis import inference_detector, init_detector
-from mmpose.evaluation.functional import nms
-from mmpose.structures import merge_data_samples
+from mmpose.apis import inference_topdown, MMPoseInferencer
 from mmpose.utils import adapt_mmdet_pipeline
 
-def process_one_image(img, detector, pose_estimator, threshold):
-    category_ids = [0]
-    # predict bbox
-    det_result = inference_detector(detector, img)
-    pred_instances = det_result.pred_instances.cpu().numpy()
-    bboxes = np.concatenate(
-        (pred_instances.bboxes, pred_instances.scores[:, None]), axis=1)
-    bboxes = bboxes[np.logical_and(pred_instances.labels == category_ids,
-                                   pred_instances.scores > threshold)]
-    bboxes = bboxes[nms(bboxes, threshold), :4]
-
-    # predict keypoints
-    pose_results = inference_topdown(pose_estimator, img, bboxes)
-    data_samples = merge_data_samples(pose_results)
-
-    # if there is no instance detected, return None
-    return data_samples.get('pred_instances', None)
-
-
 def init_context(context):
+    context.logger.info("Init detector...")
+
     det_config = '/opt/nuclio/mmpose/projects/rtmpose/rtmdet/person/rtmdet_nano_320-8xb32_coco-person.py'
     det_checkpoint = '/opt/nuclio/rtmdet_nano_8xb32-100e_coco-obj365-person-05d8511e.pth'
     pose_config = '/opt/nuclio/mmpose/configs/wholebody_2d_keypoint/topdown_heatmap/ubody2d/td-hm_hrnet-w32_8xb64-210e_ubody-256x192.py'
     pose_checkpoint = '/opt/nuclio/td-hm_hrnet-w32_8xb64-210e_ubody-coco-256x192-7c227391_20230807.pth'
 
-    # build person detector
-    context.logger.info("Init detector...")
-    detector = init_detector(det_config, det_checkpoint, device='cpu')
-    detector.cfg = adapt_mmdet_pipeline(detector.cfg)
+    inferencer = MMPoseInferencer(
+        pose2d=pose_config,
+        pose2d_weights=pose_checkpoint,
+        det_model=det_config,
+        det_weights=det_checkpoint,
+        det_cat_ids=[0],  # the category id of 'human' class
+        device='cpu'
+    )
 
     # build pose estimator
-    context.logger.info("Init pose estimator...")
-    pose_estimator = init_pose_estimator(pose_config, pose_checkpoint, device='cpu')
-
     context.logger.info("Init labels...")
     with open("/opt/nuclio/function.yaml", 'rb') as function_file:
         functionconfig = yaml.safe_load(function_file)
@@ -60,50 +40,39 @@ def init_context(context):
         } for item in json.loads(labels_spec)]
 
     context.user_data.labels = labels
-    context.user_data.detector = detector
-    context.user_data.pose_estimator = pose_estimator
-
+    context.user_data.inferencer = inferencer
     context.logger.info("Function initialized")
 
 def handler(context, event):
     context.logger.info("Run mmpose ubody-2d model")
     data = event.body
     buf = io.BytesIO(base64.b64decode(data["image"]))
-    threshold = float(data.get("threshold", 0.3))
     image = Image.open(buf).convert('RGB')
 
     results = []
-    pred_instances = process_one_image(
-        np.array(image)[...,::-1],
-        context.user_data.detector,
-        context.user_data.pose_estimator,
-        threshold
-    )
+    pred_instances = next(context.user_data.inferencer(np.array(image)[...,::-1]))['predictions'][0]
 
-    if pred_instances:
-        keypoints = pred_instances["keypoints"]
-        keypoint_scores = pred_instances["keypoint_scores"]
+    for pred_instance in pred_instances:
+        keypoints = pred_instance["keypoints"]
+        keypoint_scores = pred_instance["keypoint_scores"]
+        for label in context.user_data.labels:
+            skeleton = {
+                "confidence": 1,
+                "label": label["name"],
+                "type": "skeleton",
+                "elements": [{
+                    "label": label["elements"][j]["name"],
+                    "type": "points",
+                    "outside": 0 if keypoint_scores[j - 1] > 0.66 and keypoint_scores[j  - 1] < 0.98 else 1,
+                    "points": [
+                        float(keypoints[j - 1][0]),
+                        float(keypoints[j - 1][1])
+                    ],
+                    "confidence": str(keypoint_scores[j  - 1]),
+                } for j in label["elements"]],
+            }
 
-        for i, instance_keypoints in enumerate(keypoints):
-            instance_scores = keypoint_scores[i]
-
-            for label in context.user_data.labels:
-                skeleton = {
-                    "confidence": 1,
-                    "label": label["name"],
-                    "type": "skeleton",
-                    "elements": [{
-                        "label": label["elements"][j]["name"],
-                        "type": "points",
-                        "outside": 0 if instance_scores[j - 1] > 0.66 and instance_scores[j  - 1] < 0.98 else 1,
-                        "points": [
-                            float(instance_keypoints[j - 1][0]),
-                            float(instance_keypoints[j - 1][1])
-                        ],
-                        "confidence": str(instance_scores[j  - 1]),
-                    } for j in label["elements"]],
-                }
-                if not all([element['outside'] for element in skeleton['elements']]):
-                    results.append(skeleton)
+            if not all([element['outside'] for element in skeleton['elements']]):
+                results.append(skeleton)
 
     return context.Response(body=json.dumps(results), headers={}, content_type='application/json', status_code=200)
