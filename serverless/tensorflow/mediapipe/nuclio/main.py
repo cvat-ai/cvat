6c6b9f13862bed900f7e60861df2a52d79c06fc8
Paul Khimyack <paul.khimyack@gmail.com>
2023-11-10 17:37:15 +0600

    add mediapipe skeleton

--- serverless/tensorflow/mediapipe/nuclio/main.py
+++ serverless/tensorflow/mediapipe/nuclio/main.py
@@ -31,7 +31,6 @@ def init_context(context):
     with open("/opt/nuclio/function.yaml", "rb") as function_file:
         functionconfig = yaml.safe_load(function_file)
         labels_spec = functionconfig['metadata']['annotations']['spec']
-        #labels = {item['id']: item['name'] for item in json.loads(labels_spec)}
         labels = json.loads(labels_spec)
 
     context.user_data.labels =  labels
@@ -43,13 +42,6 @@ def init_context(context):
 def handler(context, event):
     context.logger.info("Run mediapipe 33kps model")
 
-   # landmark_labels = ['nose', 'left_eye_inner', 'left_eye', 'left_eye_outer', 'right_eye_inner',
-   #          'right_eye', 'right_eye_outer', 'left_ear', 'right_ear', 'mouth_left',
-   #          'mouth_right', 'left_shoulder', 'right_shoulder ', 'left_elbow', 'right_elbow',
-   #          'left_wrist', 'right_wrist', 'left_pinky', 'right_pinky', 'left_index', 'right_index',
-   #          'left_thumb', 'right_thumb ', 'left_hip', 'right_hip', 'left_knee', 'right_knee',
-   #          'left_ankle', 'right_ankle ', 'left_heel', 'right_heel', 'left_foot_index', 'right_foot_index']
-
     data = event.body
     threshold = data.get("threshold", 0.55)
     buf = io.BytesIO(base64.b64decode(data["image"].encode('utf-8')))
@@ -58,17 +50,12 @@ def handler(context, event):
     image_format=mp.ImageFormat.SRGB, data=np.asarray(pil_img)
     )
 
-
-    #results.pose_landmarks.landmark
     detection_result = context.user_data.model_handler(image)
     pose_landmarks_instances = detection_result.pose_landmarks
 
     results = []
     for pose_landmarks_instance in pose_landmarks_instances:
-        #for landmark in pose_landmarks_instance:
         for label in context.user_data.labels:
-            #scaled_x = int(landmark.x * image.width)
-            #scaled_y = int(landmark.y * image.height)
             skeleton = {
                 "confidence": "1", #str(pred_instance["bbox_score"]),
                 "label": label["name"],
@@ -76,11 +63,10 @@ def handler(context, event):
                 "elements": [{
                     "label": element["name"],
                     "type": "points",
+                    # @NOTICE supose outside attribute  correspondents to presence  in mediapipe
                     "outside": 0 if threshold < pose_landmarks_instance[element["id"]].presence else 1,
+                    # @NOTICE supose occluded attriute correspondents visibility in mediapipe
                     "occluded": 0 if threshold < pose_landmarks_instance[element["id"]].visibility else 1,
-                    "presence_origin": float(pose_landmarks_instance[element["id"]].presence),
-                    "visibility_origin": float(pose_landmarks_instance[element["id"]].visibility),
-                    "id_origin": int(element["id"]),
                     "points": [
                         float(pose_landmarks_instance[element["id"]].x * image.width),
                         float(pose_landmarks_instance[element["id"]].y * image.height)
