f0c329d00b354adbe57d7abd6bb2755afc91a1a0
Kirill Lakhov <kirill.9992@gmail.com>
2023-11-24 20:19:44 +0300

    updated default opacity

--- cvat-ui/src/reducers/settings-reducer.ts
+++ cvat-ui/src/reducers/settings-reducer.ts
@@ -17,8 +17,8 @@ import { clampOpacity } from 'utils/opacity-checker';
 const defaultState: SettingsState = {
     shapes: {
         colorBy: ColorBy.LABEL,
-        opacity: 3,
-        selectedOpacity: 30,
+        opacity: 30,
+        selectedOpacity: 60,
         outlined: false,
         outlineColor: '#000000',
         showBitmap: false,
