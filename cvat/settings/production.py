b04c4b423d14346cdfa7a10748e4e05f376ecdd7
Podesta <11512008+Podesta@users.noreply.github.com>
2023-11-25 00:13:58 +0000

    feat: Disable django disk usage health check

--- cvat/settings/production.py
+++ cvat/settings/production.py
@@ -12,3 +12,9 @@ NUCLIO['HOST'] = os.getenv('CVAT_NUCLIO_HOST', 'nuclio')
 # https://github.com/moggers87/django-sendfile2
 SENDFILE_BACKEND = 'django_sendfile.backends.nginx'
 SENDFILE_URL = '/'
+
+# Checking for disk usage should be the user responsibility
+# https://django-health-check.readthedocs.io/en/stable/settings.html
+HEALTH_CHECK = {
+    'DISK_USAGE_MAX': None,
+}
