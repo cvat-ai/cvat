5cc875ee9e457006c40338850c57796a3183ada6
Kirill Lakhov <kirill.9992@gmail.com>
2024-02-14 13:49:28 +0300

    rewritten quality component with state/reducer

--- cvat-ui/src/components/analytics-page/task-quality/task-quality-component.tsx
+++ cvat-ui/src/components/analytics-page/task-quality/task-quality-component.tsx
@@ -10,8 +10,9 @@ import JobItem from 'components/job-item/job-item';
 import {
     Job, JobType, QualityReport, QualitySettings, Task, getCore,
 } from 'cvat-core-wrapper';
-import React, { useEffect } from 'react';
-import { useIsMounted, useStateIfMounted } from 'utils/hooks';
+import React, { useEffect, useReducer } from 'react';
+import { useIsMounted } from 'utils/hooks';
+import { ActionUnion, createAction } from 'utils/redux';
 import EmptyGtJob from './empty-job';
 import GtConflicts from './gt-conflicts';
 import Issues from './issues';
@@ -26,19 +27,120 @@ interface Props {
     onJobUpdate: (job: Job) => void
 }
 
+interface State {
+    fetching: boolean,
+    taskReport: QualityReport | null,
+    jobsReports: QualityReport[],
+    qualitySettings: {
+        settings: QualitySettings | null,
+        fetching: boolean,
+        visible: boolean,
+    },
+}
+
+enum ReducerActionType {
+    SET_FETCHING = 'SET_FETCHING',
+    SET_TASK_REPORT = 'SET_TASK_REPORT',
+    SET_JOBS_REPORTS = 'SET_JOBS_REPORTS',
+    SET_QUALITY_SETTINGS = 'SET_QUALITY_SETTINGS',
+    SET_QUALITY_SETTINGS_VISIBLE = 'SET_QUALITY_SETTINGS_VISIBLE',
+    SET_QUALITY_SETTINGS_FETCHING = 'SET_QUALITY_SETTINGS_FETCHING',
+}
+
+export const reducerActions = {
+    setFetching: (fetching: boolean) => (
+        createAction(ReducerActionType.SET_FETCHING, { fetching })
+    ),
+    setTaskReport: (qualityReport: QualityReport) => (
+        createAction(ReducerActionType.SET_TASK_REPORT, { qualityReport })
+    ),
+    setJobsReports: (qualityReports: QualityReport[]) => (
+        createAction(ReducerActionType.SET_JOBS_REPORTS, { qualityReports })
+    ),
+    setQualitySettings: (qualitySettings: QualitySettings) => (
+        createAction(ReducerActionType.SET_QUALITY_SETTINGS, { qualitySettings })
+    ),
+    setQualitySettingsVisible: (visible: boolean) => (
+        createAction(ReducerActionType.SET_QUALITY_SETTINGS_VISIBLE, { visible })
+    ),
+    setQualitySettingsFetching: (fetching: boolean) => (
+        createAction(ReducerActionType.SET_QUALITY_SETTINGS_FETCHING, { fetching })
+    ),
+};
+
+const reducer = (state: State, action: ActionUnion<typeof reducerActions>): State => {
+    if (action.type === ReducerActionType.SET_FETCHING) {
+        return {
+            ...state,
+            fetching: action.payload.fetching,
+        };
+    }
+
+    if (action.type === ReducerActionType.SET_TASK_REPORT) {
+        return {
+            ...state,
+            taskReport: action.payload.qualityReport,
+        };
+    }
+
+    if (action.type === ReducerActionType.SET_JOBS_REPORTS) {
+        return {
+            ...state,
+            jobsReports: action.payload.qualityReports,
+        };
+    }
+
+    if (action.type === ReducerActionType.SET_QUALITY_SETTINGS) {
+        return {
+            ...state,
+            qualitySettings: {
+                ...state.qualitySettings,
+                settings: action.payload.qualitySettings,
+            },
+        };
+    }
+
+    if (action.type === ReducerActionType.SET_QUALITY_SETTINGS_VISIBLE) {
+        return {
+            ...state,
+            qualitySettings: {
+                ...state.qualitySettings,
+                visible: action.payload.visible,
+            },
+        };
+    }
+
+    if (action.type === ReducerActionType.SET_QUALITY_SETTINGS_FETCHING) {
+        return {
+            ...state,
+            qualitySettings: {
+                ...state.qualitySettings,
+                fetching: action.payload.fetching,
+            },
+        };
+    }
+
+    return state;
+};
+
 function TaskQualityComponent(props: Props): JSX.Element {
     const { task, onJobUpdate } = props;
     const isMounted = useIsMounted();
-    const [fetching, setFetching] = useStateIfMounted<boolean>(true);
-    const [taskReport, setTaskReport] = useStateIfMounted<QualityReport | null>(null);
-    const [jobsReports, setJobsReports] = useStateIfMounted<QualityReport[]>([]);
-    const [qualitySettings, setQualitySettings] = useStateIfMounted<QualitySettings | null>(null);
-    const [qualitySettingsVisible, setQualitySettingsVisible] = useStateIfMounted<boolean>(false);
-    const [qualitySettingsFetching, setQualitySettingsFetching] = useStateIfMounted<boolean>(true);
+
+    const [state, dispatch] = useReducer(reducer, {
+        fetching: true,
+        taskReport: null,
+        jobsReports: [],
+        qualitySettings: {
+            settings: null,
+            fetching: true,
+            visible: false,
+        },
+    });
 
     useEffect(() => {
-        setFetching(true);
-        setQualitySettingsFetching(true);
+        dispatch(reducerActions.setFetching(true));
+        dispatch(reducerActions.setQualitySettingsFetching(true));
 
         function handleError(error: Error): void {
             if (isMounted()) {
@@ -61,17 +163,24 @@ function TaskQualityComponent(props: Props): JSX.Element {
             const settingsRequest = core.analytics.quality.settings.get({ taskID: task.id });
 
             Promise.all([reportRequest, settingsRequest]).then(([jobReports, settings]) => {
-                setQualitySettings(settings);
-                setTaskReport(report || null);
-                setJobsReports(jobReports);
+                dispatch(reducerActions.setQualitySettings(settings));
+                dispatch(reducerActions.setTaskReport(report || null));
+                dispatch(reducerActions.setJobsReports(jobReports));
             }).catch(handleError).finally(() => {
-                setQualitySettingsFetching(false);
-                setFetching(false);
+                dispatch(reducerActions.setQualitySettingsFetching(false));
+                dispatch(reducerActions.setFetching(false));
             });
         }).catch(handleError);
     }, [task?.id]);
 
     const gtJob = task.jobs.find((job: Job) => job.type === JobType.GROUND_TRUTH);
+
+    const {
+        fetching, taskReport, jobsReports,
+        qualitySettings: {
+            settings: qualitySettings, fetching: qualitySettingsFetching, visible: qualitySettingsVisible,
+        },
+    } = state;
 
     return (
         <div className='cvat-task-quality-page'>
@@ -86,7 +195,9 @@ function TaskQualityComponent(props: Props): JSX.Element {
                                     <Row>
                                         <MeanQuality
                                             taskReport={taskReport}
-                                            setQualitySettingsVisible={setQualitySettingsVisible}
+                                            setQualitySettingsVisible={
+                                                (visible) => dispatch(reducerActions.setQualitySettingsVisible(visible))
+                                            }
                                             taskID={task.id}
                                         />
                                     </Row>
@@ -122,9 +233,13 @@ function TaskQualityComponent(props: Props): JSX.Element {
                         <QualitySettingsModal
                             fetching={qualitySettingsFetching}
                             qualitySettings={qualitySettings}
-                            setQualitySettings={setQualitySettings}
+                            setQualitySettings={
+                                (settings) => dispatch(reducerActions.setQualitySettings(settings))
+                            }
                             visible={qualitySettingsVisible}
-                            setVisible={setQualitySettingsVisible}
+                            setVisible={
+                                (visible) => dispatch(reducerActions.setQualitySettingsVisible(visible))
+                            }
                         />
                     </>
                 )
