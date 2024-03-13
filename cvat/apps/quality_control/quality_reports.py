50c6cbe4e9f4852d279b630e70f883a3813bd492
vidit <vidit.agarwal.eee20@itbhu.ac.in>
2024-03-14 01:24:25 +0530

    added accumulation of confusion matrix across various jobs

--- cvat/apps/quality_control/quality_reports.py
+++ cvat/apps/quality_control/quality_reports.py
@@ -2369,6 +2369,8 @@ class QualityReportUpdateManager:
         task_mean_shape_ious = []
         task_frame_results = {}
         task_frame_results_counts = {}
+        confusion_matrices = []
+        count_confusion_matrix_labels = 0
         for r in job_reports.values():
             task_intersection_frames.update(r.comparison_summary.frames)
             task_conflicts.extend(r.conflicts)
@@ -2377,6 +2379,9 @@ class QualityReportUpdateManager:
                 task_annotations_summary.accumulate(r.comparison_summary.annotations)
             else:
                 task_annotations_summary = deepcopy(r.comparison_summary.annotations)
+
+            confusion_matrices.append(r.comparison_summary.annotations.confusion_matrix.rows)
+            count_confusion_matrix_labels = len(r.comparison_summary.annotations.confusion_matrix.labels)
 
             if task_ann_components_summary:
                 task_ann_components_summary.accumulate(r.comparison_summary.annotation_components)
@@ -2407,6 +2412,15 @@ class QualityReportUpdateManager:
 
                 task_frame_results_counts[frame_id] = 1 + frame_results_count
                 task_frame_results[frame_id] = task_frame_result
+
+        if confusion_matrices:
+            confusion_matrix = np.sum(confusion_matrices, axis=0)
+        else:
+            confusion_matrix = np.zeros(
+                (count_confusion_matrix_labels, count_confusion_matrix_labels), dtype=int
+            )
+
+        task_annotations_summary.confusion_matrix.rows = confusion_matrix
 
         task_ann_components_summary.shape.mean_iou = np.mean(task_mean_shape_ious)
 
