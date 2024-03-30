cc5f614e83024cf8d68c569e451bb59101f2551a
gkarthik <gkraja@mindkosh.com>
2024-03-30 10:53:09 +0000

    remove containers only when they have old volume mounted as per git isolation add labels to backing service containers by the git branch name

--- .devcontainer/local/init.sh
+++ .devcontainer/local/init.sh
@@ -39,22 +39,44 @@ function get_git_branch {
     fi
 }
 
+# Stop and remove the container if it is tagged with old git branch label
+function remove_container {
+    container="${1}"
+    current_git_branch="$(git branch --show-current)"
+    container_git_branch="$(docker inspect --type container \
+        --format '{{ index .Config.Labels "com.devcontainer.git_branch" }}' \
+        "${container}")" || true
+    if [ -n "${container_git_branch}" ] && \
+        ! [ "${container_git_branch}" == "${current_git_branch}" ]; then
+        docker container stop "${container}" >/dev/null 2>&1 && \
+        docker container rm "${container}" >/dev/null 2>&1
+        is_remove_container=true
+        echo "INFO: done stop and remove ${container}"
+    fi
+}
+
 # The container volumes are parameterized with git branch values in
 # .devcontainer/docker-compose-yml. The volumes get created automatically,
 # however they do not automatically get mounted upon the container restart
 # The containers need to be removed and created again with the new volume mount
 # Tracking this issue at https://github.com/docker/compose/issues/11642
-# Stop and remove each container separately instead of docker compsoe down
+# Stop and remove each container separately instead of docker compose down
 # as we need to preseve the orginal network to be reused
 if [ "${git_branch_isolation}" == true ]; then
-    echo "INFO: stop and remove backing services for new volume mount"
-    services=("cvat_db" "cvat_opa" "cvat_redis_inmem" "cvat_redis_ondisk")
+    services=("cvat_db" "cvat_redis_inmem" "cvat_redis_ondisk")
+    is_remove_container=false
+
+    echo "INFO: try stop and remove backing services for new volume mount"
+
     for service in "${services[@]}"; do
-        docker container stop "${service}" >/dev/null 2>&1 && \
-        docker container rm "${service}" >/dev/null 2>&1
-        echo "INFO: done stop and remove ${service}"
+        remove_container "${service}"
     done
-    echo "INFO: done remove containers"
+
+    if ${is_remove_container}; then
+        echo "INFO: done remove containers"
+    else :
+        echo "INFO: no containers to remove"
+    fi
 fi
 
 # VS Code Remote does not yet support merge tags for docker compose files,
