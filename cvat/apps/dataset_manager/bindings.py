68ed9928b73a02227d1b4496fffbafe52851528b
Yeek020407 <yeek020407@gmail.com>
2024-04-16 00:46:14 +0800

    fix bug

--- cvat/apps/dataset_manager/bindings.py
+++ cvat/apps/dataset_manager/bindings.py
@@ -2163,7 +2163,7 @@ def import_dm_annotations(dm_dataset: dm.Dataset, instance_data: Union[ProjectDa
 
             prev_is_visible = prev_shape is not None and not prev_shape.outside
             has_gap = False
-            if not prev_is_visible or shape.keyframe (
+            if not prev_is_visible or shape.keyframe or (
                 has_gap := prev_shape.frame + instance_data.frame_step < shape.frame
             ):
                 if has_gap:
