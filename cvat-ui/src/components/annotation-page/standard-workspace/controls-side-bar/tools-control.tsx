a52fc280ede47bb1ff03d960c7f22855bbb21b3c
Taha <tahamukhtar20@gmail.com>
2024-04-17 14:59:30 +0500

    revert: tools-control.tsx

--- cvat-ui/src/components/annotation-page/standard-workspace/controls-side-bar/tools-control.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/controls-side-bar/tools-control.tsx
@@ -50,7 +50,6 @@ import ApproximationAccuracy, {
 } from 'components/annotation-page/standard-workspace/controls-side-bar/approximation-accuracy';
 import { switchToolsBlockerState } from 'actions/settings-actions';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-import { InteractorResults, TrackerResults, SerializedShape } from 'cvat-core/src/lambda-manager';
 import withVisibilityHandling from './handle-popover-visibility';
 import ToolsTooltips from './interactor-tooltips';
 
@@ -379,17 +378,17 @@ export class ToolsControlComponent extends React.PureComponent<Props, State> {
                     jobInstance.taskId,
                     interactor,
                     { ...data, job: jobInstance.id },
-                ) as InteractorResults;
+                );
 
                 // if only mask presented, let's receive points
                 if (response.mask && !response.points) {
                     const left = response.bounds ? response.bounds[0] : 0;
                     const top = response.bounds ? response.bounds[1] : 0;
-                    response.points = await this.receivePointsFromMask(response.mask, left, top) as number[];
+                    response.points = await this.receivePointsFromMask(response.mask, left, top);
                 }
 
                 // approximation with cv.approxPolyDP
-                const approximated = await this.approximateResponsePoints(response.points as number[][]);
+                const approximated = await this.approximateResponsePoints(response.points);
                 const rle = core.utils.mask2Rle(response.mask.flat());
                 if (response.bounds) {
                     rle.push(...response.bounds);
@@ -399,6 +398,8 @@ export class ToolsControlComponent extends React.PureComponent<Props, State> {
                     rle.push(0, 0, width - 1, height - 1);
                 }
 
+                response.mask = rle;
+
                 if (this.interaction.id !== interactionId || this.interaction.isAborted) {
                     // new interaction session or the session is aborted
                     return;
@@ -406,7 +407,7 @@ export class ToolsControlComponent extends React.PureComponent<Props, State> {
 
                 this.interaction.latestResponse = {
                     bounds: response.bounds,
-                    points: response.points as number[][],
+                    points: response.points,
                     rle,
                 };
                 this.interaction.lastestApproximatedPoints = approximated;
@@ -766,7 +767,7 @@ export class ToolsControlComponent extends React.PureComponent<Props, State> {
                             frame: frame - 1,
                             shapes: trackableObjects.shapes,
                             job: jobInstance.id,
-                        }) as TrackerResults;
+                        });
 
                         const { states: serverlessStates } = response;
                         const statefullContainer = trackingData.statefull[trackerID] || {
@@ -815,7 +816,7 @@ export class ToolsControlComponent extends React.PureComponent<Props, State> {
                             shapes: trackableObjects.shapes,
                             states: trackableObjects.states,
                             job: jobInstance.id,
-                        }) as TrackerResults;
+                        });
 
                         response.shapes = response.shapes.map(trackedRectangleMapper);
                         for (let i = 0; i < trackableObjects.clientIDs.length; i++) {
@@ -1227,7 +1228,18 @@ export class ToolsControlComponent extends React.PureComponent<Props, State> {
                         this.setState({ mode: 'detection', fetching: true });
                         const result = await core.lambda.call(jobInstance.taskId, model, {
                             ...body, frame, job: jobInstance.id,
-                        }) as SerializedShape[];
+                        });
+
+                        type SerializedShape = {
+                            type: ShapeType;
+                            rotation?: number;
+                            attributes: { name: string; value: string }[];
+                            label: string;
+                            outside?: boolean;
+                            points?: number[];
+                            mask?: number[];
+                            elements: SerializedShape[];
+                        };
 
                         const states = result.map(
                             (data: SerializedShape): ObjectState | null => {
