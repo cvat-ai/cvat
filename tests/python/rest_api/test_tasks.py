dfb9ecfe4088de38e41c3b5527df9725a9e5a62b
Maria Khrustaleva <maria@cvat.ai>
2024-06-03 13:55:45 +0200

    Fixed several issues related to creating tasks with cloud data (#7969)
    <!-- Raise an issue to propose your change
    (https://github.com/cvat-ai/cvat/issues).
    It helps to avoid duplication of efforts from multiple independent
    contributors.
    Discuss your ideas with maintainers to be sure that changes will be
    approved and merged.
    Read the [Contribution guide](https://docs.cvat.ai/docs/contributing/).
    -->
    
    <!-- Provide a general summary of your changes in the Title above -->
    
    ### Motivation and context
    <!-- Why is this change required? What problem does it solve? If it
    fixes an open
    issue, please link to the issue here. Describe your changes in detail,
    add
    screenshots. -->
    @coderabbitai summary
    ### How has this been tested?
    <!-- Please describe in detail how you tested your changes.
    Include details of your testing environment, and the tests you ran to
    see how your change affects other areas of the code, etc. -->
    
    ### Checklist
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply.
    If an item isn't applicable for some reason, then ~~explicitly
    strikethrough~~ the whole
    line. If you don't do that, GitHub will show incorrect progress for the
    pull request.
    If you're unsure about any of these, don't hesitate to ask. We're here
    to help! -->
    - [x] I submit my changes into the `develop` branch
    - [x] I have created a changelog fragment <!-- see top comment in
    CHANGELOG.md -->
    - [ ] I have updated the documentation accordingly
    - [x] I have added tests to cover my changes (*partially*)
    - [ ] I have linked related issues (see [GitHub docs](
    
    https://help.github.com/en/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword))
    - [ ] I have increased versions of npm packages if it is necessary
    
    ([cvat-canvas](https://github.com/cvat-ai/cvat/tree/develop/cvat-canvas#versioning),
    
    [cvat-core](https://github.com/cvat-ai/cvat/tree/develop/cvat-core#versioning),
    
    [cvat-data](https://github.com/cvat-ai/cvat/tree/develop/cvat-data#versioning)
    and
    
    [cvat-ui](https://github.com/cvat-ai/cvat/tree/develop/cvat-ui#versioning))
    
    ### License
    
    - [x] I submit _my code changes_ under the same [MIT License](
    https://github.com/cvat-ai/cvat/blob/develop/LICENSE) that covers the
    project.
      Feel free to contact the maintainers if that's a concern.
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
      - Improved media download performance with parallel downloading.
      - Enhanced file handling with the new `NamedBytesIO` class.
    - Added support for specifying stop frames in task manifest generation.
      - Enhanced `DatasetImagesReader` to handle generator sources.
    
    - **Performance Improvements**
    - Optimized image download methods to use threading for faster
    processing.
    
    - **Configuration**
    - Introduced new settings for maximum threads and files per thread in
    cloud data downloading.
    
    These updates enhance the flexibility, performance, and configurability
    of media handling and downloading in the application.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- tests/python/rest_api/test_tasks.py
+++ tests/python/rest_api/test_tasks.py
@@ -1314,9 +1314,12 @@ class TestPostTaskData:
         sorting_method: str = "lexicographical",
         server_files_exclude: Optional[List[str]] = None,
         org: Optional[str] = None,
+        filenames: Optional[List[str]] = None,
     ) -> Tuple[int, Any]:
         s3_client = s3.make_client()
-        images = generate_image_files(3, prefixes=["img_"] * 3)
+        images = generate_image_files(
+            3, **({"prefixes": ["img_"] * 3} if not filenames else {"filenames": filenames})
+        )
 
         for image in images:
             for i in range(2):
@@ -1638,6 +1641,11 @@ class TestPostTaskData:
             assert "No media data found" in status.message
 
     @pytest.mark.with_external_services
+    @pytest.mark.parametrize("use_manifest", [True, False])
+    @pytest.mark.parametrize("use_cache", [True, False])
+    @pytest.mark.parametrize(
+        "sorting_method", ["natural", "predefined", "lexicographical", "random"]
+    )
     @pytest.mark.parametrize(
         "cloud_storage_id, org",
         [
@@ -1646,19 +1654,24 @@ class TestPostTaskData:
     )
     def test_create_task_with_cloud_storage_and_retrieve_data(
         self,
-        cloud_storage_id,
+        use_manifest: bool,
+        use_cache: bool,
+        sorting_method: str,
+        cloud_storage_id: int,
+        org: str,
         cloud_storages,
         request,
-        org,
     ):
         cloud_storage = cloud_storages[cloud_storage_id]
         task_id, _ = self._create_task_with_cloud_data(
             request=request,
             cloud_storage=cloud_storage,
-            use_manifest=True,
-            use_cache=True,
-            server_files=[],
+            # manifest file should not be uploaded if random sorting is used or if cache is not used
+            use_manifest=use_manifest and use_cache and (sorting_method != "random"),
+            use_cache=use_cache,
+            server_files=[f"test/sub_{i}/img_{j}.jpeg" for i in range(2) for j in range(3)],
             org=org,
+            sorting_method=sorting_method,
         )
 
         with make_api_client(self._USERNAME) as api_client:
@@ -1666,6 +1679,49 @@ class TestPostTaskData:
                 task_id, type="chunk", quality="compressed", number=0
             )
             assert response.status == HTTPStatus.OK
+
+    @pytest.mark.with_external_services
+    @pytest.mark.parametrize(
+        "filenames, sorting_method",
+        [
+            (["img_1.jpeg", "img_2.jpeg", "img_10.jpeg"], "natural"),
+            (["img_10.jpeg", "img_1.jpeg", "img_2.jpeg"], "predefined"),
+            (["img_1.jpeg", "img_10.jpeg", "img_2.jpeg"], "lexicographical"),
+        ],
+    )
+    @pytest.mark.parametrize(
+        "cloud_storage_id, org",
+        [
+            (1, ""),
+        ],
+    )
+    def test_create_task_with_cloud_storage_and_check_data_sorting(
+        self,
+        filenames: List[str],
+        sorting_method: str,
+        cloud_storage_id: int,
+        org: str,
+        cloud_storages,
+        request,
+    ):
+        cloud_storage = cloud_storages[cloud_storage_id]
+
+        task_id, _ = self._create_task_with_cloud_data(
+            request=request,
+            cloud_storage=cloud_storage,
+            use_manifest=False,
+            use_cache=True,
+            server_files=["test/sub_0/" + f for f in filenames],
+            org=org,
+            sorting_method=sorting_method,
+            filenames=filenames,
+        )
+
+        with make_api_client(self._USERNAME) as api_client:
+            data_meta, _ = api_client.tasks_api.retrieve_data_meta(task_id)
+
+            for image_name, frame in zip(filenames, data_meta.frames):
+                assert frame.name.rsplit("/", maxsplit=1)[1] == image_name
 
     def test_can_specify_file_job_mapping(self):
         task_spec = {
