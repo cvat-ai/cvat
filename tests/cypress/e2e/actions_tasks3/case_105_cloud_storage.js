aa21db46acb1889879056c49728673180639f4ae
zaha <z@adhamov.com>
2024-06-04 08:53:17 +0300

    Check non-existent cloud storage update page (#7972)

--- tests/cypress/e2e/actions_tasks3/case_105_cloud_storage.js
+++ tests/cypress/e2e/actions_tasks3/case_105_cloud_storage.js
@@ -172,5 +172,18 @@ context('Cloud storage.', () => {
                 cy.get('#cloudStorageSelect').should('exist');
             });
         });
+
+        it('Verify that attempting to update a non-existent cloud storage displays an error message', () => {
+            const nonExistCloudstorageId = 99;
+            cy.intercept({
+                method: 'GET',
+                url: `/api/cloudstorages/${nonExistCloudstorageId}`,
+            }).as('cloudstorageRequest');
+
+            cy.visit(`/cloudstorages/update/${nonExistCloudstorageId}`);
+            cy.wait('@cloudstorageRequest').its('response.statusCode').should('eq', 404);
+            cy.get('.cvat-spinner').should('not.exist');
+            cy.contains('Sorry, but the requested cloud storage was not found');
+        });
     });
 });
