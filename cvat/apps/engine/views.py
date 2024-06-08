034528c521029ce39774885194347c36999c9225
vidit <vidit.agarwal.eee20@itbhu.ac.in>
2024-06-09 02:41:52 +0530

    added a parent_job_id as a filter field to quickly get consensus jobs through the parent_job_id

--- cvat/apps/engine/views.py
+++ cvat/apps/engine/views.py
@@ -1595,7 +1595,7 @@ class JobViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
     iam_organization_field = 'segment__task__organization'
     search_fields = ('task_name', 'project_name', 'assignee', 'state', 'stage')
     filter_fields = list(search_fields) + [
-        'id', 'task_id', 'project_id', 'updated_date', 'dimension', 'type'
+        'id', 'task_id', 'project_id', 'updated_date', 'dimension', 'type', 'parent_job_id',
     ]
     simple_filters = list(set(filter_fields) - {'id', 'updated_date'})
     ordering_fields = list(filter_fields)
