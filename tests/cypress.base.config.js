ba75f8a870c5b58ada34292cc827e95677a5d255
zaha <zahadhamov@gmail.com>
2024-06-11 20:53:19 +0300

    fix cypress base config

--- tests/cypress.base.config.js
+++ tests/cypress.base.config.js
@@ -20,6 +20,6 @@ module.exports = {
         },
         testIsolation: false,
         baseUrl: 'http://localhost:8080',
-        minioUrl: `${Cypress.config('baseUrl').includes('3000') ? 'localhost' : 'minio'}:9000`,
+        minioUrl: `${this.baseUrl.includes('3000') ? 'localhost' : 'minio'}:9000`,
     },
 };
