511b4dcfa6475d8558a96c0559d6b9eaa1e34582
Boris Sekachev <boris@cvat.ai>
2024-06-14 10:05:10 +0300

    Enabled removing objects

--- cvat-ui/src/components/annotation-page/single-shape-workspace/single-shape-workspace.tsx
+++ cvat-ui/src/components/annotation-page/single-shape-workspace/single-shape-workspace.tsx
@@ -7,12 +7,16 @@ import React from 'react';
 import Layout from 'antd/lib/layout';
 
 import CanvasLayout from 'components/annotation-page/canvas/grid-layout/canvas-layout';
+import CanvasContextMenuContainer from 'containers/annotation-page/canvas/canvas-context-menu';
+import RemoveConfirmComponent from 'components/annotation-page/standard-workspace/remove-confirm';
 import SingleShapeSidebar from './single-shape-sidebar/single-shape-sidebar';
 
 export default function SingleShapeWorkspace(): JSX.Element {
     return (
         <Layout hasSider className='cvat-single-shape-workspace'>
             <CanvasLayout />
+            <CanvasContextMenuContainer />
+            <RemoveConfirmComponent />
             <SingleShapeSidebar />
         </Layout>
     );
