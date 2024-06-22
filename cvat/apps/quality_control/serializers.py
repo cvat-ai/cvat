7a86c954b9da39499f4c6c60aec77d80a9d0d7bc
ashish <iamashish1125@gmail.com>
2024-06-22 16:10:41 +0530

    Add: settings parameter from user

--- cvat/apps/quality_control/serializers.py
+++ cvat/apps/quality_control/serializers.py
@@ -32,6 +32,8 @@ class QualityReportSummarySerializer(serializers.Serializer):
     warning_count = serializers.IntegerField()
     error_count = serializers.IntegerField()
     conflicts_by_type = serializers.DictField(child=serializers.IntegerField())
+    word_error_rate = serializers.FloatField()
+    character_error_rate = serializers.FloatField()
 
     # This set is enough for basic characteristics, such as
     # DS_unmatched, GT_unmatched, accuracy, precision and recall
@@ -82,6 +84,8 @@ class QualitySettingsSerializer(serializers.ModelSerializer):
             "object_visibility_threshold",
             "panoptic_comparison",
             "compare_attributes",
+            "wer_threshold",
+            "cer_threshold",
         )
         read_only_fields = (
             "id",
@@ -92,6 +96,8 @@ class QualitySettingsSerializer(serializers.ModelSerializer):
 
         for field_name, help_text in {
             "iou_threshold": "Used for distinction between matched / unmatched shapes",
+            "wer_threshold": "Used for evaluating the Word Error Rate (WER) in transcripts of annotations",
+            "cer_threshold": "Used for evaluating the Character Error Rate (CER) in transcripts of annotations",
             "low_overlap_threshold": """
                 Used for distinction between strong / weak (low_overlap) matches
             """,
