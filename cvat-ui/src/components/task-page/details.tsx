27575d22e595636bfc70d1c2234ae703232d8bc4
Boris Sekachev <boris@cvat.ai>
2024-06-24 09:45:02 +0300

    Fixed extra server requests

--- cvat-ui/src/components/task-page/details.tsx
+++ cvat-ui/src/components/task-page/details.tsx
@@ -11,7 +11,10 @@ import Text from 'antd/lib/typography/Text';
 import Title from 'antd/lib/typography/Title';
 import moment from 'moment';
 
-import { getCore, Task, User } from 'cvat-core-wrapper';
+import {
+    Dumper, User,
+    getCore, Project, Task,
+} from 'cvat-core-wrapper';
 import AutomaticAnnotationProgress from 'components/tasks-page/automatic-annotation-progress';
 import MdGuideControl from 'components/md-guide/md-guide-control';
 import Preview from 'components/common/preview';
@@ -29,9 +32,9 @@ interface OwnProps {
 
 interface StateToProps {
     activeInference: ActiveInference | null;
-    projectSubsets: string[];
-    dumpers: any[];
-    user: any;
+    project?: Project;
+    dumpers: Dumper[];
+    user: User;
 }
 
 interface DispatchToProps {
@@ -39,18 +42,12 @@ interface DispatchToProps {
 }
 
 function mapStateToProps(state: CombinedState, own: OwnProps): StateToProps & OwnProps {
-    const [taskProject] = state.projects.current.filter((project) => project.id === own.task.projectId);
-
     return {
         ...own,
         dumpers: state.formats.annotationFormats.dumpers,
-        user: state.auth.user,
+        user: state.auth.user as User,
         activeInference: state.models.inferences[own.task.id] || null,
-        projectSubsets: taskProject ?
-            ([
-                ...new Set(taskProject.subsets),
-            ] as string[]) :
-            [],
+        project: state.projects.current.find((project) => project.id === own.task.projectId),
     };
 }
 
@@ -166,11 +163,7 @@ class DetailsComponent extends React.PureComponent<Props, State> {
 
     private renderSubsetField(): JSX.Element {
         const { subset } = this.state;
-        const {
-            task: taskInstance,
-            projectSubsets,
-            onUpdateTask,
-        } = this.props;
+        const { task: taskInstance, project, onUpdateTask } = this.props;
 
         return (
             <Row>
@@ -181,11 +174,9 @@ class DetailsComponent extends React.PureComponent<Props, State> {
                     <ProjectSubsetField
                         value={subset}
                         projectId={taskInstance.projectId as number}
-                        projectSubsets={projectSubsets}
+                        projectSubsets={project?.subsets ?? null}
                         onChange={(value) => {
-                            this.setState({
-                                subset: value,
-                            });
+                            this.setState({ subset: value });
 
                             if (taskInstance.subset !== value) {
                                 taskInstance.subset = value;
