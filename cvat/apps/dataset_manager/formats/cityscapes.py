ba5a730e0339fb2bb249fc29e8cc34c051cf466c
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-07-01 20:47:28 +0300

    Add annotation type filter in cityscapes export

--- cvat/apps/dataset_manager/formats/cityscapes.py
+++ cvat/apps/dataset_manager/formats/cityscapes.py
@@ -5,13 +5,14 @@
 
 import os.path as osp
 
-from datumaro.components.dataset import Dataset
+import datumaro as dm
 from datumaro.plugins.cityscapes_format import write_label_map
 from pyunpack import Archive
 
 from cvat.apps.dataset_manager.bindings import (GetCVATDataExtractor, detect_dataset,
                                                 import_dm_annotations)
 from cvat.apps.dataset_manager.util import make_zip_archive
+from cvat.apps.engine.models import LabelType
 
 from .transformations import MaskToPolygonTransformation, RotatedBoxesToPolygons
 from .registry import dm_env, exporter, importer
@@ -19,19 +20,33 @@ from .utils import make_colormap
 
 
 @exporter(name='Cityscapes', ext='ZIP', version='1.0')
-def _export(dst_file, temp_dir, instance_data, save_images=False):
-    with GetCVATDataExtractor(instance_data, include_images=save_images) as extractor:
-        dataset = Dataset.from_extractors(extractor, env=dm_env)
-        dataset.transform(RotatedBoxesToPolygons)
-        dataset.transform('polygons_to_masks')
-        dataset.transform('boxes_to_masks')
-        dataset.transform('merge_instance_segments')
+class _Exporter:
+    SUPPORTED_ANNOTATION_TYPES = [LabelType.BBOX, LabelType.POLYGON, LabelType.MASK]
 
-        dataset.export(temp_dir, 'cityscapes', save_images=save_images,
-            apply_colormap=True, label_map={label: info[0]
-                for label, info in make_colormap(instance_data).items()})
+    def __call__(self, dst_file, temp_dir, instance_data, save_images=False):
+        with GetCVATDataExtractor(
+            instance_data, include_images=save_images,
+            included_label_types=self.SUPPORTED_ANNOTATION_TYPES
+        ) as extractor:
+            dataset = dm.Dataset.from_extractors(extractor, env=dm_env)
+            dataset.transform(RotatedBoxesToPolygons)
+            dataset.transform('polygons_to_masks')
+            dataset.transform('boxes_to_masks')
+            dataset.transform('merge_instance_segments')
 
-    make_zip_archive(temp_dir, dst_file)
+            label_cat: dm.LabelCategories = dataset.categories()[dm.AnnotationType.label]
+
+            dataset.export(
+                temp_dir, 'cityscapes', save_images=save_images,
+                apply_colormap=True,
+                label_map={
+                    label: info[0]
+                    for label, info in make_colormap(instance_data).items()
+                    if label_cat.find(label)[0]
+                }
+            )
+
+        make_zip_archive(temp_dir, dst_file)
 
 @importer(name='Cityscapes', ext='ZIP', version='1.0')
 def _import(src_file, temp_dir, instance_data, load_data_callback=None, **kwargs):
@@ -44,7 +59,7 @@ def _import(src_file, temp_dir, instance_data, load_data_callback=None, **kwargs
         write_label_map(labelmap_file, colormap)
 
     detect_dataset(temp_dir, format_name='cityscapes', importer= dm_env.importers.get('cityscapes'))
-    dataset = Dataset.import_from(temp_dir, 'cityscapes', env=dm_env)
+    dataset = dm.Dataset.import_from(temp_dir, 'cityscapes', env=dm_env)
     dataset = MaskToPolygonTransformation.convert_dataset(dataset, **kwargs)
     if load_data_callback is not None:
         load_data_callback(dataset, instance_data)
