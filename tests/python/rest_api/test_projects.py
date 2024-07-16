2c7c1ff17cdaa25663ccbc8fed9c8f998d2c8c3c
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2024-07-16 23:22:15 +0400

    testing almost all formats whether they make folders for subsets

--- tests/python/rest_api/test_projects.py
+++ tests/python/rest_api/test_projects.py
@@ -816,8 +816,34 @@ class TestImportExportDatasetProject:
 
             self._test_import_project(admin_user, project_id, "CVAT 1.1", import_data)
 
-    @pytest.mark.parametrize("export_format", ["COCO 1.0", "COCO Keypoints 1.0"])
-    def test_creates_subfolders_in_coco_export(self, tasks, admin_user, export_format):
+    @pytest.mark.parametrize(
+        "export_format, subset_path_template",
+        [
+            ("COCO 1.0", "images/{subset}/"),
+            ("COCO Keypoints 1.0", "images/{subset}/"),
+            ("CVAT for images 1.1", "images/{subset}/"),
+            ("CVAT for video 1.1", "images/{subset}/"),
+            ("Datumaro 1.0", "images/{subset}/"),
+            ("Datumaro 3D 1.0", "point_clouds/{subset}/"),
+            ("LabelMe 3.0", "{subset}/"),
+            ("MOTS PNG 1.0", "{subset}/images/"),
+            ("YOLO 1.1", "obj_{subset}_data/"),
+            ("CamVid 1.0", "{subset}/"),
+            ("WiderFace 1.0", "WIDER_{subset}/images/"),
+            ("VGGFace2 1.0", "{subset}/"),
+            ("Market-1501 1.0", "bounding_box_{subset}/"),
+            ("ICDAR Recognition 1.0", "{subset}/images/"),
+            ("ICDAR Localization 1.0", "{subset}/images/"),
+            ("ICDAR Segmentation 1.0", "{subset}/images/"),
+            ("KITTI 1.0", "{subset}/image_2/"),
+            ("LFW 1.0", "{subset}/images/"),
+            ("Cityscapes 1.0", "imgsFine/leftImg8bit/{subset}/"),
+            ("Open Images V6 1.0", "images/{subset}/"),
+        ],
+    )
+    def test_creates_subfolders_for_subsets_on_export(
+        self, tasks, admin_user, export_format, subset_path_template
+    ):
         group_key_func = itemgetter("project_id")
         subsets = ["Train", "Validation"]
         project_id = next(
@@ -831,9 +857,10 @@ class TestImportExportDatasetProject:
         response = self._test_export_project(admin_user, project_id, format=export_format)
         with zipfile.ZipFile(io.BytesIO(response.data)) as zip_file:
             for subset in subsets:
+                folder_prefix = subset_path_template.format(subset=subset)
                 assert (
-                    len([f for f in zip_file.namelist() if f.startswith(f"images/{subset}/")]) > 0
-                )
+                    len([f for f in zip_file.namelist() if f.startswith(folder_prefix)]) > 0
+                ), f"No {folder_prefix} in {zip_file.namelist()}"
 
 
 @pytest.mark.usefixtures("restore_db_per_function")
