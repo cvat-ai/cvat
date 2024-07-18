27297f45011355940ddbf614c60d7c1ce4bb1612
vidit <vidit.agarwal.eee20@itbhu.ac.in>
2024-07-18 23:59:15 +0530

    after merging consensus jobs regular job's state is marked as COMPLETE

--- cvat/apps/consensus/merge_consensus_jobs.py
+++ cvat/apps/consensus/merge_consensus_jobs.py
@@ -115,8 +115,7 @@ def _merge_consensus_jobs(task_id: int) -> None:
             consensus_job_data_providers=consensus_job_data_providers,
         )
         parent_job = Job.objects.filter(id=parent_job_id, type=JobType.ANNOTATION.value).first()
-        parent_job.stage = StageChoice.VALIDATION.value
-        parent_job.state = StateChoice.NEW.value
+        parent_job.state = StateChoice.COMPLETED.value
         parent_job.save()
 
     task_report_data = generate_task_consensus_report(job_comparison_reports)
