f598b8faf2b795d1c678588d4f6b56f6aa7d719d
Taha <tahamukhtar20@gmail.com>
2024-07-22 12:57:51 +0200

    fix: minor requested changes

--- cvat-ui/src/components/header/settings-modal/multiple-shortcuts-display.tsx
+++ cvat-ui/src/components/header/settings-modal/multiple-shortcuts-display.tsx
@@ -1,5 +1,5 @@
 import { Select } from 'antd/lib';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 interface Props {
     id: string;
@@ -7,24 +7,55 @@ interface Props {
     onKeySequenceUpdate: (keyMapId: string, updatedSequence: string[]) => void;
 }
 
+const ShortcutMapper = (key: string): string => {
+    const mapping: Record<string, string> = {
+        control: 'ctrl',
+        ' ': 'space',
+        escape: 'esc',
+    };
+    return mapping[key] || key;
+};
+
 function MultipleShortcutsDisplay(props: Props): JSX.Element {
     const { id, item, onKeySequenceUpdate } = props;
     const { sequences } = item;
     const selectRef = useRef<any>(null);
     const [focus, setFocus] = useState(false);
-    const [pressedKeys, setPressedKeys] = useState<string[]>([]);
+    const [pressedKeys, setPressedKeys] = useState<string[][]>([[]]);
+    const [currentIdx, setCurrentIdx] = useState<number>(0);
+    const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+
+    useEffect(() => () => {
+        if (timer) clearTimeout(timer);
+    }, [timer]);
+
+    const finalizeCombination = (): void => {
+        const keyCombination = pressedKeys.map((keys) => keys.map((key) => ShortcutMapper(key)).join('+')).join(' ');
+        if (!sequences.includes(keyCombination)) {
+            onKeySequenceUpdate(id, [...sequences, keyCombination]);
+        }
+        setPressedKeys([[]]);
+        setCurrentIdx(0);
+        setTimer(null);
+        selectRef.current.blur();
+    };
 
     const handleKeyDown = (event: React.KeyboardEvent): void => {
         event.stopPropagation();
         event.preventDefault();
-        if (focus) {
-            setPressedKeys((prevKeys) => {
-                const key = event.key.toLowerCase();
-                if (!prevKeys.includes(key)) {
-                    return [...prevKeys, key];
-                }
-                return prevKeys;
-            });
+        const key = ShortcutMapper(event.key.toLowerCase());
+        if (!focus) return;
+
+        if (timer) {
+            clearTimeout(timer);
+            setTimer(null);
+            const newPressedKeys = [...pressedKeys, [key]];
+            setPressedKeys(newPressedKeys);
+            setCurrentIdx(currentIdx + 1);
+        } else if (!pressedKeys[currentIdx].includes(key)) {
+            const newPressedKeys = [...pressedKeys];
+            newPressedKeys[currentIdx].push(key);
+            setPressedKeys(newPressedKeys);
         }
     };
 
@@ -32,12 +63,8 @@ function MultipleShortcutsDisplay(props: Props): JSX.Element {
         event.stopPropagation();
         event.preventDefault();
         if (focus) {
-            const newKeyCombination = pressedKeys.join('+');
-            if (!sequences.includes(newKeyCombination)) {
-                onKeySequenceUpdate(id, [...sequences, newKeyCombination]);
-            }
-            setPressedKeys([]);
-            selectRef.current.blur();
+            const newTimer = setTimeout(finalizeCombination, 1000);
+            setTimer(newTimer);
         }
     };
 
@@ -45,10 +72,16 @@ function MultipleShortcutsDisplay(props: Props): JSX.Element {
         <Select
             allowClear
             onFocus={() => setFocus(true)}
-            onBlur={() => setFocus(false)}
+            onBlur={() => {
+                setFocus(false);
+                if (timer) {
+                    clearTimeout(timer);
+                    finalizeCombination();
+                }
+            }}
             onClear={() => onKeySequenceUpdate(id, [])}
             ref={selectRef}
-            searchValue=''
+            searchValue={pressedKeys.map((keys) => keys.map((key) => ShortcutMapper(key)).join('+')).join(' ')}
             onChange={(value: string[]) => onKeySequenceUpdate(id, value)}
             suffixIcon={null}
             dropdownStyle={{ display: 'none' }}
