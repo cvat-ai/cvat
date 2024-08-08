59f2daa59b4cc427482da798c56db4c34b43493f
vidit <vidit.agarwal.eee20@itbhu.ac.in>
2024-08-09 02:42:08 +0530

    added structure of consensus conflict and report

---
+++ cvat-core/src/consensus-conflict.ts
@@ -0,0 +1,105 @@
+// Copyright (C) 2023 CVAT.ai Corporation
+//
+// SPDX-License-Identifier: MIT
+
+import { SerializedAnnotationConsensusConflictData, SerializedConsensusConflictData } from './server-response-types';
+import { ObjectType } from './enums';
+
+export enum ConsensusConflictType {
+    NO_MATCHING_ITEM = 'no_matching_item',
+    FAILED_ATTRIBUTE_VOTING = 'failed_attribute_voting',
+    NO_MATCHING_ANNOTATION = 'no_matching_annotation',
+    ANNOTATION_TOO_CLOSE = 'annotation_too_close',
+    WRONG_GROUP = 'wrong_group',
+    FAILED_LABEL_VOTING = 'failed_label_voting',
+}
+
+export class AnnotationConflict {
+    #jobID: number;
+    #serverID: number;
+    #type: ObjectType;
+    #shapeType: string | null;
+    #description: string;
+    #conflictType: ConsensusConflictType;
+
+    constructor(initialData: SerializedAnnotationConsensusConflictData) {
+        this.#jobID = initialData.job_id;
+        this.#serverID = initialData.obj_id;
+        this.#type = initialData.type;
+        this.#shapeType = initialData.shape_type;
+        this.#conflictType = initialData.conflict_type as ConsensusConflictType;
+
+        const desc = this.#conflictType.split('_').join(' ');
+        this.#description = desc.charAt(0).toUpperCase() + desc.slice(1);
+    }
+
+    get jobID(): number {
+        return this.#jobID;
+    }
+
+    get serverID(): number {
+        return this.#serverID;
+    }
+
+    get type(): ObjectType {
+        return this.#type;
+    }
+
+    get shapeType(): string | null {
+        return this.#shapeType;
+    }
+
+    get conflictType(): ConsensusConflictType {
+        return this.#conflictType;
+    }
+
+    get description(): string {
+        return this.#description;
+    }
+}
+
+export default class ConsensusConflict {
+    #id: number;
+    #frame: number;
+    #type: ConsensusConflictType;
+    #annotationConflicts: AnnotationConflict[];
+    #description: string;
+
+    constructor(initialData: SerializedConsensusConflictData) {
+        this.#id = initialData.id;
+        this.#frame = initialData.frame;
+        this.#type = initialData.type as ConsensusConflictType;
+        this.#annotationConflicts = initialData.annotation_ids
+            .map((rawData: SerializedAnnotationConsensusConflictData) => new AnnotationConflict({
+                ...rawData,
+                conflict_type: initialData.type,
+            }));
+
+        const desc = initialData.type.split('_').join(' ');
+        this.#description = desc.charAt(0).toUpperCase() + desc.slice(1);
+    }
+
+    get id(): number {
+        return this.#id;
+    }
+
+    get frame(): number {
+        return this.#frame;
+    }
+
+    get type(): ConsensusConflictType {
+        return this.#type;
+    }
+
+    get annotationConflicts(): AnnotationConflict[] {
+        return this.#annotationConflicts;
+    }
+
+    get description(): string {
+        return this.#description;
+    }
+
+    set description(newDescription: string) {
+        this.#description = newDescription;
+    }
+}
---
+++ cvat-core/src/consensus-report.ts
@@ -0,0 +1,89 @@
+// Copyright (C) 2024 CVAT.ai Corporation
+//
+// SPDX-License-Identifier: MIT
+
+import { SerializedConsensusReportData } from './server-response-types';
+import User from './user';
+
+export interface ConsensusSummary {
+    frameCount: number;
+    conflictCount: number;
+    conflictsByType: {
+        no_matching_item: number;
+        failed_attribute_voting: number;
+        no_matching_annotation: number;
+        annotation_too_close: number;
+        wrong_group: number;
+        failed_label_voting: number;
+    }
+}
+
+export default class ConsensusReport {
+    #id: number;
+    #taskID: number;
+    #jobID: number | null;
+    #createdDate: string;
+    #assignee: User | null;
+    #consensus_score: number;
+    #target: string;
+    #summary: Partial<SerializedConsensusReportData['summary']>;
+
+    constructor(initialData: SerializedConsensusReportData) {
+        this.#id = initialData.id;
+        this.#taskID = initialData.task_id;
+        this.#jobID = initialData.job_id;
+        this.#createdDate = initialData.created_date;
+        this.#target = initialData.target;
+        this.#consensus_score = initialData.consensus_score;
+        this.#summary = initialData.summary;
+
+        if (initialData.assignee) {
+            this.#assignee = new User(initialData.assignee);
+        } else {
+            this.#assignee = null;
+        }
+    }
+
+    get id(): number {
+        return this.#id;
+    }
+
+    get taskID(): number {
+        return this.#taskID;
+    }
+
+    get jobID(): number | null {
+        return this.#jobID;
+    }
+
+    get createdDate(): string {
+        return this.#createdDate;
+    }
+
+    get assignee(): User | null {
+        return this.#assignee;
+    }
+
+    get consensus_score(): number {
+        return this.#consensus_score;
+    }
+
+    get target(): string {
+        return this.#target;
+    }
+
+    get summary(): ConsensusSummary {
+        return {
+            frameCount: this.#summary.frame_count,
+            conflictCount: this.#summary.conflict_count,
+            conflictsByType: {
+                no_matching_item: this.#summary.conflicts_by_type?.no_matching_item,
+                failed_attribute_voting: this.#summary.conflicts_by_type?.failed_attribute_voting,
+                no_matching_annotation: this.#summary.conflicts_by_type?.no_matching_annotation,
+                annotation_too_close: this.#summary.conflicts_by_type?.annotation_too_close,
+                wrong_group: this.#summary.conflicts_by_type?.wrong_group,
+                failed_label_voting: this.#summary.conflicts_by_type?.failed_label_voting,
+            },
+        };
+    }
+}
