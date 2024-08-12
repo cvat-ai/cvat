10a5f661eb2497b4f8e1a303ea1fd8b28af619e4
Boris Sekachev <boris.sekachev@yandex.ru>
2024-08-12 12:21:49 +0300

    Segment Anything: supported bounding box input (#8270)

--- cvat-ui/src/reducers/annotation-reducer.ts
+++ cvat-ui/src/reducers/annotation-reducer.ts
@@ -496,9 +496,8 @@ export default (state = defaultState, action: AnyAction): AnnotationState => {
                     activeControl,
                 },
                 drawing: {
-                    ...state.drawing,
+                    ...defaultState.drawing,
                     ...payload,
-                    activeInteractor: undefined,
                 },
             };
         }
@@ -933,7 +932,7 @@ export default (state = defaultState, action: AnyAction): AnnotationState => {
             };
         }
         case AnnotationActionTypes.INTERACT_WITH_CANVAS: {
-            const { activeInteractor, activeLabelID } = action.payload;
+            const { activeInteractor, activeLabelID, activeInteractorParameters } = action.payload;
             return {
                 ...state,
                 annotations: {
@@ -943,6 +942,7 @@ export default (state = defaultState, action: AnyAction): AnnotationState => {
                 drawing: {
                     ...state.drawing,
                     activeInteractor,
+                    activeInteractorParameters,
                     activeLabelID,
                 },
                 canvas: {
