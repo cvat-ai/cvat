3b287edd7632741006c0f16fa21c7ae41446c09f
Taha <tahamukhtar20@gmail.com>
2024-08-14 21:02:16 +0200

    fix: update for reducing code duplication

--- cvat-ui/src/components/annotation-page/standard-workspace/objects-side-bar/labels-list.tsx
+++ cvat-ui/src/components/annotation-page/standard-workspace/objects-side-bar/labels-list.tsx
@@ -22,68 +22,17 @@ import { ShortcutScope } from 'utils/enums';
 import { registerComponentShortcuts } from 'actions/shortcuts-actions';
 import { subKeyMap } from 'utils/component-subkeymap';
 
-const componentShortcuts: Record<string, KeyMapItem> = {
-    SWITCH_LABEL_1: {
-        name: 'Switch label 1',
-        description: 'Changes the label to label 1 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+1'],
+const componentShortcuts: Record<string, KeyMapItem> = {};
+
+for (const i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) {
+    componentShortcuts[`SWITCH_LABEL_${i}`] = {
+        name: `Switch label ${i}`,
+        description: `Changes the label to label ${i} for the activated
+        object or for the next drawn object if no objects are activated`,
+        sequences: [`ctrl+${i}`],
         scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_2: {
-        name: 'Switch label 2',
-        description: 'Changes the label to label 2 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+2'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_3: {
-        name: 'Switch label 3',
-        description: 'Changes the label to label 3 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+3'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_4: {
-        name: 'Switch label 4',
-        description: 'Changes the label to label 4 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+4'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_5: {
-        name: 'Switch label 5',
-        description: 'Changes the label to label 5 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+5'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_6: {
-        name: 'Switch label 6',
-        description: 'Changes the label to label 6 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+6'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_7: {
-        name: 'Switch label 7',
-        description: 'Changes the label to label 7 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+7'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_8: {
-        name: 'Switch label 8',
-        description: 'Changes the label to label 8 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+8'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_9: {
-        name: 'Switch label 9',
-        description: 'Changes a label for object 9 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+9'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-    SWITCH_LABEL_0: {
-        name: 'Switch label 0',
-        description: 'Changes the label to label 0 for the activated object or for the next drawn object if no objects are activated',
-        sequences: ['ctrl+0'],
-        scope: ShortcutScope.OBJECTS_SIDE_BAR,
-    },
-};
+    };
+}
 
 registerComponentShortcuts(componentShortcuts);
 
@@ -204,88 +153,18 @@ function LabelsListComponent(): JSX.Element {
         }
     };
 
-    const handlers: Record<keyof typeof componentShortcuts, (event: KeyboardEvent, shortcut: string) => void> = {
-        SWITCH_LABEL_1: (event: KeyboardEvent) => {
+    const handlers: Record<keyof typeof componentShortcuts, (event: KeyboardEvent, shortcut: string) => void> = {};
+
+    for (const i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) {
+        handlers[`SWITCH_LABEL_${i}`] = (event: KeyboardEvent) => {
             if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[1];
+            const labelID = keyToLabelMapping[i];
             const label = labels.filter((_label: any) => _label.id === labelID)[0];
             if (Number.isInteger(labelID) && label) {
                 handleHelper(labelID);
             }
-        },
-        SWITCH_LABEL_2: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[2];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_3: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[3];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_4: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[4];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_5: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[5];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_6: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[6];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_7: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[7];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_8: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[8];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_9: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[9];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-        SWITCH_LABEL_0: (event: KeyboardEvent) => {
-            if (event) event.preventDefault();
-            const labelID = keyToLabelMapping[0];
-            const label = labels.filter((_label: any) => _label.id === labelID)[0];
-            if (Number.isInteger(labelID) && label) {
-                handleHelper(labelID);
-            }
-        },
-    };
+        };
+    }
 
     return (
         <div className='cvat-objects-sidebar-labels-list'>
