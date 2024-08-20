5bff076fa7e039ba6a9b3741efc60b0b3f108b51
Kirill Lakhov <kirill.9992@gmail.com>
2024-08-20 13:10:13 +0300

    removed quality components

--- cvat-ui/src/components/analytics-page/analytics-page.tsx
+++ cvat-ui/src/components/analytics-page/analytics-page.tsx
@@ -5,7 +5,6 @@
 import './styles.scss';
 
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
 import { useLocation, useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { Row, Col } from 'antd/lib/grid';
@@ -14,15 +13,12 @@ import Title from 'antd/lib/typography/Title';
 import notification from 'antd/lib/notification';
 import moment from 'moment';
 import { useIsMounted } from 'utils/hooks';
-import { Project, Task } from 'reducers';
 import {
-    AnalyticsReport, Job, RQStatus, getCore,
+    AnalyticsReport, Job, Project, RQStatus, Task, getCore,
 } from 'cvat-core-wrapper';
-import { updateJobAsync } from 'actions/jobs-actions';
 import CVATLoadingSpinner from 'components/common/loading-spinner';
 import GoBackButton from 'components/common/go-back-button';
 import AnalyticsOverview, { DateIntervals } from './analytics-performance';
-import TaskQualityComponent from './task-quality/task-quality-component';
 
 const core = getCore();
 
@@ -80,7 +76,6 @@ function readInstanceId(type: InstanceType): number {
 type InstanceType = 'project' | 'task' | 'job';
 
 function AnalyticsPage(): JSX.Element {
-    const dispatch = useDispatch();
     const location = useLocation();
 
     const requestedInstanceType: InstanceType = readInstanceType(location);
@@ -224,15 +219,6 @@ function AnalyticsPage(): JSX.Element {
         });
     }, [requestedInstanceType, requestedInstanceID, timePeriod]);
 
-    const onJobUpdate = useCallback((job: Job, data: Parameters<Job['save']>[0]): void => {
-        setFetching(true);
-        dispatch(updateJobAsync(job, data)).finally(() => {
-            if (isMounted()) {
-                setFetching(false);
-            }
-        });
-    }, []);
-
     const onTabKeyChange = useCallback((key: string): void => {
         setTab(key as AnalyticsTabs);
     }, []);
@@ -282,12 +268,7 @@ function AnalyticsPage(): JSX.Element {
                             onCreateReport={onCreateReport}
                         />
                     ),
-                },
-                ...(instanceType === 'task' ? [{
-                    key: AnalyticsTabs.QUALITY,
-                    label: 'Quality',
-                    children: <TaskQualityComponent task={instance} onJobUpdate={onJobUpdate} />,
-                }] : [])]}
+                }]}
             />
         );
     }
