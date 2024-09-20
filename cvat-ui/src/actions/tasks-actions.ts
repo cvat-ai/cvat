9bdfbaf51781c147330f9ff1cc58efb498a05a1a
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-09-20 17:40:24 +0300

    Fix task creation with validation params

--- cvat-ui/src/actions/tasks-actions.ts
+++ cvat-ui/src/actions/tasks-actions.ts
@@ -262,8 +262,12 @@ ThunkAction {
                 validation_params: {
                     mode: data.quality.validationMode,
                     frame_selection_method: data.quality.frameSelectionMethod,
-                    frame_percent: data.quality.validationFramesPercent,
-                    frames_per_job_percent: data.quality.validationFramesPerJobPercent,
+                    frame_share: data.quality.validationFramesPercent ? (
+                        data.quality.validationFramesPercent / 100
+                    ) : data.quality.validationFramesPercent,
+                    frames_per_job_share: data.quality.validationFramesPerJobPercent ? (
+                        data.quality.validationFramesPerJobPercent / 100
+                    ) : data.quality.validationFramesPerJobPercent,
                 },
             };
         }
