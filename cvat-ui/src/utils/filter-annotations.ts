565573c0acc22d40d596d7dc06caa3e38472596b
Boris Sekachev <sekachev.bs@gmail.com>
2024-10-11 14:30:44 +0300

    Updated comment

--- cvat-ui/src/utils/filter-annotations.ts
+++ cvat-ui/src/utils/filter-annotations.ts
@@ -34,7 +34,7 @@ export function filterAnnotations(annotations: ObjectState[], params: FilterAnno
         // GT tracks are shown only on GT frames
         if (workspace === Workspace.REVIEW && groundTruthJobFramesMeta?.includedFrames && frame) {
             if (state.objectType === ObjectType.TRACK && state.isGroundTruth) {
-                // includedFrames has exactly absolute numeration of frames
+                // includedFrames has exactly numeration of frames
                 // frame N actually corresponds to N * frameStep in absolute numeration
                 return groundTruthJobFramesMeta.includedFrames.includes(frame * groundTruthJobFramesMeta.frameStep);
             }
