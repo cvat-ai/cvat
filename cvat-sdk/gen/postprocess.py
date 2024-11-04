23315fc63be02b1fe5021a3e876e4950875ffc8d
Roman Donchenko <roman@cvat.ai>
2024-11-04 14:13:07 +0200

    Fix bandit false positives

--- cvat-sdk/gen/postprocess.py
+++ cvat-sdk/gen/postprocess.py
@@ -30,8 +30,8 @@ def collect_operations(schema):
 
 
 class Replacer:
-    REPLACEMENT_TOKEN = r"%%%"
-    ARGS_TOKEN = r"!!!"
+    REPLACEMENT_TOKEN = r"%%%"  # nosec: hardcoded_password_string
+    ARGS_TOKEN = r"!!!"  # nosec: hardcoded_password_string
 
     def __init__(self, schema):
         self._schema = schema
