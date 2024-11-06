b56481cb0449cf24aa567ece90fa95d0ab81d739
Boris Sekachev <sekachev.bs@gmail.com>
2024-11-06 18:09:27 +0200

    Ignore not relevant tasks/projects/jobs responses

--- cvat-ui/src/containers/tasks-page/tasks-list.tsx
+++ cvat-ui/src/containers/tasks-page/tasks-list.tsx
@@ -1,20 +1,15 @@
 // Copyright (C) 2020-2022 Intel Corporation
-// Copyright (C) 2022 CVAT.ai Corporation
+// Copyright (C) 2022-2024 CVAT.ai Corporation
 //
 // SPDX-License-Identifier: MIT
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { TasksState, TasksQuery, CombinedState } from 'reducers';
+import { TasksState, CombinedState } from 'reducers';
 import TasksListComponent from 'components/tasks-page/task-list';
-import { getTasksAsync } from 'actions/tasks-actions';
 
 interface StateToProps {
     tasks: TasksState;
-}
-
-interface DispatchToProps {
-    getTasks: (query: TasksQuery) => void;
 }
 
 function mapStateToProps(state: CombinedState): StateToProps {
@@ -23,17 +18,7 @@ function mapStateToProps(state: CombinedState): StateToProps {
     };
 }
 
-function mapDispatchToProps(dispatch: any): DispatchToProps {
-    return {
-        getTasks: (query: TasksQuery): void => {
-            dispatch(getTasksAsync(query));
-        },
-    };
-}
-
-type TasksListContainerProps = StateToProps & DispatchToProps;
-
-function TasksListContainer(props: TasksListContainerProps): JSX.Element {
+function TasksListContainer(props: StateToProps): JSX.Element {
     const { tasks } = props;
 
     return (
@@ -43,4 +28,4 @@ function TasksListContainer(props: TasksListContainerProps): JSX.Element {
     );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TasksListContainer);
+export default connect(mapStateToProps)(TasksListContainer);
