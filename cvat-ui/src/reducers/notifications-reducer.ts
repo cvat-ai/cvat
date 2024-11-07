e2b2a512785e659cae2ec94261d3ac5006f6faf9
Roman Donchenko <roman@cvat.ai>
2024-11-07 12:35:11 +0200

    Remove gratuitous non-ASCII characters (#7683)
    In `cvat-ui/src/index.html`, the meta element seems useless (the default
    is already to index and follow; I'm not even sure `index` and `follow`
    are valid values). So just remove it entirely.

--- cvat-ui/src/reducers/notifications-reducer.ts
+++ cvat-ui/src/reducers/notifications-reducer.ts
@@ -355,7 +355,7 @@ export default function (state = defaultState, action: AnyAction): Notifications
                         ...state.messages.auth,
                         requestPasswordResetDone: {
                             message: `Check your email for a link to reset your password.
-                            If it doesn’t appear within a few minutes, check your spam folder.`,
+                            If it doesn't appear within a few minutes, check your spam folder.`,
                         },
                     },
                 },
