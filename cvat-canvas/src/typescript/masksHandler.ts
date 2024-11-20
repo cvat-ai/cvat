d10a8ed0906b084fc83d9a2ccd538aec7693d581
Boris Sekachev <sekachev.bs@gmail.com>
2024-11-21 01:38:33 +0200

    Fixed copying attributes

--- cvat-canvas/src/typescript/masksHandler.ts
+++ cvat-canvas/src/typescript/masksHandler.ts
@@ -404,6 +404,9 @@ export class MasksHandlerImpl implements MasksHandler {
                 rle.push(wrappingBbox.left, wrappingBbox.top, wrappingBbox.right, wrappingBbox.bottom);
 
                 this.onDrawDone({
+                    occluded: this.drawData.initialState.occluded,
+                    attributes: { ...this.drawData.initialState.attributes },
+                    color: this.drawData.initialState.color,
                     objectType: this.drawData.initialState.objectType,
                     shapeType: this.drawData.shapeType,
                     points: rle,
