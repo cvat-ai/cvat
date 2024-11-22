9ec428a7f90a48a4eb3c7fd15203d4d09a4c78ae
Rodrigo Agundez <rragundez@users.noreply.github.com>
2024-11-22 21:34:41 +0800

    Update cvat/apps/iam/signals.py
    Co-authored-by: Maria Khrustaleva <maria@cvat.ai>

--- cvat/apps/iam/signals.py
+++ cvat/apps/iam/signals.py
@@ -42,7 +42,7 @@ elif settings.IAM_TYPE == 'LDAP':
                     if role == settings.IAM_ADMIN_ROLE:
                         user.is_staff = user.is_superuser = True
                     break
-        # add default group if no other group has been assigned and the user is not a superuser
+        # add default group if no other group has been assigned
         if not len(user_groups):
             user_groups.append(Group.objects.get(name=settings.IAM_DEFAULT_ROLE))
 
