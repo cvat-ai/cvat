9091be814da4f95f85c9a533d24e83d2bfc87a77
Andrey Zhavoronkov <andrey@cvat.ai>
2024-11-28 17:32:50 +0300

    Prepare chunks in a worker process (#8618)
    <!-- Raise an issue to propose your change
    (https://github.com/cvat-ai/cvat/issues).
    It helps to avoid duplication of efforts from multiple independent
    contributors.
    Discuss your ideas with maintainers to be sure that changes will be
    approved and merged.
    Read the [Contribution guide](https://docs.cvat.ai/docs/contributing/).
    -->
    
    <!-- Provide a general summary of your changes in the Title above -->
    Added:
    
    - Prepare chunks in rq workers instead of application server process
    - Increase TTL for cached preview images
    
    ### Motivation and context
    <!-- Why is this change required? What problem does it solve? If it
    fixes an open
    issue, please link to the issue here. Describe your changes in detail,
    add
    screenshots. -->
    
    ### How has this been tested?
    <!-- Please describe in detail how you tested your changes.
    Include details of your testing environment, and the tests you ran to
    see how your change affects other areas of the code, etc. -->
    
    ### Checklist
    <!-- Go over all the following points, and put an `x` in all the boxes
    that apply.
    If an item isn't applicable for some reason, then ~~explicitly
    strikethrough~~ the whole
    line. If you don't do that, GitHub will show incorrect progress for the
    pull request.
    If you're unsure about any of these, don't hesitate to ask. We're here
    to help! -->
    - [x] I submit my changes into the `develop` branch
    - [x] I have created a changelog fragment <!-- see top comment in
    CHANGELOG.md -->
    - [ ] I have updated the documentation accordingly
    - [ ] I have added tests to cover my changes
    - [ ] I have linked related issues (see [GitHub docs](
    
    https://help.github.com/en/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword))
    - [ ] I have increased versions of npm packages if it is necessary
    
    ([cvat-canvas](https://github.com/cvat-ai/cvat/tree/develop/cvat-canvas#versioning),
    
    [cvat-core](https://github.com/cvat-ai/cvat/tree/develop/cvat-core#versioning),
    
    [cvat-data](https://github.com/cvat-ai/cvat/tree/develop/cvat-data#versioning)
    and
    
    [cvat-ui](https://github.com/cvat-ai/cvat/tree/develop/cvat-ui#versioning))
    
    ### License
    
    - [x] I submit _my code changes_ under the same [MIT License](
    https://github.com/cvat-ai/cvat/blob/develop/LICENSE) that covers the
    project.
      Feel free to contact the maintainers if that's a concern.
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced new debugging configurations for various RQ workers,
    enhancing debugging capabilities.
    - Added a new service for processing chunks in Docker and Kubernetes
    configurations.
    - Enhanced error handling in data retrieval methods to manage timeout
    scenarios effectively.
    
    - **Bug Fixes**
    - Improved robustness of job-related functionalities with enhanced test
    coverage and validation checks.
    
    - **Documentation**
    - Updated API documentation settings and configurations for better
    clarity and usability.
    
    - **Tests**
    - Expanded test suite with new test cases and parameterization for
    comprehensive validation of task and cloud storage functionalities.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- cvat/apps/engine/default_settings.py
+++ cvat/apps/engine/default_settings.py
@@ -14,3 +14,13 @@ will be automatically switched to the dynamic cache.
 When enabled, this option can increase data access speed and reduce server load,
 but significantly increase disk space occupied by tasks.
 """
+
+CVAT_CHUNK_CREATE_TIMEOUT = 50
+"""
+Sets the chunk preparation timeout in seconds after which the backend will respond with 429 code.
+"""
+
+CVAT_CHUNK_CREATE_CHECK_INTERVAL = 0.2
+"""
+Sets the frequency of checking the readiness of the chunk
+"""
