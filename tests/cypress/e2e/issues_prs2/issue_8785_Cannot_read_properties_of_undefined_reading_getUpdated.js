577bb060be85335e58d45d930cf59e116a02aea4
Oleg Valiulin <oleg.valiulin@cvat.ai>
2024-12-18 14:23:10 +0000

    add middleware

--- tests/cypress/e2e/issues_prs2/issue_8785_Cannot_read_properties_of_undefined_reading_getUpdated.js
+++ tests/cypress/e2e/issues_prs2/issue_8785_Cannot_read_properties_of_undefined_reading_getUpdated.js
@@ -16,7 +16,35 @@ context('When saving after deleting a frame, job metadata is inconsistent.', () 
 
     describe(`Testing issue ${issueId}`, () => {
         it('Crash on Save job. Save again.', () => {
-            cy.deleteFrame(); // FIXME: does normal saving with 200, better to just push a button
+            // cy.deleteFrame(); // FIXME: does normal saving with 200, better to just push a button
+            function clickDelete() {
+                cy.get('.cvat-player-delete-frame').click();
+                cy.get('.cvat-modal-delete-frame').within(() => {
+                    cy.contains('button', 'Delete').click();
+                });
+            }
+            function clickSave() {
+                cy.get('button').contains('Save').click({ force: true });
+                cy.get('button').contains('Save').trigger('mouseout');
+            }
+            function middleware(request, response) {
+                let calls = 0;
+                const responseStub = { statusCode: 502, body: 'Network error' };
+                function handle(req, res) {
+                    if (calls === 0) {
+                        calls++;
+                        res.send(responseStub);
+                    } else {
+                        req.continue();
+                    }
+                }
+                handle(request, response);
+            }
+            cy.intercept('PATCH', '/api/jobs/**/data/meta**', middleware);
+            clickDelete();
+            clickSave();
+
+            cy.wait('@patchMeta');
 
             // Check that frame is deleted
             cy.contains('button', 'Restore').should('be.visible');
