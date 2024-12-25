2bcbfbfacb72eec647c5f927d1a2329bc7ed76c9
Kirill Lakhov <kirill.9992@gmail.com>
2024-12-25 11:08:09 +0300

    update to styles for passing tests

--- cvat-ui/src/components/quality-control/task-quality/allocation-table.tsx
+++ cvat-ui/src/components/quality-control/task-quality/allocation-table.tsx
@@ -164,6 +164,7 @@ function AllocationTable(props: Readonly<Props>): JSX.Element {
                     selection.selectedRowKeys.length !== 0 ? (
                         <>
                             <DeleteOutlined
+                                className='cvat-allocation-selection-frame-delete'
                                 onClick={() => {
                                     const framesToUpdate = selection.selectedRows
                                         .filter((frameData) => frameData.active)
@@ -173,6 +174,7 @@ function AllocationTable(props: Readonly<Props>): JSX.Element {
                                 }}
                             />
                             <Icon
+                                className='cvat-allocation-selection-frame-restore'
                                 onClick={() => {
                                     const framesToUpdate = selection.selectedRows
                                         .filter((frameData) => !frameData.active)
