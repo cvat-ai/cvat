20cada4c725affefbe0b818ce9f928752ffccc02
Roman Donchenko <roman@cvat.ai>
2024-12-25 19:42:55 +0200

    Simplify the code formatting script
    After #8611 and #8866, it's no longer necessary to apply the formatters to
    each module separately. So don't!
    
    In addition, update the black exclusion list in `pyproject.toml` to only contain
    files that were _not_ listed in `format_python_code.sh`. This way, formatting
    will automatically be enforced for all new files.
    
    For a few of the files reformatting only creates a small diff, so don't even
    bother excluding them.

--- dev/format_python_code.sh
+++ dev/format_python_code.sh
@@ -15,29 +15,5 @@ if ! ${ISORT} --version >/dev/null 2>&1; then
     exit 1
 fi
 
-# The commands must be run on each module directory separately,
-# otherwise tools confuse the "current" module
-for paths in \
-    "cvat-sdk" \
-    "cvat-cli" \
-    "tests/python/" \
-    "cvat/apps/quality_control" \
-    "cvat/apps/analytics_report" \
-    "cvat/apps/engine/lazy_list.py" \
-    "cvat/apps/engine/background.py" \
-    "cvat/apps/engine/frame_provider.py" \
-    "cvat/apps/engine/cache.py" \
-    "cvat/apps/engine/default_settings.py" \
-    "cvat/apps/engine/field_validation.py" \
-    "cvat/apps/engine/model_utils.py" \
-    "cvat/apps/engine/task_validation.py" \
-    "cvat/apps/dataset_manager/cron.py" \
-    "cvat/apps/dataset_manager/tests/test_annotation.py" \
-    "cvat/apps/dataset_manager/tests/utils.py" \
-    "cvat/apps/events/signals.py" \
-    "cvat/apps/engine/management/commands/syncperiodicjobs.py" \
-    "cvat/apps/dataset_manager/management/commands/cleanuplegacyexportcache.py" \
-    ; do
-    ${BLACK} -- ${paths}
-    ${ISORT} -- ${paths}
-done
+${BLACK} .
+${ISORT} --resolve-all-configs .
