d6e5503b1f3d6c0050481f1a471358ea6eb357c6
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-01-17 22:35:33 +0000

    Ensure GET /data/meta's happened, restore state before deletion'

--- tests/cypress/e2e/issues_prs2/issue_8872_delete_frame.js
+++ tests/cypress/e2e/issues_prs2/issue_8872_delete_frame.js
@@ -22,42 +22,46 @@ context('UI and job metadata work correctly when deleting frames', () => {
             cy.intercept('GET', '/api/jobs/**/data/meta**').as('getMeta');
             cy.intercept('PATCH', '/api/jobs/**/data/meta**').as('patchMeta');
 
-            // Intercept first request after loading the job
             cy.openTaskJob(taskName);
+            // Ensure first request is sent after loading the job
             cy.wait('@getMeta');
 
             const frameAlias = 'oldCurrentFrame';
 
             cy.goToNextFrame(1);
-            cy.wait('@getMeta').then((interceptor) => {
-                expect(interceptor.response.body).to.haveOwnProperty('frames');
+            cy.wait('@getMeta').then(() => {
                 // enqueue current frame number
                 // save it in context
                 cy.getCurrentFrameNumber(frameAlias);
             });
 
-            // Delete frame, ignore new request
             cy.clickDeleteFrame();
-            cy.get('.cvat-player-restore-frame').should('not.exist'); // ???: not sure about this one, it might be visible
+            cy.get('.cvat-player-restore-frame').should('not.exist');
+            cy.wait('@getMeta');
 
             // Save and intercept request to confirm validate deleted frames
             cy.clickSave();
-            cy.wait('@patchMeta').then((interceptor) => {
-                cy.get(`@${frameAlias}`).then((frameNum) => {
-                    expect(interceptor.response.body).to.haveOwnProperty('deleted_frames');
-                    const deletedFrames = interceptor.response.body.deleted_frames;
-                    assert(Array.isArray(deletedFrames) && deletedFrames.length === 1);
+            cy.get(`@${frameAlias}`).then((frameNum) => {
+                cy.wait('@patchMeta').then((interceptDeleted) => {
+                    const deletedFrames = interceptDeleted.request.body.deleted_frames;
 
                     // Check old frame is unavailable
                     cy.checkFrameNum(frameNum + 1);
 
-                    // Check deleted frames are correct
-                    const [actualDeletedFrames, expectedDeletedFrames] = [deletedFrames[0], frameNum];
-                    expect(actualDeletedFrames).to.equal(expectedDeletedFrames);
+                    // Check deleted frame are correct
+                    expect(deletedFrames).to.include(frameNum);
+                });
+                // Restore state and save
+                // Validate UI and that no frames are marked deleted
+                cy.contains('.cvat-annotation-header-button', 'Undo').click();
+                cy.clickSave();
+                cy.wait('@patchMeta').then((interceptRestored) => {
+                    const deletedFrames = interceptRestored.request.body.deleted_frames;
+                    cy.wrap(deletedFrames).should('be.empty');
+                    cy.checkFrameNum(frameNum);
                 });
             });
         });
-
         after(() => {
             cy.window().then((window) => {
                 window.cvat.config.jobMetaDataReloadPeriod = defaultJobMetadataReloadPreiod;
