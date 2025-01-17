580f9a860663e4171ce20f7e678c3f4f63a475cb
Maria Khrustaleva <maria@cvat.ai>
2025-01-17 10:21:39 +0100

    Simplify the code

--- cvat/apps/redis_handler/migration_loader.py
+++ cvat/apps/redis_handler/migration_loader.py
@@ -4,10 +4,8 @@
 
 import importlib
 from pathlib import Path
-from typing import Any, Generator, Mapping
 
 from django.apps import AppConfig, apps
-from django.utils.module_loading import module_has_submodule
 
 from cvat.apps.redis_handler.models import RedisMigration
 from cvat.apps.redis_handler.redis_migrations import BaseMigration
@@ -21,56 +19,36 @@ class MigrationLoader:
     REDIS_MIGRATIONS_DIR_NAME = "redis_migrations"
     REDIS_MIGRATION_CLASS_NAME = "Migration"
 
-    class AppConfigs:
-        def __init__(self):
-            self._app_configs = self._find_app_configs()
-            self._app_configs_mapping: Mapping[str, AppConfig] = self._init_app_configs_mapping()
-
-        def _find_app_configs(self):
-            return [
-                app_config
-                for app_config in apps.get_app_configs()
-                if app_config.name.startswith("cvat")
-                and module_has_submodule(
-                    app_config.module, MigrationLoader.REDIS_MIGRATIONS_DIR_NAME
-                )
-            ]
-
-        def _init_app_configs_mapping(self):
-            return {app_config.label: app_config for app_config in self._app_configs}
-
-        def __getitem__(self, label: str) -> AppConfig:
-            return self._app_configs_mapping[label]
-
-        def __iter__(self) -> Generator[AppConfig, Any, Any]:
-            yield from self._app_configs
-
     def __init__(self) -> None:
-        self._app_configs = self.AppConfigs()
+        self._app_config_mapping = {
+            app_config.label: app_config for app_config in self._find_app_configs()
+        }
         self._disk_migrations_per_app: dict[str, list[str]] = {}
         self._unapplied_migrations: list[BaseMigration] = []
 
         self._load_from_disk()
         self._init_unapplied_migrations()
 
-    @property
-    def app_configs(self) -> "AppConfigs":
-        return self._app_configs
+    def _find_app_configs(self) -> list[AppConfig]:
+        return [
+            app_config
+            for app_config in apps.get_app_configs()
+            if app_config.name.startswith("cvat")
+            and (Path(app_config.path) / self.REDIS_MIGRATIONS_DIR_NAME).exists()
+        ]
 
     def _load_from_disk(self):
-        for app_config in self._app_configs:
+        for app_label, app_config in self._app_config_mapping.items():
             migrations_dir = Path(app_config.path) / self.REDIS_MIGRATIONS_DIR_NAME
             for migration_file in sorted(migrations_dir.glob("[0-9]*.py")):
                 migration_name = migration_file.stem
-                (self._disk_migrations_per_app.setdefault(app_config.label, [])).append(
-                    migration_name
-                )
+                (self._disk_migrations_per_app.setdefault(app_label, [])).append(migration_name)
 
     def _init_unapplied_migrations(self):
         applied_migrations = RedisMigration.objects.all()
 
         for app_label, migration_names in self._disk_migrations_per_app.items():
-            app_config = self.app_configs[app_label]
+            app_config = self._app_config_mapping[app_label]
             app_applied_migrations = {
                 m.name for m in applied_migrations if m.app_label == app_config.label
             }
