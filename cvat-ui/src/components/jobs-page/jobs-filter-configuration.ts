054e262fab798ddc424620fd0f61f836d16f6a3b
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-01-28 13:23:49 +0200

    Fix job type filter name

--- cvat-ui/src/components/jobs-page/jobs-filter-configuration.ts
+++ cvat-ui/src/components/jobs-page/jobs-filter-configuration.ts
@@ -105,7 +105,7 @@ export const config: Partial<Config> = {
                 listValues: [
                     { value: 'annotation', title: 'Annotation' },
                     { value: 'ground_truth', title: 'Ground truth' },
-                    { value: 'consensus_replica', title: 'Consensus' },
+                    { value: 'consensus_replica', title: 'Consensus replica' },
                 ],
             },
         },
