7238cf359251096cc13c85d573e75b00a07a383d
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-01-30 15:58:19 +0300

    Consensus task creation (#8939)

--- cvat-ui/src/actions/tasks-actions.ts
+++ cvat-ui/src/actions/tasks-actions.ts
@@ -262,18 +262,23 @@ ThunkAction {
         if (data.cloudStorageId) {
             description.cloud_storage_id = data.cloudStorageId;
         }
+        if (data.advanced.consensusReplicas) {
+            description.consensus_replicas = +data.advanced.consensusReplicas;
+        }
 
-        let extras = {};
+        const extras: Record<string, any> = {};
 
         if (data.quality.validationMode !== ValidationMode.NONE) {
-            extras = {
-                validation_params: {
-                    mode: data.quality.validationMode,
-                    frame_selection_method: data.quality.frameSelectionMethod,
-                    frame_share: data.quality.validationFramesPercent,
-                    frames_per_job_share: data.quality.validationFramesPerJobPercent,
-                },
+            extras.validation_params = {
+                mode: data.quality.validationMode,
+                frame_selection_method: data.quality.frameSelectionMethod,
+                frame_share: data.quality.validationFramesPercent,
+                frames_per_job_share: data.quality.validationFramesPerJobPercent,
             };
+        }
+
+        if (data.advanced.consensusReplicas) {
+            extras.consensus_replicas = description.consensus_replicas;
         }
 
         const taskInstance = new cvat.classes.Task(description);
