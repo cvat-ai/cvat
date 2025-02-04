f3e8bc6bea4f9582891ef43e6e79f17b62c7760b
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-02-04 12:21:54 +0200

    Update calls

--- cvat/apps/quality_control/quality_reports.py
+++ cvat/apps/quality_control/quality_reports.py
@@ -2540,7 +2540,7 @@ class QualityReportUpdateManager:
             )
             db_job_reports.append(db_job_report)
 
-        db_job_reports = bulk_create(db_model=models.QualityReport, objects=db_job_reports)
+        db_job_reports = bulk_create(db_model=models.QualityReport, objs=db_job_reports)
 
         db_conflicts = []
         db_report_iter = itertools.chain([db_task_report], db_job_reports)
@@ -2555,7 +2555,7 @@ class QualityReportUpdateManager:
                 )
                 db_conflicts.append(db_conflict)
 
-        db_conflicts = bulk_create(db_model=models.AnnotationConflict, objects=db_conflicts)
+        db_conflicts = bulk_create(db_model=models.AnnotationConflict, objs=db_conflicts)
 
         db_ann_ids = []
         db_conflicts_iter = iter(db_conflicts)
@@ -2571,7 +2571,7 @@ class QualityReportUpdateManager:
                     )
                     db_ann_ids.append(db_ann_id)
 
-        db_ann_ids = bulk_create(db_model=models.AnnotationId, objects=db_ann_ids)
+        db_ann_ids = bulk_create(db_model=models.AnnotationId, objs=db_ann_ids)
 
         return db_task_report
 
