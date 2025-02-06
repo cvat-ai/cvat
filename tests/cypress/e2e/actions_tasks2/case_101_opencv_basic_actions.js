ac1c3d4073c872709523db84d063e8ff1f8e4a42
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-02-06 18:07:46 +0000

    wip: correct tracking, bad delta

--- tests/cypress/e2e/actions_tasks2/case_101_opencv_basic_actions.js
+++ tests/cypress/e2e/actions_tasks2/case_101_opencv_basic_actions.js
@@ -42,15 +42,6 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
         secondY: 145,
         labelName,
     };
-    // const createRectangleTrack2Points = {
-    //     points: 'By 2 Points',
-    //     type: 'Track',
-    //     firstX: 82,
-    //     firstY: 129,
-    //     secondX: 107,
-    //     secondY: 145,
-    //     labelName,
-    // };
 
     const keyCodeN = 78;
     const pointsMap = [
@@ -66,13 +57,14 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
     const textDefaultValue = 'Some default value for type Text';
     const imagesCount = 5;
     const imageFileName = `image_${labelName.replace(' ', '_').toLowerCase()}`;
-    const width = 400;
-    const height = 400;
-    const delta = 3;
-    const maxTextWidth = 2000;
-    const textHeightPx = 70;
-    const posX = 10;
-    const posY = 10; // TODO: determine scale of picture
+    const scaleFrom400 = 12.5;
+    const width = 400 * scaleFrom400; // =5000
+    const height = 400 * scaleFrom400;
+    const delta = 3; // ???
+    const maxTextWidth = undefined;
+    const textHeightPx = 77 * scaleFrom400;
+    const posX = 10 * scaleFrom400;
+    const posY = 10 * scaleFrom400; // TODO: determine scale of picture
     const archiveName = `${imageFileName}.zip`;
     const archivePath = `cypress/fixtures/${archiveName}`;
     const imagesFolder = `cypress/fixtures/${imageFileName}`;
@@ -86,12 +78,16 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
         cy.visit('/auth/login');
         cy.login();
         for (let i = 0; i < imagesCount; i++) {
-            cy.task('log', `x:${posX + i * 5}, y:${posY + i * 5}`);
+            // cy.task('log', `x:${posX + i * 5}, y:${posY + i * 5}`);
             // cy.imageGenerator(imagesFolder, imageFileName + i, width, height, color, posX + i * 5,
             //     posY + i * 5, labelName, 1, extension);
+            // TODO: compare times, makeCustomImage might take to long (from 00:58 to 1:30)
             cy.makeCustomImage(imagesFolder, `${imageFileName}_${i}`,
-                width, height, fontSize, color, textColor,
-                posX + i * 5, posY + i * 5, `${labelName}. Num ${i}`, extension, maxTextWidth);
+                width, height,
+                fontSize, color, textColor,
+                posX + i * 5 * scaleFrom400, posY + i * 5 * scaleFrom400,
+                `${labelName}. Num ${i}`, extension,
+                maxTextWidth);
             /* TODO:
                 1. recreate scale and position of the text as it was previously with jimp but now with canvas
                     - side quest: draw from ofscreencanvas
@@ -120,7 +116,7 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
             cy.opencvCreateShape(createOpencvShapeSecondLabel);
         });
 
-        it('Change the number of points when the shape is drawn. Cancel drawing.', () => {
+        it.skip('Change the number of points when the shape is drawn. Cancel drawing.', () => {
             cy.interactOpenCVControlButton();
             cy.get('.cvat-opencv-drawing-tool').click();
             pointsMap.forEach((element) => {
@@ -170,7 +166,7 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
             cy.get('.cvat_canvas_shape').should('have.length', 2);
         });
 
-        it('Check "Intelligent scissors blocking feature". Cancel drawing.', () => {
+        it.skip('Check "Intelligent scissors blocking feature". Cancel drawing.', () => {
             cy.interactOpenCVControlButton();
             cy.get('.cvat-opencv-drawing-tool').click();
             cy.get('.cvat-annotation-header-block-tool-button').click();
@@ -198,7 +194,7 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
             cy.get('body').type('{Esc}');
         });
 
-        it('Check "Histogram Equalization" feature.', () => {
+        it.skip('Check "Histogram Equalization" feature.', () => {
             cy.checkPopoverHidden('opencv-control');
             cy.interactOpenCVControlButton();
             cy.get('.cvat-opencv-control-popover').contains('[role="tab"]', 'Image').click();
@@ -245,11 +241,16 @@ context('OpenCV. Intelligent scissors. Histogram Equalization. TrackerMIL.', () 
                         // In the beginning of this test we created images with text
                         // On each frame text is moved by 5px on x and y axis,
                         // so we expect shape to be close to real text positions
-                        cy.get('#cvat_canvas_shape_4').invoke('attr', 'x').then((xVal) => {
-                            expect(parseFloat(xVal)).to.be.closeTo(x + i * 5, delta);
-                        });
-                        cy.get('#cvat_canvas_shape_4').invoke('attr', 'y').then((yVal) => {
-                            expect(parseFloat(yVal)).to.be.closeTo(y + i * 5, delta);
+                        // cy.get('#cvat_canvas_shape_4').invoke('attr', 'x').then((xVal) => {
+                        //     expect(parseFloat(xVal)).to.be.closeTo(x + i * 5 * scaleFrom400, delta);
+                        // });
+                        // cy.get('#cvat_canvas_shape_4').invoke('attr', 'y').then((yVal) => {
+                        //     expect(parseFloat(yVal)).to.be.closeTo(y + i * 5 * scaleFrom400, delta);
+                        // }); // TODO: refactor to $shape
+                        cy.get('#cvat_canvas_shape_4').then(($shape) => {
+                            cy.task('log', `x:${+$shape.attr('x')}, y:${+$shape.attr('x')}`);
+                            expect(+$shape.attr('x')).to.be.closeTo(x + i * 5 * scaleFrom400, delta);
+                            expect(+$shape.attr('y')).to.be.closeTo(y + i * 5 * scaleFrom400, delta);
                         });
                         cy.get('#cvat-objects-sidebar-state-item-4')
                             .should('contain', 'RECTANGLE TRACK')
