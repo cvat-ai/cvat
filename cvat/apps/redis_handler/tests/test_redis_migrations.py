aea7dd2eefe03af93731b5c709aa00c823c9dd84
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-02-18 12:59:38 +0000

    wip: first test, assert migration added

--- cvat/apps/redis_handler/tests/test_redis_migrations.py
+++ cvat/apps/redis_handler/tests/test_redis_migrations.py
@@ -26,9 +26,9 @@ WORKDIR = PosixPath("cvat/apps")
 MIGRATION_DIR = MigrationLoader.REDIS_MIGRATIONS_DIR_NAME
 MIGRATION_CLASS_NAME = MigrationLoader.REDIS_MIGRATION_CLASS_NAME
 MIGRATION_BASE_CLASS_NAME = f"Base{MIGRATION_CLASS_NAME}"
-MIGRATION_BLUEPRINT_MODULE_PATH = f"cvat.apps.engine.redis_migrations.001_cleanup_scheduled_jobs"
+BASELINE_MIGRATION_NAME = f"engine.001_cleanup_scheduled_jobs"
 
-MIGRATION_FILENAME_FORMAT = '{:03}_{}.py'
+MIGRATION_NAME_FORMAT = '{:03}_{}'
 
 MIGRATION_FILE_CONTENT = '''\
 from cvat.apps.redis_handler.migration_loader import BaseMigration
@@ -53,61 +53,43 @@ class TestRedisMigrations(TestCase):
         migration_dir = app_path / MIGRATION_DIR
         if not os.path.exists:
             os.mkdir(migration_dir)
-        filename = MIGRATION_FILENAME_FORMAT.format(number, migration_name)
+        filename = MIGRATION_NAME_FORMAT.format(number, migration_name) + ".py"
         migration_file_path = migration_dir / filename
         with open(migration_file_path, 'w', encoding='utf8') as file:
             file.write(MIGRATION_FILE_CONTENT)
         return migration_file_path
 
     def _init_test_migration(
-        self, app_name: str, migration_name: str, runner: Callable[[MigrationType], None]
+        self, app_name: str, migration_name: str, runner: Callable[[MigrationType], None], number: int = 0
     ):
 
         def path_to_module(path: PosixPath) -> str:
             return str(path).removesuffix(".py").replace("/", ".")
 
-        def app_module_to_label(appModule: str):
-            return appModule.rpartition(".")[2]
-
-        # def init_migration_mock(mock: Mock, app_path: PosixPath, migration_name: str):
-        #     app_module_path = path_to_module(app_path)
-        #     app_label = app_module_to_label(app_module_path)
-        #     self.test_migration = mock
-        #     # self.test_migration.__class__ = BaseMigration
-        #     self.test_migration.app_label = app_label
-        #     self.test_migration.name = migration_name
-        #     self.test_migration.run = runner
-
         app_path = WORKDIR / app_name
         assert app_path.exists()
 
-        print(app_path)
-
-        migration_file_path = TestRedisMigrations.generate_migration_file(app_path, migration_name)
+        migration_file_path = TestRedisMigrations.generate_migration_file(app_path, migration_name, number)
         mock_migration_module_path = path_to_module(migration_file_path)
-        # obj_path = f"{mock_migration_module_path}.{MIGRATION_CLASS_NAME}"
 
-        # print(migration_file_path)
         Dummy = get_class_from_module(mock_migration_module_path, MIGRATION_CLASS_NAME)
         # ???: what does loader do if a migration doesn't have a number?
 
         self.patcher: _patch = patch.object(Dummy, 'run', new_callable=lambda: runner)
-        mock = self.patcher.start()
-        print("INIT")
-        # init_migration_mock(mock, app_path, migration_name)
+        self.patcher.start()
 
+        self.exp_migration_name = ".".join([app_name, MIGRATION_NAME_FORMAT.format(number, migration_name)])
 
-    @classmethod
-    def setUpClass(cls):
-        cls.Blueprint = get_class_from_module(MIGRATION_BLUEPRINT_MODULE_PATH, MIGRATION_CLASS_NAME)
-        # cls.redis = fakeredis.FakeRedis()
 
     def setUp(self):
+
+        # TODO: cleanup test files on setup
+        # garbage collection in a factory?
 
         def _runner1(cls: BaseMigration):
             print("Running,,,,")
             with cls.connection as conn:
-                # conn.hset
+                # conn.hset ...
                 print("Migrating.....")
             print("Finished...")
 
@@ -123,7 +105,17 @@ class TestRedisMigrations(TestCase):
 
     def test_add_migration(self, redis1, redis2):
         call_command("migrateredis")
-        tmp = 1
-        ...
+        exp_migrations = {
+            bytes(BASELINE_MIGRATION_NAME, encoding='utf8'),
+            bytes(self.exp_migration_name, encoding='utf8')
+        }
+        with redis1() as conn:
+            self.assertEqual(
+                conn.smembers("cvat:applied_migrations"),
+                exp_migrations
+            )
+            print("hello")
+        # with redis1() as conn:
+            # conn.keys()
         # '''Add a migration, validate it is there'''
         # '''Restore redis, validate it is not there'''
