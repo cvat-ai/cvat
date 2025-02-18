73b4a0c6fd1f9e849c5cdb05d990f66cb43260a7
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-02-18 14:41:54 +0000

    refactor funcs, remove comments

--- cvat/apps/redis_handler/tests/test_redis_migrations.py
+++ cvat/apps/redis_handler/tests/test_redis_migrations.py
@@ -13,8 +13,7 @@ from django.core.management import call_command
 
 
 
-from unittest.mock import Mock, patch, _patch
-from .utils import mock_class
+from unittest.mock import patch, _patch
 from unittest import TestCase
 
 from cvat.apps.redis_handler.migration_loader import MigrationLoader, BaseMigration#, LoaderError
@@ -48,17 +47,6 @@ MigrationType = type[BaseMigration]
 @patch(f"{MUT}.management.commands.migrateredis.Redis", return_value=fakeredis.FakeRedis())
 class TestRedisMigrations(TestCase):
 
-    @staticmethod
-    def generate_migration_file(app_path: PosixPath, migration_name: str, number: int = 0) -> PosixPath:
-        migration_dir = app_path / MIGRATION_DIR
-        if not os.path.exists:
-            os.mkdir(migration_dir)
-        filename = MIGRATION_NAME_FORMAT.format(number, migration_name) + ".py"
-        migration_file_path = migration_dir / filename
-        with open(migration_file_path, 'w', encoding='utf8') as file:
-            file.write(MIGRATION_FILE_CONTENT)
-        return migration_file_path
-
     def _init_test_migration(
         self, app_name: str, migration_name: str, runner: Callable[[MigrationType], None], number: int = 0
     ):
@@ -66,10 +54,23 @@ class TestRedisMigrations(TestCase):
         def path_to_module(path: PosixPath) -> str:
             return str(path).removesuffix(".py").replace("/", ".")
 
+        def make_migration_name(migration_name: str, number: int):
+            return MIGRATION_NAME_FORMAT.format(number, migration_name)
+
+        def generate_migration_file(app_path: PosixPath, migration_name: str, number: int = 0) -> PosixPath:
+            migration_dir = app_path / MIGRATION_DIR
+            if not os.path.exists(migration_dir):
+                os.mkdir(migration_dir)
+            filename = make_migration_name(migration_name, number) + ".py"
+            migration_file_path = migration_dir / filename
+            with open(migration_file_path, 'w', encoding='utf8') as file:
+                file.write(MIGRATION_FILE_CONTENT)
+            return migration_file_path
+
         app_path = WORKDIR / app_name
         assert app_path.exists()
 
-        migration_file_path = TestRedisMigrations.generate_migration_file(app_path, migration_name, number)
+        migration_file_path = generate_migration_file(app_path, migration_name, number)
         mock_migration_module_path = path_to_module(migration_file_path)
 
         Dummy = get_class_from_module(mock_migration_module_path, MIGRATION_CLASS_NAME)
@@ -78,7 +79,7 @@ class TestRedisMigrations(TestCase):
         self.patcher: _patch = patch.object(Dummy, 'run', new_callable=lambda: runner)
         self.patcher.start()
 
-        self.exp_migration_name = ".".join([app_name, MIGRATION_NAME_FORMAT.format(number, migration_name)])
+        self.exp_migration_name = app_name + "." + make_migration_name(migration_name, number)
 
 
     def setUp(self):
@@ -93,15 +94,10 @@ class TestRedisMigrations(TestCase):
                 print("Migrating.....")
             print("Finished...")
 
-        # def unset_test_migration():
-        #     self.test_migration = None
-
         self.patcher = None
-        # self.test_migration = None
         self._init_test_migration("redis_handler", "first", _runner1)
 
         self.addCleanup(self.patcher.stop)
-        # self.addCleanup(unset_test_migration)
 
     def test_add_migration(self, redis1, redis2):
         call_command("migrateredis")
@@ -114,8 +110,5 @@ class TestRedisMigrations(TestCase):
                 conn.smembers("cvat:applied_migrations"),
                 exp_migrations
             )
-            print("hello")
-        # with redis1() as conn:
-            # conn.keys()
         # '''Add a migration, validate it is there'''
         # '''Restore redis, validate it is not there'''
