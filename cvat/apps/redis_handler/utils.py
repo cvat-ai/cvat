b87c283c94e7af8d2db03d478f00260bf00ac604
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-02-18 16:36:13 +0000

    satisfy MigrationLoader.get_migration_class which is ready to receive a None

--- cvat/apps/redis_handler/utils.py
+++ cvat/apps/redis_handler/utils.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import importlib
 
 
-def get_class_from_module(module_path: str | Path, class_name: str) -> type:
+def get_class_from_module(module_path: str | Path, class_name: str) -> type | None:
     module = importlib.import_module(module_path)
     klass = getattr(module, class_name, None)
     return klass
