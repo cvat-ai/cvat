164a1902cd3fa15398fd6a59d58d768adc300c95
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-02-18 16:34:16 +0000

    wip: case 'migration is added and applied'

--- cvat/apps/redis_handler/tests/test_redis_migrations.py
+++ cvat/apps/redis_handler/tests/test_redis_migrations.py
@@ -13,11 +13,12 @@ from django.core.management import call_command
 
 
 
-from unittest.mock import patch, _patch
+from unittest.mock import Mock, patch, _patch
 from unittest import TestCase
 
 from cvat.apps.redis_handler.migration_loader import MigrationLoader, BaseMigration#, LoaderError
 from cvat.apps.redis_handler.utils import get_class_from_module
+from .utils import path_to_module
 
 
 WORKDIR = PosixPath("cvat/apps")
@@ -36,7 +37,6 @@ class Migration(BaseMigration):
     @classmethod
     def run(cls): ...
 
-
 '''
 
 MUT = 'cvat.apps.redis_handler'
@@ -46,69 +46,66 @@ MigrationType = type[BaseMigration]
 @patch(f"{MUT}.migration_loader.Redis", return_value=fakeredis.FakeRedis())
 @patch(f"{MUT}.management.commands.migrateredis.Redis", return_value=fakeredis.FakeRedis())
 class TestRedisMigrations(TestCase):
+    class TestMigration:
 
-    def _init_test_migration(
-        self, app_name: str, migration_name: str, runner: Callable[[MigrationType], None], number: int = 0
-    ):
+        def __init__ (
+            self, app_name: str, migration_name: str, runner: Callable[[MigrationType], None], number: int = 0
+        ):
+            self.app_name = app_name
+            self.app_path = WORKDIR / app_name
+            assert self.app_path.exists()
+            self.migration_name = migration_name
+            self.number = number
 
-        def path_to_module(path: PosixPath) -> str:
-            return str(path).removesuffix(".py").replace("/", ".")
+            self.migration_file_path = self.generate_migration_file()
+            mock_migration_module_path = path_to_module(self.migration_file_path)
 
-        def make_migration_name(migration_name: str, number: int):
-            return MIGRATION_NAME_FORMAT.format(number, migration_name)
+            self.test_class = get_class_from_module(mock_migration_module_path, MIGRATION_CLASS_NAME)
+            self.patcher: _patch = patch.object(self.test_class, 'run')#new_callable=lambda: runner)
+            self.runner_mock: Mock = self.patcher.start()
 
-        def generate_migration_file(app_path: PosixPath, migration_name: str, number: int = 0) -> PosixPath:
-            migration_dir = app_path / MIGRATION_DIR
+            self.exp_migration_name = app_name + "." + self.make_migration_name()
+            # ???: what does loader do if a migration doesn't have a number?
+
+        def make_migration_name(self):
+            return MIGRATION_NAME_FORMAT.format(self.number, self.migration_name)
+
+        def generate_migration_file(self) -> PosixPath:
+            migration_dir = self.app_path / MIGRATION_DIR
             if not os.path.exists(migration_dir):
                 os.mkdir(migration_dir)
-            filename = make_migration_name(migration_name, number) + ".py"
+            filename = self.make_migration_name() + ".py"
             migration_file_path = migration_dir / filename
             with open(migration_file_path, 'w', encoding='utf8') as file:
                 file.write(MIGRATION_FILE_CONTENT)
             return migration_file_path
 
-        app_path = WORKDIR / app_name
-        assert app_path.exists()
-
-        migration_file_path = generate_migration_file(app_path, migration_name, number)
-        mock_migration_module_path = path_to_module(migration_file_path)
-
-        Dummy = get_class_from_module(mock_migration_module_path, MIGRATION_CLASS_NAME)
-        # ???: what does loader do if a migration doesn't have a number?
-
-        self.patcher: _patch = patch.object(Dummy, 'run', new_callable=lambda: runner)
-        self.patcher.start()
-
-        self.exp_migration_name = app_name + "." + make_migration_name(migration_name, number)
+        def cleanup(self):
+            self.patcher.stop()
+            self.logic = None
+            os.remove(self.migration_file_path)
 
 
     def setUp(self):
 
-        # TODO: cleanup test files on setup
-        # garbage collection in a factory?
-
         def _runner1(cls: BaseMigration):
-            print("Running,,,,")
-            with cls.connection as conn:
-                # conn.hset ...
-                print("Migrating.....")
-            print("Finished...")
+            ...
 
-        self.patcher = None
-        self._init_test_migration("redis_handler", "first", _runner1)
+        self.test_migration = self.TestMigration("redis_handler", "first", _runner1)
 
-        self.addCleanup(self.patcher.stop)
+        self.addCleanup(self.test_migration.cleanup)
 
-    def test_add_migration(self, redis1, redis2):
+    def test_migration_added_and_applied(self, redis1, _):
         call_command("migrateredis")
         exp_migrations = {
             bytes(BASELINE_MIGRATION_NAME, encoding='utf8'),
-            bytes(self.exp_migration_name, encoding='utf8')
+            bytes(self.test_migration.exp_migration_name, encoding='utf8')
         }
         with redis1() as conn:
             self.assertEqual(
                 conn.smembers("cvat:applied_migrations"),
                 exp_migrations
             )
+        self.test_migration.runner_mock.assert_called_once()
         # '''Add a migration, validate it is there'''
         # '''Restore redis, validate it is not there'''
