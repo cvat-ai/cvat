37fd6107f8c4d43c08542f77855e694aaaed197e
Roman Donchenko <roman@cvat.ai>
2025-02-18 19:27:04 +0200

    Blacken remaining migrations (#9103)

--- cvat/apps/engine/migrations/0019_frame_selection.py
+++ cvat/apps/engine/migrations/0019_frame_selection.py
@@ -6,35 +6,35 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('engine', '0018_jobcommit'),
+        ("engine", "0018_jobcommit"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='video',
-            name='start_frame',
+            model_name="video",
+            name="start_frame",
         ),
         migrations.RemoveField(
-            model_name='video',
-            name='step',
+            model_name="video",
+            name="step",
         ),
         migrations.RemoveField(
-            model_name='video',
-            name='stop_frame',
+            model_name="video",
+            name="stop_frame",
         ),
         migrations.AddField(
-            model_name='task',
-            name='frame_filter',
-            field=models.CharField(default='', max_length=256),
+            model_name="task",
+            name="frame_filter",
+            field=models.CharField(default="", max_length=256),
         ),
         migrations.AddField(
-            model_name='task',
-            name='start_frame',
+            model_name="task",
+            name="start_frame",
             field=models.PositiveIntegerField(default=0),
         ),
         migrations.AddField(
-            model_name='task',
-            name='stop_frame',
+            model_name="task",
+            name="stop_frame",
             field=models.PositiveIntegerField(default=0),
         ),
     ]
