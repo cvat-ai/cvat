06a15a1c1ba56551a66598f3b1fe88faf12f6169
Roman Donchenko <roman@cvat.ai>
2025-02-26 19:06:57 +0200

    Blacken settings

--- cvat/settings/testing.py
+++ cvat/settings/testing.py
@@ -8,52 +8,52 @@ import tempfile
 from .development import *  # pylint: disable=wildcard-import
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     },
 }
 
 _temp_dir = tempfile.TemporaryDirectory(dir=BASE_DIR, suffix="cvat")
 BASE_DIR = _temp_dir.name
 
-DATA_ROOT = os.path.join(BASE_DIR, 'data')
+DATA_ROOT = os.path.join(BASE_DIR, "data")
 os.makedirs(DATA_ROOT, exist_ok=True)
 
-MEDIA_DATA_ROOT = os.path.join(DATA_ROOT, 'data')
+MEDIA_DATA_ROOT = os.path.join(DATA_ROOT, "data")
 os.makedirs(MEDIA_DATA_ROOT, exist_ok=True)
 
-CACHE_ROOT = os.path.join(DATA_ROOT, 'cache')
+CACHE_ROOT = os.path.join(DATA_ROOT, "cache")
 os.makedirs(CACHE_ROOT, exist_ok=True)
 
-EXPORT_CACHE_ROOT = os.path.join(CACHE_ROOT, 'export')
+EXPORT_CACHE_ROOT = os.path.join(CACHE_ROOT, "export")
 os.makedirs(EXPORT_CACHE_ROOT, exist_ok=True)
 
-JOBS_ROOT = os.path.join(DATA_ROOT, 'jobs')
+JOBS_ROOT = os.path.join(DATA_ROOT, "jobs")
 os.makedirs(JOBS_ROOT, exist_ok=True)
 
-TASKS_ROOT = os.path.join(DATA_ROOT, 'tasks')
+TASKS_ROOT = os.path.join(DATA_ROOT, "tasks")
 os.makedirs(TASKS_ROOT, exist_ok=True)
 
-PROJECTS_ROOT = os.path.join(DATA_ROOT, 'projects')
+PROJECTS_ROOT = os.path.join(DATA_ROOT, "projects")
 os.makedirs(PROJECTS_ROOT, exist_ok=True)
 
-SHARE_ROOT = os.path.join(BASE_DIR, 'share')
+SHARE_ROOT = os.path.join(BASE_DIR, "share")
 os.makedirs(SHARE_ROOT, exist_ok=True)
 
-MODELS_ROOT = os.path.join(DATA_ROOT, 'models')
+MODELS_ROOT = os.path.join(DATA_ROOT, "models")
 os.makedirs(MODELS_ROOT, exist_ok=True)
 
-LOGS_ROOT = os.path.join(BASE_DIR, 'logs')
+LOGS_ROOT = os.path.join(BASE_DIR, "logs")
 os.makedirs(LOGS_ROOT, exist_ok=True)
 
-MIGRATIONS_LOGS_ROOT = os.path.join(LOGS_ROOT, 'migrations')
+MIGRATIONS_LOGS_ROOT = os.path.join(LOGS_ROOT, "migrations")
 os.makedirs(MIGRATIONS_LOGS_ROOT, exist_ok=True)
 
-CLOUD_STORAGE_ROOT = os.path.join(DATA_ROOT, 'storages')
+CLOUD_STORAGE_ROOT = os.path.join(DATA_ROOT, "storages")
 os.makedirs(CLOUD_STORAGE_ROOT, exist_ok=True)
 
-TMP_FILES_ROOT = os.path.join(DATA_ROOT, 'tmp')
+TMP_FILES_ROOT = os.path.join(DATA_ROOT, "tmp")
 os.makedirs(TMP_FILES_ROOT, exist_ok=True)
 
 # To avoid ERROR django.security.SuspiciousFileOperation:
@@ -67,7 +67,7 @@ for logger in LOGGING["loggers"].values():
 
 LOGGING["handlers"]["server_file"] = LOGGING["handlers"]["console"]
 
-PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 
 # When you run ./manage.py test, Django looks at the TEST_RUNNER setting to
 # determine what to do. By default, TEST_RUNNER points to
@@ -98,5 +98,5 @@ class PatchedDiscoverRunner(DiscoverRunner):
 
 
 # No need to profile unit tests
-INSTALLED_APPS.remove('silk')
-MIDDLEWARE.remove('silk.middleware.SilkyMiddleware')
+INSTALLED_APPS.remove("silk")
+MIDDLEWARE.remove("silk.middleware.SilkyMiddleware")
