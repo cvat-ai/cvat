1c6262876fc741b3ae2874845bb1b460643e2a44
acecode <acecode.tk@gmail.com>
2025-03-11 04:12:33 +0800

    feat: translate empty-list.tsx

--- cvat-ui/src/components/projects-page/empty-list.tsx
+++ cvat-ui/src/components/projects-page/empty-list.tsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import Text from 'antd/lib/typography/Text';
 import { Row, Col } from 'antd/lib/grid';
 import Empty from 'antd/lib/empty';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
     notFound: boolean;
@@ -15,26 +16,29 @@ interface Props {
 
 export default function EmptyListComponent(props: Props): JSX.Element {
     const { notFound } = props;
+    const { t } = useTranslation('base');
+    const { t: tSearch } = useTranslation('base', { keyPrefix: 'search' });
+    const items = t('Projects');
 
     return (
         <div className='cvat-empty-projects-list'>
             <Empty description={notFound ? (
-                <Text strong>No results matched your search...</Text>
+                <Text strong>{tSearch('empty')}</Text>
             ) : (
                 <>
                     <Row justify='center' align='middle'>
                         <Col>
-                            <Text strong>No projects created yet...</Text>
+                            <Text strong>{tSearch('no_created', { items })}</Text>
                         </Col>
                     </Row>
                     <Row justify='center' align='middle'>
                         <Col>
-                            <Text type='secondary'>To get started with your annotation project</Text>
+                            <Text type='secondary'>{tSearch('description')}</Text>
                         </Col>
                     </Row>
                     <Row justify='center' align='middle'>
                         <Col>
-                            <Link to='/projects/create'>create a new one</Link>
+                            <Link to='/projects/create'>{tSearch('create')}</Link>
                         </Col>
                     </Row>
                 </>
