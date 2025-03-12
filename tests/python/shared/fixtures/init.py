d64c0c4017587d2dcce288e7fc1aba3e7f2537fd
Andrey Zhavoronkov <andrey@cvat.ai>
2025-03-12 14:03:34 +0300

    Fix tests

--- tests/python/shared/fixtures/init.py
+++ tests/python/shared/fixtures/init.py
@@ -42,7 +42,7 @@ class Container(str, Enum):
     WORKER_EXPORT = "cvat_worker_export"
     WORKER_QUALITY_REPORTS = "cvat_worker_quality_reports"
     WORKER_WEBHOOKS = "cvat_worker_webhooks"
-    UTILS = "cvat_utils"
+    UTILS = "cvat_worker_utils"
 
     def __str__(self):
         return self.value
