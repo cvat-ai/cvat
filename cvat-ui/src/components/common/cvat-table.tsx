37678146207af76b101f8fee4f9c6917a2781b41
Boris Sekachev <sekachev.bs@gmail.com>
2025-03-25 18:47:35 +0200

    Added CVAT table component

---
+++ cvat-ui/src/components/common/cvat-table.tsx
@@ -0,0 +1,254 @@
+// Copyright (C) CVAT.ai Corporation
+//
+// SPDX-License-Identifier: MIT
+
+import React, { useCallback, useEffect, useState } from 'react';
+import { Row, Col } from 'antd/lib/grid';
+import Table, { TableProps } from 'antd/lib/table';
+import Input from 'antd/lib/input';
+import Text from 'antd/lib/typography/Text';
+import Button from 'antd/lib/button';
+import Space from 'antd/lib/space';
+import Popover from 'antd/lib/popover';
+import Checkbox, { CheckboxChangeEvent } from 'antd/lib/checkbox';
+import { DownloadOutlined, MoreOutlined } from '@ant-design/icons';
+import { Config } from '@react-awesome-query-builder/antd';
+import jsonLogic from 'json-logic-js';
+
+import { ResourceFilterHOC, defaultVisibility } from 'components/resource-sorting-filtering';
+
+type Props = TableProps & {
+    onFilterDataSource?(data: TableProps['dataSource']): void;
+    onChangeColumnVisibility?(id: number, isHidden: boolean): void;
+    queryBuilderConfig?: Partial<Config>;
+    queryBuilderPredefinedQueries?: Record<string, string>;
+    queryBuilderCache?: {
+        key: string;
+        capacity: number;
+    };
+    csvExport?: {
+        filename: string;
+    };
+    tableTitle?: string;
+    searchDataIndex?: (string | string[])[];
+};
+
+/*
+    Enhanced table component
+    Supports:
+    * titles
+    * advanced filtration with json query
+    * overall search
+    * csv export
+    * show/hide columns
+    Current restrictions:
+    * queryBuilderConfig, queryBuilderPredefinedQueries, queryBuilderCache are supposed to be static
+*/
+function CVATTable(props: Props): JSX.Element {
+    const {
+        onChangeColumnVisibility,
+        onFilterDataSource,
+        queryBuilderConfig,
+        queryBuilderCache,
+        queryBuilderPredefinedQueries,
+        searchDataIndex,
+        tableTitle,
+        dataSource,
+        csvExport,
+        columns,
+        ...rest
+    } = props;
+
+    const [FilteringComponent, setFilteringComponent] = useState<ReturnType<typeof ResourceFilterHOC> | null>(null);
+    const [filterValue, setFilterValue] = useState<string | null>(null);
+    const [searchPhrase, setSearchPhrase] = useState<string | null>(null);
+    const [visibility, setVisibility] = useState(defaultVisibility);
+    const [filteredDataSource, setFilteredDataSource] = useState<typeof dataSource>(dataSource);
+    const [modifiedColumns, setModifiedColumns] = useState<typeof columns>([]);
+
+    const downloadCSV = useCallback(() => {
+        if (csvExport && columns) {
+            const header = columns.map((column) => ({
+                title: column.title ? `"${column.title}"` : undefined,
+                dataIndex: (column as any).dataIndex as string | string[],
+            })).filter((column) => !!column.title);
+
+            let csv = '';
+            if (dataSource) {
+                const rows = dataSource
+                    .map((dataItem) => header.map(({ dataIndex }) => {
+                        if (Array.isArray(dataIndex)) {
+                            return dataIndex.reduce((acc: unknown, path: string) => (typeof acc === 'object' && acc ? (acc as Record<string, unknown>)[path] : null), dataItem);
+                        }
+                        return dataItem[dataIndex];
+                    }).join(','));
+                csv = `${header.map((column) => column.title).join(',')}\n${rows.join('\n')}`;
+            } else {
+                csv = header.map((column) => column.title).join(',');
+            }
+
+            const blob = new Blob([csv], { type: 'text/csv' });
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+
+            try {
+                a.setAttribute('href', url);
+                a.setAttribute('download', csvExport.filename);
+                a.click();
+            } finally {
+                a.remove();
+            }
+        }
+    }, [csvExport?.filename, dataSource, columns]);
+
+    useEffect(() => {
+        setModifiedColumns(columns);
+    }, [columns]);
+
+    useEffect(() => {
+        if (dataSource) {
+            let filtered = dataSource;
+            if (filterValue) {
+                const parsed = JSON.parse(filterValue);
+                filtered = filtered.filter((dataItem: object) => jsonLogic.apply(parsed, dataItem));
+            }
+
+            if (searchPhrase && searchDataIndex) {
+                const lowerCaseQuery = searchPhrase.toLowerCase();
+                filtered = filtered.filter((dataItem) => {
+                    for (const dataIndex of searchDataIndex) {
+                        if (Array.isArray(dataIndex)) {
+                            const dataValue = dataIndex.reduce((acc: unknown, path: string) => (typeof acc === 'object' && acc ? (acc as Record<string, unknown>)[path] : null), dataItem);
+
+                            if (typeof dataValue === 'string' && dataValue.toLowerCase().includes(lowerCaseQuery)) {
+                                return true;
+                            }
+                        } else {
+                            const dataValue = dataItem[dataIndex];
+                            if (typeof dataValue === 'string' && dataValue.toLowerCase().includes(lowerCaseQuery)) {
+                                return true;
+                            }
+                        }
+                    }
+
+                    return false;
+                });
+            }
+
+            setFilteredDataSource(filtered);
+            if (onFilterDataSource) {
+                onFilterDataSource(filtered);
+            }
+        }
+    }, [dataSource, searchDataIndex, filterValue, searchPhrase]);
+
+    useEffect(() => {
+        if (queryBuilderConfig && queryBuilderCache?.key) {
+            const capacity = queryBuilderCache?.capacity ?? 0;
+            setFilteringComponent(
+                ResourceFilterHOC(
+                    queryBuilderConfig,
+                    queryBuilderCache.key,
+                    capacity,
+                    queryBuilderPredefinedQueries ?? undefined,
+                ),
+            );
+        }
+    }, [queryBuilderConfig, queryBuilderCache?.key, queryBuilderCache?.capacity, queryBuilderPredefinedQueries]);
+
+    return (
+        <div className='cvat-table-wrapper'>
+            <Row justify='space-between' align='middle'>
+                <Col>
+                    <Space align='center'>
+                        {!!tableTitle && <Text strong className='cvat-text-color cvat-table-header'>{tableTitle}</Text> }
+                        { !!csvExport && !!columns && (
+                            <Button
+                                type='link'
+                                icon={<DownloadOutlined />}
+                                onClick={downloadCSV}
+                            />
+                        ) }
+                    </Space>
+                </Col>
+                <Col>
+                    <Space align='center'>
+                        {Array.isArray(searchDataIndex) && !!searchDataIndex.length && (
+                            <Input.Search placeholder='Search ..' onSearch={setSearchPhrase} enterButton />
+                        )}
+                        <div>
+                            { FilteringComponent !== null && (
+                                <FilteringComponent
+                                    value={null}
+                                    predefinedVisible={visibility.predefined}
+                                    builderVisible={visibility.builder}
+                                    recentVisible={visibility.recent}
+                                    onPredefinedVisibleChange={(visible: boolean) => (
+                                        setVisibility({ ...defaultVisibility, predefined: visible })
+                                    )}
+                                    onBuilderVisibleChange={(visible: boolean) => (
+                                        setVisibility({ ...defaultVisibility, builder: visible })
+                                    )}
+                                    onRecentVisibleChange={(visible: boolean) => (
+                                        setVisibility({
+                                            ...defaultVisibility,
+                                            builder: visibility.builder,
+                                            recent: visible,
+                                        })
+                                    )}
+                                    onApplyFilter={setFilterValue}
+                                />
+                            )}
+                        </div>
+                        <Popover
+                            placement='right'
+                            trigger={['click']}
+                            content={() => {
+                                const items = modifiedColumns?.map((column, idx: number) => {
+                                    const isHidden = column.hidden ?? false;
+                                    return (
+                                        <Checkbox
+                                            onChange={(e: CheckboxChangeEvent) => {
+                                                const newIsHidden = !e.target.checked;
+                                                setModifiedColumns([
+                                                    ...modifiedColumns.slice(0, idx),
+                                                    {
+                                                        ...column,
+                                                        hidden: newIsHidden,
+                                                    },
+                                                    ...modifiedColumns.slice(idx + 1),
+                                                ]);
+
+                                                if (onChangeColumnVisibility) {
+                                                    onChangeColumnVisibility(idx, newIsHidden);
+                                                }
+                                            }}
+                                            checked={!isHidden}
+                                        >
+                                            {column.title as string}
+                                        </Checkbox>
+                                    );
+                                });
+
+                                return (
+                                    <div className='cvat-table-columns-settings-menu'>
+                                        {items}
+                                    </div>
+                                );
+                            }}
+                        >
+                            <MoreOutlined />
+                        </Popover>
+                    </Space>
+                </Col>
+            </Row>
+            <Table
+                {...rest}
+                columns={modifiedColumns}
+                dataSource={filteredDataSource}
+            />
+        </div>
+    );
+}
+
+export default React.memo(CVATTable);
