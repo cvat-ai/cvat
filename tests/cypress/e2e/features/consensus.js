baa970fb6a220c00e5588c99e6d4c18bf5842a65
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-04-24 11:46:37 +0000

    Revert "change register"
    This reverts commit 22b0b000edd7bfeb0acfdc478adaa1516979b970.
    not relevant to this pr

--- tests/cypress/e2e/features/consensus.js
+++ tests/cypress/e2e/features/consensus.js
@@ -214,7 +214,7 @@ context('Basic manipulations with consensus job replicas', () => {
                 cy.get('#cvat_canvas_text_content').should('be.visible')
                     .invoke('text')
                     .should('include', `${labelName}`)
-                    .and('include', 'Consensus');
+                    .and('include', 'consensus');
             });
             cy.go('back'); // go to previous page
             // After returning to task page, consensus job should be 'completed'
