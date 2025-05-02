a468e7424cc287fdf9618cf334a596946e714521
Boris Sekachevw <sekachev.bs@gmail.com>
2025-05-02 15:50:56 +0000

    Updated test

--- tests/python/rest_api/test_analytics.py
+++ tests/python/rest_api/test_analytics.py
@@ -65,7 +65,8 @@ class TestGetAuditEvents:
         return project.id, response.headers.get("X-Request-Id")
 
     @pytest.fixture(autouse=True)
-    def setup(self, restore_clickhouse_db_per_function, restore_redis_inmem_per_function):
+    def setup(self, find_users):
+        self._user = find_users(username=self._USERNAME)[0]
         project_spec = {
             "name": f"Test project created by {self._USERNAME}",
             "labels": [
@@ -202,6 +203,7 @@ class TestGetAuditEvents:
         query_params = {
             "_from": from_datetime.isoformat(),
             "to": to_datetime.isoformat(),
+            "user_id": self._user["id"],
         }
 
         data = self._test_get_audit_logs_as_csv(**query_params)
