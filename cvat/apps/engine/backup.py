f2f29ba26928e56f929ed8fcf21a1ae01ba71112
Boris Sekachev <sekachev.bs@gmail.com>
2025-05-27 18:16:09 +0300

    Measure content_size for Data and Assets (#9448)

--- cvat/apps/engine/backup.py
+++ cvat/apps/engine/backup.py
@@ -21,6 +21,7 @@ from zipfile import ZipFile
 import rapidjson
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.db import transaction
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
@@ -114,7 +115,7 @@ def _read_annotation_guide(zip_object, guide_filename, assets_dirname):
 
     return None, []
 
-def _import_annotation_guide(guide_data, assets):
+def _import_annotation_guide(owner, guide_data, assets):
     guide_serializer = AnnotationGuideWriteSerializer(data=guide_data)
     markdown = guide_data['markdown']
     if guide_serializer.is_valid(raise_exception=True):
@@ -122,18 +123,18 @@ def _import_annotation_guide(guide_data, assets):
 
     for asset in assets:
         name, data = asset
-        basename = os.path.basename(name)
         asset_serializer = AssetWriteSerializer(data={
-            'filename': basename,
-            'guide_id': guide_serializer.instance.id,
+            "file": SimpleUploadedFile(
+                os.path.basename(name),
+                data,
+                mimetypes.guess_type(name)[0]
+            ),
+            "guide_id": guide_serializer.instance.id,
         })
-        if asset_serializer.is_valid(raise_exception=True):
-            asset_serializer.save()
-            markdown = markdown.replace(f'{name}', f'/api/assets/{asset_serializer.instance.pk}')
-            path = os.path.join(settings.ASSETS_ROOT, str(asset_serializer.instance.uuid))
-            os.makedirs(path)
-            with open(os.path.join(path, basename), 'wb') as destination:
-                destination.write(data)
+
+        asset_serializer.is_valid(raise_exception=True)
+        asset_serializer.save(owner=owner)
+        markdown = markdown.replace(f'{name}', f'/api/assets/{asset_serializer.instance.pk}')
 
     guide_serializer.instance.markdown = markdown
     guide_serializer.instance.save()
@@ -934,7 +935,11 @@ class TaskImporter(_ImporterBase, _TaskBackupBase):
     def _import_annotation_guide(self):
         if self._annotation_guide:
             markdown = self._annotation_guide.decode()
-            _import_annotation_guide({ 'markdown': markdown, 'task_id': self._db_task.id }, self._assets)
+            _import_annotation_guide(
+                self._db_task.owner,
+                { 'markdown': markdown, 'task_id': self._db_task.id },
+                self._assets
+            )
 
     def import_task(self):
         self._import_task()
@@ -1069,7 +1074,11 @@ class ProjectImporter(_ImporterBase, _ProjectBackupBase):
     def _import_annotation_guide(self):
         if self._annotation_guide:
             markdown = self._annotation_guide.decode()
-            _import_annotation_guide({ 'markdown': markdown, 'project_id': self._db_project.id }, self._assets)
+            _import_annotation_guide(
+                self._db_project.owner,
+                { 'markdown': markdown, 'project_id': self._db_project.id },
+                self._assets
+            )
 
     def import_project(self):
         self._import_project()
