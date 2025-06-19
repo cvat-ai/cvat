1f6445b81ba2ea971b4d89ab77f9deb6b1041813
Ruslan Orazvaliev <ruslan.orazvaliev@cvat.ai>
2025-06-19 15:46:14 +0200

    Add sleep after cluster is up

--- tests/perf/k6.sh
+++ tests/perf/k6.sh
@@ -34,6 +34,7 @@ if ! docker ps --filter "name=${MAIN_CONTAINER}" --filter "status=running" | gre
       status_code=$(curl -s -o /tmp/server_response -w "%{http_code}" ${api_about_page})
       (( max_tries-- ))
   done
+  sleep 30
 
   echo -e "Create default admin user"
   docker exec -i cvat_server \
