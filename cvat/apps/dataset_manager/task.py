c86efb0b6f4031490512d1843d679838b1ca70f4
Boris Sekachevw <sekachev.bs@gmail.com>
2025-07-08 10:42:39 +0000

    do not read anno chunk size on module level

--- cvat/apps/dataset_manager/task.py
+++ cvat/apps/dataset_manager/task.py
@@ -35,7 +35,6 @@ from cvat.apps.events.handlers import handle_annotations_change
 from cvat.apps.profiler import silk_profile
 
 dlogger = DatasetLogManager()
-anno_chunk_size = settings.DEFAULT_DB_ANNO_CHUNK_SIZE
 
 
 class dotdict(OrderedDict):
@@ -68,7 +67,7 @@ def _receive_attributes_from_db(related_manager, foreign_key: str) -> defaultdic
         "spec_id",
         "value",
         "id",
-    ).iterator(chunk_size=anno_chunk_size):
+    ).iterator(chunk_size=settings.DEFAULT_DB_ANNO_CHUNK_SIZE):
         attributes[attr[foreign_key]].append(
             dotdict(
                 {
@@ -613,7 +612,7 @@ class JobAnnotation:
                 "source",
             )
             .order_by("frame")
-            .iterator(chunk_size=anno_chunk_size)
+            .iterator(chunk_size=settings.DEFAULT_DB_ANNO_CHUNK_SIZE)
         ]
 
         labeledimage_attributes = _receive_attributes_from_db(
@@ -648,7 +647,7 @@ class JobAnnotation:
                 "parent",
             )
             .order_by("frame")
-            .iterator(chunk_size=anno_chunk_size)
+            .iterator(chunk_size=settings.DEFAULT_DB_ANNO_CHUNK_SIZE)
         ]
 
         labeledshape_attributes = _receive_attributes_from_db(
@@ -703,7 +702,7 @@ class JobAnnotation:
                 "shape__outside",
             )
             .order_by("id", "shape__frame")
-            .iterator(chunk_size=anno_chunk_size)
+            .iterator(chunk_size=settings.DEFAULT_DB_ANNO_CHUNK_SIZE)
         )
 
         db_tracks = merge_table_rows(
