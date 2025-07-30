98a4046cfd9b34a9f5c09f11c4946992baed4aae
Boris Sekachevw <sekachev.bs@gmail.com>
2025-07-30 08:58:53 +0000

    Added deletion command

---
+++ cvat/apps/engine/management/commands/deleteuser.py
@@ -0,0 +1,33 @@
+# Copyright (C) CVAT.ai Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.encoding import force_str
+from rest_framework.exceptions import ValidationError
+
+from cvat.apps.engine.models import User
+from cvat.apps.engine.user_deletion import UserDeletionManager
+
+
+class Command(BaseCommand):
+    help = "Delete a user and all associated resources by user ID."
+
+    def add_arguments(self, parser):
+        parser.add_argument("user_id", type=int, help="ID of the user to delete")
+        parser.add_argument("--dry-run", action="store_true")
+
+    def handle(self, *args, **options):
+        user_id = options["user_id"]
+        dry_run = options["dry_run"]
+
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            raise CommandError(f"User #{user_id} does not exist.")
+
+        manager = UserDeletionManager(user)
+        try:
+            manager.delete_user_with_cleanup(dry_run=dry_run)
+        except ValidationError as e:
+            raise CommandError(e.detail)
