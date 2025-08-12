47355657ee0ad00318980835bf734db1a00e4cde
Boris Sekachev <sekachev.bs@gmail.com>
2025-08-12 12:38:55 +0300

    Added support to remove users having organizations with other members (#9702)

--- cvat/apps/engine/user_deletion.py
+++ cvat/apps/engine/user_deletion.py
@@ -2,12 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
-from abc import ABC, ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from typing import TypedDict
 
 from django.db import transaction
 from django.db.models import Count, Q, QuerySet
-from rest_framework.exceptions import ValidationError
 
 from cvat.apps.engine.model_utils import _ModelT
 from cvat.apps.engine.models import CloudStorage, Project, Task, User
@@ -16,20 +15,17 @@ from cvat.apps.organizations.models import Organization
 _USER_DELETION_VALIDATORS = []
 
 
-class AutoRegisterValidatorMeta(ABCMeta):
-    def __new__(mcs, name, bases, namespace):
-        cls = super().__new__(mcs, name, bases, namespace)
-        if not getattr(cls, "__abstractmethods__", None):
-            _USER_DELETION_VALIDATORS.append(cls)
-        return cls
-
-
-class UserDeletionValidator(ABC, metaclass=AutoRegisterValidatorMeta):
+class UserDeletionValidator(ABC):
     """
     Base class for user deletion validators.
     To add a new validator, inherit from this class and implement the `validate` method.
-    The implemented class will be automatically registered and used during user deletion.
+    Then call classmethod 'register_validator()' to enable the class as validator.
     """
+
+    @classmethod
+    def register_validator(cls):
+        if cls not in _USER_DELETION_VALIDATORS:
+            _USER_DELETION_VALIDATORS.append(cls)
 
     @abstractmethod
     def validate(self, user: User) -> None:
@@ -39,22 +35,6 @@ class UserDeletionValidator(ABC, metaclass=AutoRegisterValidatorMeta):
         If the user cannot be deleted, this method must raise
         rest_framework.exceptions.ValidationError with a description of the reason.
         """
-
-
-class NonEmptyOrgsValidator(UserDeletionValidator):
-    def validate(self, user: User) -> None:
-        orgs = (
-            Organization.objects.filter(owner=user)
-            .annotate(members_count=Count("members"))
-            .values_list("slug", "members_count")
-        )
-
-        for org_slug, members_count in orgs:
-            if members_count > 1:
-                raise ValidationError(
-                    "Cannot delete the user, who is the owner of the organization "
-                    + f"'{org_slug}' with {members_count} members."
-                )
 
 
 class _DeletedResources(TypedDict):
@@ -85,11 +65,29 @@ def delete_user_with_cleanup(user_id: int, dry_run: bool = True) -> _DeletedReso
     for ValidatorClass in _USER_DELETION_VALIDATORS:
         ValidatorClass().validate(user)
 
-    db_orgs = Organization.objects.filter(owner=user).select_for_update()
+    db_orgs = (
+        Organization.objects.filter(owner=user)
+        .annotate(members_count=Count("members"))
+        .filter(members_count__lt=2)
+        .prefetch_related("members")
+    )
+
+    # The database schema allows a situation where Organization.owner
+    # is not the same as the only member of the organization.
+    # Therefore, we must explicitly check that the only member (if any) is the owner,
+    # and handle cases where the organization has no members or inconsistent membership.
+    db_orgs_to_delete = Organization.objects.filter(
+        owner=user,
+        id__in=[
+            db_org.id
+            for db_org in db_orgs
+            if not db_org.members_count or db_org.members.first().user_id == user_id
+        ],
+    ).select_for_update()
 
     def filter_by_owner_and_org(queryset: QuerySet[_ModelT]) -> QuerySet[_ModelT]:
         return queryset.filter(owner=user).filter(
-            Q(organization=None) | Q(organization__in=db_orgs)
+            Q(organization=None) | Q(organization__in=db_orgs_to_delete)
         )
 
     db_projects = list(filter_by_owner_and_org(Project.objects).select_for_update())
@@ -97,7 +95,7 @@ def delete_user_with_cleanup(user_id: int, dry_run: bool = True) -> _DeletedReso
     db_cloud_storages = list(filter_by_owner_and_org(CloudStorage.objects).select_for_update())
 
     for resource_type, db_resources in (
-        ("organization", db_orgs),
+        ("organization", db_orgs_to_delete),
         ("project", db_projects),
         ("task", db_tasks),
         ("cloud_storage", db_cloud_storages),
@@ -108,7 +106,7 @@ def delete_user_with_cleanup(user_id: int, dry_run: bool = True) -> _DeletedReso
     if not dry_run:
         # call delete on each instance instead of qs.delete()
         # to perform custom .delete() method (e.g. query optimizations, audit logs) if any
-        for db_resource in list(db_orgs) + db_cloud_storages + db_projects + db_tasks:
+        for db_resource in list(db_orgs_to_delete) + db_cloud_storages + db_projects + db_tasks:
             db_resource.delete()
         user.delete()
 
