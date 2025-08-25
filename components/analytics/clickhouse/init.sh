98e319d8d22e0bd3c06e2b8f35098ccf83d0a181
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-08-25 14:17:32 +0300

    Remove comment

--- components/analytics/clickhouse/init.sh
+++ components/analytics/clickhouse/init.sh
@@ -5,7 +5,6 @@ set -e
 CLICKHOUSE_DB="${CLICKHOUSE_DB:-cvat}";
 
 # Run scripts in the directory sequentially
-# Alpine/Busybox's find uses posix basic regex syntax
 INIT_SCRIPTS_DIR="$(dirname "$0")"
 for migration in "${INIT_SCRIPTS_DIR}"/[0-9][0-9][0-9]-*.cmf; do
     echo "Running migration file $migration"
