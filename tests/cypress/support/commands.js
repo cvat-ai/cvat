da4cb682e86d732c7581e8ccbe4b32a89e3b438b
Oleg Valiulin <liam141810@gmail.com>
2025-09-03 22:05:29 +0100

    Add cy.cmds to open tasks and projects by ids with checks (#9777)
    Co-authored-by: Oleg Valiulin <oleg.valiulin@cvat.ai>

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -493,6 +493,12 @@ Cypress.Commands.add('openTask', (taskName, projectSubsetFieldValue) => {
     }
 });
 
+Cypress.Commands.add('openTaskById', (taskId) => {
+    cy.visit(`/tasks/${taskId}`);
+    cy.get('.cvat-spinner').should('not.exist');
+    cy.get('.cvat-task-details').should('exist').and('be.visible');
+});
+
 Cypress.Commands.add('saveJob', (method = 'PATCH', status = 200, as = 'saveJob') => {
     cy.intercept(method, '/api/jobs/**').as(as);
     cy.clickSaveAnnotationView();
