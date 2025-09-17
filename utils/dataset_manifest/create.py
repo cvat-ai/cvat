a4451d7c8f21f1c7fc1ac4ea0dcd77445b577fe0
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-09-17 10:58:39 +0000

    isort

--- utils/dataset_manifest/create.py
+++ utils/dataset_manifest/create.py
@@ -13,7 +13,6 @@ from glob import glob
 
 from tqdm import tqdm
 
-
 if __name__ == "__main__":
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.append(base_dir)
