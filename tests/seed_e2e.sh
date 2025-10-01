ebb8f732c3297031fff4ef118ff4ac613339b9d8
Oleg Valiulin <oleg.valiulin@cvat.ai>
2025-10-01 18:24:46 +0000

    update setup script to use headless chrome

--- tests/seed_e2e.sh
+++ tests/seed_e2e.sh
@@ -7,4 +7,4 @@ PYCMD="\"from django.contrib.auth.models import User; User.objects.create_superu
 docker exec -i cvat_server /bin/bash -c "echo $PYCMD | python3 ~/manage.py shell"
 
 # setup shared resources
-npx cypress run --spec cypress/e2e/setup/setup.js
+npx cypress run --headless --browser chrome --spec cypress/e2e/setup/setup.js
