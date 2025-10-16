0c4e929f04ef8c847466e70c0bad29c260307d6c
Dmitrii Lavrukhin <dmitrii.lavrukhin@cvat.ai>
2025-10-17 00:51:44 +0200

    fixing pylint

--- cvat/apps/dataset_manager/tests/test_streaming_efficiency.py
+++ cvat/apps/dataset_manager/tests/test_streaming_efficiency.py
@@ -3,7 +3,7 @@ import tempfile
 from collections import namedtuple
 from typing import Callable, Optional
 from unittest import TestCase, mock
-from unittest.mock import MagicMock, Mock
+from unittest.mock import Mock
 
 import numpy as np
 from datumaro import AnnotationType, Bbox, LabelCategories
