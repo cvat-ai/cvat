3130637f758f4edf192128c24cb897d547c89a25
Dmitrii Lavrukhin <dmitrii.lavrukhin@cvat.ai>
2025-10-17 00:46:24 +0200

    fixing tests

--- cvat/apps/dataset_manager/tests/test_streaming_efficiency.py
+++ cvat/apps/dataset_manager/tests/test_streaming_efficiency.py
@@ -143,47 +143,56 @@ class TestExtractors(TestCase):
 
         return instance_data, shape_generator_was_iterated
 
-    def _make_mock_instance_data(self, data_cls, item_ids):
-        instance_data = MagicMock(spec_set=data_cls)
-        instance_data.META_FIELD = "meta"
-        instance_data.meta = dict(
-            meta=dict(
-                subset="foo",
+    def _make_mock_project_data(self, item_ids):
+        class MockProjectData(ProjectData):
+            def __init__(self):
+                pass
+
+        instance_data = MockProjectData()
+        instance_data._subsets = list(set(subset for _, subset in item_ids))
+        instance_data._meta = {
+            instance_data.META_FIELD: dict(
+                subsets=instance_data._subsets,
                 labels=[
-                    ("label", dict(name="name", attributes=[], type=LabelType.ANY)),
+                    ("label", dict(name="LabelName", attributes=[], type=LabelType.ANY)),
                 ],
             ),
-        )
-        subsets = sorted(set(subset for _, subset in item_ids))
-        if data_cls is TaskData:
-            assert len(subsets) == 1
-            instance_data.db_instance = Mock(id=0)
-        elif data_cls is ProjectData:
-            instance_data.tasks = [Mock(id=index) for index in range(len(subsets))]
+        }
 
-        def mock_group_by_frame(*args, **kwargs):
-            for index, (item_id, subset) in enumerate(item_ids):
-                yield self._make_mock_frame(
-                    data_cls=data_cls,
-                    id=index,
-                    task_id=subsets.index(subset),
-                    name=f"{item_id}.png",
+        instance_data._db_tasks = {}
+        instance_data._tasks_data = {}
+        instance_data._frame_info = {}
+        instance_data._task_frame_offsets = {}
+        task_shape_generators = []
+        for task_id, subset in enumerate(instance_data._subsets):
+            task_item_ids = [
+                (item_id, item_subset) for item_id, item_subset in item_ids if item_subset == subset
+            ]
+            instance_data._db_tasks[task_id] = Mock(data=Mock(size=len(task_item_ids)), id=task_id)
+            task_instance, task_shape_generator_was_iterated = self._make_mock_job_data(
+                task_item_ids
+            )
+            task_shape_generators.append(task_shape_generator_was_iterated)
+            instance_data._tasks_data[task_id] = task_instance
+            instance_data._task_frame_offsets[task_id] = (
+                0
+                if task_id == 0
+                else instance_data._task_frame_offsets[task_id - 1]
+                + instance_data._db_tasks[task_id - 1].data.size
+            )
+
+            for index, (item_id, item_subset) in enumerate(task_item_ids):
+                instance_data._frame_info[(task_id, index)] = dict(
+                    id=item_id,
                     subset=subset,
-                    frame=index,
+                    path=f"path_{subset}_{item_id}.jpg",
                 )
 
-        instance_data.group_by_frame.side_effect = mock_group_by_frame
+        def shape_generator_was_iterated():
+            assert len(set(was_iterated() for was_iterated in task_shape_generators)) == 1
+            return task_shape_generators[0]()
 
-        if data_cls is not ProjectData:
-            instance_data.is_stream = False
-            instance_data.__len__.return_value = len(item_ids)
-            instance_data.frame_info = {
-                index: dict(id=item_id, subset=subset)
-                for index, (item_id, subset) in enumerate(item_ids)
-            }
-            instance_data.get_included_frames.return_value = set(instance_data.frame_info.keys())
-
-        return instance_data
+        return instance_data, shape_generator_was_iterated
 
     @mock.patch("attr.evolve", lambda x, **kwargs: x)
     def test_can_stream_efficiently_on_export(self):
@@ -198,7 +207,6 @@ class TestExtractors(TestCase):
             else:
                 item_ids = {("image", "foo"), ("another_image", "foo")}
             with self.subTest(data_cls=data_cls.__name__):
-                instance_data = self._make_mock_instance_data(data_cls, item_ids)
                 extractor_cls = (
                     CVATProjectDataExtractor
                     if data_cls is ProjectData
@@ -208,76 +216,42 @@ class TestExtractors(TestCase):
 
                 if data_cls in [JobData, TaskData]:
                     instance_data, shape_generator_was_iterated = self._make_mock_job_data(item_ids)
-                    with extractor_cls(instance_data=instance_data) as extractor:
-                        dataset = StreamDataset.from_extractors(extractor, env=dm_env)
-
-                        # does not generate shapes to get various dataset properties
-                        len(dataset)
-                        list(dataset.subsets())
-                        for subset in dataset.subsets():
-                            subset_dataset = dataset.get_subset(subset).as_dataset()
-                            len(subset_dataset)
-                            list(subset_dataset.subsets())
-                        assert not shape_generator_was_iterated()
-
-                        # does not generate shapes to iterate items
-                        assert len(list(dataset)) == len(item_ids)
-                        assert not shape_generator_was_iterated()
-
-                        # does not generate shapes to iterate subset items
-                        for subset in dataset.subsets():
-                            subset_dataset = dataset.get_subset(subset).as_dataset()
-                            list(subset_dataset)
-                        assert not shape_generator_was_iterated()
-
-                        # generates shapes only when annotations are accessed
-                        annotations = list(item.annotations for item in dataset)
-                        assert any(annotations), annotations
-                        assert shape_generator_was_iterated()
-
-                        # does not keep shapes or annotations in memory
-                        # annotations can be accessed only once
-                        annotations = list(item.annotations for item in dataset)
-                        assert not any(annotations), annotations
                 else:
-                    with extractor_cls(instance_data=instance_data) as extractor:
-                        dataset = StreamDataset.from_extractors(extractor, env=dm_env)
-                        # iterated over instance frame data on init
-                        assert instance_data.group_by_frame.call_count == 1
+                    instance_data, shape_generator_was_iterated = self._make_mock_project_data(
+                        item_ids
+                    )
 
-                        # does not convert annotations to get various dataset properties
-                        len(dataset)
-                        list(dataset.subsets())
-                        assert extractor.item_anns_processed == 0
+                with extractor_cls(instance_data=instance_data) as extractor:
+                    dataset = StreamDataset.from_extractors(extractor, env=dm_env)
 
-                        # does not convert annotations to get various subset properties
-                        for subset in dataset.subsets():
-                            subset_dataset = dataset.get_subset(subset).as_dataset()
-                            len(subset_dataset)
-                            list(subset_dataset.subsets())
-                        assert extractor.item_anns_processed == 0
+                    # does not generate shapes to get various dataset properties
+                    len(dataset)
+                    list(dataset.subsets())
+                    for subset in dataset.subsets():
+                        subset_dataset = dataset.get_subset(subset).as_dataset()
+                        len(subset_dataset)
+                        list(subset_dataset.subsets())
+                    assert not shape_generator_was_iterated()
 
-                        # does not convert annotations to iterate items
-                        list(dataset)
-                        for subset in dataset.subsets():
-                            subset_dataset = dataset.get_subset(subset).as_dataset()
-                            list(subset_dataset)
-                        assert extractor.item_anns_processed == 0
+                    # does not generate shapes to iterate items
+                    assert len(list(dataset)) == len(item_ids)
+                    assert not shape_generator_was_iterated()
 
-                        # initiates annotations only when they are accessed
-                        list(item.annotations for item in dataset)
-                        assert extractor.item_anns_processed == len(
-                            item_ids
-                        ), extractor.item_anns_processed
+                    # does not generate shapes to iterate subset items
+                    for subset in dataset.subsets():
+                        subset_dataset = dataset.get_subset(subset).as_dataset()
+                        list(subset_dataset)
+                    assert not shape_generator_was_iterated()
 
-                        # does not keep annotations in memory
-                        list(item.annotations for item in dataset)
-                        assert (
-                            extractor.item_anns_processed == len(item_ids) * 2
-                        ), extractor.item_anns_processed
+                    # generates shapes only when annotations are accessed
+                    annotations = list(item.annotations for item in dataset)
+                    assert any(annotations), annotations
+                    assert shape_generator_was_iterated()
 
-                        # did not iterate over instance frame data anymore
-                        assert instance_data.group_by_frame.call_count == 1
+                    # does not keep shapes or annotations in memory
+                    # annotations can be accessed only once
+                    annotations = list(item.annotations for item in dataset)
+                    assert not any(annotations), annotations
 
 
 class TestImporters(ApiTestBase):
