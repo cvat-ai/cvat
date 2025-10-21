cfc34f91a99c677e1f15ef3f08f6cc631c340630
Max Christoph <max.christoph@pentaxmedical.com>
2025-10-21 09:33:16 +0200

    Rest API test for chapters

--- tests/python/rest_api/test_task_data.py
+++ tests/python/rest_api/test_task_data.py
@@ -11,6 +11,7 @@ import zipfile
 from collections import Counter
 from collections.abc import Iterable, Sequence
 from copy import deepcopy
+from fractions import Fraction
 from functools import partial
 from http import HTTPStatus
 from itertools import chain, groupby, product
@@ -1780,6 +1781,7 @@ class TestPostTaskData:
 class TestTaskData(TestTasksBase):
     @parametrize("task_spec, task_id", TestTasksBase._all_task_cases)
     def test_can_get_task_meta(self, task_spec: ITaskSpec, task_id: int):
+
         with make_api_client(self._USERNAME) as api_client:
             (task_meta, _) = api_client.tasks_api.retrieve_data_meta(task_id)
 
@@ -1798,8 +1800,10 @@ class TestTaskData(TestTasksBase):
 
             if task_spec.source_data_type == SourceDataType.video:
                 assert len(task_meta.frames) == 1
+                assert task_meta.chapters == getattr(task_spec, "chapters", [])
             else:
                 assert len(task_meta.frames) == task_meta.size
+                assert task_meta.chapters is None
 
     @pytest.mark.timeout(
         # This test has to check all the task frames availability, it can make many requests
