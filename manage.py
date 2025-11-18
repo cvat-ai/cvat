416eb4b9807976b4030a4e42bf4c0b2b0af708d3
Grigorii777 <grigorio714@gmail.com>
2025-11-18 16:17:44 +0400

    rollback space

--- manage.py
+++ manage.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-     execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
