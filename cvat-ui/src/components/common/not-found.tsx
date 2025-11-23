08207d4f13cfd9224597a2249fa1c654f8f041b5
Prathamesh Patil <122prathamesh6002@sjcem.edu.in>
2025-11-23 15:36:24 +0530

    Fix: Issue #9225 Solved

--- cvat-ui/src/components/common/not-found.tsx
+++ cvat-ui/src/components/common/not-found.tsx
@@ -4,39 +4,49 @@
 
 import React from 'react';
 import Result from 'antd/lib/result';
-
-export const JobNotFoundComponent = React.memo((): JSX.Element => (
-    <Result
+import Button from 'antd/lib/button';
+import { useHistory } from 'react-router-dom';
+export const JobNotFoundComponent = React.memo((): JSX.Element => {
+    const history = useHistory()
+    return <Result
         className='cvat-not-found'
         status='404'
         title='Sorry, but this job was not found'
         subTitle='Please, be sure information you tried to get exist and you have access'
     />
-));
+});
 
-export const TaskNotFoundComponent = React.memo((): JSX.Element => (
-    <Result
-        className='cvat-not-found'
-        status='404'
-        title='There was something wrong during getting the task'
-        subTitle='Please, be sure, that information you tried to get exist and you are eligible to access it'
-    />
-));
+export const TaskNotFoundComponent = React.memo((): JSX.Element => {
+    const history = useHistory();
+    return (
+            <Result
+                className='cvat-not-found'
+                status='404'
+                title='There was something wrong during getting the task'
+                subTitle='Please, be sure information you tried to get exist and you have access'
+                extra={<Button type='primary' onClick={() => history.goBack()}>Return to Previous Page</Button>}
+            />
+        );
+});
 
-export const ProjectNotFoundComponent = React.memo((): JSX.Element => (
-    <Result
+export const ProjectNotFoundComponent = React.memo((): JSX.Element => {
+    const history = useHistory();
+    return <Result
         className='cvat-not-found'
         status='404'
         title='There was something wrong during getting the project'
         subTitle='Please, be sure, that information you tried to get exist and you are eligible to access it'
+        extra={<Button type='primary' onClick={() => history.goBack()}>Return to Previous Page</Button>}
     />
-));
+});
 
-export const CloudStorageNotFoundComponent = React.memo((): JSX.Element => (
-    <Result
+export const CloudStorageNotFoundComponent = React.memo((): JSX.Element => {
+    const history = useHistory();
+    return (<Result
         className='cvat-not-found'
         status='404'
         title='Sorry, but the requested cloud storage was not found'
         subTitle='Please, be sure id you requested exists and you have appropriate permissions'
-    />
-));
+        extra={<Button type='primary' onClick={() => history.goBack()}>Return to Previous Page</Button>}
+    />)
+});
