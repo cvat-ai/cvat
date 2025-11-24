dad52a81417f64d2695d21bd8d31d071fa642bb4
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-11-24 13:39:02 +0200

    t

--- cvat/apps/quality_control/signals.py
+++ cvat/apps/quality_control/signals.py
@@ -5,7 +5,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from cvat.apps.engine.models import Project, Task
+from cvat.apps.engine.models import Job, JobType, Project, Task
 from cvat.apps.quality_control.models import QualitySettings
 
 
@@ -21,3 +21,9 @@ def __save_project__initialize_quality_settings(
 def __save_task__initialize_quality_settings(instance: Task, created: bool, **kwargs):
     if created and not kwargs.get("raw"):
         QualitySettings.objects.get_or_create(task_id=instance.id)
+
+
+@receiver(post_save, sender=Job)
+def __save_gt_job__initialize_quality_settings(instance: Job, created: bool, **kwargs):
+    if created and not kwargs.get("raw") and instance.type == JobType.GROUND_TRUTH:
+        QualitySettings.objects.get_or_create(task_id=instance.segment.task_id)
