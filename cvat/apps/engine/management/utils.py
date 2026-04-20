ee8bbcaeca910e9013663d994d1fcfa75650e518
Roman Donchenko <roman@cvat.ai>
2026-04-20 14:03:50 +0300

    Add overall stats

--- cvat/apps/engine/management/utils.py
+++ cvat/apps/engine/management/utils.py
@@ -42,7 +42,9 @@ def move_multiple_tasks(
     command: BaseCommand, task_ids: Iterable[int], move_one: Callable[[Task], bool]
 ) -> None:
     succeeded = failed = ignored = 0
+    total_content_size = 0
 
+    time_before_all = time.perf_counter()
     for task_id in task_ids:
         try:
             try:
@@ -67,21 +69,33 @@ def move_multiple_tasks(
                 time_taken = time_after - time_before
                 mb_moved = (data.content_size or math.nan) / (1024 * 1024)
                 throughput = mb_moved / time_taken if time_taken > 0 else math.inf
+
                 command.stdout.write(
                     command.style.SUCCESS(
                         f"Task #{task_id}: success;"
-                        f" taken {time_taken:.3f} s for {mb_moved:.3f} MiB"
-                        f" ({throughput:.3f} MiB/s)"
+                        f" taken {time_taken:.3f} s for {mb_moved:.3f} MiB ({throughput:.3f} MiB/s)"
                     )
                 )
+
+                # Not using NaN here, since it would make the total useless.
+                total_content_size += data.content_size or 0
             else:
                 ignored += 1
                 # We're relying on move_one to log the reason for ignoring.
+
+    time_after_all = time.perf_counter()
+    total_time_taken = time_after_all - time_before_all
+    total_mb_moved = total_content_size / (1024 * 1024)
+    total_throughput = total_mb_moved / total_time_taken if total_time_taken > 0 else math.inf
 
     processed = succeeded + failed + ignored
 
     command.stdout.write(
         f"Processed {processed} task(s): {succeeded} succeeded, {failed} failed, {ignored} ignored"
+    )
+    command.stdout.write(
+        f"Total time taken: {total_time_taken:.3f} s"
+        f" for {total_mb_moved:.3f} MiB ({total_throughput:.3f} MiB/s)"
     )
 
     if failed:
