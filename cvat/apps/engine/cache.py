cvat-ai/cvat

commit 405a70befed18f5606034770998d6c2b06a9a257

Add openable file protocols and make manifest classes use them (#10106)

cvat/apps/engine/cache.py
@@ -17,6 +17,7 @@ from collections.abc import Callable, Collection, Generator, Iterator, Sequence
 from contextlib import ExitStack, closing
 from datetime import datetime, timezone
 from itertools import groupby, pairwise
+from pathlib import Path
 from typing import Any, TypeAlias, overload
 
 import attrs
@@ -787,7 +788,7 @@ class MediaCache:
             )
             if not os.path.isfile(manifest_path):
                 try:
-                    reader.manifest.link(source_path, force=True)
+                    reader.manifest.link(Path(source_path), force=True)
                     reader.manifest.create()
                 except Exception as e:
                     slogger.task[db_task.id].warning(
@@ -1048,8 +1049,8 @@ class MediaCache:
             slogger.cloud_storage[db_storage.pk].info(msg)
             raise NotFound(msg)
 
-        buff = storage.download_fileobj(preview_path)
-        image = PIL.Image.open(buff)
+        preview_bytes = storage.download_fileobj(preview_path)
+        image = PIL.Image.open(io.BytesIO(preview_bytes))
         return prepare_preview_image(image)
 
     def prepare_context_images_chunk(

cvat/apps/engine/cloud_provider.py
@@ -6,7 +6,6 @@
 from __future__ import annotations
 
 import functools
-import io
 import json
 import os
 from abc import ABC, abstractmethod
@@ -40,18 +39,13 @@ from cvat.apps.engine.models import CloudProviderChoice, CredentialsTypeChoice, 
 from cvat.apps.engine.rq import ExportRQMeta
 from cvat.apps.engine.utils import get_cpu_number
 from cvat.utils.http import PROXIES_FOR_UNTRUSTED_URLS
-from utils.dataset_manifest.utils import InvalidPcdError, PcdReader
-
-
-class NamedBytesIO(BytesIO):
-    @property
-    def filename(self) -> str | None:
-        return getattr(self, "_filename", None)
-
-    @filename.setter
-    def filename(self, value: str) -> None:
-        self._filename = value
-
+from utils.dataset_manifest.utils import (
+    InvalidPcdError,
+    MemNamedOpenable,
+    MemOpenable,
+    NamedOpenable,
+    PcdReader,
+)
 
 slogger = ServerLogManager(__name__)
 
@@ -182,12 +176,10 @@ class _CloudStorage(ABC):
 
     @validate_file_status
     @validate_bucket_status
-    def download_fileobj(self, key: str, /) -> NamedBytesIO:
-        buf = NamedBytesIO()
+    def download_fileobj(self, key: str, /) -> bytes:
+        buf = BytesIO()
         self._download_fileobj_to_stream(key, buf)
-        buf.seek(0)
-        buf.filename = key
-        return buf
+        return buf.getvalue()
 
     @validate_file_status
     @validate_bucket_status
@@ -226,9 +218,8 @@ class _CloudStorage(ABC):
         pass
 
     def bulk_download_to_memory(
-        self, files: list[str], *, object_downloader: Callable[[str], NamedBytesIO] = None
-    ) -> Iterator[BytesIO]:
-        func = object_downloader or self.download_fileobj
+        self, files: list[str], *, object_downloader: Callable[[str], NamedOpenable]
+    ) -> Iterator[NamedOpenable]:
         threads_number = get_max_threads_number(len(files))
 
         # We're using a custom queue to limit the maximum number of downloaded unprocessed
@@ -236,7 +227,7 @@ class _CloudStorage(ABC):
         # For example, the builtin executor.map() could also be used here, but it
         # would enqueue all the file list in one go, and the downloaded files
         # would all be stored in memory until processed.
-        queue: Queue[Future] = Queue(maxsize=threads_number)
+        queue: Queue[Future[NamedOpenable]] = Queue(maxsize=threads_number)
         input_iter = iter(files)
         with ThreadPoolExecutor(max_workers=threads_number) as executor:
             while not queue.empty() or input_iter is not None:
@@ -246,7 +237,7 @@ class _CloudStorage(ABC):
                         input_iter = None
                         break
 
-                    next_job = executor.submit(func, next_job_params)
+                    next_job = executor.submit(object_downloader, next_job_params)
                     queue.put(next_job)
 
                 top_job = queue.get()
@@ -398,15 +389,17 @@ class HeaderFirstDownloader(ABC):
         self.client = client
 
     @abstractmethod
-    def try_parse_header(self, header: NamedBytesIO) -> Any | None: ...
+    def try_parse_header(self, key: str, header: bytes) -> Any | None: ...
 
-    def log_header_miss(self, key: str, header_size: int, *, full_file: NamedBytesIO | None = None):
+    def log_header_miss(
+        self, key: str, header_size: int, *, full_contents: bytes | None = None
+    ) -> None:
         message = (
             f'The first {header_size} bytes were not enough to parse the "{key}" object header. '
         )
 
-        if full_file:
-            full_object_size = len(full_file.getvalue())
+        if full_contents is not None:
+            full_object_size = len(full_contents)
 
             message += (
                 f"Object size was {full_object_size} bytes. "
@@ -428,7 +421,7 @@ class HeaderFirstDownloader(ABC):
             65536,
         )
 
-    def download(self, key: str) -> NamedBytesIO:
+    def download(self, key: str) -> NamedOpenable:
         """
         Method downloads the file using the following approach:
         First we try to download the file header (first N bytes).
@@ -441,48 +434,45 @@ class HeaderFirstDownloader(ABC):
             buffer with the image
         """
 
-        buff = NamedBytesIO()
-        buff.filename = key
+        buff = BytesIO()
 
         headers_to_try = self.get_header_sizes_to_try()
         for i, header_size in enumerate(headers_to_try):
-            buff.seek(0, io.SEEK_END)
             cur_pos = buff.tell()
             chunk = self.client.download_range_of_bytes(
                 key, start_byte=cur_pos, stop_byte=header_size - 1
             )
             buff.write(chunk)
-            buff.seek(0)
 
-            if self.try_parse_header(buff):
-                buff.seek(0)
-                return buff
+            partial_contents = buff.getvalue()
+            if self.try_parse_header(key, partial_contents):
+                return MemNamedOpenable(partial_contents, key)
 
             if i + 1 < len(headers_to_try):
                 self.log_header_miss(key=key, header_size=header_size)
 
-        buff = self.client.download_fileobj(key)
-        self.log_header_miss(key=key, header_size=header_size, full_file=buff)
-        return buff
+        full_contents = self.client.download_fileobj(key)
+        self.log_header_miss(key=key, header_size=header_size, full_contents=full_contents)
+        return MemNamedOpenable(full_contents, key)
 
 
 class _HeaderFirstImageDownloader(HeaderFirstDownloader):
-    def try_parse_header(self, header):
+    def try_parse_header(self, key: str, header: bytes):
         image_parser = ImageFile.Parser()
-        image_parser.feed(header.getvalue())
+        image_parser.feed(header)
         return image_parser.image
 
-    def log_header_miss(self, key, header_size, *, full_file=None):
+    def log_header_miss(self, key, header_size, *, full_contents: bytes | None = None) -> None:
         message = (
             f'The first {header_size} bytes were not enough to parse the "{key}" object header. '
         )
 
-        if full_file:
-            full_object_size = len(full_file.getvalue())
+        if full_contents is not None:
+            full_object_size = len(full_contents)
 
             message += (
                 f"Object size was {full_object_size} bytes. "
-                f"Image resolution was {Image.open(full_file).size}. "
+                f"Image resolution was {Image.open(BytesIO(full_contents)).size}. "
                 f"Downloaded percentage was "
                 f"{min(header_size, full_object_size) / full_object_size:.0%}"
             )
@@ -499,10 +489,10 @@ class _HeaderFirstImageDownloader(HeaderFirstDownloader):
 
 
 class _HeaderFirstPcdDownloader(HeaderFirstDownloader):
-    def try_parse_header(self, header):
+    def try_parse_header(self, key: str, header: bytes):
         pcd_parser = PcdReader()
-        file = header
-        file_ext = os.path.splitext(file.filename)[1].lower()
+        file = MemOpenable(header)
+        file_ext = os.path.splitext(key)[1].lower()
 
         if file_ext == ".bin":
             # We need to ensure the file is a valid .bin file

cvat/apps/engine/media_extractors.py
@@ -19,6 +19,7 @@ from contextlib import ExitStack, closing
 from dataclasses import dataclass
 from enum import IntEnum
 from fractions import Fraction
+from pathlib import Path
 from random import shuffle
 from typing import Any, ClassVar, Protocol, TypeAlias, TypedDict, TypeVar
 
@@ -43,7 +44,7 @@ from cvat.apps.engine.mime_types import mimetypes
 from utils.dataset_manifest import ImageManifestManager, VideoManifestManager
 from utils.dataset_manifest.errors import InvalidPcdError
 from utils.dataset_manifest.utils import MediaDimension as _MediaDimension
-from utils.dataset_manifest.utils import PcdReader, detect_media_dimension
+from utils.dataset_manifest.utils import Openable, PcdReader, detect_media_dimension
 
 ORIENTATION_EXIF_TAG = 274
 
@@ -340,9 +341,8 @@ class ImageListReader(IMediaReader):
 
     def get_image_size(self, i):
         if self._dimension == DimensionType.DIM_3D:
-            with open(self.get_path(i), "rb") as f:
-                properties = ValidateDimension.get_pcd_properties(f)
-                return int(properties["WIDTH"]), int(properties["HEIGHT"])
+            properties = ValidateDimension.get_pcd_properties(Path(self.get_path(i)))
+            return int(properties["WIDTH"]), int(properties["HEIGHT"])
         with Image.open(self._source_path[i]) as img:
             return image_size_within_orientation(img)
 
@@ -507,9 +507,8 @@ class ZipReader(ImageListReader):
 
     def get_image_size(self, i):
         if self._dimension == DimensionType.DIM_3D:
-            with open(self.get_path(i), "rb") as f:
-                properties = PcdReader.parse_pcd_header(f)
-                return int(properties["WIDTH"]), int(properties["HEIGHT"])
+            properties = PcdReader.parse_pcd_header(Path(self.get_path(i)))
+            return int(properties["WIDTH"]), int(properties["HEIGHT"])
         with Image.open(io.BytesIO(self._zip_source.read(self._source_path[i]))) as img:
             return image_size_within_orientation(img)
 
@@ -1222,10 +1221,8 @@ class ValidateDimension:
         self.converted_files = []
 
     @staticmethod
-    def get_pcd_properties(
-        fp: str | io.RawIOBase, *, verify_version: bool = False
-    ) -> dict[str, str] | None:
-        return PcdReader.parse_pcd_header(fp, verify_version=verify_version)
+    def get_pcd_properties(pcd: Openable, *, verify_version: bool = False) -> dict[str, str]:
+        return PcdReader.parse_pcd_header(pcd, verify_version=verify_version)
 
     @staticmethod
     def convert_bin_to_pcd(path, delete_source=True):
@@ -1241,7 +1238,7 @@ class ValidateDimension:
 
     def pcd_operation(self, file_path: str, dataset_root: str) -> str | None:
         try:
-            self.get_pcd_properties(file_path, verify_version=True)
+            self.get_pcd_properties(Path(file_path), verify_version=True)
             return os.path.relpath(file_path, dataset_root)
         except InvalidPcdError as e:
             raise ValidationError(f"Could not read pcd file '{os.path.basename(file_path)}': {e}")

cvat/apps/engine/task.py
@@ -568,6 +568,7 @@ def _create_task_manifest_from_cloud_data(
         },
         DIM_3D=(dimension == models.DimensionType.DIM_3D),
         stop=len(sorted_media) - 1,
+        data_dir=".",
     )
     manifest.create()
 
@@ -1078,8 +1079,7 @@ def create_thread(
                     # TODO: maybe generate manifest in a temp directory
                     manifest = VideoManifestManager(db_data.get_manifest_path())
                     manifest.link(
-                        media_file=media_files[0],
-                        upload_dir=upload_dir,
+                        media_file=Path(upload_dir, media_files[0]),
                         chunk_size=db_data.chunk_size, # TODO: why it's needed here?
                         force=True
                     )
@@ -1128,7 +1128,7 @@ def create_thread(
                 # us to avoid downloading such images from cloud storage (when using static chunks),
                 # or copying them from the attached share (when using copy_data).
                 manifest.link(
-                    sources=extractor.absolute_source_paths,
+                    sources=list(map(Path, extractor.absolute_source_paths)),
                     meta={
                         k: {'related_images': related_images[k] }
                         for k in related_images

cvat/apps/engine/tests/test_rest_api.py
@@ -78,7 +78,7 @@ from cvat.apps.engine.tests.utils import (
 )
 from cvat.apps.redis_handler.serializers import RequestStatus
 from utils.dataset_manifest import ImageManifestManager, VideoManifestManager
-from utils.dataset_manifest.utils import PcdReader, find_related_images
+from utils.dataset_manifest.utils import MemOpenable, PcdReader, find_related_images
 
 from .utils import ASSETS_DIR, check_annotation_response, compare_objects
 
@@ -1492,6 +1492,7 @@ class ProjectBackupAPITestCase(ExportApiTestBase, ImportApiTestBase):
             sources=[
                 os.path.join(settings.SHARE_ROOT, imagename_pattern.format(i)) for i in range(1, 8)
             ],
+            root_dir=settings.SHARE_ROOT,
         )
         cls.media["files"].append(manifest_path)
         cls.media_data.append(
@@ -3152,6 +3153,7 @@ class TaskImportExportAPITestCase(ExportApiTestBase, ImportApiTestBase):
             sources=[
                 os.path.join(settings.SHARE_ROOT, imagename_pattern.format(i)) for i in range(1, 8)
             ],
+            root_dir=settings.SHARE_ROOT,
         )
         cls.media_data.append(
             {
@@ -3553,31 +3555,27 @@ def generate_manifest_file(
     root_dir=None,
 ):
     if data_type == "video":
-        kwargs = {
-            "media_file": sources[0],
-            "upload_dir": os.path.dirname(sources[0]),
-            "force": True,
-        }
         manifest = VideoManifestManager(manifest_path, create_index=False)
+        manifest.link(media_file=Path(sources[0]), force=True)
     else:
-        kwargs = {
-            "sources": sources,
-            "sorting_method": sorting_method,
-            "use_image_hash": True,
-            "data_dir": root_dir,
-            "DIM_3D": data_type == ManifestDataType.point_clouds,
-        }
+        assert root_dir
 
         scenes, related_images = find_related_images(sources, root_path=root_dir)
-        kwargs["meta"] = {k: {"related_images": related_images[k]} for k in related_images}
-        kwargs["sources"] = [
-            p
-            for p in sources
-            if (root_dir is not None and os.path.relpath(p, root_dir) or p) in scenes
-        ]
 
         manifest = ImageManifestManager(manifest_path, create_index=False)
-    manifest.link(**kwargs)
+        manifest.link(
+            sources=[
+                Path(p)
+                for p in sources
+                if (root_dir is not None and os.path.relpath(p, root_dir) or p) in scenes
+            ],
+            sorting_method=sorting_method,
+            use_image_hash=True,
+            data_dir=root_dir,
+            DIM_3D=data_type == ManifestDataType.point_clouds,
+            meta={k: {"related_images": related_images[k]} for k in related_images},
+        )
+
     manifest.create()
 
 
@@ -3668,8 +3666,7 @@ class TaskDataAPITestCase(ApiTestBase):
                 if not info.endswith(".pcd"):
                     continue
 
-                with zip_file.open(info, "r") as file:
-                    pcd_properties = ValidateDimension.get_pcd_properties(file)
+                pcd_properties = ValidateDimension.get_pcd_properties(zipfile.Path(zip_file, info))
 
                 image_sizes.append((int(pcd_properties["WIDTH"]), int(pcd_properties["HEIGHT"])))
 
@@ -3694,12 +3691,9 @@ class TaskDataAPITestCase(ApiTestBase):
                 if not info.endswith(".bin"):
                     continue
 
-                with zip_file.open(info, "r") as bin_file:
-                    pcd_file = io.BytesIO()
-                    PcdReader.convert_bin_to_pcd_file(bin_file, output_file=pcd_file)
-                    pcd_file.seek(0)
+                pcd_bytes = PcdReader.convert_bin_to_pcd_buffer(zipfile.Path(zip_file, info))
 
-                pcd_properties = ValidateDimension.get_pcd_properties(pcd_file)
+                pcd_properties = ValidateDimension.get_pcd_properties(MemOpenable(pcd_bytes))
                 image_sizes.append((int(pcd_properties["WIDTH"]), int(pcd_properties["HEIGHT"])))
 
         cls._share_image_sizes[filename] = image_sizes
@@ -3857,7 +3851,7 @@ class TaskDataAPITestCase(ApiTestBase):
         chunk = zipfile.ZipFile(archive, mode="r")
         if dimension == DimensionType.DIM_3D:
             return [
-                (f, BytesIO(chunk.read(f)))
+                (f, chunk.read(f))
                 for f in sorted(chunk.namelist())
                 if f.rsplit(".", maxsplit=1)[-1] == "pcd"
             ]
@@ -3992,7 +3986,7 @@ class TaskDataAPITestCase(ApiTestBase):
 
             for image_idx, received_image in enumerate(images):
                 if dimension == DimensionType.DIM_3D:
-                    properties = ValidateDimension.get_pcd_properties(received_image)
+                    properties = ValidateDimension.get_pcd_properties(MemOpenable(received_image))
                     self.assertEqual(
                         (int(properties["WIDTH"]), int(properties["HEIGHT"])),
                         expected_image_sizes[image_idx],
@@ -4015,7 +4009,7 @@ class TaskDataAPITestCase(ApiTestBase):
 
             for image_idx, received_image in enumerate(images):
                 if dimension == DimensionType.DIM_3D:
-                    properties = ValidateDimension.get_pcd_properties(received_image)
+                    properties = ValidateDimension.get_pcd_properties(MemOpenable(received_image))
                     self.assertEqual(
                         (int(properties["WIDTH"]), int(properties["HEIGHT"])),
                         expected_image_sizes[image_idx],
@@ -4089,14 +4083,9 @@ class TaskDataAPITestCase(ApiTestBase):
                     ]
 
                 for received_image, source_image in zip(images, source_images):
-                    if dimension == DimensionType.DIM_3D:
-                        server_image = np.array(received_image.getbuffer())
-                        source_image = np.array(source_image.getbuffer())
-                        self.assertTrue(np.array_equal(source_image, server_image))
-                    else:
-                        server_image = np.array(received_image)
-                        source_image = np.array(source_image)
-                        self.assertTrue(np.array_equal(source_image, server_image))
+                    server_image = np.array(received_image)
+                    source_image = np.array(source_image)
+                    self.assertTrue(np.array_equal(source_image, server_image))
 
     def _test_api_v2_tasks_id_data_create_can_upload_local_images(self, user):
         task_spec = {
@@ -4910,6 +4899,7 @@ class TaskDataAPITestCase(ApiTestBase):
                     manifest_path,
                     image_paths,
                     sorting_method=SortingMethod.PREDEFINED,
+                    root_dir=test_dir,
                 )
 
                 task_data_common["use_cache"] = caching_enabled
@@ -5071,6 +5061,7 @@ class TaskDataAPITestCase(ApiTestBase):
                         manifest_path,
                         image_paths,
                         sorting_method=SortingMethod.PREDEFINED,
+                        root_dir=test_dir,
                     )
 
                     task_data["use_cache"] = caching_enabled

cvat/apps/engine/tests/test_rest_api_3D.py
@@ -245,9 +245,8 @@ class Task3DTest(_DbTestBase):
         zip_file = zipfile.ZipFile(cls.pointcloud_pcd_path)
         for info in zip_file.namelist():
             if info.endswith(".pcd"):
-                with zip_file.open(info, "r") as file:
-                    data = ValidateDimension.get_pcd_properties(file)
-                    image_sizes.append((int(data["WIDTH"]), int(data["HEIGHT"])))
+                data = ValidateDimension.get_pcd_properties(zipfile.Path(zip_file, info))
+                image_sizes.append((int(data["WIDTH"]), int(data["HEIGHT"])))
 
         cls.task = {
             "name": "main task",

commit e6bf2db2c36d0e568b5763107679d05168118579

Fix chunk creation for tasks with .bin files in cloud storage
I inadvertently broke this in #10106 by changing the order of file
operations in `PcdReader.convert_bin_to_pcd`. The current version of
`read_raw_images` saves the `.bin` file with a `.pcd` extension and then
tries to convert it to the PCD format. `convert_bin_to_pcd` opens the output
file first; but in this case the input and output paths are the same, so
this truncates the input file to 0 bytes before `convert_bin_to_pcd` can
read it.

We could probably reorder the opens again to fix this, but IMO, this scheme
with saving the `.bin` file with a `.pcd` extension is rather confusing and
it's better to fix this instead. Save the file with its original extension
and let `convert_bin_to_pcd` save the output to a new file instead.

cvat/apps/engine/cache.py
@@ -592,7 +592,7 @@ class MediaCache:
         db_data = db_task.require_data()
         manifest_path = db_data.get_manifest_path()
 
-        def requested_db_images():
+        def requested_db_images() -> Iterator[str]:
             # TODO: find a way to use prefetched results, if provided
             db_images = (
                 db_data.images.order_by("frame")
@@ -624,48 +624,43 @@ class MediaCache:
                 media = []
                 if db_data.local_storage_backing_cs_id:
                     for frame_path in requested_db_images():
-                        storage_filename = frame_path
-                        fs_filename = tmp_dir / storage_filename
+                        abs_frame_path = tmp_dir / frame_path
 
-                        files_to_download.append((storage_filename, fs_filename))
+                        files_to_download.append((frame_path, abs_frame_path))
                         checksums.append(None)
-                        media.append((fs_filename, os.fspath(fs_filename)))
+                        media.append((abs_frame_path, os.fspath(abs_frame_path)))
 
                 else:
                     assert manifest_path.is_file()
                     reader = ImageReaderWithManifest(manifest_path)
                     for item in reader.iterate_frames(frame_ids):
-                        task_filename = f"{item['name']}{item['extension']}"
-                        storage_filename = item.get("meta", {}).get("original_name", task_filename)
-                        fs_filename = tmp_dir / task_filename
+                        frame_path = item.get("meta", {}).get(
+                            "original_name", f"{item['name']}{item['extension']}"
+                        )
+                        abs_frame_path = tmp_dir / frame_path
 
-                        files_to_download.append((storage_filename, fs_filename))
+                        files_to_download.append((frame_path, abs_frame_path))
                         checksums.append(item.get("checksum", None))
-                        media.append((fs_filename, os.fspath(fs_filename)))
+                        media.append((abs_frame_path, os.fspath(abs_frame_path)))
 
                 storage_client.bulk_download_to_dir(files=files_to_download, upload_dir=tmp_dir)
 
-                for (storage_filename, _), checksum, media_item in zip(
-                    files_to_download, checksums, media
-                ):
-                    if checksum and not md5_hash(media_item[1]) == checksum:
+                for checksum, media_item in zip(checksums, media):
+                    frame_path = media_item[1]
+                    if checksum and not md5_hash(frame_path) == checksum:
                         slogger.task[db_task.id].warning(
-                            "Hash sums of files {} do not match".format(media_item[1])
+                            "Hash sums of files {} do not match".format(frame_path)
                         )
 
                     if db_task.dimension == models.DimensionType.DIM_3D and (
-                        storage_filename.endswith(".bin")
+                        frame_path.endswith(".bin")
                     ):
-                        media_item = (
-                            ValidateDimension().convert_bin_to_pcd(
-                                media_item[0],
-                                delete_source=(
-                                    False
-                                    # one file can be used several times for honeypots
-                                ),
-                            ),
-                            *media_item[1:],
+                        frame_path = ValidateDimension().convert_bin_to_pcd(
+                            frame_path,
+                            # one file can be used several times for honeypots
+                            delete_source=False,
                         )
+                        media_item = (frame_path, frame_path)
 
                     if db_task.dimension == models.DimensionType.DIM_2D and decode:
                         media_item = load_image(media_item)