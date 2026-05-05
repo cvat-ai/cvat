44d23452d45a58e8da1140e2c0744df006fbbbbd
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2026-05-05 14:52:36 +0300

    Add backward compatibility stub

---
+++ cvat/apps/engine/frame_provider.py
@@ -0,0 +1,6 @@
+# Copyright (C) CVAT.ai Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# Backward compatibility stub; TODO: remove when it's safe
+from cvat.apps.engine.media_io.frame_provider import *  # pylint: disable=unused-import, disable=wildcard-import
