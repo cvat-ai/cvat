7f0fd941d30486ce51de4b4616d71065669ba554
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2026-05-08 21:11:54 +0300

    Return org id in requests api

--- cvat/apps/redis_handler/serializers.py
+++ cvat/apps/redis_handler/serializers.py
@@ -53,6 +53,7 @@ class RequestDataOperationSerializer(serializers.Serializer):
     project_id = serializers.IntegerField(required=False, allow_null=True)
     task_id = serializers.IntegerField(required=False, allow_null=True)
     job_id = serializers.IntegerField(required=False, allow_null=True)
+    org_id = serializers.IntegerField(required=False, allow_null=True)
     format = serializers.CharField(required=False, allow_null=True)
     function_id = serializers.CharField(required=False, allow_null=True)
     lightweight = serializers.BooleanField(required=False, allow_null=True)
@@ -67,6 +68,7 @@ class RequestDataOperationSerializer(serializers.Serializer):
             "project_id": base_rq_job_meta.project_id,
             "task_id": base_rq_job_meta.task_id,
             "job_id": base_rq_job_meta.job_id,
+            "org_id": base_rq_job_meta.org_id,
         }
         if parsed_request_id.action == RequestAction.AUTOANNOTATE:
             representation["function_id"] = LambdaRQMeta.for_job(rq_job).function_id
