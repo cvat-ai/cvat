2f4779ab46ea67cfeca7d1b500a556bdc8003b98
Boris Sekachevw <sekachev.bs@gmail.com>
2026-05-18 05:38:19 +0000

    updated test

--- tests/cypress/e2e/actions_objects2/case_16_z_order_features.js
+++ tests/cypress/e2e/actions_objects2/case_16_z_order_features.js
@@ -191,28 +191,30 @@ context('Actions on polygon', () => {
         });
 
         it('Moves a layer with shifting intervening layers', () => {
-            cy.contains('.cvat-objects-sidebar-z-layer-mark', '2').then(($layer) => {
-                const targetRect = $layer[0].getBoundingClientRect();
+            cy.contains('.cvat-objects-sidebar-z-layer-mark', '2')
+                .parents('.cvat-objects-sidebar-z-layer')
+                .prev('.cvat-objects-sidebar-z-layer-move-drop-area')
+                .then(($dropArea) => {
+                    const targetRect = $dropArea[0].getBoundingClientRect();
 
-                cy.contains('.cvat-objects-sidebar-z-layer-mark', '3')
-                    .find('.cvat-objects-sidebar-z-layer-drag-handle')
-                    .trigger('pointerdown', { button: 0, isPrimary: true, pointerId: 1 });
-                cy.get('body').trigger('pointermove', {
-                    clientX: targetRect.left + targetRect.width / 2,
-                    clientY: targetRect.top + targetRect.height / 2,
-                    button: 0,
-                    isPrimary: true,
-                    pointerId: 1,
+                    cy.contains('.cvat-objects-sidebar-z-layer-mark', '3')
+                        .find('.cvat-objects-sidebar-z-layer-drag-handle')
+                        .trigger('pointerdown', { button: 0, isPrimary: true, pointerId: 1 });
+                    cy.get('body').trigger('pointermove', {
+                        clientX: targetRect.left + targetRect.width / 2,
+                        clientY: targetRect.top + targetRect.height / 2,
+                        button: 0,
+                        isPrimary: true,
+                        pointerId: 1,
+                    });
+                    cy.get('body').trigger('pointerup', { button: 0, isPrimary: true, pointerId: 1 });
                 });
-                cy.get('body').trigger('pointerup', { button: 0, isPrimary: true, pointerId: 1 });
-            });
 
             cy.get('#cvat_canvas_shape_1').should('have.attr', 'data-z-order', '2');
             cy.get('#cvat_canvas_shape_2').should('have.attr', 'data-z-order', '3');
         });
 
         it('Merges one layer into another layer', () => {
-            cy.contains('.cvat-objects-sidebar-z-layer-mode-switcher label', 'Merge').click();
             cy.contains('.cvat-objects-sidebar-z-layer-mark', '2').then(($layer) => {
                 const targetRect = $layer[0].getBoundingClientRect();
 
