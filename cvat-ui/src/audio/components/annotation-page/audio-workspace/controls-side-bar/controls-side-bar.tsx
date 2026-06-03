fde373f7b44f00d9e3c55cafb3b8babdbaf8262b
Boris Sekachevw <sekachev.bs@gmail.com>
2026-06-03 14:46:46 +0000

    Updated controls sidebar

--- cvat-ui/src/audio/components/annotation-page/audio-workspace/controls-side-bar/controls-side-bar.tsx
+++ cvat-ui/src/audio/components/annotation-page/audio-workspace/controls-side-bar/controls-side-bar.tsx
@@ -16,9 +16,7 @@ import {
 import ControlVisibilityObserver, { ExtraControlsControl } from 'components/annotation-page/standard-workspace/controls-side-bar/control-visibility-observer';
 
 import AudioCursorControl, { Props as CursorControlProps } from './cursor-control';
-import CreateRegionControl, { Props as CreateRegionControlProps } from './create-region-control';
-import RecordRegionControl, { Props as RecordRegionControlProps } from './record-region-control';
-import ExtendRegionControl, { Props as ExtendRegionControlProps } from './extend-region-control';
+import IntervalRegionControl, { Props as IntervalRegionControlProps } from './interval-region-control';
 import EditRegionControl, { Props as EditRegionControlProps } from './edit-region-control';
 import LoopControl, { Props as LoopControlProps } from './loop-control';
 import ZoomControl, { Props as ZoomControlProps } from './zoom-control';
@@ -27,9 +25,7 @@ import VolumeControl, { Props as VolumeControlProps } from './volume-control';
 import { computeMaxZoom } from '../utils/zoom-bounds';
 
 const ObservedCursorControl = ControlVisibilityObserver<CursorControlProps>(AudioCursorControl, 'audioCursorControl');
-const ObservedCreateRegionControl = ControlVisibilityObserver<CreateRegionControlProps>(CreateRegionControl, 'audioCreateRegionControl');
-const ObservedRecordRegionControl = ControlVisibilityObserver<RecordRegionControlProps>(RecordRegionControl, 'audioRecordRegionControl');
-const ObservedExtendRegionControl = ControlVisibilityObserver<ExtendRegionControlProps>(ExtendRegionControl, 'audioExtendRegionControl');
+const ObservedIntervalRegionControl = ControlVisibilityObserver<IntervalRegionControlProps>(IntervalRegionControl, 'audioIntervalRegionControl');
 const ObservedEditRegionControl = ControlVisibilityObserver<EditRegionControlProps>(EditRegionControl, 'audioEditRegionControl');
 const ObservedLoopControl = ControlVisibilityObserver<LoopControlProps>(LoopControl, 'audioLoopControl');
 const ObservedZoomControl = ControlVisibilityObserver<ZoomControlProps>(ZoomControl, 'audioZoomControl');
@@ -95,38 +91,24 @@ export default function AudioControlsSideBarComponent(): JSX.Element {
                 editRegionShortkey={normalizedKeyMap.EDIT_AUDIO_REGION}
                 updateActiveControl={updateAudioActiveControl}
             />
-            <div className='cvat-audio-controls-divider' />
-            <ObservedCreateRegionControl
+            <hr />
+            <ObservedIntervalRegionControl
                 activeControl={activeControl}
                 createRegionShortkey={normalizedKeyMap.CREATE_AUDIO_REGION}
-                labels={labels}
-                activeLabelId={activeLabelId}
-                updateActiveControl={updateAudioActiveControl}
-                onSetActiveLabel={onSetActiveLabel}
-            />
-            <ObservedRecordRegionControl
-                activeControl={activeControl}
                 recordRegionShortkey={normalizedKeyMap.RECORD_AUDIO_REGION}
-                labels={labels}
-                activeLabelId={activeLabelId}
-                updateActiveControl={updateAudioActiveControl}
-                onSetActiveLabel={onSetActiveLabel}
-            />
-            <ObservedExtendRegionControl
                 extendRegionShortkey={normalizedKeyMap.EXTEND_AUDIO_REGION_FROM_LAST}
                 labels={labels}
                 activeLabelId={activeLabelId}
-                recording={activeControl === ActiveControl.AUDIO_REGION_RECORD}
                 onExtendRegion={onExtendRegion}
                 onSetActiveLabel={onSetActiveLabel}
                 updateActiveControl={updateAudioActiveControl}
             />
-            <div className='cvat-audio-controls-divider' />
+            <hr />
             <ObservedLoopControl
                 loop={loop}
                 onLoopChange={onLoopChange}
             />
-            <div className='cvat-audio-controls-divider' />
+            <hr />
             <ObservedZoomControl
                 zoom={zoom}
                 maxZoom={maxZoom}
