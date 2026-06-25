afe819fee3e3cca1aab405d32d7556378d9f72f6
Casper Lundberg <casperl@axis.com>
2026-06-25 15:08:41 +0200

    Update automatic test to include video

--- tests/cypress/e2e/actions_objects2/case_change_data_quality.js
+++ tests/cypress/e2e/actions_objects2/case_change_data_quality.js
@@ -6,7 +6,9 @@
 
 context('Change data quality switch.', () => {
     const labelName = 'Change data quality test';
-    const taskName = `New annotation task for ${labelName}`;
+    const imageTaskName = `New annotation task for ${labelName}`;
+    const videoTaskName = `${labelName} video task`;
+    const localVideoPath = 'mounted_file_share/videos/video_1.mp4';
     const attrName = `Attr for ${labelName}`;
     const textDefaultValue = 'Some default value for type Text';
     const imagesCount = 10;
@@ -24,56 +26,105 @@ context('Change data quality switch.', () => {
         chunkSize: 1,
     };
 
+    function checkQualitySwitchRequests() {
+        cy.openSettings();
+        cy.get('.cvat-player-settings-data-quality-checkbox').within(() => {
+            cy.get('[type="checkbox"]').should('not.be.checked').check();
+        });
+        cy.closeSettings();
+        cy.intercept('GET', '/api/jobs/**/data**').as('chunkGet');
+        cy.goToNextFrame(1);
+        cy.wait('@chunkGet');
+        cy.goToNextFrame(2);
+        cy.wait('@chunkGet').then((req) => {
+            expect(req.request.url).to.include('quality=original');
+        });
+
+        cy.openSettings();
+        cy.get('.cvat-player-settings-data-quality-checkbox').within(() => {
+            cy.get('[type="checkbox"]').should('be.checked').uncheck();
+        });
+        cy.closeSettings();
+        cy.intercept('GET', '/api/jobs/**/data**').as('chunkGetCompressed');
+        cy.goToPreviousFrame(1);
+        cy.wait('@chunkGetCompressed');
+        cy.goToPreviousFrame(0);
+        cy.wait('@chunkGetCompressed').then((req) => {
+            expect(req.request.url).to.include('quality=compressed');
+        });
+    }
+
     before(() => {
         cy.visit('/');
         cy.login();
         cy.get('.cvat-tasks-page').should('exist');
         cy.document().then((doc) => {
             const tasks = Array.from(doc.querySelectorAll('.cvat-item-task-name')).map((el) => el.innerText);
-            if (!tasks.includes(taskName)) {
+            if (!tasks.includes(imageTaskName)) {
                 cy.task('log', 'Task does not exist. Creating.');
                 cy.imageGenerator(imagesFolder, imageFileName, width, height, color, posX, posY, labelName, imagesCount);
                 cy.createZipArchive(directoryToArchive, archivePath);
                 cy.createAnnotationTask(
-                    taskName, labelName, attrName, textDefaultValue, archiveName,
+                    imageTaskName, labelName, attrName, textDefaultValue, archiveName,
                     null, advancedConfigurationParams,
                 );
             } else {
                 cy.task('log', 'Task already exists. Skipping creation.');
             }
         });
-        cy.openTaskJob(taskName);
+        cy.openTaskJob(imageTaskName);
     });
 
-    describe('Tests feature to switch between downloading original and compressed chunks.', () => {
-        it('Check switching to show original changes query to quality=original.', () => {
-            cy.openSettings();
-            cy.get('.cvat-player-settings-data-quality-checkbox').within(() => {
-                cy.get('[type="checkbox"]').should('not.be.checked').check();
+    describe('Tests feature to switch between downloading original and compressed chunks (images).', () => {
+        it('Checks quality switch requests for image chunks.', () => {
+            checkQualitySwitchRequests();
+        });
+    });
+
+    describe('Tests feature to switch between downloading original and compressed chunks (video).', () => {
+        before(() => {
+            cy.visit('/tasks');
+            cy.get('.cvat-tasks-page').should('exist');
+            cy.document().then((doc) => {
+                const tasks = Array.from(doc.querySelectorAll('.cvat-item-task-name')).map((el) => el.innerText);
+                if (!tasks.includes(videoTaskName)) {
+                    cy.task('log', 'Video task does not exist. Creating.');
+                    cy.get('.cvat-create-task-dropdown').click();
+                    cy.get('.cvat-create-task-button').click({ force: true });
+                    cy.url().should('include', '/tasks/create');
+
+                    cy.get('[id="name"]').type(videoTaskName);
+                    cy.get('.cvat-constructor-viewer-new-item').click();
+                    cy.get('[placeholder="Label name"]').type(labelName);
+                    cy.get('.cvat-new-attribute-button').click();
+                    cy.get('[placeholder="Name"]').type(attrName);
+                    cy.get('.cvat-attribute-type-input').click();
+                    cy.get('.cvat-attribute-type-input-text').click();
+                    cy.get('[placeholder="Default value"]').type(textDefaultValue);
+                    cy.contains('button', 'Continue').click();
+
+                    cy.get('input[type="file"]').selectFile(localVideoPath, {
+                        action: 'drag-drop',
+                        force: true,
+                    });
+                    cy.advancedConfiguration(advancedConfigurationParams);
+                    cy.get('.cvat-submit-continue-task-button').scrollIntoView();
+                    cy.get('.cvat-submit-continue-task-button').click();
+                    cy.get('.cvat-notification-create-task-success').should('exist')
+                        .find('[data-icon="close"]').click();
+                    cy.goToTaskList();
+                } else {
+                    cy.task('log', 'Video task already exists. Skipping creation.');
+                }
             });
-            cy.closeSettings();
-            cy.intercept('GET', '/api/jobs/**/data**').as('chunkGet');
-            cy.goToNextFrame(1);
-            cy.wait('@chunkGet');
-            cy.goToNextFrame(2);
-            cy.wait('@chunkGet').then((req) => {
-                expect(req.request.url).to.include('quality=original');
-            });
+
+            cy.openTaskJob(videoTaskName);
+            cy.get('.cvat-spinner').should('not.exist');
+            cy.get('.cvat-canvas-container').should('exist');
         });
 
-        it('Change back to compressed and check next chunk.', () => {
-            cy.openSettings();
-            cy.get('.cvat-player-settings-data-quality-checkbox').within(() => {
-                cy.get('[type="checkbox"]').should('be.checked').uncheck();
-            });
-            cy.closeSettings();
-            cy.intercept('GET', '/api/jobs/**/data**').as('chunkGet');
-            cy.goToNextFrame(3);
-            cy.wait('@chunkGet');
-            cy.goToNextFrame(4);
-            cy.wait('@chunkGet').then((req) => {
-                expect(req.request.url).to.include('quality=compressed');
-            });
+        it('Checks quality switch requests for video chunks.', () => {
+            checkQualitySwitchRequests();
         });
     });
 });
