41a162419c0bdc36259767a3bd913bc360632da8
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-07-09 12:11:07 +0000

    add new comment

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -322,8 +322,10 @@ REDIS_INMEM_SETTINGS = {
     "DB": REDIS_INMEM_DATABASES.RQ,
     "PASSWORD": redis_inmem_password,
     "REDIS_CLIENT_KWARGS": {
+        # Work around an RQ < 2.0 bug where Redis socket timeouts can be too short
+        # for blocking operations such as BLPOP. Fixed upstream in RQ 2.0:
+        # https://github.com/rq/rq/pull/2120
         "socket_timeout": None,
-        # workaround for RQ 2.0: https://github.com/rq/rq/pull/2120
     },
 }
 
