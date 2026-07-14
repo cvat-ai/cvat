010d24edb2cdbb87a431d34765ba37a47f95ca24
Aleksei Sosov <aleksei.sosov@cvat.ai>
2026-07-14 10:04:19 +0400

    Added depends_on into `add_to_queue` to prevent race condition when original task is not finished yet, but "send_webhook" already started working

--- cvat/apps/webhooks/dispatch.py
+++ cvat/apps/webhooks/dispatch.py
@@ -7,11 +7,17 @@ from copy import deepcopy
 import django_rq
 from django.conf import settings
 from rq import Retry
+from rq.types import JobDependencyType
 
 from .models import Webhook
 
 
-def add_to_queue(webhook: Webhook, payload: dict, redelivery: bool = False) -> None:
+def add_to_queue(
+    webhook: Webhook,
+    payload: dict,
+    redelivery: bool = False,
+    depends_on: JobDependencyType | None = None,
+) -> None:
     from .tasks import send_webhook
 
     queue = django_rq.get_queue(settings.CVAT_QUEUES.WEBHOOKS.value)
@@ -21,10 +27,15 @@ def add_to_queue(webhook: Webhook, payload: dict, redelivery: bool = False) -> N
         args=(webhook.id, payload, redelivery),
         retry=Retry(max=len(retry_intervals), interval=retry_intervals),
         failure_ttl=7 * 24 * 60 * 60,
+        depends_on=depends_on,
     )
 
 
-def batch_add_to_queue(webhooks: list | dict, data: dict | None) -> None:
+def batch_add_to_queue(
+    webhooks: list | dict,
+    data: dict | None,
+    depends_on: JobDependencyType | None = None,
+) -> None:
     # webhooks batch with different events; webhooks are grouped by them
     if isinstance(webhooks, dict):
         for event_type, webhooks_data in webhooks.items():
@@ -37,10 +48,11 @@ def batch_add_to_queue(webhooks: list | dict, data: dict | None) -> None:
                         "webhook_id": webhook.id,
                         "event": event_type,
                     },
+                    depends_on=depends_on,
                 )
         return
 
     payload = deepcopy(data)
     for webhook in webhooks:
         payload["webhook_id"] = webhook.id
-        add_to_queue(webhook=webhook, payload=payload)
+        add_to_queue(webhook=webhook, payload=payload, depends_on=depends_on)
