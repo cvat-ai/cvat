a9b912d0e9ed4d556388a62cf3b269e9e2b7d969
Roman Donchenko <roman@cvat.ai>
2026-07-30 18:36:43 +0300

    Remove `AnnotationGuide.is_public`
    As far as I know, this field has only been used for the Toloka integration
    (removed a long time ago), so it's not needed anymore. There is a surprising
    amount of code that exists to support it, which can now be removed too.

--- cvat/apps/engine/models.py
+++ cvat/apps/engine/models.py
@@ -1782,7 +1782,6 @@ class AnnotationGuide(TimestampedModel):
         Project, null=True, blank=True, on_delete=models.CASCADE, related_name="annotation_guide"
     )
     markdown = models.TextField(blank=True, default="")
-    is_public = models.BooleanField(default=False)
 
     @property
     def target(self) -> Task | Project:
