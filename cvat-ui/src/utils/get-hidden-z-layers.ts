9ef0fcea0e5bc0f6343177f695c5e3bd4448745e
Gleb Potapov <glebpotapov2001@gmail.com>
2026-08-13 14:50:44 +0400

    Fix hidden layer selector fallback

--- cvat-ui/src/utils/get-hidden-z-layers.ts
+++ cvat-ui/src/utils/get-hidden-z-layers.ts
@@ -4,9 +4,7 @@
 
 import type { CombinedState } from 'reducers';
 
-const EMPTY_HIDDEN_Z_LAYERS = new Set<number>();
-
 export default function getHiddenZLayers(state: CombinedState): Set<number> {
     const frame = state.annotation.player.frame.number;
-    return state.annotation.annotations.zLayer.hiddenByFrame.get(frame) || EMPTY_HIDDEN_Z_LAYERS;
+    return state.annotation.annotations.zLayer.hiddenByFrame.get(frame) || new Set<number>();
 }
