08755ff0f433c419f65f76f17a12538da9a86ca9
Aleksei Sosov <aleksei.sosov@cvat.ai>
2026-08-21 14:01:42 +0400

    Fixed the get_sender to return user from request/job
    Originally, `get_user` that was called inside `get_sender` have `if isinstance(instance, User): return instsance` that break webhooks where actor is not equal to `instance` in general

--- cvat/apps/webhooks/signals.py
+++ cvat/apps/webhooks/signals.py
@@ -119,7 +119,7 @@ def post_save_resource_event(
 
     _webhook_payload = {
         resource_name: utils.get_serializer(instance=retrieved_instance).data,
-        "sender": utils.get_sender(instance=instance),
+        "sender": utils.get_sender(),
     }
 
     webhook_payload_pairs = [
@@ -239,7 +239,7 @@ def post_delete_resource_event(sender: type[ModelT], instance: ModelT, **kwargs)
     webhook_payload = {
         "event": event_key_,
         resource_name: deleted_instance_snapshot,
-        "sender": utils.get_sender(instance=instance),
+        "sender": utils.get_sender(),
     }
 
     webhook_payload_pairs = [
