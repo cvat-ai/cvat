62f5b3d91b4fb89b4a63d078299ad95fb5b8c24f
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-08-24 08:48:23 +0000

    fix calls

--- tests/cypress/support/commands_audio.js
+++ tests/cypress/support/commands_audio.js
@@ -216,7 +216,7 @@ Cypress.Commands.add('audioClearAnnotations', () => {
 });
 
 Cypress.Commands.add('audioClearAnnotationsAndSave', () => {
-    removeAudioAnnotations(true);
+    removeAudioAnnotations({ save: true });
 });
 
 Cypress.Commands.add('audioUndo', () => {
--- tests/cypress/support/utils.cy.js
+++ tests/cypress/support/utils.cy.js
@@ -187,7 +187,7 @@ export function imageRotate(direction = 'anticlockwise', deg) {
     cy.get('body').click();
 }
 
-export function removeAudioAnnotations(save = false) {
+export function removeAudioAnnotations({ save = false } = {}) {
     cy.get('.cvat-audio-regions-list-wrapper').then(($list) => {
         if ($list.find('.cvat-audio-region-item').length) {
             cy.removeAnnotations();
