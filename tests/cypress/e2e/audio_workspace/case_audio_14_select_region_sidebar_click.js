479f2ccef89b4b88a5ab74228692337d9883ac54
Alexey Zinoviev <aleksey.zinovyev@cvat.ai>
2026-08-26 09:43:45 +0400

    Audio workspace: add new interval-related functions (#11076)

--- tests/cypress/e2e/audio_workspace/case_audio_14_select_region_sidebar_click.js
+++ tests/cypress/e2e/audio_workspace/case_audio_14_select_region_sidebar_click.js
@@ -19,7 +19,8 @@ context('Audio annotation. Sidebar item click activates region.', () => {
     describe(`Testing case "${caseId}"`, () => {
         it('Click first item in regions list activates it and deactivates the second', () => {
             cy.get('.cvat-audio-region-item').should('have.length', 2);
-            cy.get('.cvat-audio-region-item').first().click();
+            cy.get('.cvat-audio-region-item').first()
+                .find('.cvat-audio-interval-header-index').click();
             cy.get('.cvat-audio-region-item').first().should('have.class', 'cvat-audio-region-item-active');
             cy.get('.cvat-audio-region-item').eq(1).should('not.have.class', 'cvat-audio-region-item-active');
         });
