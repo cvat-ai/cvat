c0d7cedf40f8b2477e007c53a2b23c0a3041d4d1
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-09-09 14:55:52 +0000

    use storage method type from cvat_sdk

--- tests/python/shared/fixtures/params.py
+++ tests/python/shared/fixtures/params.py
@@ -4,19 +4,23 @@
 
 import pytest
 
-from cvat.apps.engine.models import StorageMethodChoice
+from cvat_sdk.api_client import models
 
+_STORAGE_METHOD_VALUES: dict = models.StorageMethod.allowed_values[("value",)]
+CACHE_VALUE = _STORAGE_METHOD_VALUES["FILE_SYSTEM"]
+FILE_SYSTEM_VALUE = _STORAGE_METHOD_VALUES["FILE_SYSTEM"]
 
-def _cache_param(mode: StorageMethodChoice):
+def _cache_param(storage_method: models.StorageMethod):
     """
     For explicit test-level use with @pytest.mark.parametrize
-    ex: @pytest.mark.parametrize("use_cache", StorageMethodChoice.CACHE/FILE_SYSTEM)
+    ex: @pytest.mark.parametrize("use_cache", CACHE/FILE_SYSTEM)
     """
-    str_value = str(mode)
-    use_cache: bool = str_value == str(StorageMethodChoice.CACHE)
-    return pytest.param(use_cache, id=str_value, marks=getattr(pytest.mark, str_value))
+
+    value = storage_method.value
+    use_cache: bool = value == CACHE_VALUE
+    return pytest.param(use_cache, id=value, marks=getattr(pytest.mark, value))
 
 
-CACHE = (_cache_param(StorageMethodChoice.CACHE),)
-FILE_SYSTEM = (_cache_param(StorageMethodChoice.FILE_SYSTEM),)
+CACHE = (_cache_param(models.StorageMethod(CACHE_VALUE)),)
+FILE_SYSTEM = (_cache_param(models.StorageMethod(FILE_SYSTEM_VALUE)),)
 STORAGE_METHODS = CACHE + FILE_SYSTEM
