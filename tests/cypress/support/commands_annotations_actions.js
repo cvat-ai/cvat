07736ecdc4a15bfd3e94776e53154ad363ec0377
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-09-09 13:51:19 +0000

    assert on init, then on progress bar

--- tests/cypress/support/commands_annotations_actions.js
+++ tests/cypress/support/commands_annotations_actions.js
@@ -18,8 +18,7 @@ Cypress.Commands.add('openAnnotationsActionsModal', () => {
 Cypress.Commands.add('runAnnotationsAction', () => {
     cy.get('.cvat-action-runner-run-btn').click();
     cy.get('.cvat-action-runner-run-btn').should('be.disabled');
-    cy.contains('Action initialization').should('exist').and('be.visible');
-    cy.contains(/Actions? (is|are) running/).should('exist').and('be.visible');
+    cy.contains(/Actions? initialization/).should('exist').and('be.visible');
     cy.get('.cvat-action-runner-progress').should('exist').and('be.visible');
 });
 
