2fe3e76b957d93243b3d9394224d655e25954696
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-09-10 17:58:35 +0000

    Revert "test: add container-side coverage contexts"
    This reverts commit 9468a3c8850dfd1935cc7ab19916ea7eff0b36b4.

--- tests/python/coverage/sitecustomize.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) CVAT.ai Corporation
-#
-# SPDX-License-Identifier: MIT
-
-"""Start coverage in every Python process launched by a backend container."""
-
-import coverage
-
-coverage.process_startup()
