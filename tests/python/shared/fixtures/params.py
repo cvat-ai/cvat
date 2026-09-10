13c21cc8d852a2a0a39f854a7cd6adf3be892f64
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-09-10 11:52:54 +0000

    use one param and mark to denote cache usage

--- tests/python/shared/fixtures/params.py
+++ tests/python/shared/fixtures/params.py
@@ -4,23 +4,5 @@
 
 import pytest
 
-from cvat_sdk.api_client import models
-
-_STORAGE_METHOD_VALUES: dict = models.StorageMethod.allowed_values[("value",)]
-CACHE_VALUE = _STORAGE_METHOD_VALUES["CACHE"]
-FILE_SYSTEM_VALUE = _STORAGE_METHOD_VALUES["FILE_SYSTEM"]
-
-def _cache_param(storage_method: models.StorageMethod):
-    """
-    For explicit test-level use with @pytest.mark.parametrize
-    ex: @pytest.mark.parametrize("use_cache", CACHE/FILE_SYSTEM)
-    """
-
-    value = storage_method.value
-    use_cache: bool = value == CACHE_VALUE
-    return pytest.param(use_cache, id=value, marks=getattr(pytest.mark, value))
-
-
-CACHE = (_cache_param(models.StorageMethod(CACHE_VALUE)),)
-FILE_SYSTEM = (_cache_param(models.StorageMethod(FILE_SYSTEM_VALUE)),)
-STORAGE_METHODS = CACHE + FILE_SYSTEM
+CACHE = pytest.param(True, id='cache', marks=pytest.mark.cache)
+FILE_SYSTEM = pytest.param(False, id='file_system')
