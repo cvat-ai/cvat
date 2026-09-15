260e0c4d8f5105d7d7f38471412f676e435c6d80
Oleg Valiulin <oleg.valiulin@cvat.ai>
2026-09-15 10:47:22 +0000

    revert changes from resource test

--- tests/python/rest_api/test_resource_import_export.py
+++ tests/python/rest_api/test_resource_import_export.py
@@ -17,7 +17,6 @@ from cvat_sdk.core.uploading import Uploader
 from pytest_cases import fixture, fixture_ref, parametrize
 
 from shared.fixtures.data import Container
-from shared.fixtures.params import CACHE
 from shared.utils.config import get_method, make_sdk_client, post_method
 from shared.utils.resource_import_export import (
     _CloudStorageResourceTest,
@@ -140,14 +139,8 @@ class TestExportResourceToS3(_S3ResourceTest):
             ("jobs", "dataset"),
         ],
     )
-    @pytest.mark.parametrize("use_cache", CACHE)
     def test_user_cannot_export_to_cloud_storage_with_specific_location_without_access(
-        self,
-        storage_id,
-        regular_lonely_user,
-        obj,
-        resource,
-        use_cache,
+        self, storage_id, regular_lonely_user, obj, resource
     ):
         user = regular_lonely_user
 
@@ -160,7 +153,7 @@ class TestExportResourceToS3(_S3ResourceTest):
         }
         data_spec = {
             "image_quality": 75,
-            "use_cache": use_cache,
+            "use_cache": True,
             "server_files": ["images/image_1.jpg"],
             "project_id": project_id,
         }
@@ -302,15 +295,8 @@ class TestImportResourceFromS3(_S3ResourceTest):
             ("projects", "backup"),
         ],
     )
-    @pytest.mark.parametrize("use_cache", CACHE)
     def test_user_cannot_import_from_cloud_storage_with_specific_location_without_access(
-        self,
-        storage_id,
-        regular_lonely_user,
-        obj,
-        resource,
-        cloud_storages,
-        use_cache,
+        self, storage_id, regular_lonely_user, obj, resource, cloud_storages
     ):
         user = regular_lonely_user
 
@@ -323,7 +309,7 @@ class TestImportResourceFromS3(_S3ResourceTest):
         }
         data_spec = {
             "image_quality": 75,
-            "use_cache": use_cache,
+            "use_cache": True,
             "server_files": ["images/image_1.jpg"],
             "project_id": project_id,
         }
